2fe4b95b5ce24aeb1c6692d974a726bcb4063ddb
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-02-17 00:57:11 -0500

    Comment explaining the rationale for the reversion in 658e080fd4, so that it's clear in intent.

--- base.py
+++ base.py
@@ -36,6 +36,10 @@ class Block(object):
         are, in your judgment, typically learned in this
         model.
         """
+        # NOTE: We return list(self._params) rather than self._params
+        # in order to explicitly make a copy, so that the list isn't
+        # absentmindedly modified. If a user really knows what they're
+        # doing they can modify self._params.
         return list(self._params)
 
     def outputs(self):
