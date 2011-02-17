4d6f7bf541664b5008249c68a918cb42c164ec05
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-02-17 04:29:13 -0500

    Added example of training a stacked DA. Quite rough around the edges, but it works.

--- example.py
+++ example.py
@@ -50,9 +50,49 @@ if __name__ == "__main__":
     # automate this a litle bit.
     for epoch in xrange(5):
         for offset in xrange(0, data.shape[0], batchsize):
-            minibatch_err = train(data[offset:(offset + batchsize)])
+            minibatch_err = train_fn(data[offset:(offset + batchsize)])
             print "epoch %d, batch %d-%d: %f" % \
-                    (epoch, offset, offset + batchsize, minibatch_err)
+                    (epoch, offset, offset + batchsize - 1, minibatch_err)
 
     # Suppose you then want to use the representation for something.
     transform = theano.function([minibatch], da([minibatch])[0])
+
+    print "Transformed data:"
+    print numpy.histogram(transform(data))
+
+    # We'll now create a stacked denoising autoencoder. First, we change
+    # the number of hidden units to be a list. This tells the StackedDA
+    # class how many layers to make.
+    sda_conf = conf.copy()
+    sda_conf['n_hid'] = [20, 20, 10]
+    sda = StackedDA.alloc(corruptor, sda_conf)
+
+    # To pretrain it, we'll create a DATrainer for each layer.
+    trainers = []
+    inp = [minibatch]
+    for d in sda.layers():
+        trainers.append(DATrainer.alloc(d, d.mse, inp[0], sda_conf))
+        # Each time, we'll be passing the data through the layer to
+        # obtain a symbolic representation for the input to the next
+        # layer.
+        inp = d(inp)
+
+    # We'll do roughly the same thing as above, but inside a loop over layers.
+    thislayer_input = [minibatch]
+    for trainer, layer in zip(trainers, sda.layers()):
+        # Compile a Theano function for training this layer.
+        thislayer_train_fn = theano.function([minibatch],
+                                             layer.mse(thislayer_input),
+                                             updates=trainer.updates())
+
+        # Train as before.
+        for epoch in xrange(10):
+            for offset in xrange(0, data.shape[0], batchsize):
+                minibatch_err = thislayer_train_fn(
+                    data[offset:(offset + batchsize)]
+                )
+                print "epoch %d, batch %d-%d: %f" % \
+                        (epoch, offset, offset + batchsize - 1, minibatch_err)
+
+        # Now, get a symbolic input for the next layer.
+        thislayer_input = layer(thislayer_input)
