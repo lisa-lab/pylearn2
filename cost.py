0d4ce1a503036b517dcc6499593c6f4e3b575f4e
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-02-25 14:37:56 -0500

    Modifications to the 'cost' part of the framework.

--- cost.py
+++ cost.py
@@ -8,48 +8,53 @@ from itertools import izip
 # Third-party imports
 from theano import tensor
 
-class Cost(object):
+class SupervisedCost(object):
     """
     A cost object is allocated in the same fashion as other
     objects in this file, with a 'conf' dictionary (or object
     supporting __getitem__) containing relevant hyperparameters.
     """
-    def __init__(self, conf, da):
-        self.reconstruction = da.reconstruction
+    def __init__(self, conf, model):
         self.conf = conf
         # TODO: Do stuff depending on conf parameters (for example
         # use different cross-entropy if act_end == "tanh" or not)
 
-    def __call__(self, inputs):
+    def __call__(self, *inputs):
         """Symbolic expression denoting the reconstruction error."""
         raise NotImplementedError()
 
-class MeanSquaredError(Cost):
+class MeanSquaredError(SupervisedCost):
     """
     Symbolic expression for mean-squared error between the input and the
     denoised reconstruction.
     """
-    def __call__(self, inputs):
-        pairs = izip(inputs, self.reconstruction(inputs))
-        # TODO: Think of something more sensible to do than sum(). On one
-        # hand, if we're treating everything in parallel it should return
-        # a list. On the other, we need a scalar for everything else to
-        # work.
+    def __call__(self, prediction, target):
+        msq = lambda p, t: ((p - t)**2).sum(axis=1).mean()
+        if isinstance(prediction, tensor.Variable):
+            return msq(prediction, target)
+        else:
+            pairs = izip(prediction, target)
+            # TODO: Think of something more sensible to do than sum(). On one
+            # hand, if we're treating everything in parallel it should return
+            # a list. On the other, we need a scalar for everything else to
+            # work.
 
-        # This will likely get refactored out into a "costs" module or
-        # something like that.
-        return sum([((inp - rec) ** 2).sum(axis=1).mean() for inp, rec in pairs])
+            # This will likely get refactored out into a "costs" module or
+            # something like that.
+            return sum([msq(p, t) for p, t in pairs])
 
-class CrossEntropy(Cost):
+class CrossEntropy(SupervisedCost):
     """
     Symbolic expression for elementwise cross-entropy between input
     and reconstruction. Use for binary-valued features (but not for,
     e.g., one-hot codes).
     """
-    def __call__(self, inputs):
-        pairs = izip(inputs, self.reconstruction(inputs))
+    def __call__(self, prediction, target):
         ce = lambda x, z: x * tensor.log(z) + (1 - x) * tensor.log(1 - z)
-        return sum([ce(inp, rec).sum(axis=1).mean() for inp, rec in pairs])
+        if isinstance(prediction, tensor.Variable):
+            return ce(prediction, target)
+        pairs = izip(prediction, target)
+        return sum([ce(p, t).sum(axis=1).mean() for p, t in pairs])
 
 ##################################################
 def get(str):
