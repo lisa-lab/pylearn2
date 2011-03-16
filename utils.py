e4444bd77e2b9e59269f954541ff19b22c15aec2
Jérémie Dumas <kirby.tln@gmail.com>
2011-03-16 16:51:14 -0400

    Bugfix: index issue in BatchIterator.

--- utils.py
+++ utils.py
@@ -225,8 +225,11 @@ class BatchIterator(object):
         self.limit = map(int, set_limit)
 
         # Number of rows in the resulting union
+        set_tsign = sub(set_limit, flo(div(set_sizes, set_batch)))
+        set_tsize = mul(set_tsign, flo(div(set_range, set_limit)))
+        
         l_trun = mul(flo(div(set_range, set_limit)), mod(set_sizes, set_batch))
-        l_full = mul(sub(set_range, flo(div(set_range, set_limit))), set_batch)
+        l_full = mul(sub(set_range, set_tsize), set_batch)
 
         self.length = sum(l_full) + sum(l_trun)
 
@@ -270,14 +273,12 @@ class BatchIterator(object):
 # Miscellaneous
 ##################################################
 
-def blend(dataset, set_proba, batch_size=20, **kwargs):
+def blend(dataset, set_proba, **kwargs):
     """
     Randomized blending of datasets in data according to parameters in conf
     """
-    iterator = BatchIterator(dataset, set_proba, batch_size, **kwargs)
+    iterator = BatchIterator(dataset, set_proba, 1, **kwargs)
     nrow = len(iterator)
-    #print [get_constant(data.shape[0]) for data in dataset]
-    #print set_proba, nrow
     ncol = dataset[0].get_value().shape[1]
     array = numpy.empty((nrow, ncol), dataset[0].dtype)
     index = 0
