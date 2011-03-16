daa7a086127dde44e37cb2608582f0cd767f73d6
Philippe Serhal <philippe.serhal@umontreal.ca>
2011-03-16 10:34:47 -0400

    Refactored framework.base load/save functions.
    In both cases, combined 'directory' and 'file' arguments.
    As far as I can tell, only pca.py and experiment.py were using these.

--- base.py
+++ base.py
@@ -39,28 +39,22 @@ class Block(object):
     def __call__(self, inputs):
         raise NotImplementedError('__call__')
 
-    def save(self, save_dir, save_file):
+    def save(self, save_file):
         """
         Dumps the entire object to a pickle file.
         Individual classes should override __getstate__ and __setstate__
         to deal with object versioning in the case of API changes.
         """
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        if not os.path.isdir(save_dir):
-            raise IOError('save_dir %s is not a directory' % save_dir)
-        else:
-            fhandle = open(os.path.join(save_dir, save_file), 'w')
-            cPickle.dump(self, fhandle, -1)
-            fhandle.close()
+        fhandle = open(save_file, 'w')
+        cPickle.dump(self, fhandle, -1)
+        fhandle.close()
 
     @classmethod
-    def load(cls, load_dir, load_file):
+    def load(cls, load_file):
         """Load a serialized block."""
-        filename = os.path.join(load_dir, load_file)
-        if not os.path.isfile(filename):
-            raise IOError('File %s does not exist' % filename)
-        obj = cPickle.load(open(filename))
+        if not os.path.isfile(load_file):
+            raise IOError('File %s does not exist' % load_file)
+        obj = cPickle.load(open(load_file))
         if isinstance(obj, cls):
             return obj
         else:
