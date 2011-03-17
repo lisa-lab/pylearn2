5fc3a1d24e82508c0cf5e69d7db7ad14dbfffdc4
Philippe Serhal <philippe.serhal@umontreal.ca>
2011-03-17 13:32:27 -0400

    BUGFIX: Properly handle saving to cwd ('./').
    Bug was introduced by #7270768cfd.

--- base.py
+++ base.py
@@ -49,7 +49,7 @@ class Block(object):
         to deal with object versioning in the case of API changes.
         """
         save_dir = os.path.dirname(save_file)
-        if not os.path.exists(save_dir):
+        if save_dir and not os.path.exists(save_dir):
             os.makedirs(save_dir)
         
         fhandle = open(save_file, 'w')
