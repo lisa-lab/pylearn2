ef5272bacde70d99f93d80e8869cbc92905906e3
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-04-19 23:59:53 -0400

    started tracking the show_weights script

---
+++ scripts/show_weights.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#usage: python show_dimred_weights <whitener network>
+import sys
+from framework.gui import get_weights_report
+
+print 'loading model'
+path = sys.argv[1]
+print 'loading done'
+
+rescale = True
+
+if len(sys.argv) > 2:
+	rescale = eval(sys.argv[2])
+
+pv = get_weights_report.get_weights_report(path, rescale)
+
+pv.show()
