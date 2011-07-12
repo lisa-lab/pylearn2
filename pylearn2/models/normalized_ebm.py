62c847b7b94b641c6e681b3b2fc22a4389c0f4d6
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-07-12 16:14:40 -0400

    implemented the rest of the methods needed to train normalized_ebm

--- pylearn2/models/normalized_ebm.py
+++ pylearn2/models/normalized_ebm.py
@@ -7,23 +7,27 @@ class NormalizedEBM:
         In practice, this parameter is only approximately correct, though it
         can be learned through methods such as noise-contrastive estimation. """
 
-    def __init__(self, ebm, init_logZ, learn_logZ):
+    def __init__(self, ebm, init_logZ, learn_logZ, logZ_lr_scale = 1.0):
         self.ebm = ebm
-        self.logZ = shared(N.cast[floatX](init_logZ))
+        self.logZ_driver = shared(N.cast[floatX](init_logZ/logZ_lr_scale))
         self.learn_logZ = learn_logZ
+        self.logZ_lr_scale = logZ_lr_scale
 
         self.batches_seen = 0
         self.examples_seen = 0
     #
 
     def log_prob(self, X):
-        return - self.ebm.free_energy(X) - self.logZ
+        return - self.ebm.free_energy(X) - self.logZ_driver * self.logZ_lr_scale
     #
+
+    def free_energy(self, X):
+        return self.ebm.free_energy(X)
 
     def get_params(self):
         params = self.ebm.get_params()
         if self.learn_logZ:
-            params.append(self.logZ)
+            params.append(self.logZ_driver)
         #
         return params
     #
