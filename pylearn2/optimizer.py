fc83a08c4a3610bc7013ac13ceca298da5439812
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-07-26 17:34:49 -0400

    Add a little more error checking to SGDOptimizer.

--- pylearn2/optimizer.py
+++ pylearn2/optimizer.py
@@ -51,8 +51,11 @@ class SGDOptimizer(Optimizer):
         """
         if hasattr(params, '__iter__'):
             self.params = params
-        else:
+        elif hasattr(params, 'params') and hasattr(params.params, '__call__'):
             self.params = params.params()
+        else:
+            raise ValueError("SGDOptimizer couldn't figure out what to do "
+                             "with first argument: '%s'" % str(params))
         if anneal_start == None:
             self.anneal_start = None
         else:
