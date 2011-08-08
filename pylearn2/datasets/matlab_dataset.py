12f880343f5c9b3bbd040c32701c3656c3e66de1
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-08-07 21:08:26 -0400

    Cast to Theano's floatX rather than float32.

--- pylearn2/datasets/matlab_dataset.py
+++ pylearn2/datasets/matlab_dataset.py
@@ -1,17 +1,13 @@
 import numpy as N
 from scipy import io
 from pylearn2.datasets import dense_design_matrix
+import theano
+floatX = theano.config.floatX
+
 
 class MatlabDataset(dense_design_matrix.DenseDesignMatrix):
     def __init__(self, path, which_set):
-
         Xs = io.loadmat(path)
-
         X = Xs[which_set]
-
-        super(MatlabDataset,self).__init__(X = N.cast['float32'](X) )
-
+        super(MatlabDataset, self).__init__(X=N.cast[floatX](X))
         assert not N.any(N.isnan(self.X))
-    #
-
-#
