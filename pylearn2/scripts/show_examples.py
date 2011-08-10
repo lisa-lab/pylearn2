202ce1872118c3964388ec12f632d18a3552235a
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-08-10 16:49:02 -0400

    Clean up show_examples script.

--- pylearn2/scripts/show_examples.py
+++ pylearn2/scripts/show_examples.py
@@ -1,51 +1,52 @@
 #!/bin/env python
+"""TODO: document me."""
 import numpy as N
 import sys
 from pylearn2.gui import patch_viewer
 from pylearn2.config import yaml_parse
 
-assert len(sys.argv) == 2
-path = sys.argv[1]
+if __name__ == "__main__":
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
 
-if path.endswith('.pkl'):
-    from pylearn2.utils import serial
-    dataset = serial.load(path)
-elif path.endswith('.yaml'):
-    dataset =yaml_parse.load_path(path)
-else:
-    dataset = yaml_parse.load(path)
+    if path.endswith('.pkl'):
+        from pylearn2.utils import serial
+        dataset = serial.load(path)
 
-rows = 20
-cols = 20
+    elif path.endswith('.yaml'):
+        dataset = yaml_parse.load_path(path)
+    else:
+        dataset = yaml_parse.load(path)
 
-examples = dataset.get_batch_topo(rows*cols)
+    rows = 20
+    cols = 20
 
-print ('examples range',examples.min(),examples.max(), examples.dtype)
+    examples = dataset.get_batch_topo(rows * cols)
 
-examples /= N.abs(examples).max()
+    print 'examples range', examples.min(), examples.max(), examples.dtype
 
-if len(examples.shape) != 4:
-    print 'sorry, view_examples.py only supports image examples for now.'
-    print 'this dataset has '+str(len(examples)-2)+' topological dimensions'
-    quit(-1)
-#
+    examples /= N.abs(examples).max()
 
-if examples.shape[3] == 1:
-    is_color = False
-elif examples.shape[3] == 3:
-    is_color = True
-else:
-    print 'got unknown image format with '+str(examples.shape[3])+' channels'
-    print 'supported formats are 1 channel greyscale or three channel RGB'
-    quit(-1)
-#
+    if len(examples.shape) != 4:
+        print 'sorry, view_examples.py only supports image examples for now.'
+        print 'this dataset has %d topological dimensions' % (len(examples) - 2)
+        quit(-1)
 
-print examples.shape[1:3]
+    if examples.shape[3] == 1:
+        is_color = False
+    elif examples.shape[3] == 3:
+        is_color = True
+    else:
+        print 'got unknown image format with %d channels' % examples.shape[3]
+        print 'supported formats are 1 channel greyscale or three channel RGB'
+        quit(-1)
 
-pv = patch_viewer.PatchViewer( (rows, cols), examples.shape[1:3], is_color = is_color)
+    print examples.shape[1:3]
 
-for i in xrange(rows*cols):
-    pv.add_patch(examples[i,:,:,:], activation = 0.0, rescale = False)
-#
+    pv = patch_viewer.PatchViewer((rows, cols), examples.shape[1:3],
+                                  is_color=is_color)
 
-pv.show()
+    for i in xrange(rows * cols):
+        pv.add_patch(examples[i], activation=0.0, rescale=False)
+
+    pv.show()
