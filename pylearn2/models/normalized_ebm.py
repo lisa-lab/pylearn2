c273680e5f6f03ce0bc2b2d386a0b0c1d83ec4d9
David Warde-Farley <wardefar@iro.umontreal.ca>
2011-08-23 13:37:15 -0400

    PEP8-ify models/.

--- pylearn2/models/normalized_ebm.py
+++ pylearn2/models/normalized_ebm.py
@@ -1,24 +1,26 @@
 import numpy as N
 from theano import config, shared
 
-class NormalizedEBM:
-    """ An Energy-Based Model with an additional parameter representing log Z.
-        In practice, this parameter is only approximately correct, though it
-        can be learned through methods such as noise-contrastive estimation. """
 
-    def __init__(self, ebm, init_logZ, learn_logZ, logZ_lr_scale = 1.0):
+class NormalizedEBM(object):
+    """
+    An Energy-Based Model with an additional parameter representing log Z.
+    In practice, this parameter is only approximately correct, though it
+    can be learned through methods such as noise-contrastive estimation.
+    """
+    def __init__(self, ebm, init_logZ, learn_logZ, logZ_lr_scale=1.0):
         self.ebm = ebm
-        self.logZ_driver = shared(N.cast[config.floatX](init_logZ/logZ_lr_scale))
+        self.logZ_driver = shared(N.cast[config.floatX](
+            init_logZ / logZ_lr_scale
+        ))
         self.learn_logZ = learn_logZ
         self.logZ_lr_scale = logZ_lr_scale
 
         self.batches_seen = 0
         self.examples_seen = 0
-    #
 
     def log_prob(self, X):
-        return - self.ebm.free_energy(X) - self.logZ_driver * self.logZ_lr_scale
-    #
+        return -self.ebm.free_energy(X) - self.logZ_driver * self.logZ_lr_scale
 
     def free_energy(self, X):
         return self.ebm.free_energy(X)
@@ -27,24 +29,17 @@ class NormalizedEBM:
         params = self.ebm.get_params()
         if self.learn_logZ:
             params.append(self.logZ_driver)
-        #
         return params
-    #
 
     def censor_updates(self, updates):
         self.ebm.censor_updates(updates)
-    #
 
     def redo_theano(self):
         self.ebm.redo_theano()
         self.E_X_batch_func = self.ebm.E_X_batch_func
-    #
 
     def get_weights(self):
         return self.ebm.get_weights()
-    #
 
     def get_weights_format(self):
         return self.ebm.get_weights_format()
-    #
-#
