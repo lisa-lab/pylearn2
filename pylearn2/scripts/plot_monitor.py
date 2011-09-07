6b5a93a15a03ff9290f7f3f06b6912fbd8e7aadd
Ian Goodfellow <goodfellow.ian@gmail.com>
2011-09-07 10:35:33 -0400

    plot_monitor now understands ranges of codes, such as A-G

--- pylearn2/scripts/plot_monitor.py
+++ pylearn2/scripts/plot_monitor.py
@@ -16,27 +16,72 @@ for channel_name in sorted(channels):
     code = tag_generator.get_tag()
     codebook[code] = channel_name
 
-#Display the codebook
-for code in sorted(codebook):
-    print code + '. ' + codebook[code]
-
-print
 
 #if there is more than one channel in the monitor ask which ones to plot
 prompt = len(channels.values()) > 0
 
 if prompt:
 
-    response = raw_input('Enter a list of channels to plot (example: A, B)): ')
+    sorted_codes = sorted(codebook)
 
+    #Display the codebook
+    for code in sorted_codes:
+        print code + '. ' + codebook[code]
+
+    print
+
+    response = raw_input('Enter a list of channels to plot (example: A, C,F-G)): ')
+
+    #Remove spaces
     response = response.replace(' ','')
 
+    #Split into list
     codes = response.split(',')
+
+    final_codes = set([])
+
+    for code in codes:
+        if code.find('-') != -1:
+            #The current list element is a range of codes
+
+            rng = code.split('-')
+
+            if len(rng) != 2:
+                print "Input not understood: "+code
+                quit(-1)
+
+            found = False
+            for i in xrange(len(sorted_codes)):
+                if sorted_codes[i] == rng[0]:
+                    found = True
+                    break
+
+            if not found:
+                print "Invalid code: "+rng[0]
+                quit(-1)
+
+            found = False
+            for j in xrange(i,len(sorted_codes)):
+                if sorted_codes[j] == rng[1]:
+                    found = True
+                    break
+
+            if not found:
+                print "Invalid code: "+rng[1]
+                quit(-1)
+
+            final_codes = final_codes.union(set(sorted_codes[i:j+1]))
+        else:
+            #The current list element is just a single code
+            final_codes = final_codes.union(set([code]))
+
 else:
-    codes ,= codebook.keys()
+    final_codes ,= set(codebook.keys())
+
+plt.xlabel('# examples')
 
 #plot the requested channels
-for code in codes:
+for code in final_codes:
 
     channel_name= codebook[code]
 
