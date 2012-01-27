a6213237ab2bb67c987ed878e73422e847974e44
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-01-27 17:14:22 -0500

    implemented make_cifar10_whitened

--- pylearn2/scripts/datasets/make_cifar10_whitened.py
+++ pylearn2/scripts/datasets/make_cifar10_whitened.py
@@ -1,8 +1,5 @@
 """
-This script makes a dataset of 32x32 approximately whitened STL-10 images.
-
-It assumes that you have already run make_downsampled_stl10.py, which downsamples the STL-10 images to
-1/3 of their original resolution.
+This script makes a dataset of 32x32 approximately whitened CIFAR-10 images.
 
 """
 
@@ -10,21 +7,15 @@ from pylearn2.utils import serial
 from pylearn2.datasets import preprocessing
 from pylearn2.utils import string
 import numpy as np
+from pylearn2.datsets.cifar10 import CIFAR10
 
-data_dir = string.preprocess('${PYLEARN2_DATA_PATH}/stl10')
+data_dir = string.preprocess('${PYLEARN2_DATA_PATH}/cifar10')
 
-print 'Loading STL10-10 unlabeled and train datasets...'
-downsampled_dir = data_dir + '/stl10_32x32'
-
-data = serial.load(downsampled_dir + '/unlabeled.pkl')
-supplement = serial.load(downsampled_dir + '/train.pkl')
-
-print 'Concatenating datasets...'
-data.set_design_matrix(np.concatenate((data.X,supplement.X),axis=0))
-
+print 'Loading CIFAR-10 train dataset...'
+train = CIFAR10(which_set = 'train')
 
 print "Preparing output directory..."
-output_dir = data_dir + '/stl10_32x32_whitened'
+output_dir = data_dir + '/pylearn2_whitened'
 serial.mkdir( output_dir )
 README = open(output_dir + '/README','w')
 
@@ -32,19 +23,17 @@ README.write("""
 The .pkl files in this directory may be opened in python using
 cPickle, pickle, or pylearn2.serial.load.
 
-unsupervised.pkl, unlabeled.pkl, train.pkl, and test.pkl each contain
-a pylearn2 Dataset object defining an unlabeled
+train.pkl, and test.pkl each contain
+a pylearn2 Dataset object defining a labeled
 dataset of a 32x32 approximately whitened version of the STL-10
-dataset. unlabeled.pkl contains unlabeled train examples. train.pkl
-contains labeled train examples. unsupervised.pkl contains the union
-of these (without any labels). test.pkl contains the labeled test
-examples.
+dataset. train.pkl contains labeled train examples. test.pkl
+contains labeled test examples.
 
 preprocessor.pkl contains a pylearn2 ZCA object that was used
 to approximately whiten the images. You may want to use this
 object later to preprocess other images.
 
-They were created with the pylearn2 script make_stl10_whitened.py.
+They were created with the pylearn2 script make_cifar10_whitened.py.
 
 All other files in this directory, including this README, were
 created by the same script and are necessary for the other files
@@ -55,33 +44,14 @@ README.close()
 
 print "Learning the preprocessor and preprocessing the unsupervised train data..."
 preprocessor = preprocessing.ZCA()
-data.apply_preprocessor(preprocessor = preprocessor, can_fit = True)
+train.apply_preprocessor(preprocessor = preprocessor, can_fit = True)
 
 print 'Saving the unsupervised data'
-data.use_design_loc(output_dir+'/unsupervised.npy')
-serial.save(output_dir + '/unsupervised.pkl', data)
-
-X = data.X
-unlabeled = X[0:100*1000,:]
-labeled = X[100*1000:,:]
-del X
-
-print "Saving the unlabeled data"
-data.X = unlabeled
-data.use_design_loc(output_dir + '/unlabeled.npy')
-serial.save(output_dir + '/unlabeled.pkl',data)
-del data
-del unlabeled
-
-print "Saving the labeled train data"
-supplement.X = labeled
-supplement.use_design_loc(output_dir+'/train.npy')
-serial.save(output_dir+'/train.pkl', supplement)
-del supplement
-del labeled
+train.use_design_loc(output_dir+'/train.npy')
+serial.save(output_dir + '/train.pkl', train)
 
 print "Loading the test data"
-test = serial.load(downsampled_dir + '/test.pkl')
+test = CIFAR10(which_set = 'test')
 
 print "Preprocessing the test data"
 test.apply_preprocessor(preprocessor = preprocessor, can_fit = False)
@@ -91,3 +61,4 @@ test.use_design_loc(output_dir+'/test.npy')
 serial.save(output_dir+'/test.pkl', test)
 
 serial.save(output_dir + '/preprocessor.pkl',preprocessor)
+
