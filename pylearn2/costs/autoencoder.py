a683d21b3c85f8a7c2e2b26921d8f30eb529ea48
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-01-31 16:46:50 -0500

    Fix the autoencoder MSE cost.

--- pylearn2/costs/autoencoder.py
+++ pylearn2/costs/autoencoder.py
@@ -1,3 +1,3 @@
-def MeanSquaredReconstructionError(object):
+class MeanSquaredReconstructionError(object):
     def __call__(self, model, X):
-        return ((model.encode(X) - X) ** 2).sum(axis=1).mean()
+        return ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean()
