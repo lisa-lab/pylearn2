0de82fa7d3d284f5e80ab06c116f6707f052201e
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-02-03 08:45:21 -0500

    updates to find_gpu_fields

--- pylearn2/scripts/find_gpu_fields.py
+++ pylearn2/scripts/find_gpu_fields.py
@@ -7,15 +7,29 @@ import sys
 path = sys.argv[1]
 
 from pylearn2.utils import serial
+import inspect
 
 obj = serial.load(path)
 
 from theano.sandbox.cuda import CudaNdarray
 
+visited = set([])
+
 def find(cur_obj, cur_name):
+    global visited
+
     if isinstance(cur_obj, CudaNdarray):
         print cur_name
-    for field in dir(cur_obj):
-        find(getattr(cur_obj,field),cur_name+'.'+field)
+    print cur_name
+    for field, new_obj in inspect.getmembers(cur_obj):
+
+        if new_obj in visited:
+            continue
+
+        visited = visited.union([new_obj])
+
+        print visited
+
+        find(new_obj,cur_name+'.'+field)
 
 find(obj,'')
