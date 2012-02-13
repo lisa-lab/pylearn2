a43806d2251470c7987117dfb23d4e07d6b37f33
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-02-13 13:22:31 -0500

    Cost functions for binary-valued reconstructions.

--- pylearn2/costs/autoencoder.py
+++ pylearn2/costs/autoencoder.py
@@ -1,3 +1,22 @@
+from theano import tensor
+
 class MeanSquaredReconstructionError(object):
     def __call__(self, model, X):
         return ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean()
+
+
+class MeanBinaryCrossEntropy(object):
+    def __call__(self, model, X):
+        return (
+            tensor.xlogx.xlogx(model.reconstruct(X)) +
+            tensor.xlogx.xlogx(1 - model.reconstruct(X))
+        ).sum(axis=1).mean()
+
+
+class MeanBinaryCrossEntropyTanh(object):
+    def __call__(self, model, X):
+        X = (X + 1) / 2.
+        return (
+            tensor.xlogx.xlogx(model.reconstruct(X)) +
+            tensor.xlogx.xlogx(1 - model.reconstruct(X))
+        ).sum(axis=1).mean()
