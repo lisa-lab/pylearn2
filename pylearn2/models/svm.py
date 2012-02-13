e264bcc87ce2644ee83d4376f742be1157fc4370
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-02-13 16:14:01 -0500

    PEP8+257.

--- pylearn2/models/svm.py
+++ pylearn2/models/svm.py
@@ -1,18 +1,21 @@
+"""Wrappers for SVM models."""
 
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 
-def DenseMulticlassSVM(C, kernel = 'linear'):
-    """ sklearn does very different things behind the scenes depending
-        upon the exact identity of the class you use. the only way to
-        get an svm implementation that works with dense data is to use
-        the SVC class, which implements one-against-one classification.
-        this wrapper uses it to implement one-against-rest classification,
-        which generally works better in my experiments.
+def DenseMulticlassSVM(C, kernel='linear'):
+    """
+    sklearn does very different things behind the scenes depending
+    upon the exact identity of the class you use. The only way to
+    get an SVM implementation that works with dense data is to use
+    the `SVC` class, which implements one-against-one
+    classification. This wrapper uses it to implement one-against-
+    rest classification, which generally works better in my
+    experiments.
 
-        To avoid duplicating the training data, use only numpy ndarrays
-        whose tags.c_contigous flag is true, and which are in float64 format"""
-
-    estimator = SVC(C = C, kernel = kernel)
-
+    To avoid duplicating the training data, use only numpy ndarrays
+    whose tags.c_contigous flag is true, and which are in float64
+    format.
+    """
+    estimator = SVC(C=C, kernel=kernel)
     return OneVsRestClassifier(estimator)
