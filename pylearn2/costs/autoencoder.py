998ff8f897f175b4127ed2e7a6603ccc0724c9c0
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-02-17 13:31:17 -0500

    Some utility wrappers for costs.

--- pylearn2/costs/autoencoder.py
+++ pylearn2/costs/autoencoder.py
@@ -20,3 +20,20 @@ class MeanBinaryCrossEntropyTanh(object):
             tensor.xlogx.xlogx(model.reconstruct(X)) +
             tensor.xlogx.xlogx(1 - model.reconstruct(X))
         ).sum(axis=1).mean()
+
+
+class ModelMethodPenalty(object):
+    def __init__(self, method_name):
+        self._method_name = method_name
+
+    def __call__(self, model, X):
+        if not hasattr(model, self._method_name):
+            return getattr(model, self._method_name)(X)
+
+class ScaleBy(object):
+    def __init__(self, cost, coefficient):
+        self._cost = cost
+        self._coefficient = coefficient
+
+    def __call__(self, model, X):
+        return self._coefficient * self._cost(model, X)
