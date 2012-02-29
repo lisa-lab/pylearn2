f09c1568749d61e91fa17fd89b0c8c64c7e39d4d
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-02-29 17:30:15 -0500

    Extensive documentation for the new iterator interface.

--- pylearn2/datasets/dataset.py
+++ pylearn2/datasets/dataset.py
@@ -24,13 +24,56 @@ class Dataset(object):
     def set_iteration_scheme(self, mode=None, batch_size=None,
                              num_batches=None, topo=False):
         """
-        WRITEME
+        Modify the default iteration behaviour for the dataset.
+
+        Parameters
+        ----------
+        mode : str or object, optional
+            One of 'sequential', 'random_slice', or 'random_uniform',
+            *or* a class that instantiates an iterator that returns
+            slices or index sequences on every call to next().
+        batch_size : int, optional
+            The size of an individual batch. Unnecessary if `mode` is
+            'sequential' and `num_batches` is specified.
+        num_batches : int, optional
+            The size of an individual batch. Unnecessary if `mode` is
+            'sequential' and `batch_size` is specified.
+        topo : boolean, optional
+            Whether batches returned by the iterator should present
+            examples in a topological view or not. Defaults to
+            `False`.
         """
         raise NotImplementedError()
 
     def iterator(self, mode=None, batch_size=None, num_batches=None,
                  topo=None, rng=None):
         """
-        WRITEME
+        Return an iterator for this dataset with the specified
+        behaviour. Unspecified values are filled-in by the default.
+
+        Parameters
+        ----------
+        mode : str or object, optional
+            One of 'sequential', 'random_slice', or 'random_uniform',
+            *or* a class that instantiates an iterator that returns
+            slices or index sequences on every call to next().
+        batch_size : int, optional
+            The size of an individual batch. Unnecessary if `mode` is
+            'sequential' and `num_batches` is specified.
+        num_batches : int, optional
+            The size of an individual batch. Unnecessary if `mode` is
+            'sequential' and `batch_size` is specified.
+        topo : boolean, optional
+            Whether batches returned by the iterator should present
+            examples in a topological view or not. Defaults to
+            `False`.
+
+        Returns
+        -------
+        iter_obj : object
+            An iterator object implementing the standard Python
+            iterator protocol (i.e. it has an `__iter__` method that
+            return the object itself, and a `next()` method that
+            returns results until it raises `StopIteration`).
         """
         raise NotImplementedError()
