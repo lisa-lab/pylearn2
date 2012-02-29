8cb3d6c7baf4cf916b2fa0fab598362e6ecbb7d5
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-02-29 16:05:57 -0500

    added iteration methods to pure virtual Dataset base class

--- pylearn2/datasets/dataset.py
+++ pylearn2/datasets/dataset.py
@@ -17,3 +17,17 @@ class Dataset(object):
             The last index is over separate channels.
         """
         raise NotImplementedError()
+
+    def set_iteration_scheme(self, mode=None, batch_size=None,
+                             num_batches=None, topo=False):
+        """
+        WRITEME
+        """
+        raise NotImplementedError()
+
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
+                 topo=None, rng=None):
+        """
+        WRITEME
+        """
+        raise NotImplementedError()
