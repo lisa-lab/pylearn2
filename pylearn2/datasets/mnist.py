d4bd9d6c47b28ed95d14fa2bae10478459468a2e
Mehdi Mirza <memirzamo@gmail.com>
2012-03-01 13:37:35 -0500

    datasets.mnist dependency on pylearn1 removed.
    appropriate pcikle files added to datasets for mnist, but not mnist roated background.

--- pylearn2/datasets/mnist.py
+++ pylearn2/datasets/mnist.py
@@ -1,7 +1,6 @@
 import numpy as N
 from pylearn2.datasets import dense_design_matrix
 from pylearn2.utils import serial
-#from pylearn.datasets import icml07
 
 class MNIST(dense_design_matrix.DenseDesignMatrix):
     def __init__(self, which_set, center = False):
@@ -26,24 +25,19 @@ class MNIST(dense_design_matrix.DenseDesignMatrix):
 class MNIST_rotated_background(dense_design_matrix.DenseDesignMatrix):
 
     def __init__(self, which_set, center = False):
+        path = "${PYLEARN2_DATA_PATH}/mnist/mnist_rotation_back_image/"+which_set
 
-        orig = icml07.MNIST_rotated_background()
-
-        Xs = {'train': orig.train.x,
-              'valid': orig.valid.x,
-              'test' : orig.test.x}
-        X = N.cast['float32'](Xs[which_set])
+        obj = serial.load(path)
+        X = obj['data']
+        X = N.cast['float32'](X)
+        y = N.asarray(obj['labels'])
 
         if center:
-            X -= 0.5#X.mean(axis=0)
+            X -= X.mean(axis=0)
 
         view_converter = dense_design_matrix.DefaultViewConverter((28,28,1))
 
-        super(MNIST_rotated_background,self).__init__(X = X, view_converter = view_converter)
+        super(MNIST,self).__init__(X = X, y = y, view_converter = view_converter)
 
         assert not N.any(N.isnan(self.X))
-    #
-
-#
-
 
