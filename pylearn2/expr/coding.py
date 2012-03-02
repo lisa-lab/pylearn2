d8b014b8a0bfbd9472d83edd9d3a7fd5153ca1b4
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-03-02 16:11:25 -0500

    made an expression for Adam Coates' triangle code

---
+++ pylearn2/expr/coding.py
@@ -0,0 +1,26 @@
+""" Expressions for encoding features """
+
+import theano.tensor as T
+
+def triangle(X, centroids):
+    """ Compute the triangle activation function used
+        in Adam Coates' AISTATS 2011 paper
+
+        X: a design matrix
+        centroids: a k-means dictionary, one centroid in each row
+
+        Returns a design matrix of triangle code activations
+    """
+
+    X_sqr = T.sqr(X).sum(axis=1)
+    c_sqr = T.sqr(centroids).sum(axis=1)
+    Xc = T.dot(X, centroids)
+
+    Z = T.sqrt( c_sqr + X_sqr - 2. * Xc)
+
+    mu = Z.mean(axis=1)
+
+    mu = mu.dimshuffle(0,'x')
+
+    rval = T.clip( mu - Z, 0., 1e30)
+
