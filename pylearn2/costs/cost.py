38cb493c4904b003dbcd37b5a93cee6bfc8e46b3
Razvan Pascanu <r.pascanu@gmail.com>
2012-03-12 22:54:58 -0400

    PEP8 fixes

--- pylearn2/costs/cost.py
+++ pylearn2/costs/cost.py
@@ -1,13 +1,17 @@
 import warnings
 
+
 class SupervisedCost(object):
     def __init__(self):
         pass
+
     def __call__(self):
         warnings.warn('You should implement a call method properly.')
+
 
 class UnsupervisedCost(object):
     def __init__(self):
         pass
+
     def __call__(self):
         warnings.warn('You should implement a call method properly.')
