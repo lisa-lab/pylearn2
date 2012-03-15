f12b61a48249309a18415f6204edba9eef359b8c
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2012-03-14 20:30:58 -0400

    Add YAML-friendly dict type, update doc.

--- pylearn2/scripts/jobman/experiment.py
+++ pylearn2/scripts/jobman/experiment.py
@@ -5,6 +5,42 @@ import jobman
 from jobman.tools import expand, flatten
 
 
+class ydict(dict):
+    '''
+    YAML-friendly subclass of dictionary.
+
+    The special key "__builder__" is interpreted as the name of an object
+    constructor.
+
+    For instance, building a ydict from the following dictionary:
+
+        {
+            '__builder__': 'pylearn2.training_algorithms.sgd.EpochCounter',
+            'max_epochs': 2
+        }
+
+    Will be displayed like:
+
+        !obj:pylearn2.training_algorithms.sgd.EpochCounter {'max_epochs': 2}
+    '''
+    def __str__(self):
+        args_dict = dict(self)
+        builder = args_dict.pop('__builder__', '')
+        ret_list = []
+        if builder:
+            ret_list.append('!obj:%s {' % builder)
+        else:
+            ret_list.append('{')
+
+        for key, val in args_dict.iteritems():
+            # This will call str() on keys and values, not repr(), so unicode
+            # objects will have the form 'blah', not "u'blah'".
+            ret_list.append('%s: %s,' % (key, val))
+
+        ret_list.append('}')
+        return '\n'.join(ret_list)
+
+
 def train_experiment(state, channel):
     """
     Train a model specified in state, and extract required results.
@@ -19,8 +55,11 @@ def train_experiment(state, channel):
     (in the same directory).
     """
     yaml_template = state.yaml_template
-    hyper_parameters = state.hyper_parameters
-    #This will be the complete yaml file that should be executed
+
+    # Convert nested DD into nested ydict.
+    hyper_parameters = expand(flatten(state.hyper_parameters), dict_type=ydict)
+
+    # This will be the complete yaml string that should be executed
     final_yaml_str = yaml_template % hyper_parameters
 
     # Instantiate an object from YAML string
