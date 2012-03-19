65959c46910a1ed7c7bc6e68a453b703b6c500d5
Caglar <ca9lar@gmail.com>
2012-03-19 18:10:55 -0400

    CrossValidation cost function is added and refactored the code.

--- pylearn2/crossval/crossval.py
+++ pylearn2/crossval/crossval.py
@@ -3,17 +3,22 @@ import copy
 import numpy as np
 
 from pylearn2.monitor import Monitor
+from pylearn2.costs.cost import SupervisedCost
 
+"""
+    The C's enum-like python class for different k-fold cross-validation
+    error modes.
+"""
 class KFoldCVMode:
     OPTIMISTIC = "optimistic"
     PESSIMISTIC = "pessimistic"
     AVERAGE = "average"
 
 
-"""
-CrossValidation base class
-"""
 class CrossValidation(object):
+"""
+    CrossValidation base class
+"""
 
     def __init__(self, model=None, algorithm=None, cost=None, dataset=None,
             callbacks=None):
@@ -42,7 +47,13 @@ class CrossValidation(object):
         self.model = model
         self.dataset = dataset
         self.algorithm = algorithm
-        self.cost = cost
+        if cost == None:
+            if algorithm.cost == None:
+                raise ValueError("You should specify a cost function for the validation.")
+            else:
+                self.cost = algorithm.cost
+        else:
+            self.cost = cost
         self.dataset = dataset
         self.callbacks = callbacks
 
@@ -52,6 +63,12 @@ class CrossValidation(object):
         if dataset == None:
             raise ValueError("Dataset can't be None")
 
+    """
+    Training function, similar to the main_loop function in train.py.
+    Parameters:
+    ---------
+    train_dataset: The dataset to train our algorithm on.
+    """
     def train_model(self, train_dataset):
         if self.algorithm is None:
             self.model.monitor = Monitor.get_monitor(self.model)
@@ -78,8 +95,13 @@ class CrossValidation(object):
                 print 'Failure during callback '+str(callback)
                 raise
 
-    def test_model(self):
-        raise NotImplementedError()
+    def test_model(self, test_data):
+        if isinstance(self.cost, SupervisedCost):
+            J = self.cost(self.model, test_data.X, test_data.Y)
+        else:
+            J = self.cost(self.model, test_data.X)
+        self.error = J
+
 
     def crossvalidate_model(self, validation_dataset):
         """
@@ -92,20 +114,40 @@ class CrossValidation(object):
 
 
 class KFoldCrossValidation(CrossValidation):
+"""
+    Class for KFoldCrossValidation.
+    Explanation:
+    K-fold cross validation is one way to improve over the holdout method.
+    The data set is divided into k subsets, and the holdout method is 
+    repeated k times. Each time, one of the k subsets is used as the test 
+    set and the other k-1 subsets are put together to form a training set.
+    Then the average error across all k trials is computed. The advantage of 
+    this method is that it matters less how the data gets divided. Every data
+    point gets to be in a test set exactly once, and gets to be in a training
+    set k-1 times. The variance of the resulting estimate is reduced as k is
+    increased. The disadvantage of this method is that the training algorithm
+    has to be rerun from scratch k times, which means it takes k times as much
+    computation to make an evaluation. A variant of this method is to randomly
+    divide the data into a test and training set k different times. The 
+    advantage of doing this is that you can independently choose how large each
+    test set is and how many trials you average over. 
 
+    Ref: http://www.cs.cmu.edu/~schneide/tut5/node42.html
+"""
     def __init__(self, nfolds=10, model=None, algorithm=None, cost=None, dataset=None,
-            callbacks=None, mode=KFoldCVMode.PESSIMISTIC):
+            callbacks=None, mode=KFoldCVMode.PESSIMISTIC, bootstrap=False):
         self.nfolds = nfolds
         self.errors = np.array([])
         self.mode = mode
+        self.bootstrap = bootstrap
         super(KFoldCrossValidation, self).__init__(model, algorithm, cost, dataset,
             callbacks, dataset)
 
-    def crossvalidate_model(self, validation_dataset, nfolds=-1, mode=KFoldCVMode.PESSIMISTIC):
-        if nfolds is -1:
-            datasets = validation_dataset.split_dataset_nfolds(self.nfolds)
+    def crossvalidate_model(self, validation_dataset, mode=KFoldCVMode.PESSIMISTIC, rng=None):
+        if bootstrap:
+            datasets = validation_dataset.bootstrap_nfolds(self.nfolds, rng)
         else:
-            datasets = validation_dataset.split_dataset_nfolds(nfolds)
+            datasets = validation_dataset.split_dataset_nfolds(self.nfolds)
         for i in xrange(nfolds):
             tmp_datasets = copy.copy(datasets) # Create a shallow copy
             test_data = tmp_datasets.pop(i)
@@ -114,9 +156,6 @@ class KFoldCrossValidation(CrossValidation):
                 training_data.merge_datasets(tmp_datasets)
             self.train_model(training_data)
             self.test_model(test_data)
-
-    def test_model(self, test_data):
-        pass
 
     def get_error(self):
         if self.mode == KFoldCVMode.PESSIMISTIC:
@@ -127,19 +166,35 @@ class KFoldCrossValidation(CrossValidation):
             error = np.mean(self.errors)
         return error
 
-
 class HoldoutCrossValidation(CrossValidation):
+"""
+    CrossValidation class for Holdout crossvalidation.
+    Explanation:
+    The holdout method is the simplest kind of cross validation.
+    The data set is separated into two sets, called the training set and the
+    testing set. The model is trained on the first part(training set) and tested on
+    the test set. The advantage of this method is that it is usually preferable to
+    the residual method and takes no longer to compute. However, its evaluation can
+    have a high variance. The evaluation may depend heavily on which data points 
+    end up in the training set and which end up in the test set, and thus the 
+    evaluation may be significantly different depending on how the division is 
+    made.
 
+    Ref: http://www.cs.cmu.edu/~schneide/tut5/node42.html
+    Note: HoldoutCrossValidation isn't a special case of KFoldCrossValidation.
+"""
     def __init__(self, model=None, algorithm=None, cost=None, dataset=None,
-            callbacks=None, split_size=0, split_prop=0):
+            callbacks=None, train_size=0, train_prop=0):
         self.error = 0
-        self.split_size = split_size
-        self.split_prop = split_prop
+        self.train_size = train_size
+        self.train_prop = train_prop
         super(HoldoutCrossValidation, self).__init__(model, algorithm, cost, 
                 dataset, callbacks, dataset)
 
     def crossvalidate_model(self, validation_dataset):
-        pass
-
-    def get_error(self):
-        return self.error
+        if train_size == 0:
+            datasets = validation_dataset.split_dataset_holdout(train_prop=self.train_prop)
+        else:
+            datasets = validation_dataset.split_dataset_holdout(train_size=self.train_size)
+        self.train_model(datasets[0])
+        self.test_model(datasets[1])
