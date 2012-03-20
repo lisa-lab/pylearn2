b89fc2d1dc34ea013538c9b89a5b31b291c2bfe2
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-03-20 13:34:03 -0400

    PEP8 and use os.path.join rather than hardcoding /

--- pylearn2/devtools/list_files.py
+++ pylearn2/devtools/list_files.py
@@ -1,19 +1,21 @@
 import pylearn2
 import os
 
-def list_files(suffix = ""):
+
+def list_files(suffix=""):
     """ Lists all files in pylearn2 whose filepath
     ends with suffix """
 
-    pl2_path ,= pylearn2.__path__
+    pl2_path, = pylearn2.__path__
 
     return _list_files(pl2_path, suffix)
 
-def _list_files(path, suffix = ""):
+
+def _list_files(path, suffix=""):
     if os.path.isdir(path):
         incomplete = os.listdir(path)
-        complete = [ path + '/' + entry for entry in incomplete ]
-        lists = [ _list_files(subpath, suffix) for subpath in complete ]
+        complete = [os.path.join(path, entry) for entry in incomplete]
+        lists = [_list_files(subpath, suffix) for subpath in complete]
         flattened = []
         for l in lists:
             for elem in l:
@@ -22,10 +24,10 @@ def _list_files(path, suffix = ""):
     else:
         assert os.path.exists(path)
         if path.endswith(suffix):
-            return [ path ]
+            return [path]
         return []
 
 if __name__ == '__main__':
-    result =  list_files('.py')
+    result = list_files('.py')
     for path in result:
         print path
