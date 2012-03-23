3df4bd98d716eedeb760509336bae4ed70c7690d
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-03-23 13:32:55 -0400

    PEP8 cleanup and fix error message in new test.

--- pylearn2/tests/test_monitor.py
+++ pylearn2/tests/test_monitor.py
@@ -10,34 +10,34 @@ BATCHES = 4
 BATCH_SIZE = 5
 NUM_EXAMPLES = BATCHES * BATCH_SIZE
 
+
 class DummyModel(Model):
     input_space = VectorSpace(NVIS)
 
+
 class DummyDataset(DenseDesignMatrix):
     def __init__(self):
-        rng = np.random.RandomState([4,12,17])
-        DenseDesignMatrix.__init__(self, X = rng.uniform(1.,2.,(NUM_EXAMPLES,NVIS)))
+        rng = np.random.RandomState([4, 12, 17])
+        super(DenseDesignMatrix, self).__init__(
+            X=rng.uniform(1., 2., (NUM_EXAMPLES, NVIS))
+        )
+
 
 def test_channel_scaling():
-    monitor = Monitor( DummyModel())
+    monitor = Monitor(DummyModel())
     dataset = DummyDataset()
-    monitor.set_dataset( dataset = dataset, batches = BATCHES, batch_size = BATCH_SIZE)
+    monitor.set_dataset(dataset=dataset, batches=BATCHES,
+                        batch_size=BATCH_SIZE)
     V = T.matrix()
     m = V.mean()
-
-    monitor.add_channel(name = 'mean', ipt = V, val = m)
-
+    monitor.add_channel(name='mean', ipt=V, val=m)
     monitor()
-
     assert 'mean' in monitor.channels
     mean = monitor.channels['mean']
-
     assert len(mean.val_record) == 1
     actual = mean.val_record[0]
-
     X = dataset.get_design_matrix()
     expected = X.mean()
-
-    if not np.allclose(expected,actual):
-        raise AssertionError("Expected monitor to contain %d but it has %d",expected,actual)
-
+    if not np.allclose(expected, actual):
+        raise AssertionError("Expected monitor to contain %d but it has "
+                             "%d" % (expected, actual))
