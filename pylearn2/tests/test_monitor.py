769727ea1a8d55b774e91b0504961c8b4512544d
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-03-23 18:24:43 -0400

    Greatly enhanced tests for monitor.

--- pylearn2/tests/test_monitor.py
+++ pylearn2/tests/test_monitor.py
@@ -5,39 +5,66 @@ from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 import numpy as np
 from theano import tensor as T
 
-NVIS = 3
-BATCHES = 4
-BATCH_SIZE = 5
-NUM_EXAMPLES = BATCHES * BATCH_SIZE
-
 
 class DummyModel(Model):
-    input_space = VectorSpace(NVIS)
+    def  __init__(self, num_features):
+        self.input_space = VectorSpace(num_features)
 
 
 class DummyDataset(DenseDesignMatrix):
-    def __init__(self):
+    def __init__(self, num_examples, num_features):
         rng = np.random.RandomState([4, 12, 17])
-        super(DenseDesignMatrix, self).__init__(
-            X=rng.uniform(1., 2., (NUM_EXAMPLES, NVIS))
+        super(DummyDataset, self).__init__(
+            X=rng.uniform(1., 2., (num_examples, num_features))
         )
 
 
-def test_channel_scaling():
-    monitor = Monitor(DummyModel())
-    dataset = DummyDataset()
-    monitor.set_dataset(dataset=dataset, batches=BATCHES,
-                        batch_size=BATCH_SIZE)
-    V = T.matrix()
-    m = V.mean()
-    monitor.add_channel(name='mean', ipt=V, val=m)
-    monitor()
-    assert 'mean' in monitor.channels
-    mean = monitor.channels['mean']
-    assert len(mean.val_record) == 1
-    actual = mean.val_record[0]
-    X = dataset.get_design_matrix()
-    expected = X.mean()
-    if not np.allclose(expected, actual):
-        raise AssertionError("Expected monitor to contain %d but it has "
-                             "%d" % (expected, actual))
+def test_channel_scaling_sequential():
+    def channel_scaling_checker(num_examples, mode, num_batches, batch_size):
+        num_features = 2
+        monitor = Monitor(DummyModel(num_features))
+        dataset = DummyDataset(num_examples, num_features)
+        monitor.set_dataset(dataset=dataset, mode=mode,
+                            num_batches=num_batches, batch_size=batch_size)
+        vis_batch = T.matrix()
+        mean = vis_batch.mean()
+        monitor.add_channel(name='mean', ipt=vis_batch, val=mean)
+        monitor()
+        assert 'mean' in monitor.channels
+        mean = monitor.channels['mean']
+        assert len(mean.val_record) == 1
+        actual = mean.val_record[0]
+        X = dataset.get_design_matrix()
+        if batch_size is not None and num_batches is not None:
+            total = min(num_examples, num_batches * batch_size)
+        else:
+            total = num_examples
+        expected = X[:total].mean()
+        if not np.allclose(expected, actual):
+            raise AssertionError("Expected monitor to contain %f but it has "
+                                 "%f" % (expected, actual))
+
+    # Specifying num_batches; even split
+    yield channel_scaling_checker, 10, 'sequential', 5, None
+    # Specifying num_batches; even split
+    yield channel_scaling_checker, 10, 'sequential', 2, None
+    # Specifying batch_size; even split
+    yield channel_scaling_checker, 10, 'sequential', None, 5
+    # Specifying batch_size; even split
+    yield channel_scaling_checker, 10, 'sequential', None, 2
+    # Specifying num_batches; uneven split
+    yield channel_scaling_checker, 10, 'sequential', 4, None
+    # Specifying num_batches; uneven split
+    yield channel_scaling_checker, 10, 'sequential', 3, None
+    # Specifying batch_size; uneven split
+    yield channel_scaling_checker, 10, 'sequential', None, 3
+    # Specifying batch_size; uneven split
+    yield channel_scaling_checker, 10, 'sequential', None, 4
+    # Specifying both, even split
+    yield channel_scaling_checker, 10, 'sequential', 2, 5
+    # Specifying both, even split
+    yield channel_scaling_checker, 10, 'sequential', 5, 2
+    # Specifying both, uneven split, dangling batch
+    yield channel_scaling_checker, 10, 'sequential', 3, 4
+    # Specifying both, uneven split, non-exhaustive
+    yield channel_scaling_checker, 10, 'sequential', 3, 3
