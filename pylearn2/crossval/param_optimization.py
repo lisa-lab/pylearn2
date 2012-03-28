c0f88253c0add74afa0e0547b49706b5eb64fde0
Raul Chandias Ferrari <chandiar@iro.umontreal.ca>
2012-03-28 06:43:42 -0400

    Added comments to class methods and completed the constructor.

--- pylearn2/crossval/param_optimization.py
+++ pylearn2/crossval/param_optimization.py
@@ -4,8 +4,52 @@ import crossval
 class ParamOptimization(object):
     def __init__(self, nfolds, model, cost, dataset, callbacks, split_size,
                  split_prop, params_ranges):
+        """
+        Construct a ParamOptimization instance.
+
+        Parameters
+        ----------
+        n_folds: 
+        model : object
+        Object that implements the Model interface defined in
+        `pylearn2.models`.
+        cost : object, optional
+        Object that is basically evaluates the model and
+        computes the cost.
+        dataset : object
+        Object that implements the Dataset interface defined in
+        `pylearn2.datasets`.
+        callbacks:
+        split_size:
+        split_prop:
+        params_ranges : dictionary that for each parameter (key),
+        we associate a range of values (a list). Something like this:
+        {'n_fold': [2, 4, 10],'n_embeddings': [10, 12, 15, 20]}
+        """
+        self.model = model
+        self.dataset = dataset
+        self.params_ranges = params_ranges
+        self.n_folds = nfolds
+        self.cost = cost
+        self.callbacks = callbacks
+        self.split_size = split_size
+        self.split_prop = split_prop 
 
     def __call__(self, crossval_mode, search_mode, n_combinations=None):
+        """
+        It executes the user-specified parameters search algorithm.
+
+        Parameters
+        ----------
+        crossval_mode : cross validation scheme to be applied on the dataset.
+        kfold and holdout are the supported cross-validation modes.                        
+        search_mode : string
+        The type of parameter search algorithm (random_search or grid_seach).
+        n_combinations: integer
+        The number of parameters combinations the random_search alogorithm
+        should generate. This parameter is mandatory if random_search algorithm
+        is used.
+        """
         self.crossval_mode = crossval_mode
             self.n_combinations = n_combinations
             if search_mode == 'random_search':
@@ -19,7 +63,12 @@ class ParamOptimization(object):
             else:
                 raise ValueError('The search mode specified is not supported.')
                 
-    def random_search(self):            
+    def random_search(self):        
+        """
+        Implementation of the random_search algorithm.
+        It randomly selects `n_combinations` rparameter combinations
+        and calls a Train function with these different parameters values.
+        """
         # The code for randomly selecting from the combinations of parameters
         # is based from http://docs.python.org/library/itertools.html#recipes
         # look for 'random_product'.
@@ -35,6 +84,11 @@ class ParamOptimization(object):
         rval = list(func_iterator)
         
     def grid_search(self):
+        """
+        Implementation of the grid_search algorithm.
+        It generates all the possible parameter combinations
+        and calls a Train function with these different parameters values.
+        """
         func_iterator = itertools.imap(self.train_algo,
                                        list(itertools.product(*self.params_ranges.values())))
         rval = list(func_iterator)
@@ -47,4 +101,23 @@ class ParamOptimization(object):
         else:
             raise ValueError("The specified crossvalidation mode %s is not supported" 
                              %self.crossval_mode)    
-    
+
+# Testing the cross-validation implementation.
+def main():
+    test = itertools.imap(pow, (2, 3), (5, 2))
+    print list(test)
+    params_ranges = {'a': [1, 2, 3, 4, 5],
+                          'b': ['no', 'yes'],
+                          'c': [54, 76, 542],
+                          'd': [34, 54, 76]}
+    param_optimizer = ParamOptimization(model=None,
+                                        dataset=None,
+                                        params_ranges=params_ranges,
+                                        cost=None)
+
+    param_optimizer(crossval_mode='kfold',
+                    search_mode='random_search', 
+                    n_combinations=8)
+
+if __name__ == '__main__':
+    main()
