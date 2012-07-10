fixed bug in MND kl divergence, added unit test to catch it

pylearn2/models/tests/test_mnd.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pylearn2.models.mnd import DiagonalMND
 from pylearn2.models.mnd import kl_divergence
+from pylearn2.optimization.batch_gradient_descent import BatchGradientDescent
 from theano import config
 from theano import function
 floatX = config.floatX
@@ -16,14 +17,14 @@ class Test_DiagonalMND:
         self.p = DiagonalMND( nvis = dim,
                 init_beta = 1.,
                 init_mu = 0.,
-                min_beta = 1.,
-                max_beta = 1.)
+                min_beta = 1e-6,
+                max_beta = 1e6)
 
         self.q = DiagonalMND( nvis = dim,
                 init_beta = 1.,
                 init_mu = 0.,
-                min_beta = 1.,
-                max_beta = 1.)
+                min_beta = 1e-6,
+                max_beta = 1e6)
 
     def test_same_zero(self):
         """ checks that two models with the same parameters
@@ -57,8 +58,9 @@ class Test_DiagonalMND:
             #if kl is None, etc.
             assert kl <= tol
 
-    def test_nonnegative(self):
-        """ checks that the kl divergence is non-negative """
+    def test_nonnegative_samples(self):
+        """ checks that the kl divergence is non-negative
+            at sampled parameter values for q and p"""
 
         rng = np.random.RandomState([1,2,3])
 
@@ -84,3 +86,48 @@ class Test_DiagonalMND:
                 raise AssertionError("KL divergence should "
                         "be non-negative but is "+
                         str(kl))
+
+    def test_zero_optimal(self):
+        """ minimizes the kl divergence between q and p
+            using batch gradient descent and checks that
+            the result is zero"""
+
+        rng = np.random.RandomState([1,2,3])
+
+        dim = self.dim
+
+        num_trials = 3
+
+        mu = rng.randn(dim).astype(floatX)
+        beta = rng.uniform(.1,10.,(dim,)).astype(floatX)
+        self.p.mu.set_value(mu)
+        mu = rng.randn(dim).astype(floatX)
+        self.q.mu.set_value(mu)
+        self.p.beta.set_value(beta)
+        beta = rng.uniform(.1,10.,(dim,)).astype(floatX)
+        self.q.beta.set_value(beta)
+
+        kl = kl_divergence(self.q,self.p)
+
+        p = self.p
+        q = self.q
+
+        optimizer = BatchGradientDescent(
+                    objective = kl,
+                    params = [ p.mu, p.beta, q.mu, q.beta ],
+                    param_constrainers = [ p.censor_updates,
+                        q.censor_updates ])
+
+        #optimizer.verbose = True
+
+        kl = optimizer.minimize()
+
+        if kl < 0.:
+            raise AssertionError("KL divergence should "
+                    "be non-negative but is "+
+                    str(kl))
+
+        tol = 5.4e-5
+        assert kl <= tol
+        assert not (kl > tol )
+