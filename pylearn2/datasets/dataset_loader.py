3ed861b2a8c71a2acf9e9f739d2e6d8f71a4e4ce
Li <you@yourdomain.example.com>
2012-08-29 11:27:25 -0400

    dataset loader refined

--- pylearn2/datasets/dataset_loader.py
+++ pylearn2/datasets/dataset_loader.py
@@ -5,24 +5,24 @@ to read the same dataset from ${PYLEARN2_DATA_PATH}, which may cause a
 great burden on the network.
 
 The solution is to copy the required dataset into
-${PYLEARN2_LOCAL_DATA_ROOT} that is local to the node that is
+${PYLEARN2_LOCAL_DATA_PATH} that is local to the node that is
 executing the job. The job can thus read locally the dataset if
-possible.
+possible to reduce the burden on the network.
 
 Then whenever a new job is run on a node, it searches the required
-dataset in ${PYLEARN2_LOCAL_DATA_ROOT}. If the dataset is there, use
+dataset in ${PYLEARN2_LOCAL_DATA_PATH}. If the dataset is there, use
 it directly, otherwise, copy it from the remote server.
 
 workflow:
-1. Check if ${PYLEARN2_LOCAL_DATA_ROOT} exists, if not, create it.
+1. Check if ${PYLEARN2_LOCAL_DATA_PATH} exists, if not, create it.
    This step needs a lock under the dir '/tmp/'
-2. Check if the required dataset D exists under ${PYLEARN2_LOCAL_DATA_ROOT}.
-   If yes, load it.
+2. Check if the required dataset D exists under ${PYLEARN2_LOCAL_DATA_PATH}.
+   If yes, load it. END
    Otherwise:
        Check if the local node has enough space to maintain a local copy of D.
-          If no, read D directly from ${PYLEARN2_DATA_PATH}. END
+          If no, read D directly from remote ${PYLEARN2_DATA_PATH}. END
           If yes:
-            Get a lock under ${PYLEARN2_LOCAL_DATA_ROOT}
+            Get a lock under ${PYLEARN2_LOCAL_DATA_PATH}
             Check if D has been copied there by some other jobs.
                If yes, load it, release the lock. END
                if no,
@@ -33,7 +33,7 @@ workflow:
                   lock. END
 
 Whenever a folder or a dataset copy is created locally, it is granted
-the same access as it has under ${PYLEARN2_LOCAL_DATA_ROOT}. This is
+the same access as it has under ${PYLEARN2_LOCAL_DATA_PATH}. This is
 gauranteed by default copy.
 
 """
@@ -45,22 +45,13 @@ import shutil
 
 class DatasetLoader:
     def __init__(self):
-
+        
         # dataset paths
-        #
-        try:
-            self.dataset_local_dir = string_utils.preprocess(
-                '${PYLEARN2_LOCAL_DATA_ROOT}')
-            # lock to create local copies of datasets inside current_local_dir
-            self.dir_lock_in = os.path.join(self.dataset_local_dir, 'lock')
-
-        except KeyError:
-            self.dataset_local_dir = None
-            self.dir_lock_in = None
-
+                
+        self.dataset_local_dir = string_utils.preprocess('${PYLEARN2_LOCAL_DATA_PATH}')
+        
         # only allow single remote dir
-        self.dataset_remote_dir = string_utils.preprocess(
-            '${PYLEARN2_DATA_PATH}')
+        self.dataset_remote_dir = string_utils.preprocess('${PYLEARN2_DATA_PATH}')
 
         self.tmp_dir = '/tmp'
 
@@ -77,22 +68,17 @@ class DatasetLoader:
         if not os.path.exists(self.dataset_local_dir):
             self.get_lock(self.dir_lock_out)
             if not os.path.exists(self.dataset_local_dir):
-                print 'created local repo ', self.dataset_local_dir
+                print 'local repo created ', self.dataset_local_dir
                 os.makedirs(self.dataset_local_dir)
             self.release_lock()
+        else:
+            print 'local repo already exists. skip.'
 
     def get_lock(self, dir_lock):
         compilelock.get_lock(dir_lock)
 
     def release_lock(self):
         compilelock.release_lock()
-
-    def check_existence(self, fname):
-        """
-        check if the dataset is already there.
-        if yes, return True, otherwise return False
-        """
-        return os.path.exists(fname)
 
     def load_dataset(self, fname):
         # called by e.g. cifar10.py
@@ -102,34 +88,28 @@ class DatasetLoader:
                                                        self.dataset_remote_dir))
             remote_fname = fname
 
-            existing = self.check_existence(local_fname)
+            existing = os.path.exists(local_fname)
 
         else:
             existing = False
 
         if existing:
-            dict = self.load_existing_dataset(local_fname)
+            data_dict = self.load_from(local_fname, location="local")
         else:
-            dict = self.load_missing_dataset(remote_fname, local_fname)
-        return dict
+            data_dict = self.load_missing_dataset(remote_fname, local_fname)
+        return data_dict
 
-    def force_load_from_server(self, fname):
+    def load_from(self, fname, location=None):
+        print "loading from ", location
         dataset_file = open(fname, 'rb')
-        dict = cPickle.load(dataset_file)
+        data_dict = cPickle.load(dataset_file)
         dataset_file.close()
-        return dict
-
-    def load_existing_dataset(self, fname):
-        print 'loading datasets from local dataset repo...'
-        fo = open(fname, 'rb')
-        dict = cPickle.load(fo)
-        fo.close()
-        return dict
+        return data_dict
 
     def copy_from_server_to_local(self, remote_fname, local_fname):
         print "copying the required dataset from remote repo..."
         head, tail = os.path.split(local_fname)
-        head += '/'
+        head += os.path.sep
         if not os.path.exists(head):
             os.makedirs(os.path.dirname(head))
 
@@ -143,44 +123,36 @@ class DatasetLoader:
             self.get_lock(self.dir_lock_in)
             has_enough_space = self.check_enough_space(remote_fname,
                                                        local_fname)
-            existed = self.check_existence(local_fname)
+            existed = os.path.exists(local_fname)
             if has_enough_space and not existed:
                 self.copy_from_server_to_local(remote_fname, local_fname)
                 self.release_lock()
-                dict = self.load_existing_dataset(local_fname)
+                data_dict = self.load_from(local_fname, location="local")
 
             elif existed:
                 self.release_lock()
-                dict = self.load_existing_dataset(local_fname)
+                data_dict = self.load_from(local_fname, location="local")
 
             else:
                 print "not enough space in local machine, load directly from data server"
-                dict = self.force_load_from_server(fname)
+                data_dict = self.load_from(fname, location="server")
         else:
+            self.release_lock()
             print "not enough space in local machine, load directly from data server"
-            dict = self.force_load_from_server(fname)
+            data_dict = self.load_from(fname, location="server")
 
-        return dict
-
-    def get_storage_requirement(self, fname):
-        """
-        from remote server
-        """
-        rval = os.path.getsize(fname)
-        return rval
+        return data_dict
 
     def disk_usage(self, path):
         """
-        Return disk usage statistics about the given path.
-
-        Returned valus is a named tuple with attributes 'total', 'used' and
-        'free', which are the amount of total, used and free space, in bytes.
+        Return free usage about the given path, in bytes
+        
         """
 
         st = os.statvfs(path)
         free = st.f_bavail * st.f_frsize
-        total = st.f_blocks * st.f_frsize
-        used = (st.f_blocks - st.f_bfree) * st.f_frsize
+        #total = st.f_blocks * st.f_frsize
+        #used = (st.f_blocks - st.f_bfree) * st.f_frsize
 
         return free
 
@@ -188,7 +160,7 @@ class DatasetLoader:
         """
         check if the local disk have enough space to store the dataset
         """
-        storage_need = self.get_storage_requirement(remote_fname)
+        storage_need = os.path.getsize(remote_fname)
         storage_remain = self.disk_usage(self.tmp_dir)
 
         return storage_remain > storage_need
--- pylearn2/datasets/tests/test_cache_dataset.py
+++ pylearn2/datasets/tests/test_cache_dataset.py
@@ -1,7 +1,7 @@
 import unittest
 from pylearn2.datasets import cifar10 
 import multiprocessing
-import os
+import os, time
 
 class TestDatasetCache(object):
     """
@@ -9,9 +9,9 @@ class TestDatasetCache(object):
     if the desired dataset exists, load it locally
     if not, load it remotely to the cache first and then load it locally
 
-    whenever a dataset if required, search in the cache first. 
+    whenever a dataset is required, search in the cache first. 
     """
-    
+                                        
     def test_main_multi_thread(self):
         jobs = []
         for i in range(3):
@@ -21,14 +21,16 @@ class TestDatasetCache(object):
 
     def worker(self, num):
         """thread worker function"""
+        time.sleep(2)
         print 'Worker:', num
         trainset = cifar10.CIFAR10(which_set="train")
 
     def test_main(self):
+        import ipdb; ipdb.set_trace()
         trainset = cifar10.CIFAR10(which_set="train")
         
 if __name__ == '__main__':
     instance = TestDatasetCache()
-    instance.test_main_multi_thread()
+    #instance.test_main_multi_thread()
     #instance.test_main()
     #os.system()
