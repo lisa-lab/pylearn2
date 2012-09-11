ab1d89083e34a136e433a5ce4cab85dcda4eeafe
Vincent Dumoulin <vi.dumoulin@gmail.com>
2012-09-11 13:44:41 -0400

    Added the negative log-likelihood cost for a one-hot encoded target (merged)

--- pylearn2/costs/supervised_cost.py
+++ pylearn2/costs/supervised_cost.py
@@ -10,3 +10,39 @@ from pylearn2.costs.cost import SupervisedCost
 # import it
 from pylearn2.costs.cost import CrossEntropy
 
+
+class CrossEntropy(SupervisedCost):
+    def __call__(self, model, X, Y):
+        return (-Y * T.log(model(X)) - \
+                (1 - Y) * T.log(1 - model(X))).sum(axis=1).mean()
+
+
+class NegativeLogLikelihood(SupervisedCost):
+    """
+    Represents the mean negative log-likelihood of a model's output, provided
+    the target Y is one-hot encoded. Equivalent to
+    
+        .. math::
+
+        cost = - \frac{1}{N} \sum_{i=1}^N log(p(Y = y^{(i)} | x^{(i)}, \theta))
+        
+    We compute the mean of the negative log-likelihood instead of the NLL 
+    itself for the sake of simplicity and to make the cost more invariant to 
+    the dataset's size.
+    """
+    def __call__(self, model, X, Y):
+        """
+        Returns the mean negative log-likelihood of a model for input X given 
+        a one-hot encoded target Y.
+        
+        Parameters
+        ----------
+        model : pylearn2.models.model.Model
+            the model for which we want to calculate the negative 
+            log-likelihood
+        X : tensor_like
+            input to the model
+        Y : tensor_like
+            one-hot encoded target
+        """
+        return (-Y * T.log(model(X))).sum(axis=1).mean()
