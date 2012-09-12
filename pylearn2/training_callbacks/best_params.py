a1c6b5520f2d1f25e36835fe2a8dcff4a2a286b5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2012-09-12 09:00:16 -0400

    Changed comments a bit

--- pylearn2/training_callbacks/best_params.py
+++ pylearn2/training_callbacks/best_params.py
@@ -3,17 +3,17 @@ from pylearn2.training_callbacks.training_callback import TrainingCallback
 
 class KeepBestParams(TrainingCallback):
     """
-    A callback which keeps a model's best parameters based on its performance
-    on a given cost with a given dataset.
+    A callback which keeps track of a model's best parameters based on its
+    performance for a given cost on a given dataset.
     """
     def __init__(self, model, cost, monitoring_dataset, batch_size):
         """
         Parameters
         ----------
         model : pylearn2.models.model.Model
-            the model whose best parameters we want to keeps
+            the model whose best parameters we want to keep track of
         cost : tensor_like
-            used to evaluate the model's performance
+            cost function used to evaluate the model's performance
         monitoring_dataset : pylearn2.datasets.dataset.Dataset
             dataset on which to compute the cost
         batch_size : int
@@ -25,7 +25,6 @@ class KeepBestParams(TrainingCallback):
         self.batch_size = batch_size
         self.minibatch = T.matrix('minibatch')
         self.target = T.matrix('target')
-        # Compute the cost function
         if cost.supervised:
             self.supervised = True
             self.cost_function = theano.function(inputs=[self.minibatch,
@@ -45,7 +44,7 @@ class KeepBestParams(TrainingCallback):
         """
         Looks whether the model performs better than earlier. If it's the
         case, records the model's parameters.
-        
+
         Parameters
         ----------
         model : pylearn2.models.model.Model
@@ -72,4 +71,7 @@ class KeepBestParams(TrainingCallback):
             self.best_params = self.model.get_param_values()
 
     def get_best_params(self):
+        """
+        Returns the best parameters up to now for the model.
+        """
         return self.best_params
