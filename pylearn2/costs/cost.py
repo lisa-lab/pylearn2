1f8a4d0eee2dbcb8c4e08e13e3ff448b193d44f2
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-09-19 10:27:57 -0400

    added make_method_cost

--- pylearn2/costs/cost.py
+++ pylearn2/costs/cost.py
@@ -39,3 +39,41 @@ class CrossEntropy(SupervisedCost):
         """WRITEME"""
         return (-Y * T.log(model(X)) - \
                 (1 - Y) * T.log(1 - model(X))).sum(axis=1).mean()
+
+def make_method_cost(method, superclass):
+    """
+        A cost specified via the string name of a method of the model.
+        Makes a new class derived from superclass.
+        Assumes all it needs to implement is __call__ and that the
+        first argument to __call__ is called 'model'.
+        Implements this method by passing the remaining arguments
+        to getattr(model, method)
+
+        Example usage:
+
+        class MyCrazyNewModel(Model):
+
+            def my_crazy_new_loss_function(self, X):
+
+                ...
+
+        my_cost = method_cost('my_crazy_new_loss_function', UnsupervisedCost)
+    """
+
+    class MethodCost(superclass):
+        """ A Cost defined by the name of a model's method """
+        def __call__(self, model, *args, **kwargs):
+            """ Patches calls through to a user-specified method of the model """
+            fn = getattr(model, method)
+            return fn(*args, **kwargs)
+
+    rval = MethodCost()
+    if not isinstance(rval, Cost):
+        raise TypeError(("make_method_cost made something that isn't a "
+                "Cost instance (%s of type %s)."
+                " This probably means the superclass you provided isn't a "
+                "subclass of Cost.") % (str(rval),str(type(rval))))
+        # TODO: is there a way to directly check superclass?
+    return rval
+
+
