b5497c8ba33cd7bb8ec1b6ebf1480810fed3dcab
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-09-28 11:42:46 -0400

    wrap scipy imports

--- pylearn2/datasets/sparse_dataset.py
+++ pylearn2/datasets/sparse_dataset.py
@@ -1,18 +1,23 @@
 from pylearn2.datasets.dataset import Dataset
 from pylearn2.utils.iteration import SequentialSubsetIterator
-import numpy, scipy.sparse
+import numpy
+import warnings
+try:
+    import scipy.sparse
+except ImportError:
+    warnings.warn("Couldn't import scipy.sparse")
 import theano
 import gzip
 floatX = theano.config.floatX
 
 class SparseDataset(Dataset):
     """
-    SparseDataset is by itself an iterator. 
+    SparseDataset is by itself an iterator.
     """
     def __init__(self, load_path=None, from_scipy_sparse_dataset=None, zipped_npy=True):
-        
+
         self.load_path = load_path
-        
+
         if self.load_path != None:
             if zipped_npy == True:
                 print '... loading sparse data set from a zip npy file'
@@ -28,17 +33,17 @@ class SparseDataset(Dataset):
 
         self.data_n_rows = self.sparse_matrix.shape[0]
         self.num_examples = self.data_n_rows
-        
+
     def get_design_matrix(self):
         return self.sparse_matrix
-    
+
     def get_batch_design(self, batch_size, include_labels=False):
         """
         method inherited from Dataset
         """
         self.iterator(mode='sequential', batch_size=batch_size, num_batches=None, topo=None)
         return self.next()
-    
+
     def get_batch_topo(self, batch_size):
         """
         method inherited from Dataset
@@ -51,7 +56,7 @@ class SparseDataset(Dataset):
         method inherited from Dataset
         """
         return self.iterator(mode, batch_size, num_batches, topo)
-        
+
     def iterator(self, mode=None, batch_size=None, num_batches=None,
                  topo=None, targets=None, rng=None):
         """
@@ -60,19 +65,18 @@ class SparseDataset(Dataset):
         self.mode = mode
         self.batch_size = batch_size
         self._targets = targets
-        
+
         if mode == 'sequential':
             self.subset_iterator = SequentialSubsetIterator(self.data_n_rows,
                                             batch_size, num_batches, rng=None)
             return self
         else:
-            
             raise NotImplementedError('other iteration scheme not supported for now!')
-        
-    
+
+
     def __iter__(self):
         return self
-    
+
     def next(self):
         indx = self.subset_iterator.next()
         try:
@@ -81,5 +85,3 @@ class SparseDataset(Dataset):
             # the ind of minibatch goes beyond the boundary
             import ipdb; ipdb.set_trace()
         return mini_batch
-        
-    
