6d1be2eb042835a4fdb8a6b8144e87271e122fbe
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-10-09 14:06:11 -0400

    added shared_code.py

---
+++ pylearn2/sandbox/cuda_convnet/shared_code.py
@@ -0,0 +1,32 @@
+"""
+Convenience methods for accessing C code shared by the code
+generators in different parts of this module.
+"""
+
+__authors__ = "Ian Goodfellow"
+__copyright__ = "Copyright 2010-2012, Universite de Montreal"
+__credits__ = ["Ian Goodfellow"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Ian Goodfellow"
+__email__ = "goodfeli@iro"
+
+import pylearn2
+pylearn2_path, = pylearn2.__path__
+this_dir = pylearn2_path + "/sandbox/cuda_convnet/"
+
+def load_code(local_path):
+
+    path = this_dir + local_path
+    f = open(path)
+    return f.read()
+
+def get_NVMatrix_code():
+    header = load_code("nvmatrix.cuh")
+    source = load_code("nvmatrix.cu")
+
+    rval = header + source
+
+    return rval
+
+def get_filter_acts_code():
+    raise NotImplementedError()
