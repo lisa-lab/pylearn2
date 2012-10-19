bf8de2023e1277b2f38f9b4cffe5f9a7debd8b87
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-10-19 17:33:03 -0400

    added MonitorBasedSaveBest callback

--- pylearn2/training_callbacks/best_params.py
+++ pylearn2/training_callbacks/best_params.py
@@ -1,7 +1,9 @@
 import numpy
+np = numpy
 from pylearn2.training_callbacks.training_callback import TrainingCallback
 import theano
 import theano.tensor as T
+from pylearn2.utils import serial
 
 
 class KeepBestParams(TrainingCallback):
@@ -78,3 +80,47 @@ class KeepBestParams(TrainingCallback):
         Returns the best parameters up to now for the model.
         """
         return self.best_params
+
+
+class MonitorBasedSaveBest(TrainingCallback):
+    """
+    A callback that saves a copy of the model every time it achieves
+    a new minimal value of a monitoring channel.
+    """
+    def __init__(self, channel_name, save_path):
+        """
+        Parameters
+        ----------
+        channel_name: the name of the channel we want to minimize
+        save_path: the path to save the best model to
+        """
+
+        self.__dict__.update(locals())
+        del self.self
+        self.best_cost = np.inf
+
+
+    def __call__(self, model, dataset, algorithm):
+        """
+        Looks whether the model performs better than earlier. If it's the
+        case, saves the model.
+
+        Parameters
+        ----------
+        model : pylearn2.models.model.Model
+                model.monitor must contain a channel with name given by self.channel_name
+        dataset : pylearn2.datasets.dataset.Dataset
+            not used
+        algorithm : TrainingAlgorithm
+            not used
+        """
+
+        monitor = model.monitor
+        channels = monitor.channels
+        channel = channels[self.channel_name]
+        val_record = channel.val_record
+        new_cost = val_record[-1]
+
+        if new_cost < self.best_cost:
+            serial.save(self.save_path, model, on_overwrite = 'backup')
+
