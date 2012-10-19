7985b44c49451a751339257127c2527151cc96a3
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-10-19 17:46:52 -0400

    added summarize_model script

---
+++ pylearn2/scripts/summarize_model.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+__authors__ = "Ian Goodfellow"
+__copyright__ = "Copyright 2010-2012, Universite de Montreal"
+__credits__ = ["Ian Goodfellow"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Ian Goodfellow"
+__email__ = "goodfeli@iro"
+import numpy as np
+import sys
+path = sys.argv[1]
+from pylearn2.utils import serial
+model = serial.load(path)
+for param in model.get_params():
+    name = param.name
+    if name is None:
+        name = '<anon>'
+    v = param.get_value()
+    print name+': '+str((v.min(),v.mean(),v.max()))+' '+str(v.shape)
+    v = np.abs(v)
+    print 'abs('+name+'): '+str((v.min(),v.mean(),v.max()))
+if hasattr(model,'monitor'):
+    print 'trained on',model.monitor.get_examples_seen(),' examples'
+    print 'which corresponds to',model.monitor.get_batches_seen(),' batches'
