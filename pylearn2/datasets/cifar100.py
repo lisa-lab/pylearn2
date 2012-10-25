0e129e66de82c9ec6f238c01f08711297fae7cb1
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-10-25 13:14:22 -0400

    gcn support for cifar100

--- pylearn2/datasets/cifar100.py
+++ pylearn2/datasets/cifar100.py
@@ -4,7 +4,8 @@ from pylearn2.datasets import dense_design_matrix
 from pylearn2.utils import serial
 
 class CIFAR100(dense_design_matrix.DenseDesignMatrix):
-    def __init__(self, which_set, center = False):
+    def __init__(self, which_set, center = False,
+            gcn = None):
 
         assert which_set in ['train','test']
 
@@ -24,6 +25,13 @@ class CIFAR100(dense_design_matrix.DenseDesignMatrix):
         if center:
             X -= 127.5
 
+        self.gcn = gcn
+        if gcn is not None:
+            assert isinstance(gcn,float)
+            X = (X.T - X.mean(axis=1)).T
+            X = (X.T / np.sqrt(np.square(X).sum(axis=1))).T
+            X *= gcn
+
         view_converter = dense_design_matrix.DefaultViewConverter((32,32,3))
 
         super(CIFAR100,self).__init__(X = X, y =y, view_converter = view_converter)
@@ -33,22 +41,67 @@ class CIFAR100(dense_design_matrix.DenseDesignMatrix):
         self.y_fine = N.asarray(obj['fine_labels'])
         self.y_coarse = N.asarray(obj['coarse_labels'])
 
+        self.y = self.y_fine
+
 
     def adjust_for_viewer(self, X):
+        #assumes no preprocessing. need to make preprocessors mark the new ranges
+        rval = X.copy()
 
+        #patch old pkl files
+        if not hasattr(self,'center'):
+            self.center = False
+        if not hasattr(self,'rescale'):
+            self.rescale = False
+        if not hasattr(self,'gcn'):
+            self.gcn = False
 
-        #this is a bit of a hack
-        #it detects older saved pkls, which are probably preprocessed somehow
-        #really, the preprocessing needs to update the behavior of adjust_for_viewer
-        if not hasattr(self, 'center'):
+        if self.gcn is not None:
             rval = X.copy()
-            rval = rval.T
+            for i in xrange(rval.shape[0]):
+                rval[i,:] /= np.abs(rval[i,:]).max()
+            return rval
 
-            rval /= np.abs(rval).max()
-            return rval.T
+        if not self.center:
+            rval -= 127.5
 
+        if not self.rescale:
+            rval /= 127.5
 
-        if self.center:
-            return X / 127.5
+        rval = np.clip(rval,-1.,1.)
 
-        return (X - 127.5)/127.5
+        return rval
+
+    def adjust_to_be_viewed_with(self, X, orig, per_example = False):
+        # if the scale is set based on the data, display X oring the scale determined
+        # by orig
+        # assumes no preprocessing. need to make preprocessors mark the new ranges
+        rval = X.copy()
+
+        #patch old pkl files
+        if not hasattr(self,'center'):
+            self.center = False
+        if not hasattr(self,'rescale'):
+            self.rescale = False
+        if not hasattr(self,'gcn'):
+            self.gcn = False
+
+        if self.gcn is not None:
+            rval = X.copy()
+            if per_example:
+                for i in xrange(rval.shape[0]):
+                    rval[i,:] /= np.abs(orig[i,:]).max()
+            else:
+                rval /= np.abs(orig).max()
+            rval = np.clip(rval, -1., 1.)
+            return rval
+
+        if not self.center:
+            rval -= 127.5
+
+        if not self.rescale:
+            rval /= 127.5
+
+        rval = np.clip(rval,-1.,1.)
+
+        return rval
