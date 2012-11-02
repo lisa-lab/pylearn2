e7eba91e1b3024f6820db8995f6b8bafc70fe4cf
Vincent Dumoulin <vi.dumoulin@gmail.com>
2012-11-02 11:32:11 -0400

    Removed LRDecay and removed the optional decay in Momentum

--- pylearn2/search_direction/momentum.py
+++ pylearn2/search_direction/momentum.py
@@ -5,20 +5,18 @@ from pylearn2.search_direction.search_direction import SearchDirection
 
 
 class Momentum(SearchDirection):
-    def __init__(self, mass, decay_rate=0):
+    def __init__(self, mass):
         self.mass = mass
-        self.decay_rate = decay_rate
     
     def dir_from_grad(self, gradients):
         time = sharedX(0)
-        decay = 1. / (1. + self.decay_rate * time)
 
         updates = {}
         direction = {}
         for param, grad in gradients.iteritems():
             v = sharedX(numpy.zeros_like(param.get_value()))
             assert v.dtype == grad.dtype
-            updates[v] = decay * grad - self.mass * v
+            updates[v] = grad - self.mass * v
             direction[param] = v
 
         return direction, updates
