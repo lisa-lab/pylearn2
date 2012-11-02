a81a538f67309a5ce1290f6788d6c62374546da0
Vincent Dumoulin <vi.dumoulin@gmail.com>
2012-11-02 09:46:56 -0400

    Humble attempt at adding learning rate decay to momentum

--- pylearn2/search_direction/momentum.py
+++ pylearn2/search_direction/momentum.py
@@ -5,16 +5,20 @@ from pylearn2.search_direction.search_direction import SearchDirection
 
 
 class Momentum(SearchDirection):
-    def __init__(self, mass):
+    def __init__(self, mass, decay_rate=0):
         self.mass = mass
+        self.decay_rate = decay_rate
     
     def dir_from_grad(self, gradients):
+        time = sharedX(0)
+        decay = 1. / (1. + self.decay_rate * time)
+
         updates = {}
         direction = {}
         for param, grad in gradients.iteritems():
             v = sharedX(numpy.zeros_like(param.get_value()))
             assert v.dtype == grad.dtype
-            updates[v] = grad - self.mass * v
+            updates[v] = decay * grad - self.mass * v
             direction[param] = v
 
         return direction, updates
