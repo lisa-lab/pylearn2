45a18ed1ff9f606433f395066d87e27f945eb34e
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-11-07 16:49:41 -0500

    Attempt to rule out global/module level variables as source of multiprocessing crash.

--- pylearn2/scripts/show_weights.py
+++ pylearn2/scripts/show_weights.py
@@ -3,25 +3,27 @@
 from pylearn2.gui import get_weights_report
 from optparse import OptionParser
 
+def main():
+    parser = OptionParser()
 
-parser = OptionParser()
+    parser.add_option("--rescale",dest='rescale',type='string',default="individual")
+    parser.add_option("--out",dest="out",type='string',default=None)
+    parser.add_option("--border", dest="border", action="store_true",default=False)
 
-parser.add_option("--rescale",dest='rescale',type='string',default="individual")
-parser.add_option("--out",dest="out",type='string',default=None)
-parser.add_option("--border", dest="border", action="store_true",default=False)
+    options, positional = parser.parse_args()
 
-options, positional = parser.parse_args()
+    assert len(positional) == 1
+    path ,= positional
 
-assert len(positional) == 1
-path ,= positional
+    rescale = options.rescale
+    border = options.border
 
-rescale = options.rescale
-border = options.border
+    pv = get_weights_report.get_weights_report(model_path = path, rescale = rescale, border = border)
 
-pv = get_weights_report.get_weights_report(model_path = path, rescale = rescale, border = border)
+    if options.out is None:
+        pv.show()
+    else:
+        pv.save(options.out)
 
-if options.out is None:
-    pv.show()
-else:
-    pv.save(options.out)
-
+if __name__ == "__main__":
+    main()
