97df7f8dba24d037df2ef7a77f43061346b65845
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-11-08 16:13:21 -0500

    multi file printing

--- pylearn2/scripts/print_monitor.py
+++ pylearn2/scripts/print_monitor.py
@@ -6,10 +6,12 @@ __license__ = "3-clause BSD"
 __maintainer__ = "Ian Goodfellow"
 __email__ = "goodfeli@iro"
 import sys
-_, model_path = sys.argv
-from pylearn2.utils import serial
-model = serial.load(model_path)
-monitor = model.monitor
-channels = monitor.channels
-for key in sorted(channels.keys()):
-    print key, ':', channels[key].val_record[-1]
+for model_path in sys.argv[1:]:
+    if len(sys.argv) > 2:
+        print model_path
+    from pylearn2.utils import serial
+    model = serial.load(model_path)
+    monitor = model.monitor
+    channels = monitor.channels
+    for key in sorted(channels.keys()):
+        print key, ':', channels[key].val_record[-1]
