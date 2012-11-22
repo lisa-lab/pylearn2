65d065081b9d4f5761cb38784b9a209ffed23665
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-11-22 09:24:54 -0500

    started the binarizer dataset

---
+++ pylearn2/datasets/binarizer.py
@@ -0,0 +1,22 @@
+__authors__ = "Ian Goodfellow"
+__copyright__ = "Copyright 2010-2012, Universite de Montreal"
+__credits__ = ["Ian Goodfellow"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Ian Goodfellow"
+__email__ = "goodfeli@iro"
+from pylearn2.datasets.transformer_dataset import TransformerDataset
+
+class Binarizer(TransformerDataset):
+    """
+        A TransformerDataset that takes examples with features in the interval
+        [0,1], and uses these as Bernoulli parameters to sample examples
+        with features in {0,1}.
+    """
+    def __init__(self, raw):
+        """
+            raw: a pylearn2 Dataset that provides examples with features
+                in the interval [0, 1]
+        """
+
+        super(Binarizer, self).__init__(raw, transformer)
+
