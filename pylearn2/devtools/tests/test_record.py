97a276d94ca3fa56b7cf92b7b9b7db04821ebe52
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-11-27 18:16:00 -0500

    added test_record_bad

--- pylearn2/devtools/tests/test_record.py
+++ pylearn2/devtools/tests/test_record.py
@@ -1,4 +1,5 @@
 from pylearn2.devtools.record import Record
+from pylearn2.devtools.record import MismatchError
 import cStringIO
 
 def test_record_good():
@@ -35,3 +36,37 @@ def test_record_good():
         playback_checker.handle_line(str(i)+'\n')
 
 
+def test_record_bad():
+
+    """
+    Tests that when we record a sequence of events, then
+    do something different on playback, the Record class catches it.
+    """
+
+    # Record a sequence of events
+    output = cStringIO.StringIO()
+
+    recorder = Record(file_object=output, replay=False)
+
+    num_lines = 10
+
+    for i in xrange(num_lines):
+        recorder.handle_line(str(i)+'\n')
+
+    # Make sure that the playback functionality doesn't raise any errors
+    # when we repeat some of them
+    output_value = output.getvalue()
+    output = cStringIO.StringIO(output_value)
+
+    playback_checker = Record(file_object=output,  replay=True)
+
+    for i in xrange(num_lines // 2):
+        playback_checker.handle_line(str(i)+'\n')
+
+    # Make sure it raises an error when we deviate from the recorded sequence
+    try:
+        playback_checker.handle_line('0\n')
+    except MismatchError:
+        return
+    raise AssertionError("Failed to detect mismatch between recorded sequence "
+            " and repetition of it.")
