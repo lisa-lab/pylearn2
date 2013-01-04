15e7015b8cb783efb29f714d070aae223e8693ca
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-01-04 17:02:33 -0500

    made print_monitor print epochs seen

--- pylearn2/scripts/print_monitor.py
+++ pylearn2/scripts/print_monitor.py
@@ -6,12 +6,13 @@ __license__ = "3-clause BSD"
 __maintainer__ = "Ian Goodfellow"
 __email__ = "goodfeli@iro"
 import sys
+from pylearn2.utils import serial
 for model_path in sys.argv[1:]:
     if len(sys.argv) > 2:
         print model_path
-    from pylearn2.utils import serial
     model = serial.load(model_path)
     monitor = model.monitor
     channels = monitor.channels
+    print 'epochs seen: ',monitor._epochs_seen
     for key in sorted(channels.keys()):
         print key, ':', channels[key].val_record[-1]
