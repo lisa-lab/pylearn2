c8460d44c643b1032515c846182e063157561e33
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-01-05 14:55:00 -0500

    added SoftmaxPool layer to mlp

--- pylearn2/models/mlp.py
+++ pylearn2/models/mlp.py
@@ -373,11 +373,8 @@ class Softmax(Layer):
     def set_weights(self, weights):
         self.W.set_value(weights)
 
-    def set_biases(self, biases, recenter=False):
+    def set_biases(self, biases):
         self.b.set_value(biases)
-        if recenter:
-            assert self.center
-            self.offset.set_value( (np.exp(biases) / np.exp(biases).sum()).astype(self.offset.dtype))
 
     def get_biases(self):
         return self.b.get_value()
@@ -486,4 +483,283 @@ class WeightDecay(Cost):
         total_cost.name = 'weight_decay'
 
         return total_cost
+
+
+
+class SoftmaxPool(Layer):
+    """
+        A hidden layer that uses the softmax function to do
+        max pooling over groups of units.
+        When the pooling size is 1, this reduces to a standard
+        sigmoidal MLP layer.
+        """
+    
+    def __init__(self,
+                 detector_layer_dim,
+                 pool_size,
+                 layer_name,
+                 irange = None,
+                 sparse_init = None,
+                 sparse_stdev = 1.,
+                 include_prob = 1.0,
+                 init_bias = 0.,
+                 W_lr_scale = None,
+                 b_lr_scale = None,
+                 mask_weights = None,
+        ):
+        """
+            
+            include_prob: probability of including a weight element in the set
+            of weights initialized to U(-irange, irange). If not included
+            it is initialized to 0.
+            
+            """
+        self.__dict__.update(locals())
+        del self.self
+        
+        self.b = sharedX( np.zeros((self.detector_layer_dim,)) + init_bias, name = layer_name + '_b')
+    
+    def get_lr_scalers(self):
+        
+        if not hasattr(self, 'W_lr_scale'):
+            self.W_lr_scale = None
+        
+        if not hasattr(self, 'b_lr_scale'):
+            self.b_lr_scale = None
+        
+        rval = OrderedDict()
+        
+        if self.W_lr_scale is not None:
+            W, = self.transformer.get_params()
+            rval[W] = self.W_lr_scale
+        
+        if self.b_lr_scale is not None:
+            rval[self.b] = self.b_lr_scale
+        
+        return rval
+    
+    def set_input_space(self, space):
+        """ Note: this resets parameters! """
+        
+        self.input_space = space
+        
+        if isinstance(space, VectorSpace):
+            self.requires_reformat = False
+            self.input_dim = space.dim
+        else:
+            self.requires_reformat = True
+            self.input_dim = space.get_total_dimension()
+            self.desired_space = VectorSpace(self.input_dim)
+        
+        
+        if not (self.detector_layer_dim % self.pool_size == 0):
+            raise ValueError("detector_layer_dim = %d, pool_size = %d. Should be divisible but remainder is %d" %
+                             (self.detector_layer_dim, self.pool_size, self.detector_layer_dim % self.pool_size))
+        
+        self.h_space = VectorSpace(self.detector_layer_dim)
+        self.pool_layer_dim = self.detector_layer_dim / self.pool_size
+        self.output_space = VectorSpace(self.pool_layer_dim)
+        
+        rng = self.dbm.rng
+        if self.irange is not None:
+            assert self.sparse_init is None
+            W = rng.uniform(-self.irange,
+                            self.irange,
+                            (self.input_dim, self.detector_layer_dim)) * \
+                (rng.uniform(0.,1., (self.input_dim, self.detector_layer_dim))
+                 < self.include_prob)
+        else:
+            assert self.sparse_init is not None
+            W = np.zeros((self.input_dim, self.detector_layer_dim))
+            def mask_rejects(idx, i):
+                if self.mask_weights is None:
+                    return False
+                return self.mask_weights[idx, i] == 0.
+            for i in xrange(self.detector_layer_dim):
+                assert self.sparse_init <= self.input_dim
+                for j in xrange(self.sparse_init):
+                    idx = rng.randint(0, self.input_dim)
+                    while W[idx, i] != 0 or mask_rejects(idx, i):
+                        idx = rng.randint(0, self.input_dim)
+                    W[idx, i] = rng.randn()
+            W *= self.sparse_stdev
+        
+        W = sharedX(W)
+        W.name = self.layer_name + '_W'
+        
+        self.transformer = MatrixMul(W)
+        
+        W ,= self.transformer.get_params()
+        assert W.name is not None
+        
+        if self.mask_weights is not None:
+            expected_shape =  (self.input_dim, self.detector_layer_dim)
+            if expected_shape != self.mask_weights.shape:
+                raise ValueError("Expected mask with shape "+str(expected_shape)+" but got "+str(self.mask_weights.shape))
+            self.mask = sharedX(self.mask_weights)
+    
+    def censor_updates(self, updates):
+        
+        # Patch old pickle files
+        if not hasattr(self, 'mask_weights'):
+            self.mask_weights = None
+        
+        if self.mask_weights is not None:
+            W ,= self.transformer.get_params()
+            if W in updates:
+                updates[W] = updates[W] * self.mask
+    
+    def get_params(self):
+        assert self.b.name is not None
+        W ,= self.transformer.get_params()
+        assert W.name is not None
+        rval = self.transformer.get_params()
+        assert not isinstance(rval, set)
+        rval = list(rval)
+        assert self.b not in rval
+        rval.append(self.b)
+        return rval
+    
+    def get_weight_decay(self, coeff):
+        if isinstance(coeff, str):
+            coeff = float(coeff)
+        assert isinstance(coeff, float) or hasattr(coeff, 'dtype')
+        W ,= self.transformer.get_params()
+        return coeff * T.sqr(W).sum()
+    
+    def get_weights(self):
+        if self.requires_reformat:
+            # This is not really an unimplemented case.
+            # We actually don't know how to format the weights
+            # in design space. We got the data in topo space
+            # and we don't have access to the dataset
+            raise NotImplementedError()
+        W ,= self.transformer.get_params()
+        return W.get_value()
+    
+    def set_weights(self, weights):
+        W, = self.transformer.get_params()
+        W.set_value(weights)
+    
+    def set_biases(self, biases):
+        self.b.set_value(biases)
+    
+    def get_biases(self):
+        return self.b.get_value()
+    
+    def get_weights_format(self):
+        return ('v', 'h')
+    
+    def get_weights_view_shape(self):
+        total = self.detector_layer_dim
+        cols = self.pool_size
+        if cols == 1:
+            # Let the PatchViewer decide how to arrange the units
+            # when they're not pooled
+            raise NotImplementedError()
+        # When they are pooled, make each pooling unit have one row
+        rows = total / cols
+        return rows, cols
+    
+    
+    def get_weights_topo(self):
+        
+        if not isinstance(self.input_space, Conv2DSpace):
+            raise NotImplementedError()
+        
+        W ,= self.transformer.get_params()
+        
+        W = W.T
+        
+        W = W.reshape((self.detector_layer_dim, self.input_space.shape[0],
+                       self.input_space.shape[1], self.input_space.nchannels))
+        
+        W = Conv2DSpace.convert(W, self.input_space.axes, ('b', 0, 1, 'c'))
+        
+        return function([], W)()
+        
+    def get_monitoring_channels(self):
+        
+        W ,= self.transformer.get_params()
+        
+        assert W.ndim == 2
+        
+        sq_W = T.sqr(W)
+        
+        row_norms = T.sqrt(sq_W.sum(axis=1))
+        col_norms = T.sqrt(sq_W.sum(axis=0))
+        
+        return OrderedDict([
+                            ('row_norms_min'  , row_norms.min()),
+                            ('row_norms_mean' , row_norms.mean()),
+                            ('row_norms_max'  , row_norms.max()),
+                            ('col_norms_min'  , col_norms.min()),
+                            ('col_norms_mean' , col_norms.mean()),
+                            ('col_norms_max'  , col_norms.max()),
+                            ])
+    
+    
+    def get_monitoring_channels_from_state(self, state):
+        
+        P, H = state
+        
+        rval = OrderedDict()
+        
+        if self.pool_size == 1:
+            vars_and_prefixes = [ (P,'') ]
+        else:
+            vars_and_prefixes = [ (P, 'p_'), (H, 'h_') ]
+        
+        for var, prefix in vars_and_prefixes:
+            v_max = var.max(axis=0)
+            v_min = var.min(axis=0)
+            v_mean = var.mean(axis=0)
+            v_range = v_max - v_min
+            
+            # max_x.mean_u is "the mean over *u*nits of the max over e*x*amples"
+            # The x and u are included in the name because otherwise its hard
+            # to remember which axis is which when reading the monitor
+            # I use inner.outer rather than outer_of_inner or something like that
+            # because I want mean_x.* to appear next to each other in the alphabetical
+            # list, as these are commonly plotted together
+            for key, val in [
+                             ('max_x.max_u', v_max.max()),
+                             ('max_x.mean_u', v_max.mean()),
+                             ('max_x.min_u', v_max.min()),
+                             ('min_x.max_u', v_min.max()),
+                             ('min_x.mean_u', v_min.mean()),
+                             ('min_x.min_u', v_min.min()),
+                             ('range_x.max_u', v_range.max()),
+                             ('range_x.mean_u', v_range.mean()),
+                             ('range_x.min_u', v_range.min()),
+                             ('mean_x.max_u', v_mean.max()),
+                             ('mean_x.mean_u', v_mean.mean()),
+                             ('mean_x.min_u', v_mean.min())
+                             ]:
+                rval[prefix+key] = val
+        
+        return rval
+
+    def fprop(self, state_below):
+        
+        self.input_space.validate(state_below)
+        
+        if self.requires_reformat:
+            if not isinstance(state_below, tuple):
+                for sb in get_debug_values(state_below):
+                    if sb.shape[0] != self.dbm.batch_size:
+                        raise ValueError("self.dbm.batch_size is %d but got shape of %d" % (self.dbm.batch_size, sb.shape[0]))
+                    assert reduce(lambda x,y: x * y, sb.shape[1:]) == self.input_dim
+            
+            state_below = self.input_space.format_as(state_below, self.desired_space)
+        
+        z = self.transformer.lmul(state_below) + self.b
+        if self.layer_name is not None:
+            z.name = self.layer_name + '_z'
+        p,h = max_pool_channels(z, self.pool_size)
+        
+        p.name = self.layer_name + '_p_' + iter_name
+        
+        return p
+
 
