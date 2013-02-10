9bda654330a352b0c63852507ae8f5cbe1c016e8
David Warde-Farley <wardefar@iro.umontreal.ca>
2013-02-10 05:58:21 -0500

    Slightly better tests.

--- pylearn2/sandbox/cuda_convnet/tests/test_response_norm.py
+++ pylearn2/sandbox/cuda_convnet/tests/test_response_norm.py
@@ -9,18 +9,17 @@ except ImportError:
     raise SkipTest('cuda not available')
 
 
-def test_cross_map_norm_actually_runs():
-    op = CrossMapNorm(10, 5, 2, True)
-    rng = numpy.random.RandomState([2013, 02, 10])
-    x = CudaNdarray(abs(rng.randn(16, 2, 2, 2)).astype('float32'))
+def test_cross_map_norm_actually_simple():
+    op = CrossMapNorm(16, 15. / 16., 1., True)
+    x = CudaNdarray(numpy.ones((16, 2, 2, 2), dtype='float32'))
     x_ = theano.tensor.TensorVariable(CudaNdarrayType([False] * 4))
     f = theano.function([x_], op(x_)[0])
-    f(x)
+    numpy.testing.assert_allclose(numpy.asarray(f(x)), 0.0625)
 
 
-def test_cross_map_norm_grad_actually_runs():
-    op = CrossMapNorm(5, 20, 2, False)
-    x = CudaNdarray(abs(numpy.random.RandomState([2013, 02, 10]).randn(16, 2, 2, 2)).astype('float32'))
+def test_cross_map_norm_grad_simple():
+    op = CrossMapNorm(16, 15./16., 1, True)
     x_ = theano.tensor.TensorVariable(CudaNdarrayType([False] * 4))
     f = theano.function([x_], theano.grad(op(x_)[0].sum(), x_))
+    x = CudaNdarray(numpy.ones((16, 2, 2, 2), dtype='float32'))
     f(x)
