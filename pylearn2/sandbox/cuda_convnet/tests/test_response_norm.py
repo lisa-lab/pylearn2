7c968d4ac35d9f35dc1b1b786300b7f171f02d79
David Warde-Farley <wardefar@iro.umontreal.ca>
2013-02-11 00:40:23 -0500

    Better, more modular test.

--- pylearn2/sandbox/cuda_convnet/tests/test_response_norm.py
+++ pylearn2/sandbox/cuda_convnet/tests/test_response_norm.py
@@ -1,6 +1,8 @@
 import numpy
 import theano
 from nose.plugins.skip import SkipTest
+from theano.sandbox.cuda import gpu_from_host
+from theano.tests.unittest_tools import verify_grad
 
 try:
     from pylearn2.sandbox.cuda_convnet.response_norm import (
@@ -21,12 +23,14 @@ def test_cross_map_norm_simple():
 
 
 def test_cross_map_norm_grad_simple():
-    op = CrossMapNorm(16, 15./16., 1, True)
-    x_ = theano.tensor.TensorVariable(CudaNdarrayType([False] * 4))
-    f = theano.function([x_], theano.grad(op(x_)[0].sum(), x_))
-    x = CudaNdarray(numpy.ones((16, 2, 2, 2), dtype='float32'))
-    numpy.testing.assert_allclose(f(x), -0.875)
-
+    rng = numpy.random.RandomState([2013, 02, 10])
+    op = CrossMapNorm(16, 15/16., 1, True)
+    make_graph = lambda inp: op(gpu_from_host(inp))[0]
+    verify = lambda array: verify_grad(make_graph, [array])
+    inputs = [numpy.ones((16, 1, 1, 1), dtype='float32'),
+              rng.normal(size=(32, 5, 5, 10)).astype('float32')]
+    for arr in inputs:
+        yield verify, arr
 
 def test_optimization():
     op = CrossMapNorm(16, 15./16., 1, True)
