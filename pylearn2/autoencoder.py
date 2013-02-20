82f4e2d6cf43e4ec6510b09bc9daba7cee3113a3
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-02-20 18:38:14 -0500

    removed import *

--- pylearn2/autoencoder.py
+++ pylearn2/autoencoder.py
@@ -1,4 +1,4 @@
-from pylearn2.models.autoencoder import *
+from pylearn2.models.autoencoder import Autoencoder
 import warnings
 
 warnings.warn("pylearn2.autoencoder is deprecated, used pylearn2.models.autoencoder.")
