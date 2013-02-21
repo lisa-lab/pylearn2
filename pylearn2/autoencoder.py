b02cdf7e1eb85b7210dbc5b3a94302a56ee5670d
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-02-20 20:23:23 -0500

    moved autoencoder tests

--- pylearn2/autoencoder.py
+++ pylearn2/autoencoder.py
@@ -1,4 +1,5 @@
 from pylearn2.models.autoencoder import Autoencoder
+from pylearn2.models.autoencoder import HigherOrderContractiveAutoencoder
 import warnings
 
 warnings.warn("pylearn2.autoencoder is deprecated, used pylearn2.models.autoencoder.")
