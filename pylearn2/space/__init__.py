26967ade6955c653f4ac2063565c435786a8b624
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2013-05-02 14:05:33 -0400

    Call the accum function in monitor
    Conflicts:
    	pylearn2/monitor.py

--- pylearn2/space/__init__.py
+++ pylearn2/space/__init__.py
@@ -201,6 +201,8 @@ class VectorSpace(Space):
 
     @functools.wraps(Space.get_batch_size)
     def get_batch_size(self, data):
+        if isinstance(data, tuple):
+            data, = data
         return data.shape[0]
 
     @functools.wraps(Space.make_theano_batch)
@@ -381,6 +383,8 @@ class Conv2DSpace(Space):
 
     @functools.wraps(Space.get_batch_size)
     def get_batch_size(self, data):
+        if isinstance(data, tuple):
+            data, = data
         return data.shape[self.axes.index('b')]
 
 
