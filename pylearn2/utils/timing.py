7e772e3303256b5a83cd4a78d6aee65cf5554a18
Frederic <nouiz@nouiz.org>
2013-05-21 15:27:00 -0400

    py26 fix.

--- pylearn2/utils/timing.py
+++ pylearn2/utils/timing.py
@@ -45,11 +45,11 @@ def log_timing(logger, task, level=logging.INFO, final_msg=None):
     yield
     end = datetime.datetime.now()
     delta = end - start
-    if delta.total_seconds() < 60:
-        # delta.total_seconds() only defined in python 2.7
-        total_seconds = (delta.microseconds +
-                         (delta.seconds + delta.days * 24 * 3600) * 10 ** 6
-                         ) / 10 ** 6
+    # delta.total_seconds() only defined in python 2.7
+    total_seconds = (delta.microseconds +
+                     (delta.seconds + delta.days * 24 * 3600) * 10 ** 6
+                 ) / 10 ** 6
+    if total_seconds < 60:
         delta_str = '%f seconds' % total_seconds
     else:
         delta_str = str(delta)
