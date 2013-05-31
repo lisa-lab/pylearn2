2423958016d552a6f696b7124454c7b362c84a5f
Vincent Dumoulin <vi.dumoulin@gmail.com>
2013-05-31 16:14:31 -0400

    Make the script recuperate the correct method

--- pylearn2/scripts/dbm/dbm_metrics.py
+++ pylearn2/scripts/dbm/dbm_metrics.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 import argparse
+from pylearn2.utils import serial
+
+
+def compute_ais(model):
+    pass
 
 if __name__ == '__main__':
+    # Possible metrics
+    metrics = {'ais': compute_ais}
+
     # Argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument("metric", help="the desired metric",
-                        choices=["ais"])
+                        choices=metrics.keys())
     parser.add_argument("model_path", help="path to the pickled DBM model")
     args = parser.parse_args()
 
-    metric = args.metric
-    model_path = args.model_path
+    metric = metrics[args.metric]
+    model = serial.load(args.model_path)
+
+    metric(model)
