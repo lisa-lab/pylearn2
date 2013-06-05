32229c8a0ce3fb904b44d3d04386206b48f8f82c
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2013-06-05 15:08:18 -0400

    make_theano_batch for composite type
    Conflicts:
    	pylearn2/space/__init__.py
    Use a tuple of Theano variables, not a TupleVariable as return value,
    as is expected in the rest of the changes for the new data_specs
    interface.
    
    Conflicts:
    	pylearn2/space/__init__.py

--- pylearn2/space/__init__.py
+++ pylearn2/space/__init__.py
@@ -562,14 +562,21 @@ class CompositeSpace(Space):
 
     @functools.wraps(Space.make_theano_batch)
     def make_theano_batch(self, name=None, dtype=None, batch_size=None):
+        if name is None:
+            name = [None] * len(self.components)
+        elif not isinstance(name, (list, tuple)):
+            name = ['%s[%i]' % (name, i) for i in xrange(len(self.components))]
 
-        def name_generator(i):
-            if name is None:
-                return None
-            return name + '[' + str(i) +']'
+        if dtype is None:
+            dtype = [None] * len(self.components)
+        assert isinstance(name, (list, tuple))
+        assert isinstance(dtype, (list, tuple))
 
-        return TupleVariable(tuple(component.make_theano_batch(name = name_generator(i), dtype = dtype)
-                for i, component in enumerate(self.components)), name=name)
+        rval = [x.make_theano_batch(name=n, dtype=d, batch_size=batch_size)
+                for x,n,d in safe_zip(self.components,
+                                      name,
+                                      dtype)]
+        return rval
 
     @functools.wraps(Space.get_batch_size)
     def get_batch_size(self, data):
