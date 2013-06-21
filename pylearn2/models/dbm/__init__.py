5b472e13aee6ae859034bfa1f95de0759ce791e5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2013-06-21 11:06:21 -0400

    Simplify theano expression

--- pylearn2/models/dbm/__init__.py
+++ pylearn2/models/dbm/__init__.py
@@ -1615,9 +1615,7 @@ class BinaryVectorMaxPool(HiddenLayer):
         if self.copies != 1:
             raise NotImplementedError()
 
-        default_z = T.alloc(0., num_examples,
-                            self.detector_layer_dim).astype(self.b.dtype) + \
-                    self.b.dimshuffle('x', 0)
+        default_z = T.alloc(self.b, num_examples, self.detector_layer_dim)
 
         p_exp, h_exp, p_sample, h_sample = max_pool_channels(z=default_z,
                                                              pool_size=self.pool_size,
@@ -2041,9 +2039,7 @@ class Softmax(HiddenLayer):
         if self.copies != 1:
             raise NotImplementedError("need to make self.copies samples and average them together.")
 
-        default_z = T.alloc(0., num_examples,
-                            self.num_classes).astype(self.b.dtype) + \
-                    self.b.dimshuffle('x', 0)
+        default_z = T.alloc(self.b, num_examples, self.num_classes)
 
         h_exp = T.nnet.softmax(default_z)
 
