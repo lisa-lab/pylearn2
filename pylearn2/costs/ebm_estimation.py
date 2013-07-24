88737f83ca35937b07fde5c3904883d7fbda1c65
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2013-07-24 11:53:10 -0400

    fixed misnamed tensor

--- pylearn2/costs/ebm_estimation.py
+++ pylearn2/costs/ebm_estimation.py
@@ -29,10 +29,10 @@ sys.setrecursionlimit(40000)
 
 if 0:
     print 'WARNING: using SLOW rng'
-    RandomStreams = tensor.shared_randomstreams.RandomStreams
+    RandomStreams = T.shared_randomstreams.RandomStreams
 else:
     import theano.sandbox.rng_mrg
-    RandomStreams = theano.sandbox.rng_mrg.MRG_RandomStreams
+    RandomStreams = T.sandbox.rng_mrg.MRG_RandomStreams
 
 class NCE(Cost):
     """ Noise-Contrastive Estimation
