d1a9ad173fae816a04eb137c6fbac656f0508929
Vincent Dumoulin <vi.dumoulin@gmail.com>
2013-07-24 09:32:59 -0400

    Add Boltzmann machine costs draft

---
+++ pylearn2/costs/boltzmann_machine.py
@@ -0,0 +1,12 @@
+"""
+This module contains cost functions to use with Boltzmann machines
+(pylearn2.models.boltzmann_machine).
+"""
+__authors__ = ["Vincent Dumoulin"]
+__copyright__ = "Copyright 2013, Universite de Montreal"
+__credits__ = ["Vincent Dumoulin"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Vincent Dumoulin"
+
+class VariationalPCD(BaseCD):
+    pass
