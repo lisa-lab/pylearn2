c92493096941916345ca7f0a56c50ab42ce68340
Vincent Dumoulin <vi.dumoulin@gmail.com>
2013-07-25 08:10:28 -0400

    Add variational inference draft

--- pylearn2/models/boltzmann_machine.py
+++ pylearn2/models/boltzmann_machine.py
@@ -188,6 +188,9 @@ class BoltzmannMachine(Model):
             Energy vector of all samples given as input
         """
         layers = self.get_all_layers()
+        # Validate layer_to_state
+        assert all([layer in layer_to_state.keys() for layer in layers])
+        assert all([layer in layers for layer in layer_to_state.keys()])
 
         # Bias contribution
         energy = -theano.tensor.dot(layer_to_state[layers[0]],
@@ -199,13 +202,28 @@ class BoltzmannMachine(Model):
         # Inter-units contribution
         for i, layer1 in enumerate(layers[:-1]):
             for layer2 in layers[i + 1:]:
-                energy -= (theano.tensor.dot(layer_to_state[layer1],
-                                             self.weights[(layer1, layer2)])
-                           * layer_to_state[layer2]).sum(axis=1)
+                if self.connectivity[(layer1, layer2)] is not None:
+                    energy -= (theano.tensor.dot(layer_to_state[layer1],
+                                                 self.weights[(layer1, layer2)])
+                               * layer_to_state[layer2]).sum(axis=1)
 
         return energy
 
     def sample(self, layer_to_state, theano_rng, n_steps=5):
+        """
+        Generates samples from the model starting from the provided layer
+        states.
+
+        Parameters
+        ----------
+        layer_to_state : dict mapping `BoltzmannLayer` objects to tensor_like
+                         variables
+            Dictionary mapping from layers to their corresponding state
+        theano_rng : theano.sandbox.rng_mrg.MRG_RandomStreams
+            A random number generator from which samples are drawn
+        n_steps : int, optional
+            Number of Gibbs sampling steps
+        """
         # Validate n_steps
         assert isinstance(n_steps, py_integer_types)
         assert n_steps > 0
@@ -239,6 +257,76 @@ class BoltzmannMachine(Model):
                                                                 p=p,
                                                                 dtype=p.dtype,
                                                                 n=1)
+
+        return layer_to_updated_state
+
+    def variational_inference(self, layer_to_state, n_steps=5):
+        """
+        Samples from the inferred hidden unit probabilities given the state
+        of visible units.
+
+        Using a variational method, we can show that we can approximate
+        sampling hidden units given visible units by sequentially sampling
+        each :math:`h_i` according to
+
+        .. math::
+
+            p(h_i|\mathbf{v}, \mathbf{h}_{\\i}) \approx
+                sigmoid(b_i + \sum_{s_j \in N(h_i)} w_{ij} s_j)
+
+        where :math:`s_j \in N(h_i)` represents a unit in the neighborhood of
+        :math:`h_i`.
+
+        Parameters
+        ----------
+        layer_to_state : dict mapping `BoltzmannLayer` objects to tensor_like
+                         variables
+            Dictionary mapping from layers to their corresponding state. Only
+            hidden units will be updated.
+        theano_rng : theano.sandbox.rng_mrg.MRG_RandomStreams
+            A random number generator from which samples are drawn
+        n_steps : int, optional
+            Number of sampling steps
+        """
+        # Validate n_steps
+        assert isinstance(n_steps, py_integer_types)
+        assert n_steps > 0
+
+        # Implement the n_steps > 1 case by repeatedly calling the n_steps == 1
+        # case
+        if n_steps != 1:
+            for i in xrange(n_steps):
+                layer_to_state = self.sample(layer_to_state, n_steps=1)
+            return layer_to_state
+
+        layer_to_updated_state = OrderedDict()
+        for key, val in layer_to_state.items():
+            layer_to_updated[key] = val
+        for i, hidden_layer in enumerate(self.hidden_layers):
+            z = -self.biases[hidden_layer]
+
+            for visible_layer in self.visible_layers:
+                if self.connectivity[(visible_layer, hidden_layer)] is not None:
+                    z -= theano.tensor.dot(visible_layer,
+                                           self.weights[(visible_layer,
+                                                         hidden_layer)])
+
+            for other_layer in self.hidden_layers[:i]:
+                if self.connectivity[(other_layer, hidden_layer)] is not None:
+                    z -= theano.tensor.dot(other_layer,
+                                           self.weights[(other_layer,
+                                                         hidden_layer)])
+
+            for other_layer in hidden_layers[i + 1:]:
+                if self.connectivity[(hidden_layer, other_layer)] is not None:
+                    z -= theano.tensor.dot(self.weights[(hidden_layer,
+                                                         other_layer)],
+                                           other_layer)
+            p = theano.tensor.nnet.sigmoid(z)
+            layer_to_updated_state[hidden_layer] = theano_rng.binomial(
+                size=p.shape, p=p, dtype=p.dtype, n=1
+            )
+
 
         return layer_to_updated_state
 
