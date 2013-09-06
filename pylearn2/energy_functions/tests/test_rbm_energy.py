25d275c66302c254e13981548cd267943fb96301
Frederic <nouiz@nouiz.org>
2013-09-06 13:28:19 -0400

    pep8

--- pylearn2/energy_functions/tests/test_rbm_energy.py
+++ pylearn2/energy_functions/tests/test_rbm_energy.py
@@ -10,11 +10,11 @@ from pylearn2.linear.matrixmul import MatrixMul
 
 test_m = 2
 
-rng = N.random.RandomState([1,2,3])
+rng = N.random.RandomState([1, 2, 3])
 nv = 3
 nh = 4
 
-vW = rng.randn(nv,nh)
+vW = rng.randn(nv, nh)
 W = sharedX(vW)
 vbv = rng.randn(nv)
 bv = T.as_tensor_variable(vbv)
@@ -22,28 +22,30 @@ bv.tag.test_value = vbv
 vbh = rng.randn(nh)
 bh = T.as_tensor_variable(vbh)
 bh.tag.test_value = bh
-vsigma = rng.uniform(0.1,5)
+vsigma = rng.uniform(0.1, 5)
 sigma = T.as_tensor_variable(vsigma)
 sigma.tag.test_value = vsigma
 
-E = GRBM_Type_1(transformer = MatrixMul(W), bias_vis = bv, bias_hid = bh, sigma = sigma)
+E = GRBM_Type_1(transformer=MatrixMul(W), bias_vis=bv,
+                bias_hid=bh, sigma=sigma)
 
 V = T.matrix()
-V.tag.test_value = rng.rand(test_m,nv)
+V.tag.test_value = rng.rand(test_m, nv)
 H = T.matrix()
-H.tag.test_value = rng.rand(test_m,nh)
+H.tag.test_value = rng.rand(test_m, nh)
 
-E_func = function([V,H],E([V,H]))
-F_func = function([V],E.free_energy(V))
-log_P_H_given_V_func = function([H,V],E.log_P_H_given_V(H,V))
-score_func = function([V],E.score(V))
+E_func = function([V, H], E([V, H]))
+F_func = function([V], E.free_energy(V))
+log_P_H_given_V_func = function([H, V], E.log_P_H_given_V(H, V))
+score_func = function([V], E.score(V))
 
 F_of_V = E.free_energy(V)
 dummy = T.sum(F_of_V)
-negscore = T.grad(dummy,V)
+negscore = T.grad(dummy, V)
 score = - negscore
 
-generic_score_func = function([V],score)
+generic_score_func = function([V], score)
+
 
 class TestGRBM_Type_1:
     def test_mean_H_given_V(self):
@@ -55,61 +57,54 @@ class TestGRBM_Type_1:
         # E(v,h_2) - E(v,h_1) = log(a)
         # also log P(h_1 | v) - log P(h_2) = log(a)
 
-        rng = N.random.RandomState([1,2,3])
+        rng = N.random.RandomState([1, 2, 3])
 
         m = 5
 
-        Vv = as_floatX(N.zeros((m,nv))+rng.randn(nv))
+        Vv = as_floatX(N.zeros((m, nv)) + rng.randn(nv))
 
-        Hv = as_floatX(rng.randn(m,nh) > 0.)
+        Hv = as_floatX(rng.randn(m, nh) > 0.)
 
-        log_Pv = log_P_H_given_V_func(Hv,Vv)
+        log_Pv = log_P_H_given_V_func(Hv, Vv)
 
-        Ev = E_func(Vv,Hv)
+        Ev = E_func(Vv, Hv)
 
         for i in xrange(m):
-            for j in xrange(i+1,m):
+            for j in xrange(i + 1, m):
                 log_a = log_Pv[i] - log_Pv[j]
                 e = Ev[j] - Ev[i]
 
                 assert abs(e-log_a) < tol
 
-
-
     def test_free_energy(self):
 
-        rng = N.random.RandomState([1,2,3])
+        rng = N.random.RandomState([1, 2, 3])
 
         m = 2 ** nh
 
-        Vv = as_floatX(N.zeros((m,nv))+rng.randn(nv))
+        Vv = as_floatX(N.zeros((m, nv)) + rng.randn(nv))
 
+        F, = F_func(Vv[0:1, :])
 
-        F ,= F_func(Vv[0:1,:])
-
-        Hv = as_floatX(N.zeros((m,nh)))
+        Hv = as_floatX(N.zeros((m, nh)))
 
         for i in xrange(m):
             for j in xrange(nh):
-                Hv[i,j] = (i & (2 ** j)) / ( 2 ** j)
+                Hv[i, j] = (i & (2 ** j)) / (2 ** j)
 
-
-        Ev = E_func(Vv,Hv)
+        Ev = E_func(Vv, Hv)
 
         Fv = -N.log(N.exp(-Ev).sum())
         assert abs(F-Fv) < 1e-6
 
-
     def test_score(self):
-        rng = N.random.RandomState([1,2,3])
+        rng = N.random.RandomState([1, 2, 3])
 
         m = 10
 
-        Vv = as_floatX(rng.randn(m,nv))
+        Vv = as_floatX(rng.randn(m, nv))
 
         Sv = score_func(Vv)
         gSv = generic_score_func(Vv)
 
-
-        assert N.allclose(Sv,gSv)
-
+        assert N.allclose(Sv, gSv)
