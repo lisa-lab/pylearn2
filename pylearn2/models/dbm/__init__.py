a12d1799b9918f1e0568d6cc9e18249971663c08
Vincent Dumoulin <vi.dumoulin@gmail.com>
2013-10-15 16:15:19 -0400

    Factor out sampling procedure in DBM model

--- pylearn2/models/dbm/__init__.py
+++ pylearn2/models/dbm/__init__.py
@@ -79,11 +79,8 @@ class DBM(Model):
 
     """
 
-    def __init__(self,
-            batch_size,
-            visible_layer,
-            hidden_layers,
-            niter, inference_procedure=None):
+    def __init__(self, batch_size, visible_layer, hidden_layers, niter,
+                 sampling_procedure=None, inference_procedure=None):
         """
             batch_size:
                 The batch size the model should use.
@@ -109,6 +106,9 @@ class DBM(Model):
         if inference_procedure is None:
             self.setup_inference_procedure()
         self.inference_procedure.set_dbm(self)
+        if sampling_procedure is None:
+            self.setup_sampling_procedure()
+        self.sampling_procedure.set_dbm(self)
 
     def get_all_layers(self):
         return [self.visible_layer] + self.hidden_layers
@@ -217,6 +217,12 @@ class DBM(Model):
                 self.inference_procedure is None:
             self.inference_procedure = WeightDoubling()
             self.inference_procedure.set_dbm(self)
+
+    def setup_sampling_procedure(self):
+        if not hasattr(self, 'sampling_procedure') or \
+                self.sampling_procedure is None:
+            self.sampling_procedure = MCMC()
+            self.sampling_procedure.set_dbm(self)
 
     def get_output_space(self):
         return self.hidden_layers[-1].get_output_space()
@@ -401,169 +407,17 @@ class DBM(Model):
 
         return rval
 
-    def mcmc_steps(self, layer_to_state, theano_rng, layer_to_clamp = None,
-            num_steps = 1):
-        """
-            layer_to_state: a dictionary mapping the SuperDBM_Layer instances
-                            contained in self to theano variables representing
-                            batches of samples of them.
-            theano_rng: a MRG_RandomStreams object
-            layer_to_clamp: (optional) a dictionary mapping layers to bools
-                            if a layer is not in the dictionary, defaults to False
-                            True indicates that this layer should be clamped, so
-                            we are sampling from a conditional distribution rather
-                            than the joint
-            returns:
-                layer_to_updated_state
-                    dict mapping layers to theano variables representing the updated
-                    samples
-
-            The specific sampling schedule used is to sample all of the even-idexed
-            layers of model.hidden_layers, then the visible layer and all the odd-indexed
-            layers.
-        """
-
-        # Validate num_steps
-        assert isinstance(num_steps, py_integer_types)
-        assert num_steps > 0
-
-        # Implement the num_steps > 1 case by repeatedly calling the num_steps == 1 case
-        if num_steps != 1:
-            for i in xrange(num_steps):
-                layer_to_state = self.mcmc_steps(layer_to_state, theano_rng, layer_to_clamp,
-                        num_steps = 1)
-            return layer_to_state
-
-        # The rest of the function is the num_steps = 1 case
-
-        assert len(self.hidden_layers) > 0 # current code assumes this, though we could certainly
-                                           # relax this constraint
-
-        # Validate layer_to_clamp / make sure layer_to_clamp is a fully populated dictionary
-        if layer_to_clamp is None:
-            layer_to_clamp = OrderedDict()
-
-        for key in layer_to_clamp:
-            assert key is self.visible_layer or key in self.hidden_layers
-
-        for layer in [self.visible_layer] + self.hidden_layers:
-            if layer not in layer_to_clamp:
-                layer_to_clamp[layer] = False
-
-        #Assemble the return value
-        layer_to_updated = OrderedDict()
-
-        for i, this_layer in list(enumerate(self.hidden_layers))[::2]:
-            # Iteration i does the Gibbs step for hidden_layers[i]
-
-            # Get the sampled state of the layer below so we can condition
-            # on it in our Gibbs update
-            if i == 0:
-                layer_below = self.visible_layer
-            else:
-                layer_below = self.hidden_layers[i-1]
-            state_below = layer_to_state[layer_below]
-            state_below = layer_below.upward_state(state_below)
-
-            # Get the sampled state of the layer above so we can condition
-            # on it in our Gibbs step
-            if i + 1 < len(self.hidden_layers):
-                layer_above = self.hidden_layers[i + 1]
-                state_above = layer_to_state[layer_above]
-                state_above = layer_above.downward_state(state_above)
-            else:
-                state_above = None
-                layer_above = None
-
-            if layer_to_clamp[this_layer]:
-                this_state = layer_to_state[this_layer]
-                this_sample = this_state
-            else:
-                # Compute the Gibbs sampling update
-                # Sample the state of this layer conditioned
-                # on its Markov blanket (the layer above and
-                # layer below)
-                this_sample = this_layer.sample(
-                        state_below = state_below,
-                        state_above = state_above,
-                        layer_above = layer_above,
-                        theano_rng = theano_rng)
-
-            layer_to_updated[this_layer] = this_sample
-
-        #Sample the visible layer
-        vis_state = layer_to_state[self.visible_layer]
-        if layer_to_clamp[self.visible_layer]:
-            vis_sample = vis_state
-        else:
-            first_hid = self.hidden_layers[0]
-            state_above = layer_to_updated[first_hid]
-            state_above = first_hid.downward_state(state_above)
-
-            vis_sample = self.visible_layer.sample(
-                    state_above = state_above,
-                    layer_above = first_hid,
-                    theano_rng = theano_rng)
-        layer_to_updated[self.visible_layer] = vis_sample
-
-        # Sample the odd-numbered layers
-        for i, this_layer in list(enumerate(self.hidden_layers))[1::2]:
-
-            # Get the sampled state of the layer below so we can condition
-            # on it in our Gibbs update
-            layer_below = self.hidden_layers[i-1]
-
-            # We want to sample from each conditional distribution
-            # ***sequentially*** so we must use the updated version
-            # of the state for the layers whose updates we have
-            # calculcated already, in layer_to_updated.
-            # If we used the original value from
-            # layer_to_state
-            # then we would sample from each conditional
-            # ***simultaneously*** which does not implement MCMC
-            # sampling.
-            state_below = layer_to_updated[layer_below]
-
-            state_below = layer_below.upward_state(state_below)
-
-            # Get the sampled state of the layer above so we can condition
-            # on it in our Gibbs step
-            if i + 1 < len(self.hidden_layers):
-                layer_above = self.hidden_layers[i + 1]
-                state_above = layer_to_updated[layer_above]
-                state_above = layer_above.downward_state(state_above)
-            else:
-                state_above = None
-                layer_above = None
-
-            if layer_to_clamp[this_layer]:
-                this_state = layer_to_state[this_layer]
-                this_sample = this_state
-            else:
-                # Compute the Gibbs sampling update
-                # Sample the state of this layer conditioned
-                # on its Markov blanket (the layer above and
-                # layer below)
-                this_sample = this_layer.sample(
-                        state_below = state_below,
-                        state_above = state_above,
-                        layer_above = layer_above,
-                        theano_rng = theano_rng)
-
-            layer_to_updated[this_layer] = this_sample
-
-        # Check that all layers were updated
-        assert all([layer in layer_to_updated for layer in layer_to_state])
-        # Check that we didn't accidentally treat any other object as a layer
-        assert all([layer in layer_to_state for layer in layer_to_updated])
-        # Check that clamping worked
-        assert all([(layer_to_state[layer] is layer_to_updated[layer]) == \
-                layer_to_clamp[layer] for layer in layer_to_state])
-
-        return layer_to_updated
+    def mcmc_steps(self, layer_to_state, theano_rng, layer_to_clamp=None,
+                   num_steps=1):
+        warnings.warn("DBM.mcmc_steps is deprecated. You should instead call" +
+                      "DBM.sampling_procedure.sample, which defaults to what" +
+                      "DBM.mcmc_steps used to do.")
+        return self.sampling_procedure.sample(layer_to_state, theano_rng,
+                                              layer_to_clamp, num_steps)
 
     def get_sampling_updates(self, layer_to_state, theano_rng,
-            layer_to_clamp = None, num_steps = 1, return_layer_to_updated = False):
+                             layer_to_clamp=None, num_steps=1,
+                             return_layer_to_updated=False):
         """
             This method is for getting an updates dictionary for a theano function.
             It thus implies that the samples are represented as shared variables.
@@ -593,7 +447,8 @@ class DBM(Model):
             layers.
         """
 
-        updated = self.mcmc_steps(layer_to_state, theano_rng, layer_to_clamp, num_steps)
+        updated = self.sampling_procedure.sample(layer_to_state, theano_rng,
+                                                 layer_to_clamp, num_steps)
 
         rval = OrderedDict()
 
@@ -604,7 +459,8 @@ class DBM(Model):
             else:
                 rval[old] = new
 
-        # Validate layer_to_clamp / make sure layer_to_clamp is a fully populated dictionary
+        # Validate layer_to_clamp / make sure layer_to_clamp is a fully
+        # populated dictionary
         if layer_to_clamp is None:
             layer_to_clamp = OrderedDict()
 
@@ -2139,6 +1995,7 @@ class InferenceProcedure(object):
         necessary internal configurations to work with that batch size.
         """
 
+
 class WeightDoubling(InferenceProcedure):
 
     def mf(self, V, Y = None, return_history = False, niter = None, block_grad = None):
@@ -2279,6 +2136,188 @@ class WeightDoubling(InferenceProcedure):
             return history
         else:
             return H_hat
+
+
+class SamplingProcedure(object):
+    """
+    TODO WRITEME
+    """
+    def set_dbm(self, dbm):
+        self.dbm = dbm
+
+    def sample(self, layer_to_state, theano_rng, layer_to_clamp=None,
+               num_steps=1):
+        """
+        Samples from self.dbm using `layer_to_state` as starting values.
+
+        Parameters
+        ----------
+        layer_to_state : dict
+            Maps the DBM's Layer instances to theano variables representing \
+            batches of samples of them.
+        theano_rng : theano.sandbox.rng_mrg.MRG_RandomStreams
+        layer_to_clamp : dict, optional
+            Maps Layers to bools. If a layer is not in the dictionary, \
+            defaults to False. True indicates that this layer should be \
+            clamped, so we are sampling from a conditional distribution \
+            rather than the joint distribution.
+
+        Returns
+        -------
+        layer_to_updated_state : dict
+            Maps the DBM's Layer instances to theano variables representing \
+            batches of updated samples of them.
+        """
+        raise NotImplementedError(str(type(self))+" does not implement " +
+                                  "sample.")
+
+
+class MCMC(SamplingProcedure):
+    """
+    The specific sampling schedule used is to sample all of the even-idexed
+    layers of model.hidden_layers, then the visible layer and all the
+    odd-indexed layers.
+    """
+    def sample(self, layer_to_state, theano_rng, layer_to_clamp=None,
+               num_steps=1):
+        # Validate num_steps
+        assert isinstance(num_steps, py_integer_types)
+        assert num_steps > 0
+
+        # Implement the num_steps > 1 case by repeatedly calling the
+        # num_steps == 1 case
+        if num_steps != 1:
+            for i in xrange(num_steps):
+                layer_to_state = self.sample(layer_to_state, theano_rng,
+                                             layer_to_clamp, num_steps=1)
+            return layer_to_state
+
+        # The rest of the function is the num_steps = 1 case
+        # Current code assumes this, though we could certainly relax this
+        # constraint
+        assert len(self.dbm.hidden_layers) > 0
+
+        # Validate layer_to_clamp / make sure layer_to_clamp is a fully
+        # populated dictionary
+        if layer_to_clamp is None:
+            layer_to_clamp = OrderedDict()
+
+        for key in layer_to_clamp:
+            assert (key is self.dbm.visible_layer or
+                    key in self.dbm.hidden_layers)
+
+        for layer in [self.dbm.visible_layer] + self.dbm.hidden_layers:
+            if layer not in layer_to_clamp:
+                layer_to_clamp[layer] = False
+
+        # Assemble the return value
+        layer_to_updated = OrderedDict()
+
+        for i, this_layer in list(enumerate(self.dbm.hidden_layers))[::2]:
+            # Iteration i does the Gibbs step for hidden_layers[i]
+
+            # Get the sampled state of the layer below so we can condition
+            # on it in our Gibbs update
+            if i == 0:
+                layer_below = self.dbm.visible_layer
+            else:
+                layer_below = self.dbm.hidden_layers[i-1]
+            state_below = layer_to_state[layer_below]
+            state_below = layer_below.upward_state(state_below)
+
+            # Get the sampled state of the layer above so we can condition
+            # on it in our Gibbs step
+            if i + 1 < len(self.dbm.hidden_layers):
+                layer_above = self.dbm.hidden_layers[i + 1]
+                state_above = layer_to_state[layer_above]
+                state_above = layer_above.downward_state(state_above)
+            else:
+                state_above = None
+                layer_above = None
+
+            if layer_to_clamp[this_layer]:
+                this_state = layer_to_state[this_layer]
+                this_sample = this_state
+            else:
+                # Compute the Gibbs sampling update
+                # Sample the state of this layer conditioned
+                # on its Markov blanket (the layer above and
+                # layer below)
+                this_sample = this_layer.sample(state_below=state_below,
+                                                state_above=state_above,
+                                                layer_above=layer_above,
+                                                theano_rng=theano_rng)
+
+            layer_to_updated[this_layer] = this_sample
+
+        #Sample the visible layer
+        vis_state = layer_to_state[self.dbm.visible_layer]
+        if layer_to_clamp[self.dbm.visible_layer]:
+            vis_sample = vis_state
+        else:
+            first_hid = self.dbm.hidden_layers[0]
+            state_above = layer_to_updated[first_hid]
+            state_above = first_hid.downward_state(state_above)
+
+            vis_sample = self.dbm.visible_layer.sample(state_above=state_above,
+                                                       layer_above=first_hid,
+                                                       theano_rng=theano_rng)
+        layer_to_updated[self.dbm.visible_layer] = vis_sample
+
+        # Sample the odd-numbered layers
+        for i, this_layer in list(enumerate(self.dbm.hidden_layers))[1::2]:
+
+            # Get the sampled state of the layer below so we can condition
+            # on it in our Gibbs update
+            layer_below = self.dbm.hidden_layers[i-1]
+
+            # We want to sample from each conditional distribution
+            # ***sequentially*** so we must use the updated version
+            # of the state for the layers whose updates we have
+            # calculcated already, in layer_to_updated.
+            # If we used the original value from
+            # layer_to_state
+            # then we would sample from each conditional
+            # ***simultaneously*** which does not implement MCMC
+            # sampling.
+            state_below = layer_to_updated[layer_below]
+
+            state_below = layer_below.upward_state(state_below)
+
+            # Get the sampled state of the layer above so we can condition
+            # on it in our Gibbs step
+            if i + 1 < len(self.dbm.hidden_layers):
+                layer_above = self.dbm.hidden_layers[i + 1]
+                state_above = layer_to_updated[layer_above]
+                state_above = layer_above.downward_state(state_above)
+            else:
+                state_above = None
+                layer_above = None
+
+            if layer_to_clamp[this_layer]:
+                this_state = layer_to_state[this_layer]
+                this_sample = this_state
+            else:
+                # Compute the Gibbs sampling update
+                # Sample the state of this layer conditioned
+                # on its Markov blanket (the layer above and
+                # layer below)
+                this_sample = this_layer.sample(state_below=state_below,
+                                                state_above=state_above,
+                                                layer_above=layer_above,
+                                                theano_rng=theano_rng)
+
+            layer_to_updated[this_layer] = this_sample
+
+        # Check that all layers were updated
+        assert all([layer in layer_to_updated for layer in layer_to_state])
+        # Check that we didn't accidentally treat any other object as a layer
+        assert all([layer in layer_to_state for layer in layer_to_updated])
+        # Check that clamping worked
+        assert all([(layer_to_state[layer] is layer_to_updated[layer]) ==
+                    layer_to_clamp[layer] for layer in layer_to_state])
+
+        return layer_to_updated
 
 
 class DBMSampler(Block):
