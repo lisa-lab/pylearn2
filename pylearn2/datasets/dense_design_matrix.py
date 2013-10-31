50afa50be207df66d4cd44af2745589496816ce8
Frederic <nouiz@nouiz.org>
2013-10-31 09:49:44 -0400

    Fix crash in the creation of pytables datasets

--- pylearn2/datasets/dense_design_matrix.py
+++ pylearn2/datasets/dense_design_matrix.py
@@ -771,7 +771,7 @@ class DenseDesignMatrixPyTables(DenseDesignMatrix):
                                             axes = axes,
                                             rng = rng)
         ensure_tables()
-        filters = tables.Filters(complib='blosc', complevel=5)
+        self.filters = tables.Filters(complib='blosc', complevel=5)
 
     def set_design_matrix(self, X, start = 0):
         assert len(X.shape) == 2
@@ -895,11 +895,10 @@ class DenseDesignMatrixPyTables(DenseDesignMatrix):
         h5file = tables.openFile(path, mode = "w", title = "SVHN Dataset")
         gcolumns = h5file.createGroup(h5file.root, "Data", "Data")
         atom = tables.Float32Atom() if config.floatX == 'float32' else tables.Float64Atom()
-        filters = DenseDesignMatrixPyTables.filters
         h5file.createCArray(gcolumns, 'X', atom = atom, shape = x_shape,
-                                title = "Data values", filters = filters)
+                                title = "Data values", filters = self.filters)
         h5file.createCArray(gcolumns, 'y', atom = atom, shape = y_shape,
-                                title = "Data targets", filters = filters)
+                                title = "Data targets", filters = self.filters)
         return h5file, gcolumns
 
     @staticmethod
@@ -942,11 +941,10 @@ class DenseDesignMatrixPyTables(DenseDesignMatrix):
         stop = gcolumns.X.nrows if stop is None else stop
 
         atom = tables.Float32Atom() if config.floatX == 'float32' else tables.Float64Atom()
-        filters = DenseDesignMatrixPyTables.filters
         x = h5file.createCArray(gcolumns, 'X', atom = atom, shape = ((stop - start, data.X.shape[1])),
-                            title = "Data values", filters = filters)
+                            title = "Data values", filters = self.filters)
         y = h5file.createCArray(gcolumns, 'y', atom = atom, shape = ((stop - start, 10)),
-                            title = "Data targets", filters = filters)
+                            title = "Data targets", filters = self.filters)
         x[:] = data.X[start:stop]
         y[:] = data.y[start:stop]
 
