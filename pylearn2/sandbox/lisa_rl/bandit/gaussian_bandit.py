aef157e2145ed4f74f08b67b76b9acf982f9f1b3
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-11-12 20:03:24 -0500

    added control of std

--- pylearn2/sandbox/lisa_rl/bandit/gaussian_bandit.py
+++ pylearn2/sandbox/lisa_rl/bandit/gaussian_bandit.py
@@ -16,14 +16,15 @@ class GaussianBandit(Environment):
     An n-armed bandit whose rewards are drawn from a different Gaussian
     distribution for each arm.
     The mean and standard deviation of the reward for each arm is drawn
-    at initialization time from N(0, 1). (For the standard deviation we
-    use the absolute value of the Gaussian sample)
+    at initialization time from N(0, <corresponding std arg>).
+    (For the standard deviation we use the absolute value of the Gaussian
+    sample)
     """
 
-    def __init__(self, num_arms):
+    def __init__(self, num_arms, mean_std = 1.0, std_std = 1.0):
         self.rng = np.random.RandomState([2013, 11, 12])
-        self.means = sharedX(self.rng.randn(num_arms))
-        self.stds = sharedX(np.abs(self.rng.randn(num_arms)))
+        self.means = sharedX(self.rng.randn(num_arms) * mean_std)
+        self.stds = sharedX(np.abs(self.rng.randn(num_arms) * std_std))
         self.theano_rng = MRG_RandomStreams(self.rng.randint(2 ** 16))
 
     def get_action_func(self):
