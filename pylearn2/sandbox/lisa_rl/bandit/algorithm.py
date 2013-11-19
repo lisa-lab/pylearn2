b8e15e0a8545554636b85333017aa45410dd02c9
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-11-19 15:13:23 -0500

    contextual bandit learning algorithm

--- pylearn2/sandbox/lisa_rl/bandit/algorithm.py
+++ pylearn2/sandbox/lisa_rl/bandit/algorithm.py
@@ -2,16 +2,12 @@ __author__ = "Ian Goodfellow"
 
 class Algorithm(object):
     """
-    Note: this seems like it might not be a good piece of the RL software
-    ecosystem. For RL, the choice of algorithm seems more tied up with the
-    choice of what kind of model you make, so it might make more sense to
-    have the algorithm just be part of the Agent class. This implementation
-    basically does nothing but query the agent for the learning algo.
+    Bare-bones algorithm for driving a bandit learning problem.
     """
 
     def setup(self, agent, environment):
-        self.action_func = environment.get_action_func()
         self.decide_func = agent.get_decide_func()
+        self.action_func = environment.get_action_func()
         self.learn_func = agent.get_learn_func()
         agent.reward_record = []
         self.agent = agent
@@ -22,3 +18,29 @@ class Algorithm(object):
         self.agent.reward_record.append(r)
         self.learn_func(a, r)
 
+class ContextualBanditAlgorithm(Algorithm):
+    """
+    Bare-bones algorithm for driving a contextual bandit learning problem.
+    """
+
+    def setup(self, agent, environment):
+        self.context_func = environment.get_context_func()
+        self.decide_func = agent.get_decide_func()
+        self.action_func = environment.get_action_func()
+        self.learn_func = agent.get_learn_func()
+        agent.reward_record = []
+        self.agent = agent
+
+    def train(self):
+        # TODO: this could all be much more efficient on GPU if s, a, and r
+        # were stored in shared variables that all the different functions
+        # are aware of.
+        s = self.context_func()
+        a = self.decide_func(s)
+        r = self.action_func(a)
+        # TODO: figure out how to remove waste here, where forward prop is
+        # done a second time in order to do backprop
+        self.learn_func(s, a, r)
+        if r.ndim > 0:
+            r = r.mean()
+        self.agent.reward_record.append(r)
