a2e6c60286d38baaee2ebaeec6df28491901ac89
Frederic <nouiz@nouiz.org>
2013-11-25 21:22:44 -0500

    Merge the 2 setup.py files to make full installation in only 1 step.

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension("pylearn2/utils/_window_flip",
+                  ["pylearn2/utils/_window_flip.pyx"],
+                  include_dirs=[numpy.get_include()]),
+        Extension("pylearn2/utils/_video",
+                  ["pylearn2/utils/_video.pyx"],
+                  include_dirs=[numpy.get_include()])
+    ]
+)
+
 from setuptools import setup, find_packages
 
 setup(
