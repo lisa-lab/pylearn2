8212ad6fc5c00cd5f36d4bb4d465352bbeb09b09
zygmuntz <zajac.zygmunt@gmail.com>
2013-11-26 23:11:34 +0100

    formatting

--- pylearn2/datasets/tests/test_csv_dataset.py
+++ pylearn2/datasets/tests/test_csv_dataset.py
@@ -4,8 +4,8 @@ import numpy as np
 
 def test_loading():
     d = CSVDataset(path = 'test.csv', expect_headers = False)
-    assert(np.array_equal( d.X, np.array([[1., 2., 3.], [4., 5., 6.]])))
-    assert(np.array_equal( d.y, np.array([0., 1.])))
+    assert(np.array_equal(d.X, np.array([[1., 2., 3.], [4., 5., 6.]])))
+    assert(np.array_equal(d.y, np.array([0., 1.])))
     
     
 
