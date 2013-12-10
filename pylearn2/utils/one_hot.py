c71ecb4d79478ec762c42bdf469da9e54088aa4a
Vincent Dumoulin <vi.dumoulin@gmail.com>
2013-12-09 23:25:30 -0800

    Format docstrings in utils/one_hot.py

--- pylearn2/utils/one_hot.py
+++ pylearn2/utils/one_hot.py
@@ -13,6 +13,11 @@ import numpy as np
 
 
 def _validate_labels(labels, ndim):
+    """
+    .. todo::
+
+        WRITEME
+    """
     labels = np.asarray(labels)
     if labels.dtype.kind not in ('u', 'i'):
         raise ValueError("labels must have int or uint dtype")
@@ -28,6 +33,11 @@ def _validate_labels(labels, ndim):
 
 
 def _validate_max_label(labels, max_label):
+    """
+    .. todo::
+
+        WRITEME
+    """
     max_actual_label = labels.max()
     if max_label is None:
         max_label = max_actual_label
@@ -38,6 +48,11 @@ def _validate_max_label(labels, max_label):
 
 
 def _validate_dtype(labels, dtype, out):
+    """
+    .. todo::
+
+        WRITEME
+    """
     if dtype is not None and out is not None:
         raise ValueError("supplied both output array and dtype; "
                          "only supply one or the other")
@@ -49,6 +64,11 @@ def _validate_dtype(labels, dtype, out):
 
 
 def _validate_out(nlabels, max_label, dtype, out):
+    """
+    .. todo::
+
+        WRITEME
+    """
     if out is None:
         out = np.zeros((nlabels, max_label + 1), dtype=dtype)
     else:
@@ -63,6 +83,11 @@ def _validate_out(nlabels, max_label, dtype, out):
 
 
 def _one_hot_fill(labels, out):
+    """
+    .. todo::
+
+        WRITEME
+    """
     out.flat[np.arange(0, out.size, out.shape[1]) + labels] = 1
 
 
@@ -77,16 +102,16 @@ def one_hot(labels, max_label=None, dtype=None, out=None):
         The integer labels to use to construct the one hot matrix.
 
     max_label : int, optional
-        The maximum valid label. Must be greater than or equal to
+        The maximum valid label. Must be greater than or equal to \
         `numpy.amax(labels)`.
 
     dtype : str or dtype object, optional
-        The dtype you wish the returned array to have. Defaults
+        The dtype you wish the returned array to have. Defaults \
         to `labels.dtype` if not provided.
 
     out : ndarray, optional
-        An array to use in lieu of allocating one. Must be the
-        right shape, i.e. same first dimension as `labels` and
+        An array to use in lieu of allocating one. Must be the \
+        right shape, i.e. same first dimension as `labels` and \
         second dimension greater than or equal to `labels.max() + 1`.
 
     Returns
@@ -114,23 +139,23 @@ def k_hot(labels, max_label=None, dtype=None, out=None):
         The integer labels to use to construct the k-hot matrix.
 
     max_label : int, optional
-        The maximum valid label. Must be greater than or equal to
+        The maximum valid label. Must be greater than or equal to \
         `numpy.amax(labels)`.
 
     dtype : str or dtype object, optional
-        The dtype you wish the returned array to have. Defaults
+        The dtype you wish the returned array to have. Defaults \
         to `labels.dtype` if not provided.
 
     out : ndarray, optional
-        An array to use in lieu of allocating one. Must be the
-        right shape, i.e. same first dimension as `labels` and
+        An array to use in lieu of allocating one. Must be the \
+        right shape, i.e. same first dimension as `labels` and \
         second dimension greater than or equal to `labels.max() + 1`.
 
     Returns
     -------
     out : ndarray, (nlabels, max_label + 1)
-        The resulting k-hot matrix. If a given integer appeared
-        in the same row more than once then there may be less
+        The resulting k-hot matrix. If a given integer appeared \
+        in the same row more than once then there may be less \
         than k elements active in the corresponding row of `out`.
     """
     labels = _validate_labels(labels, 2)
@@ -157,19 +182,19 @@ def compressed_one_hot(labels, dtype=None, out=None, simplify_binary=True):
         The integer labels to use to construct the one hot matrix.
 
     dtype : str or dtype object, optional
-        The dtype you wish the returned array to have. Defaults
+        The dtype you wish the returned array to have. Defaults \
         to `labels.dtype` if not provided.
 
     out : ndarray, optional
-        An array to use in lieu of allocating one. Must be the
-        right shape, i.e. same first dimension as `labels` and
-        second dimension greater than or equal to the number of
+        An array to use in lieu of allocating one. Must be the \
+        right shape, i.e. same first dimension as `labels` and \
+        second dimension greater than or equal to the number of \
         unique values in `labels`.
 
     simplify_binary : bool, optional
-        If `True`, if there are only two distinct labels, return
-        an `(nlabels, 1)` matrix with 0 lesser the lesser integer
-        label and 1 denoting the greater, instead of a redundant
+        If `True`, if there are only two distinct labels, return \
+        an `(nlabels, 1)` matrix with 0 lesser the lesser integer \
+        label and 1 denoting the greater, instead of a redundant \
         `(nlabels, 2)` matrix.
 
     Returns
@@ -178,7 +203,7 @@ def compressed_one_hot(labels, dtype=None, out=None, simplify_binary=True):
         The resulting one-hot matrix.
 
     uniq : ndarray, 1-dimensional
-        The array of unique values in `labels` in the order
+        The array of unique values in `labels` in the order \
         in which the corresponding columns appear in `out`.
     """
     labels = _validate_labels(labels, ndim=1)
