c92069a4c07037db0754b4a03bc7cfd1b198e4c3
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-12-28 11:46:39 -0500

    added docstring

--- pylearn2/scripts/num_parameters.py
+++ pylearn2/scripts/num_parameters.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 __author__ = "Ian Goodfellow"
+"""
+Usage: python num_parameters.py <model_file>.pkl
+
+Prints the number of parameters in a saved model (total number of scalar
+elements in all the arrays parameterizing the model).
+"""
 
 import sys
 
