0d5ebafcc439367b927dfa491b16fe4a33c8f379
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2014-02-11 15:38:18 -0500

    changed dataset

--- pylearn2/scripts/tests/test_autoencoder.py
+++ pylearn2/scripts/tests/test_autoencoder.py
@@ -2,15 +2,10 @@
 Tests for the pylearn2 autoencoder module.
 """
 import numpy as np
+from pylearn2.scripts.tests.yaml_testing import yaml_file_execution
 
 def test_hcae_yaml():
-    from pylearn2.scripts.tests.yaml_testing import test_yaml_file
-    X = np.random.normal(size=(1000, 300))
-    np.save('garbage.npy', X)
-    test_yaml_file("pylearn2/scripts/autoencoder_example/hcae.yaml")
+    yaml_file_execution("pylearn2/scripts/autoencoder_example/hcae.yaml")
 
 def test_dae_yaml():
-    from pylearn2.scripts.tests.yaml_testing import test_yaml_file
-    X = np.random.normal(size=(1000, 300))
-    np.save('garbage.npy', X)
-    test_yaml_file("pylearn2/scripts/autoencoder_example/dae.yaml")
+    yaml_file_execution("pylearn2/scripts/autoencoder_example/dae.yaml")
--- pylearn2/scripts/tests/yaml_testing.py
+++ pylearn2/scripts/tests/yaml_testing.py
@@ -1,7 +1,7 @@
 from pylearn2.config import yaml_parse
 from pylearn2.termination_criteria import EpochCounter
 
-def test_yaml_file(file_path):
+def yaml_file_execution(file_path):
     train = yaml_parse.load_path(file_path)
     train.algorithm.termination_criterion = EpochCounter(max_epochs=3)
     train.main_loop()
