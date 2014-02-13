c66b75c8d0fd4448e0719441f03c4af5be4568d7
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2014-02-13 13:15:52 -0500

    modified path and added try except for dataset

--- pylearn2/scripts/tests/yaml_testing.py
+++ pylearn2/scripts/tests/yaml_testing.py
@@ -1,7 +1,13 @@
+from nose.plugins.skip import SkipTest
+
 from pylearn2.config import yaml_parse
 from pylearn2.termination_criteria import EpochCounter
+from pylearn2.datasets.exc import NoDataPathError
 
 def yaml_file_execution(file_path):
-    train = yaml_parse.load_path(file_path)
-    train.algorithm.termination_criterion = EpochCounter(max_epochs=3)
-    train.main_loop()
+    try:
+        train = yaml_parse.load_path(file_path)
+        train.algorithm.termination_criterion = EpochCounter(max_epochs=2)
+        train.main_loop()
+    except NoDataPathError:
+        raise SkipTest("PYLEARN2_DATA_PATH environment variable not defined")
