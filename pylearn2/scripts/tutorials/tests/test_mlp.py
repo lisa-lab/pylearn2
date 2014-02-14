d15cc5f7d8cf30ccb90e68e8fd978dfe5cfb1536
sebastien-j <sebastien.jean@mail.mcgill.ca>
2014-02-14 12:12:06 -0500

    Used autopep8

--- pylearn2/scripts/tutorials/tests/test_mlp.py
+++ pylearn2/scripts/tutorials/tests/test_mlp.py
@@ -11,23 +11,24 @@ from pylearn2.datasets.exc import NoDataPathError
 from pylearn2.config import yaml_parse
 import pylearn2
 
+
 def test_multilayer_perceptron():
     try:
-        with open(os.path.join(pylearn2.__path__[0],'scripts','tutorials','mlp_tutorial_part_2.yaml'),'r') as f:
+        with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'mlp_tutorial_part_2.yaml'), 'r') as f:
             train = f.read()
         f.close()
         print train
         train = yaml_parse.load(train)
         train.algorithm.termination_criterion = EpochCounter(max_epochs=2)
         train.main_loop()
-        with open(os.path.join(pylearn2.__path__[0],'scripts','tutorials','mlp_tutorial_part_3.yaml'),'r') as f:
+        with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'mlp_tutorial_part_3.yaml'), 'r') as f:
             train_2 = f.read()
         f.close()
         print train_2
         train_2 = yaml_parse.load(train_2)
         train_2.algorithm.termination_criterion = EpochCounter(max_epochs=2)
         train_2.main_loop()
-        with open(os.path.join(pylearn2.__path__[0],'scripts','tutorials','mlp_tutorial_part_4.yaml'),'r') as f:
+        with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'mlp_tutorial_part_4.yaml'), 'r') as f:
             train_3 = f.read()
         f.close()
         print train_3
