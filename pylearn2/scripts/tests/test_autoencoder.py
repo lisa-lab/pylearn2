9d2947bf33772697bffd1c86c581392c50ce5c98
sebastien-j <sebastien.jean@mail.mcgill.ca>
2014-02-14 12:45:25 -0500

    Fixed string concatenation

--- pylearn2/scripts/tests/test_autoencoder.py
+++ pylearn2/scripts/tests/test_autoencoder.py
@@ -7,7 +7,7 @@ from pylearn2.scripts.tests.yaml_testing import yaml_file_execution
 import pylearn2
 
 def test_hcae_yaml():
-    yaml_file_execution(os.path.join(pylearn2.__path__[0],"scripts/autoencoder_example/hcae.yaml"))
+    yaml_file_execution(os.path.join(pylearn2.__path__[0],'scripts','autoencoder_example','hcae.yaml'))
 
 def test_dae_yaml():
-    yaml_file_execution(os.path.join(pylearn2.__path__[0],"scripts/autoencoder_example/dae.yaml"))
+    yaml_file_execution(os.path.join(pylearn2.__path__[0],'scripts','autoencoder_example','dae.yaml'))
