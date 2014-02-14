3e38afc8e13a57b9118864f8d3d3c243a1b4b5f9
sebastien-j <sebastien.jean@mail.mcgill.ca>
2014-02-14 16:10:41 -0500

    Updated mlp test unit

--- pylearn2/scripts/tutorials/tests/test_mlp.py
+++ pylearn2/scripts/tutorials/tests/test_mlp.py
@@ -1,6 +1,5 @@
 """
 Test for multilayer_perceptron.ipynb
-The number of epochs has been limited.
 """
 
 import os
@@ -10,30 +9,54 @@ from pylearn2.testing.skip import skip_if_no_data
 from pylearn2.config import yaml_parse
 import pylearn2
 
-
-def test_multilayer_perceptron():
-    skip_if_no_data()
+def test_part_2():
     with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'mlp_tutorial_part_2.yaml'), 'r') as f:
         train = f.read()
     f.close()
+    hyper_params = {'train_stop' : 50,
+                    'valid_stop' : 50050,
+                    'dim_h0' : 5,
+                    'max_epochs' : 1}
+    train = train % (hyper_params)
     print train
     train = yaml_parse.load(train)
-    train.algorithm.termination_criterion = EpochCounter(max_epochs=2)
     train.main_loop()
+
+def test_part_3():
     with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'mlp_tutorial_part_3.yaml'), 'r') as f:
         train_2 = f.read()
     f.close()
+    hyper_params = {'train_stop' : 50,
+                    'valid_stop' : 50050,
+                    'dim_h0' : 5,
+                    'dim_h1' : 10,
+                    'sparse_init_h1' : 2,
+                    'max_epochs' : 1}
+    train_2 = train_2 % (hyper_params)
     print train_2
     train_2 = yaml_parse.load(train_2)
-    train_2.algorithm.termination_criterion = EpochCounter(max_epochs=2)
     train_2.main_loop()
+
+def test_part_4():
     with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials', 'mlp_tutorial_part_4.yaml'), 'r') as f:
         train_3 = f.read()
     f.close()
+    hyper_params = {'train_stop' : 50,
+                    'valid_stop' : 50050,
+                    'dim_h0' : 5,
+                    'dim_h1' : 10,
+                    'sparse_init_h1' : 2,
+                    'max_epochs' : 1}
+    train_3 = train_3 % (hyper_params)
     print train_3
     train_3 = yaml_parse.load(train_3)
-    train_3.algorithm.termination_criterion = EpochCounter(max_epochs=2)
     train_3.main_loop()
+
+def test_multilayer_perceptron():
+    skip_if_no_data()
+    test_part_2()
+    test_part_3()
+    test_part_4()
 
 if __name__ == '__main__':
     test_multilayer_perceptron()
