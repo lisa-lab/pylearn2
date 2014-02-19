8e4ccea1f0111d325110ff7427ea8528c4a481ed
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2014-02-19 00:07:57 -0500

    changed numpy and theano RNG object creations with utils.rng interface

--- pylearn2/training_algorithms/sgd.py
+++ pylearn2/training_algorithms/sgd.py
@@ -32,6 +32,7 @@ from pylearn2.utils import serial
 from pylearn2.utils import sharedX
 from pylearn2.utils.data_specs import DataSpecsMapping
 from pylearn2.utils.timing import log_timing
+from pylearn2.utils.rng import make_np_rng
 
 
 log = logging.getLogger(__name__)
@@ -185,7 +186,7 @@ class SGD(TrainingAlgorithm):
             train_iteration_mode = 'shuffled_sequential'
         self.train_iteration_mode = train_iteration_mode
         self.first = True
-        self.rng = np.random.RandomState(seed)
+        self.rng = make_np_rng(seed, which_method=["randn","randint"])
         self.theano_function_mode = theano_function_mode
         self.monitoring_costs = monitoring_costs
 
