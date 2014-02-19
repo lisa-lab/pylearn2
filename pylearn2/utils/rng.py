b7a2dfa91925c428c9d53e5bcb5f87229118839f
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2014-02-19 00:30:19 -0500

    added docstring to utils/rng functions

--- pylearn2/utils/rng.py
+++ pylearn2/utils/rng.py
@@ -33,6 +33,30 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 def make_rng(rng_or_seed=None, default_seed=None,
              which_method=None, constructor=None):
+    """
+    Returns a RNG object, using the first of these cases that produces a valid
+    result and doesn't use an argument with the value of None:
+
+    1) rng_or_seed itself
+    2) constructor(rng_or_seed)
+    3) constructor(default_seed)
+    4) constructor(42)
+
+    Parameters
+    ----------
+    rng_or_seed : RNG object or integer or list of integers
+        A random number generator or a valid seed.
+    default_seed : integer or list of integers
+        Seed used if rng_or_seed is None
+    which_method : string or list of strings
+        One or more methods that must be defined by the RNG object.
+        If one or more specified methods are not defined by it, a
+        new one will be constructed with the given constructor.
+    constructor : function or class
+        Must return a RNG object.
+        constructor is called with rng_or_seed, default_seed or 42
+        as argument.
+    """
 
     if not isinstance(which_method, list):
         which_method = [which_method]
@@ -50,23 +74,51 @@ def make_rng(rng_or_seed=None, default_seed=None,
     return rng
 
 
-def make_np_rng(rng_or_seed=None, default_seed=None, which_method=None,
-                constructor=numpy.random.RandomState):
-    return make_rng(rng_or_seed, default_seed, which_method, constructor)
+def make_np_rng(rng_or_seed=None, default_seed=None, which_method=None):
+    """
+    Returns a numpy RandomState, using the first of these cases that produces a
+    valid result and doesn't use an argument with the value of None:
+
+    1) rng_or_seed itself
+    2) RandomState(rng_or_seed)
+    3) RandomState(default_seed)
+    4) RandomState(42)
+
+    Parameters
+    ----------
+    rng_or_seed : RNG object or integer or list of integers
+        A random number generator or a valid seed.
+    default_seed : integer or list of integers
+        Seed used if rng_or_seed is None
+    which_method : string or list of strings
+        One or more methods that must be defined by the RNG object.
+        If one or more specified methods are not defined by it, a
+        new one will be constructed from RandomState.
+    """
+    return make_rng(rng_or_seed, default_seed, which_method,
+                    numpy.random.RandomState)
 
 
-def make_theano_rng(rng_or_seed=None, default_seed=None, which_method=None,
-                    constructor=RandomStreams):
-    return make_rng(rng_or_seed, default_seed, which_method, constructor)
+def make_theano_rng(rng_or_seed=None, default_seed=None, which_method=None):
+    """
+    Returns a numpy RandomState, using the first of these cases that produces a
+    valid result and doesn't use an argument with the value of None:
 
+    1) rng_or_seed itself
+    2) RandomState(rng_or_seed)
+    3) RandomState(default_seed)
+    4) RandomState(42)
 
-#def rng_ints(rng_or_seed=None, default_seed=None):
-#    return make_rng(rng_or_seed, default_seed, which_method='random_integers')
-#
-#
-#def rng_normal(rng_or_seed=None, default_seed=None):
-#    return make_rng(rng_or_seed, default_seed, which_method='normal')
-#
-#
-#def rng_uniform(rng_or_seed=None, default_seed=None):
-#    return make_rng(rng_or_seed, default_seed, which_method='uniform')
+    Parameters
+    ----------
+    rng_or_seed : RNG object or integer or list of integers
+        A random number generator or a valid seed.
+    default_seed : integer or list of integers
+        Seed used if rng_or_seed is None
+    which_method : string or list of strings
+        One or more methods that must be defined by the RNG object.
+        If one or more specified methods are not defined by it, a
+        new one will be constructed from RandomState.
+    """
+
+    return make_rng(rng_or_seed, default_seed, which_method, RandomStreams)
