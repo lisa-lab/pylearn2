52b5b4e61d734b442b30e4618d200762e02bd665
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-02-20 10:27:48 -0500

    doc

--- pylearn2/devtools/nan_guard.py
+++ pylearn2/devtools/nan_guard.py
@@ -1,7 +1,5 @@
 """
-.. todo::
-
-    WRITEME
+Functionality for detecting NaNs in a Theano graph.
 """
 __authors__ = "Ian Goodfellow"
 __copyright__ = "Copyright 2010-2012, Universite de Montreal"
@@ -17,17 +15,36 @@ from pylearn2.models.dbm import flatten
 
 class NanGuardMode(Mode):
     """
-    .. todo::
-
-        WRITEME
+    A Theano compilation Mode that makes the compiled function automatically
+    detect NaNs and Infs and detect an error if they occur.
     """
     def __init__(self, nan_is_error, inf_is_error):
         """
-        .. todo::
-
-            WRITEME
+        Parameters
+        ----------
+        nan_is_error : bool
+            If True, raise an error anytime a NaN is encountered
+        inf_is_error: bool
+            If True, raise an error anytime an Inf is encountered
         """
         def do_check_on(var, nd, f, is_input):
+            """
+            Checks `var` for NaNs / Infs. If detected, raises an exception
+            and / or prints information about `nd`, `f`, and `is_input` to
+            help the user determine the cause of the invalid values.
+
+            Parameters
+            ----------
+            var : numpy.ndarray
+                The value to be checked.
+            nd : theano.gof.Apply
+                The Apply node being executed
+            f : callable
+                The thunk for the apply node
+            is_input : bool
+                If True, `var` is an input to `nd`.
+                If False, it is an output.
+            """
             error = False
             if nan_is_error:
                 if np.any(np.isnan(var)):
@@ -57,6 +74,17 @@ class NanGuardMode(Mode):
                 assert False
 
         def nan_check(i, node, fn):
+            """
+            Runs `fn` while checking its inputs and outputs for NaNs / Infs
+
+            Parameters
+            ----------
+            i : currently ignored (TODO: determine why it is here or remove)
+            node : theano.gof.Apply
+                The Apply node currently being executed
+            fn : callable
+                The thunk to execute for this Apply node
+            """
             inputs = fn.inputs
             # TODO: figure out why individual inputs are themselves lists sometimes
             for x in flatten(inputs):
