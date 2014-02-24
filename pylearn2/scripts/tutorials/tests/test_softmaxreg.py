34a22d290ae4f79b6f9d82c63ad442ec9c741d1d
Amjad Almahairi <amjadmahayri@gmail.com>
2014-02-24 12:48:49 -0500

    changing the way we access the tutorials folder

--- pylearn2/scripts/tutorials/tests/test_softmaxreg.py
+++ pylearn2/scripts/tutorials/tests/test_softmaxreg.py
@@ -4,34 +4,32 @@ Test for softmax_regression.ipynb
 
 import os
 
-import pylearn2
 from pylearn2.testing.skip import skip_if_no_data
 from pylearn2.config import yaml_parse
 
 
 def test():
     skip_if_no_data()
-    with open(os.path.join(os.getcwd(), '..',
-                           'sr_dataset.yaml'), 'r') as f:
+
+    dirname = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+
+    with open(os.path.join(dirname, 'sr_dataset.yaml'), 'r') as f:
         dataset = f.read()
 
     hyper_params = {'train_stop': 50}
     dataset = dataset % (hyper_params)
 
-    with open(os.path.join(os.getcwd(), '..',
-                           'sr_model.yaml'), 'r') as f:
+    with open(os.path.join(dirname, 'sr_model.yaml'), 'r') as f:
         model = f.read()
 
-    with open(os.path.join(os.getcwd(), '..',
-                           'sr_algorithm.yaml'), 'r') as f:
+    with open(os.path.join(dirname, 'sr_algorithm.yaml'), 'r') as f:
         algorithm = f.read()
 
     hyper_params = {'batch_size': 10,
                     'valid_stop': 50050}
     algorithm = algorithm % (hyper_params)
 
-    with open(os.path.join(os.getcwd(), '..',
-                           'sr_train.yaml'), 'r') as f:
+    with open(os.path.join(dirname, 'sr_train.yaml'), 'r') as f:
         train = f.read()
 
     train = train % locals()
