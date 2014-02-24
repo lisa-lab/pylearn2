e4266116631fda2bc8d05ec2a563df6e8c4860f4
Amjad Almahairi <amjadmahayri@gmail.com>
2014-02-24 10:56:26 -0500

    added the updated ipython notebook for softmax_regression, and modified files according to Mehdi's suggestions

--- pylearn2/scripts/tutorials/tests/test_softmaxreg.py
+++ pylearn2/scripts/tutorials/tests/test_softmaxreg.py
@@ -11,30 +11,29 @@ from pylearn2.config import yaml_parse
 
 def test():
     skip_if_no_data()
-    with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials',
-              'sr_dataset.yaml'), 'r') as f:
+    with open(os.path.join(os.getcwd(), '..',
+                           'sr_dataset.yaml'), 'r') as f:
         dataset = f.read()
-    f.close()
+
     hyper_params = {'train_stop': 50}
     dataset = dataset % (hyper_params)
 
-    with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials',
+    with open(os.path.join(os.getcwd(), '..',
                            'sr_model.yaml'), 'r') as f:
         model = f.read()
-    f.close()
 
-    with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials',
+    with open(os.path.join(os.getcwd(), '..',
                            'sr_algorithm.yaml'), 'r') as f:
         algorithm = f.read()
-    f.close()
+
     hyper_params = {'batch_size': 10,
                     'valid_stop': 50050}
     algorithm = algorithm % (hyper_params)
 
-    with open(os.path.join(pylearn2.__path__[0], 'scripts', 'tutorials',
+    with open(os.path.join(os.getcwd(), '..',
                            'sr_train.yaml'), 'r') as f:
         train = f.read()
-    f.close()
+
     train = train % locals()
 
     train = yaml_parse.load(train)
