680cc69cededd8068de141c2b7c4b2c509c203d3
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-02-28 12:03:22 -0500

    make scripts not run on import (was messing up sphinx)

--- pylearn2/scripts/pkl_inspector.py
+++ pylearn2/scripts/pkl_inspector.py
@@ -28,176 +28,177 @@ pkl_inspector.py foo.pkl .my_field [my_key] 3
 will load an object obj from foo.pkl and analyze obj.my_field["my_key"][3]
 """
 
-if len(sys.argv) == 1:
-    usage()
-    sys.exit(-1)
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit(-1)
 
-hp = pickle.HIGHEST_PROTOCOL
+    hp = pickle.HIGHEST_PROTOCOL
 
-filepath = sys.argv[1]
+    filepath = sys.argv[1]
 
-orig_obj = serial.load(filepath)
+    orig_obj = serial.load(filepath)
 
-cycle_check = {}
+    cycle_check = {}
 
-obj_name = 'root_obj'
-cycle_check[id(orig_obj)] = obj_name
-
-for field in sys.argv[2:]:
-    if field.startswith('['):
-        assert field.endswith(']')
-        obj_name += '[' + field[1:-1] + ']'
-        orig_obj = orig_obj[field[1:-1]]
-    elif field.startswith('.'):
-        obj_name += '.' + field
-        orig_obj = getattr(orig_obj,field[1:])
-    else:
-        obj_name + '[' + field + ']'
-        orig_obj = orig_obj[eval(field)]
-    if id(orig_obj) in cycle_check:
-        print "You're going in circles, "+obj_name+" is the same as "+cycle_check[id(orig_obj)]
-        quit()
+    obj_name = 'root_obj'
     cycle_check[id(orig_obj)] = obj_name
 
+    for field in sys.argv[2:]:
+        if field.startswith('['):
+            assert field.endswith(']')
+            obj_name += '[' + field[1:-1] + ']'
+            orig_obj = orig_obj[field[1:-1]]
+        elif field.startswith('.'):
+            obj_name += '.' + field
+            orig_obj = getattr(orig_obj,field[1:])
+        else:
+            obj_name + '[' + field + ']'
+            orig_obj = orig_obj[eval(field)]
+        if id(orig_obj) in cycle_check:
+            print "You're going in circles, "+obj_name+" is the same as "+cycle_check[id(orig_obj)]
+            quit()
+        cycle_check[id(orig_obj)] = obj_name
 
-print 'type of object: '+str(type(orig_obj))
-print 'object: '+str(orig_obj)
-print 'object, longer description:\n'+min_informative_str(orig_obj, indent_level = 1)
 
-t1 = time.time()
-s = cPickle.dumps(orig_obj, hp)
-t2 = time.time()
-prev_ts = t2 - t1
+    print 'type of object: '+str(type(orig_obj))
+    print 'object: '+str(orig_obj)
+    print 'object, longer description:\n'+min_informative_str(orig_obj, indent_level = 1)
 
-prev_bytes = len(s)
-print 'orig_obj bytes: \t\t\t\t'+str(prev_bytes)
-t1 = time.time()
-x = cPickle.loads(s)
-t2 = time.time()
-prev_t = t2 - t1
-print 'orig load time: '+str(prev_t)
-print 'orig save time: '+str(prev_ts)
+    t1 = time.time()
+    s = cPickle.dumps(orig_obj, hp)
+    t2 = time.time()
+    prev_ts = t2 - t1
 
-idx = 0
+    prev_bytes = len(s)
+    print 'orig_obj bytes: \t\t\t\t'+str(prev_bytes)
+    t1 = time.time()
+    x = cPickle.loads(s)
+    t2 = time.time()
+    prev_t = t2 - t1
+    print 'orig load time: '+str(prev_t)
+    print 'orig save time: '+str(prev_ts)
 
-print 'field\t\t\tdelta bytes\t\t\tdelta load time\t\t\tdelta save time'
+    idx = 0
 
-if not (isinstance(orig_obj, dict) or isinstance(orig_obj, list) ):
-    while len(dir(orig_obj)) > idx:
-        stop = False
+    print 'field\t\t\tdelta bytes\t\t\tdelta load time\t\t\tdelta save time'
 
-        while True:
-            fields = dir(orig_obj)
-            if idx >= len(fields):
-                stop = True
+    if not (isinstance(orig_obj, dict) or isinstance(orig_obj, list) ):
+        while len(dir(orig_obj)) > idx:
+            stop = False
+
+            while True:
+                fields = dir(orig_obj)
+                if idx >= len(fields):
+                    stop = True
+                    break
+                field = fields[idx]
+
+                if field in ['names_to_del','__dict__']:
+                    print 'not deleting '+field
+                    idx += 1
+                    continue
+
+                success = True
+                try:
+                    delattr(orig_obj,field)
+
+                except:
+                    #TODO: add a config flag to allow printing the following messages
+                    #print "got error trying to delete "+field
+                    idx += 1
+                    success = False
+                if success and field in dir(orig_obj):
+                    print field + ' reappears after being deleted'
+                    idx += 1
+                if success:
+                    break
+
+            if stop:
                 break
-            field = fields[idx]
 
-            if field in ['names_to_del','__dict__']:
-                print 'not deleting '+field
-                idx += 1
-                continue
+            #print hasattr(orig_obj, 'names_to_del')
+            t1 = time.time()
+            s = cPickle.dumps(orig_obj, hp)
+            t2 = time.time()
+            new_ts = t2 - t1
+            diff_ts = prev_ts - new_ts
+            prev_ts = new_ts
+            new_bytes = len(s)
+            diff_bytes = prev_bytes - new_bytes
+            prev_bytes = new_bytes
+            t1 = time.time()
+            x = cPickle.loads(s)
+            t2 = time.time()
+            new_t = t2 - t1
+            diff_t = prev_t - new_t
+            prev_t = new_t
+            print field+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)+'\t\t\t'+str(diff_ts)
 
-            success = True
-            try:
-                delattr(orig_obj,field)
+    if isinstance(orig_obj, dict):
+        print 'orig_obj is a dictionary'
 
-            except:
-                #TODO: add a config flag to allow printing the following messages
-                #print "got error trying to delete "+field
-                idx += 1
-                success = False
-            if success and field in dir(orig_obj):
-                print field + ' reappears after being deleted'
-                idx += 1
-            if success:
-                break
+        keys = [ key for key in orig_obj.keys() ]
 
-        if stop:
-            break
+        for key in keys:
+            orig_obj[key] = None
 
-        #print hasattr(orig_obj, 'names_to_del')
-        t1 = time.time()
-        s = cPickle.dumps(orig_obj, hp)
-        t2 = time.time()
-        new_ts = t2 - t1
-        diff_ts = prev_ts - new_ts
-        prev_ts = new_ts
-        new_bytes = len(s)
-        diff_bytes = prev_bytes - new_bytes
-        prev_bytes = new_bytes
-        t1 = time.time()
-        x = cPickle.loads(s)
-        t2 = time.time()
-        new_t = t2 - t1
-        diff_t = prev_t - new_t
-        prev_t = new_t
-        print field+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)+'\t\t\t'+str(diff_ts)
+            s = cPickle.dumps(orig_obj, hp)
+            new_bytes = len(s)
+            t1 = time.time()
+            x = cPickle.loads(s)
+            t2 = time.time()
+            new_t = t2 - t1
+            diff_t = prev_t - new_t
+            prev_t = new_t
+            diff_bytes = prev_bytes - new_bytes
+            prev_bytes = new_bytes
+            print 'val for '+str(key)+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)
 
-if isinstance(orig_obj, dict):
-    print 'orig_obj is a dictionary'
+        for key in keys:
+            del orig_obj[key]
 
-    keys = [ key for key in orig_obj.keys() ]
-
-    for key in keys:
-        orig_obj[key] = None
-
-        s = cPickle.dumps(orig_obj, hp)
-        new_bytes = len(s)
-        t1 = time.time()
-        x = cPickle.loads(s)
-        t2 = time.time()
-        new_t = t2 - t1
-        diff_t = prev_t - new_t
-        prev_t = new_t
-        diff_bytes = prev_bytes - new_bytes
-        prev_bytes = new_bytes
-        print 'val for '+str(key)+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)
-
-    for key in keys:
-        del orig_obj[key]
-
-        s = cPickle.dumps(orig_obj, hp)
-        new_bytes = len(s)
-        t1 = time.time()
-        x = cPickle.loads(s)
-        t2 = time.time()
-        new_t = t2 - t1
-        diff_t = prev_t - new_t
-        prev_t = new_t
-        diff_bytes = prev_bytes - new_bytes
-        prev_bytes = new_bytes
-        print str(key)+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)
+            s = cPickle.dumps(orig_obj, hp)
+            new_bytes = len(s)
+            t1 = time.time()
+            x = cPickle.loads(s)
+            t2 = time.time()
+            new_t = t2 - t1
+            diff_t = prev_t - new_t
+            prev_t = new_t
+            diff_bytes = prev_bytes - new_bytes
+            prev_bytes = new_bytes
+            print str(key)+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)
 
 
-if isinstance(orig_obj, list):
-    print 'orig_obj is a list'
+    if isinstance(orig_obj, list):
+        print 'orig_obj is a list'
 
-    i = 0
-    while len(orig_obj) > 0:
-        stringrep = str(orig_obj[0])
-        if len(stringrep) > 15:
-            stringrep = stringrep[0:12] + "..."
-        del orig_obj[0]
+        i = 0
+        while len(orig_obj) > 0:
+            stringrep = str(orig_obj[0])
+            if len(stringrep) > 15:
+                stringrep = stringrep[0:12] + "..."
+            del orig_obj[0]
 
-        t1 = time.time()
-        s = cPickle.dumps(orig_obj, hp)
-        t2 = time.time()
-        new_ts = t2 - t1
-        diff_ts = prev_ts - new_ts
-        prev_ts = new_ts
+            t1 = time.time()
+            s = cPickle.dumps(orig_obj, hp)
+            t2 = time.time()
+            new_ts = t2 - t1
+            diff_ts = prev_ts - new_ts
+            prev_ts = new_ts
 
-        new_bytes = len(s)
-        diff_bytes = prev_bytes - new_bytes
-        prev_bytes = new_bytes
+            new_bytes = len(s)
+            diff_bytes = prev_bytes - new_bytes
+            prev_bytes = new_bytes
 
-        t1 = time.time()
-        x = cPickle.loads(s)
-        t2 = time.time()
-        new_t = t2 - t1
-        diff_t = prev_t - new_t
-        prev_t = new_t
-        print stringrep+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)+'\t\t\t'+str(diff_ts)
+            t1 = time.time()
+            x = cPickle.loads(s)
+            t2 = time.time()
+            new_t = t2 - t1
+            diff_t = prev_t - new_t
+            prev_t = new_t
+            print stringrep+': \t\t\t\t'+str(diff_bytes)+'\t\t\t'+str(diff_t)+'\t\t\t'+str(diff_ts)
 
-        i+= 1
+            i+= 1
 
--- pylearn2/scripts/summarize_model.py
+++ pylearn2/scripts/summarize_model.py
@@ -8,35 +8,37 @@ __email__ = "goodfeli@iro"
 
 import numpy as np
 import sys
-path = sys.argv[1]
-from pylearn2.utils import serial
-model = serial.load(path)
-for param in model.get_params():
-    name = param.name
-    if name is None:
-        name = '<anon>'
-    v = param.get_value()
-    print name+': '+str((v.min(),v.mean(),v.max()))+' '+str(v.shape)
-    if np.sign(v.min()) != np.sign(v.max()):
-        v = np.abs(v)
-        print 'abs('+name+'): '+str((v.min(),v.mean(),v.max()))
-    if v.ndim == 2:
-        row_norms = np.sqrt(np.square(v).sum(axis=1))
-        print name +" row norms: ",(row_norms.min(), row_norms.mean(), row_norms.max())
-        col_norms = np.sqrt(np.square(v).sum(axis=0))
-        print name +" col norms: ",(col_norms.min(), col_norms.mean(), col_norms.max())
 
-if hasattr(model,'monitor'):
-    print 'trained on',model.monitor.get_examples_seen(),' examples'
-    print 'which corresponds to',model.monitor.get_batches_seen(),'batches'
-    try:
-        print model.monitor.get_epochs_seen(),'epochs'
-    except:
-        pass
-    if hasattr(model.monitor, 'training_succeeded'):
-        if model.monitor.training_succeeded:
-            print 'Training succeeded'
+if __name__ == "__main__":
+    path = sys.argv[1]
+    from pylearn2.utils import serial
+    model = serial.load(path)
+    for param in model.get_params():
+        name = param.name
+        if name is None:
+            name = '<anon>'
+        v = param.get_value()
+        print name+': '+str((v.min(),v.mean(),v.max()))+' '+str(v.shape)
+        if np.sign(v.min()) != np.sign(v.max()):
+            v = np.abs(v)
+            print 'abs('+name+'): '+str((v.min(),v.mean(),v.max()))
+        if v.ndim == 2:
+            row_norms = np.sqrt(np.square(v).sum(axis=1))
+            print name +" row norms: ",(row_norms.min(), row_norms.mean(), row_norms.max())
+            col_norms = np.sqrt(np.square(v).sum(axis=0))
+            print name +" col norms: ",(col_norms.min(), col_norms.mean(), col_norms.max())
+
+    if hasattr(model,'monitor'):
+        print 'trained on',model.monitor.get_examples_seen(),' examples'
+        print 'which corresponds to',model.monitor.get_batches_seen(),'batches'
+        try:
+            print model.monitor.get_epochs_seen(),'epochs'
+        except:
+            pass
+        if hasattr(model.monitor, 'training_succeeded'):
+            if model.monitor.training_succeeded:
+                print 'Training succeeded'
+            else:
+                print 'Training was not yet completed at the time of this save.'
         else:
-            print 'Training was not yet completed at the time of this save.'
-    else:
-        print 'This pickle file is damaged, or was made before the Monitor tracked whether training completed.'
+            print 'This pickle file is damaged, or was made before the Monitor tracked whether training completed.'
