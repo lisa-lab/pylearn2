22d2e4812ec879866785e26b35378877d9c46d42
Frederic <nouiz@nouiz.org>
2014-02-28 13:08:45 -0500

    Skip a test in debug mode that is too slow.

--- pylearn2/scripts/tests/test_autoencoder.py
+++ pylearn2/scripts/tests/test_autoencoder.py
@@ -1,13 +1,23 @@
 """
 Tests for the pylearn2 autoencoder module.
 """
-import numpy as np
 import os
+
+from nose.plugins.skip import SkipTest
+import numpy as np
+import theano
+
 from pylearn2.scripts.tests.yaml_testing import yaml_file_execution
 import pylearn2
 
+
 def test_hcae_yaml():
-    yaml_file_execution(os.path.join(pylearn2.__path__[0],"scripts/autoencoder_example/hcae.yaml"))
+    if theano.config.mode in ["DEBUG_MODE", "DebugMode"]:
+        raise SkipTest("Too slow in debug mode")
+    yaml_file_execution(os.path.join(pylearn2.__path__[0],
+                                     "scripts/autoencoder_example/hcae.yaml"))
+
 
 def test_dae_yaml():
-    yaml_file_execution(os.path.join(pylearn2.__path__[0],"scripts/autoencoder_example/dae.yaml"))
+    yaml_file_execution(os.path.join(pylearn2.__path__[0],
+                                     "scripts/autoencoder_example/dae.yaml"))
