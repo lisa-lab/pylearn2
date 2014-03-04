eb61eed9a74c63b433ea460e72a20309cb252997
Matthew Koichi Grimes <mkg@alum.mit.edu>
2014-03-04 17:58:29 +0000

    allow SimplyTypedSpace._clean_dtype_arg() to take a numpy.dtype as its dtype argument

--- pylearn2/space/__init__.py
+++ pylearn2/space/__init__.py
@@ -650,12 +650,15 @@ class Space(object):
         either be 'float32' or 'float64'.
         """
 
-        if str(dtype) == 'floatX':
+        if isinstance(dtype, np.dtype):
+            dtype = str(dtype)
+
+        if dtype == 'floatX':
             return theano.config.floatX
 
         if dtype is None or \
-           str(dtype) in tuple(x.dtype for x in theano.scalar.all_types):
-            return str(dtype)
+           dtype in tuple(x.dtype for x in theano.scalar.all_types):
+            return dtype
 
         raise TypeError('Unrecognized value "%s" (type %s) for dtype arg' %
                         (dtype, type(dtype)))
