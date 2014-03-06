a1acf02d8acb200f5f4e449307bf5a773f9908fa
Arnaud Bergeron <abergeron@gmail.com>
2014-03-06 13:29:50 -0500

    Fix a couple of simple mistakes that would prevent the tests from running correctly.

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 
-
-if [ "xTEST_DOC" == "xYES"]; then
+if [ "x$TEST_DOC" == "xYES" ]; then
     python ./doc/scripts/docgen.py --test
 else
-    (cd pylearn2/scripts/tutorial/grbm_smd && wget http://www.iro.umontreal.ca/~lisa/datasets/cifar10_preprocessed_train.pkl)
+    # We can't build the test dataset as the original is not
+    # present. We can't download the original as it is too big to
+    # download each time. If present run: python make_dataset.py
+    (cd pylearn2/scripts/tutorials/grbm_smd && wget http://www.iro.umontreal.ca/~lisa/datasets/cifar10_preprocessed_train.pkl)
     nosetests --help
     THEANO_FLAGS=$FLAGS,warn.ignore_bug_before=all,on_opt_error=raise,on_shape_error=raise TRAVIS=1 theano-nose -v
 fi
-    
