7458791b500e5ce0c5bcd6591d9cc2f8c68c1195
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-03-06 15:10:09 -0500

    added base.py

---
+++ pylearn2/base.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn("pylearn2.base is deprecated. It is now called pylearn2.blocks,"
+        "since it was not actually the base of the library. pylearn2.base may"
+        " be removed from the library on or after 2014-09-06.")
+
+from pylearn2.blocks import Block
+from pylearn2.blocks import StackedBlocks
