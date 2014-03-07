796aae53645d34a587d0a32eebd6a6dc55049b88
Mehdi Mirza <memirzamo@gmail.com>
2014-03-07 17:55:28 -0500

    added cleaunp saved files

--- pylearn2/scripts/tutorials/multilayer_perceptron/tests/test_mlp.py
+++ pylearn2/scripts/tutorials/multilayer_perceptron/tests/test_mlp.py
@@ -12,11 +12,12 @@ from pylearn2.config import yaml_parse
 
 YAML_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                               '..'))
+SAVE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 def cleaunup(file_name):
     try:
-        os.remove(os.path.join(YAML_FILE_PATH, file_name))
+        os.remove(os.path.join(SAVE_PATH, file_name))
     except:
         pass
 
@@ -30,7 +31,8 @@ def test_part_2():
     hyper_params = {'train_stop': 50,
                     'valid_stop': 50050,
                     'dim_h0': 5,
-                    'max_epochs': 1}
+                    'max_epochs': 1,
+                    'save_path': SAVE_PATH}
     train = train % (hyper_params)
     train = yaml_parse.load(train)
     train.main_loop()
@@ -48,7 +50,8 @@ def test_part_3():
                     'dim_h0': 5,
                     'dim_h1': 10,
                     'sparse_init_h1': 2,
-                    'max_epochs': 1}
+                    'max_epochs': 1,
+                    'save_path': SAVE_PATH}
     train_2 = train_2 % (hyper_params)
     train_2 = yaml_parse.load(train_2)
     train_2.main_loop()
@@ -66,7 +69,8 @@ def test_part_4():
                     'dim_h0': 5,
                     'dim_h1': 10,
                     'sparse_init_h1': 2,
-                    'max_epochs': 1}
+                    'max_epochs': 1,
+                    'save_path': SAVE_PATH}
     train_3 = train_3 % (hyper_params)
     train_3 = yaml_parse.load(train_3)
     train_3.main_loop()
