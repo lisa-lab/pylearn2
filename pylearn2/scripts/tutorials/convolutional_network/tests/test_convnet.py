7ef11475f6948d6352b370dbfc456993816df165
Mehdi Mirza <memirzamo@gmail.com>
2014-03-07 17:26:23 -0500

    conv net test updated

--- pylearn2/scripts/tutorials/convolutional_network/tests/test_convnet.py
+++ pylearn2/scripts/tutorials/convolutional_network/tests/test_convnet.py
@@ -1,120 +1,45 @@
 """
 Test that a smaller version of convolutional_network.ipynb works.
 
-If this file has to be edited, convolutional_network.ipynb has to be updated
-in the same way.
-
 The differences (needed for speed) are:
     * output_channels: 4 instead of 64
     * train.stop: 500 instead of 50000
     * valid.stop: 50100 instead of 60000
     * test.start: 0 instead of non-specified
     * test.stop: 100 instead of non-specified
-    * termination_criterion.prop_decrease: 0.50 instead of 0.0
+    * termination_criterion.max_epochs: 1 instead of 500
 
 This should make the test run in about one minute.
 """
-from nose.plugins.skip import SkipTest
 
-from pylearn2.datasets.exc import NoDataPathError
+import os
+from pylearn2.testing import skip
 from pylearn2.testing import no_debug_mode
-
-
-@no_debug_mode
-def train_convolutional_network():
-    train = """
-!obj:pylearn2.train.Train {
-    dataset: &train !obj:pylearn2.datasets.mnist.MNIST {
-        which_set: 'train',
-        one_hot: 1,
-        start: 0,
-        stop: 500
-    },
-    model: !obj:pylearn2.models.mlp.MLP {
-        batch_size: 100,
-        input_space: !obj:pylearn2.space.Conv2DSpace {
-            shape: [28, 28],
-            num_channels: 1
-        },
-        layers: [ !obj:pylearn2.models.mlp.ConvRectifiedLinear {
-                     layer_name: 'h2',
-                     output_channels: 4,
-                     irange: .05,
-                     kernel_shape: [5, 5],
-                     pool_shape: [4, 4],
-                     pool_stride: [2, 2],
-                     max_kernel_norm: 1.9365
-                 }, !obj:pylearn2.models.mlp.ConvRectifiedLinear {
-                     layer_name: 'h3',
-                     output_channels: 4,
-                     irange: .05,
-                     kernel_shape: [5, 5],
-                     pool_shape: [4, 4],
-                     pool_stride: [2, 2],
-                     max_kernel_norm: 1.9365
-                 }, !obj:pylearn2.models.mlp.Softmax {
-                     max_col_norm: 1.9365,
-                     layer_name: 'y',
-                     n_classes: 10,
-                     istdev: .05
-                 }
-                ],
-    },
-    algorithm: !obj:pylearn2.training_algorithms.sgd.SGD {
-        batch_size: 100,
-        learning_rate: .01,
-        init_momentum: .5,
-        monitoring_dataset:
-            {
-                'valid' : !obj:pylearn2.datasets.mnist.MNIST {
-                              which_set: 'train',
-                              one_hot: 1,
-                              start: 50000,
-                              stop:  50100
-                          },
-                'test'  : !obj:pylearn2.datasets.mnist.MNIST {
-                              which_set: 'test',
-                              one_hot: 1,
-                              stop: 100
-                          }
-            },
-        cost: !obj:pylearn2.costs.cost.SumOfCosts { costs: [
-            !obj:pylearn2.costs.cost.MethodCost {
-                method: 'cost_from_X'
-            }, !obj:pylearn2.costs.mlp.WeightDecay {
-                coeffs: [ .00005, .00005, .00005 ]
-            }
-            ]
-        },
-        termination_criterion: !obj:pylearn2.termination_criteria.MonitorBased {
-            channel_name: "valid_y_misclass",
-            prop_decrease: 0.50,
-            N: 10
-        }
-    },
-    extensions:
-        [ !obj:pylearn2.train_extensions.best_params.MonitorBasedSaveBest {
-             channel_name: 'valid_y_misclass',
-             save_path: "convolutional_network_best.pkl"
-        }, !obj:pylearn2.training_algorithms.sgd.MomentumAdjustor {
-            start: 1,
-            saturate: 10,
-            final_momentum: .99
-        }
-    ]
-}
-
-"""
-    from pylearn2.config import yaml_parse
-    train = yaml_parse.load(train)
-    train.main_loop()
+from pylearn2.config import yaml_parse
 
 
 def test_convolutional_network():
+    yaml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                  '..'))
+    save_path = os.path.dirname(os.path.realpath(__file__))
+
+    yaml = open("{0}/conv.yaml".format(yaml_file_path), 'r').read()
+    hyper_params = {'train_stop': 50,
+                    'valid_stop': 50050,
+                    'test_stop': 50,
+                    'batch_size': 50,
+                    'output_channels_h2': 4,
+                    'output_channels_h3': 4,
+                    'max_epochs': 1,
+                    'save_path': save_path}
+    yaml = yaml % (hyper_params)
+    train = yaml_parse.load(yaml)
+    train.main_loop()
+
     try:
-        train_convolutional_network()
-    except NoDataPathError:
-        raise SkipTest("PYLEARN2_DATA_PATH environment variable not defined")
+        os.remove("{}/convolutional_network_best.pkl".format(save_path))
+    except:
+        pass
 
 
 if __name__ == '__main__':
