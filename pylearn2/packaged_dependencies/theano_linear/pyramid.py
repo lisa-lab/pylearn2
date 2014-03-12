30cea365a6ca126320df8c34f7a997c4222130d0
Thomas Rohée <roheethomas@gmail.com>
2014-03-12 11:23:46 -0400

    replaced prints by logging in packaged_dependencies

--- pylearn2/packaged_dependencies/theano_linear/pyramid.py
+++ pylearn2/packaged_dependencies/theano_linear/pyramid.py
@@ -6,13 +6,17 @@
 import sys
 import numpy
 import theano
+import logging
 from theano.gof import Variable, Op, utils, Type, Constant,  Value, Apply
 from theano.tensor import as_tensor_variable
+
+logger = logging.getLogger(__name__)
 
 try:
     import cv
 except ImportError:
-    print >> sys.stderr, "WARNING: cv not available"
+    logger.warning("cv not available")
+
 
 def cv_available():
     """
@@ -109,7 +113,7 @@ class GaussianPyramid(Op):
             if z0.shape[0] <=2 or z0.shape[1] <= 2:
                 raise ValueError('Cannot downsample an image smaller than 3x3',
                         z0.shape)
-            print z0.shape, z0.dtype, z0.strides
+            logger.info('%s %s %s', z0.shape, z0.dtype, z0.strides)
             out0 = cv.pyrDown(z0)
             assert out0.dtype == x.dtype
             if out0.ndim ==3:
