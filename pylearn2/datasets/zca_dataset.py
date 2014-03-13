bc9c526d78b16e82051244b79c6113a1efedc93a
sebastien-j <sebastien.jean@mail.mcgill.ca>
2014-03-13 16:52:30 -0400

    Modify docstrings
    From norb to zca_dataset

--- pylearn2/datasets/zca_dataset.py
+++ pylearn2/datasets/zca_dataset.py
@@ -32,6 +32,11 @@ class ZCA_Dataset(DenseDesignMatrix):
     """
 
     def get_test_set(self):
+        """
+        .. todo::
+
+            WRITEME
+        """
         yaml = self.preprocessed_dataset.yaml_src
         yaml = yaml.replace('train', 'test')
         args = {}
@@ -49,7 +54,11 @@ class ZCA_Dataset(DenseDesignMatrix):
                  start=None,
                  stop=None,
                  axes=['b', 0, 1, 'c']):
+        """
+        .. todo::
 
+            WRITEME
+        """
         self.args = locals()
 
         self.preprocessed_dataset = preprocessed_dataset
@@ -104,10 +113,19 @@ class ZCA_Dataset(DenseDesignMatrix):
         self.view_converter.set_axes(axes)
 
     def has_targets(self):
+        """
+        .. todo::
+
+            WRITEME
+        """
         return self.preprocessed_dataset.has_targets()
 
     def adjust_for_viewer(self, X):
+        """
+        .. todo::
 
+            WRITEME
+        """
         #rval = X - self.mn
         #rval /= (self.mx-self.mn)
 
@@ -123,7 +141,11 @@ class ZCA_Dataset(DenseDesignMatrix):
         return rval
 
     def adjust_to_be_viewed_with(self, X, other, per_example=False):
+        """
+        .. todo::
 
+            WRITEME
+        """
         #rval = X - self.mn
         #rval /= (self.mx-self.mn)
 
@@ -145,7 +167,11 @@ class ZCA_Dataset(DenseDesignMatrix):
         return rval
 
     def mapback_for_viewer(self, X):
+        """
+        .. todo::
 
+            WRITEME
+        """
         assert X.ndim == 2
         rval = self.preprocessor.inverse(X)
         rval = self.preprocessed_dataset.adjust_for_viewer(rval)
@@ -153,4 +179,9 @@ class ZCA_Dataset(DenseDesignMatrix):
         return rval
 
     def mapback(self, X):
+        """
+        .. todo::
+
+            WRITEME
+        """
         return self.preprocessor.inverse(X)
