9adc185ecb29dc8c84fb757ba8b3bb6b8b036a2a
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2014-03-17 11:50:11 -0400

    Test for conv2d's lmul_T and lmul_sq_T methods

--- pylearn2/linear/tests/test_conv2d.py
+++ pylearn2/linear/tests/test_conv2d.py
@@ -3,6 +3,7 @@ from theano import tensor
 import numpy
 from pylearn2.linear.conv2d import Conv2D, make_random_conv2D
 from pylearn2.space import Conv2DSpace
+from pylearn2.utils import sharedX
 import unittest
 try:
     scipy_available = True
@@ -19,12 +20,12 @@ class TestConv2D(unittest.TestCase):
         self.filters_values = numpy.ones(
             (1, 1, 2, 2), dtype=theano.config.floatX
         )
-        self.filters = theano.shared(value=self.filters_values, name='filters')
+        self.filters = sharedX(self.filters_values, name='filters')
         self.conv2d = Conv2D(self.filters, 1, self.input_space)
 
     def test_value_errors(self):
-        bad_filters = theano.shared(value=numpy.zeros((1, 3, 2)))
-        with self.assertRaises(ValueError):
+        bad_filters = sharedX(numpy.zeros((1, 3, 2)))
+        with self.assertRaises(TypeError):
             Conv2D(bad_filters, 1, self.input_space)
         with self.assertRaises(AssertionError):
             Conv2D(self.filters, 0, self.input_space)
@@ -41,23 +42,25 @@ class TestConv2D(unittest.TestCase):
                 scipy.ndimage.filters.convolve(
                     self.image.reshape((3, 3)),
                     self.filters_values.reshape((2, 2))
-                )
+                )[:2, :2]
             )
 
     def test_lmul_T(self):
+        conv2d = self.conv2d.lmul(self.image_tensor)
         f = theano.function([self.image_tensor],
-                            self.conv2d.lmul_T(self.image_tensor))
-        assert f(self.image).shape == (1, 4, 4, 1)
+                            self.conv2d.lmul_T(conv2d))
+        assert f(self.image).shape == self.image.shape
 
-    def test_lmul_T_sq(self):
-        conv2d_sq = Conv2D(
-            theano.shared(value=numpy.square(self.filters_values)),
+    def test_lmul_sq_T(self):
+        conv2d_sq = Conv2D(sharedX(numpy.square(self.filters_values)),
             1, self.input_space
-        )
+        ).lmul(self.image_tensor)
+        conv2d = self.conv2d.lmul(self.image_tensor)
         f = theano.function([self.image_tensor],
-                            conv2d_sq.lmul_T(self.image_tensor))
+                            self.conv2d.lmul_T(conv2d_sq))
         f2 = theano.function([self.image_tensor],
-                             self.conv2d.lmul_sq_T(self.image_tensor))
+                             self.conv2d.lmul_sq_T(conv2d))
+
         numpy.testing.assert_allclose(f(self.image), f2(self.image))
 
     def test_set_batch_size(self):
@@ -88,7 +91,7 @@ class TestConv2D(unittest.TestCase):
         filters_values = numpy.ones(
             (2, 3, 2, 2), dtype=theano.config.floatX
         )
-        filters = theano.shared(value=filters_values)
+        filters = sharedX(filters_values)
         image = numpy.random.rand(1, 3, 3, 3).astype(theano.config.floatX)
         conv2d = Conv2D(filters, 1, input_space)
         f = theano.function([self.image_tensor],
