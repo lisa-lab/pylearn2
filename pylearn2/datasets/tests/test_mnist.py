681ea3742a84297847ffb9ff222a8453c8511fa9
David Krueger <davidscottkrueger@gmail.com>
2014-03-18 15:53:19 -0400

    mnist_rotated tests

--- pylearn2/datasets/tests/test_mnist.py
+++ pylearn2/datasets/tests/test_mnist.py
@@ -8,16 +8,21 @@ class TestMNIST(unittest.TestCase):
         skip_if_no_data()
         self.train = MNIST(which_set = 'train')
         self.test = MNIST(which_set = 'test')
+        self.rtrain = MNIST_rotated_background(which_set = 'train')
+        self.rtest = MNIST_rotated_background(which_set = 'test')
+
 
     def test_range(self):
         """Tests that the data spans [0,1]"""
-        for X in [self.train.X, self.test.X ]:
+        for X in [self.train.X, self.test.X, self.rtrain.X, self.rtest.X]:
             assert X.min() == 0.0
             assert X.max() == 1.0
 
     def test_topo(self):
         """Tests that a topological batch has 4 dimensions"""
         topo = self.train.get_batch_topo(1)
+        assert topo.ndim == 4
+        topo = self.rtrain.get_batch_topo(1)
         assert topo.ndim == 4
 
     def test_topo_c01b(self):
