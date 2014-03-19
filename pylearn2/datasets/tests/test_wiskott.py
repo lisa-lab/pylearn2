868ee12f3b8838b505bfd56c482a4d7476a935e7
David Krueger <davidscottkrueger@gmail.com>
2014-03-19 19:05:20 -0400

    done

---
+++ pylearn2/datasets/tests/test_wiskott.py
@@ -0,0 +1,12 @@
+from pylearn2.datasets.wiskott import Wiskott
+import unittest
+from pylearn2.testing.skip import skip_if_no_data
+import numpy as np
+
+
+def test_wiskott():
+    data = Wiskott()
+    assert not np.any(np.isinf(data.X))
+    assert not np.any(np.isnan(data.X))
+
+
--- pylearn2/datasets/wiskott.py
+++ pylearn2/datasets/wiskott.py
@@ -4,8 +4,10 @@ __credits__ = ["Ian Goodfellow"]
 __license__ = "3-clause BSD"
 __maintainer__ = "Ian Goodfellow"
 __email__ = "goodfeli@iro"
+
 import numpy as N
 from pylearn2.datasets import dense_design_matrix
+from pylearn2.utils.serial import load
 
 class Wiskott(dense_design_matrix.DenseDesignMatrix):
     """
@@ -15,13 +17,13 @@ class Wiskott(dense_design_matrix.DenseDesignMatrix):
     """
     def __init__(self):
 
-        X = 1. - N.load("/data/lisa/data/wiskott/wiskott_fish_layer0_15_standard_64x64_shuffled.npy")
+        path = "${PYLEARN2_DATA_PATH}/data/lisa/data/wiskott/wiskott"
+             + "_fish_layer0_15_standard_64x64_shuffled.npy"
 
+        X = 1. - load(path)
 
         view_converter = dense_design_matrix.DefaultViewConverter((64,64,1))
 
         super(Wiskott,self).__init__(X = X, view_converter = view_converter)
 
         assert not N.any(N.isnan(self.X))
-    #
-#
