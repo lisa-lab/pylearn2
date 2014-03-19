c9fa781eda25d792a30c63df9bf3d4d596a5adb9
David Krueger <davidscottkrueger@gmail.com>
2014-03-19 18:28:35 -0400

    done

--- pylearn2/datasets/wiskott.py
+++ pylearn2/datasets/wiskott.py
@@ -10,9 +10,14 @@ __license__ = "3-clause BSD"
 __maintainer__ = "Ian Goodfellow"
 __email__ = "goodfeli@iro"
 
-import numpy as N
+
+import numpy as np
+N = np
+import os.path
+import commands
+
 from pylearn2.datasets import dense_design_matrix
-from pylearn2.utils.serial import load
+
 
 class Wiskott(dense_design_matrix.DenseDesignMatrix):
     """
@@ -21,13 +26,14 @@ class Wiskott(dense_design_matrix.DenseDesignMatrix):
         WRITEME
     """
     def __init__(self):
-        path = "${PYLEARN2_DATA_PATH}/wiskott/wiskott"\
-             + "_fish_layer0_15_standard_64x64_shuffled.npy"
 
-        X = 1. - load(path)
+        pylearn2_data_path = commands.getoutput("echo $PYLEARN2_DATA_PATH")
+        _path = "wiskott/wiskott_fish_layer0_15_standard_64x64_shuffled.npy"
+        data_path = os.path.join(pylearn2_data_path,_path)
 
-        view_converter = dense_design_matrix.DefaultViewConverter((64,64,1))
+        X = 1. - np.load(data_path)
+        view_converter = dense_design_matrix.DefaultViewConverter((64, 64, 1))
 
-        super(Wiskott,self).__init__(X = X, view_converter = view_converter)
+        super(Wiskott, self).__init__(X=X, view_converter=view_converter)
 
         assert not N.any(N.isnan(self.X))
