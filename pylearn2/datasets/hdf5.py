0e22a642526612ff9d19d1b421a1aacea4109f15
skearnes <kearnes@stanford.edu>
2014-03-20 19:17:10 -0700

    Support for targets in HDF5 datasets

--- pylearn2/datasets/hdf5.py
+++ pylearn2/datasets/hdf5.py
@@ -4,10 +4,26 @@ from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 
 class HDF5Dataset(DenseDesignMatrix):
     """Dense dataset loaded from an HDF5 file."""
-    def __init__(self, filename, key):
+    def __init__(self, filename, X=None, topo_view=None, y=None, **kwargs):
+        """
+        Loads data and labels from HDF5 file.
+
+        Parameters
+        ----------
+        filename: str
+            HDF5 file name.
+        X: str
+            Key into HDF5 file for dataset design matrix.
+        topo_view: str
+            Key into HDF5 file for topological view of dataset.
+        y: str
+            Key into HDF5 file for dataset targets.
+        """
         with h5py.File(filename) as f:
-            data = f[key][:]
-        if data.ndim == 2:
-            super(HDF5Dataset, self).__init__(X=data)
-        else:
-            super(HDF5Dataset, self).__init__(topo_view=data)
+            if X is not None:
+                X = f[X][:]
+            if topo_view is not None:
+                topo_view = f[topo_view][:]
+            if y is not None:
+                y = f[y][:]
+        super(HDF5Dataset, self).__init__(X, topo_view, y, **kwargs)
