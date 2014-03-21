6065d59ef21ab493a48e0e24f0353ef772a7aea9
David Krueger <davidscottkrueger@gmail.com>
2014-03-21 14:43:55 -0400

    remove wiskott edits

--- pylearn2/datasets/wiskott.py
+++ pylearn2/datasets/wiskott.py
@@ -9,15 +9,8 @@ __credits__ = ["Ian Goodfellow"]
 __license__ = "3-clause BSD"
 __maintainer__ = "Ian Goodfellow"
 __email__ = "goodfeli@iro"
-
-
-import numpy as np
-N = np
-import os.path
-import commands
-
+import numpy as N
 from pylearn2.datasets import dense_design_matrix
-
 
 class Wiskott(dense_design_matrix.DenseDesignMatrix):
     """
@@ -27,13 +20,14 @@ class Wiskott(dense_design_matrix.DenseDesignMatrix):
     """
     def __init__(self):
 
-        pylearn2_data_path = commands.getoutput("echo $PYLEARN2_DATA_PATH")
-        _path = "wiskott/wiskott_fish_layer0_15_standard_64x64_shuffled.npy"
-        data_path = os.path.join(pylearn2_data_path,_path)
+        X = 1. -
+N.load("/data/lisa/data/wiskott/wiskott_fish_layer0_15_standard_64x64_shuffled.npy")
 
-        X = 1. - np.load(data_path)
-        view_converter = dense_design_matrix.DefaultViewConverter((64, 64, 1))
 
-        super(Wiskott, self).__init__(X=X, view_converter=view_converter)
+        view_converter = dense_design_matrix.DefaultViewConverter((64,64,1))
+
+        super(Wiskott,self).__init__(X = X, view_converter = view_converter)
 
         assert not N.any(N.isnan(self.X))
+    #
+#
