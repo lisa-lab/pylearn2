f777799e23ec48a4a35e67475e5b9aadd6cc0440
David Krueger <davidscottkrueger@gmail.com>
2014-03-25 12:04:54 -0400

    docstrings

--- pylearn2/datasets/tests/test_adult.py
+++ pylearn2/datasets/tests/test_adult.py
@@ -4,10 +4,13 @@ from pylearn2.testing.skip import skip_if_no_data
 
 
 def test_adult():
+    """This tests that the train/test sets load and have no inf/Nan values."""
     skip_if_no_data()
     data = adult(which_set='train')
     assert data.X is not None
-    assert data.X is not np.inf
+    assert np.all(data.X is not np.inf)
+    assert np.all(data.X is not np.nan)
     data = adult(which_set='test')
     assert data.X is not None
-    assert data.X is not np.inf
+    assert np.all(data.X is not np.inf)
+    assert np.all(data.X is not np.nan)
