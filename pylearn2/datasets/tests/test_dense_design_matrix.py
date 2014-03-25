72fda57f9c0ff24478b9eaf818e6b6bb0e756f71
David Krueger <davidscottkrueger@gmail.com>
2014-03-25 12:18:39 -0400

    docstrings d-i

--- pylearn2/datasets/tests/test_dense_design_matrix.py
+++ pylearn2/datasets/tests/test_dense_design_matrix.py
@@ -7,10 +7,12 @@ from pylearn2.utils import serial
 
 
 def test_init_with_X_or_topo():
-    #tests that constructing with topo_view works
-    #tests that construction with design matrix works
-    #tests that conversion from topo_view to design matrix and back works
-    #tests that conversion the other way works too
+    """
+    tests that constructing with topo_view works
+    tests that construction with design matrix works
+    tests that conversion from topo_view to design matrix and back works
+    tests that conversion the other way works too
+    """
     rng = np.random.RandomState([1,2,3])
     topo_view = rng.randn(5,2,2,3)
     d1 = DenseDesignMatrix(topo_view = topo_view)
@@ -24,6 +26,7 @@ def test_init_with_X_or_topo():
     assert np.allclose(X,X2)
 
 def test_convert_to_one_hot():
+    """test ddm.convert_to_one_hot class function."""
     rng = np.random.RandomState([2013, 11, 14])
     m = 11
     d = DenseDesignMatrix(
@@ -32,26 +35,28 @@ def test_convert_to_one_hot():
     d.convert_to_one_hot()
 
 def test_init_with_vc():
+    """Tests init with X, view_converter."""
     rng = np.random.RandomState([4,5,6])
     d = DenseDesignMatrix(
             X=rng.randn(12, 5),
             view_converter = DefaultViewConverter([1,2,3]))
 
 def get_rnd_design_matrix():
+    """Tests init with topo_view."""
     rng = np.random.RandomState([1,2,3])
     topo_view = rng.randn(10,2,2,3)
     d1 = DenseDesignMatrix(topo_view = topo_view)
     return d1
 
 def test_split_datasets():
-    #Test the split dataset function.
+    """Test the split dataset function."""
     ddm = get_rnd_design_matrix()
     (train, valid) = ddm.split_dataset_holdout(train_prop=0.5)
     assert valid.shape[0] == np.ceil(ddm.num_examples * 0.5)
     assert train.shape[0] == (ddm.num_examples - valid.shape[0])
 
 def test_split_nfold_datasets():
-    #Load and create ddm from cifar100
+    """Load and create ddm from cifar100"""
     ddm = get_rnd_design_matrix()
     folds = ddm.split_dataset_nfolds(10)
     assert folds[0].shape[0] == np.ceil(ddm.num_examples / 10)
--- pylearn2/datasets/tests/test_icml07.py
+++ pylearn2/datasets/tests/test_icml07.py
@@ -7,24 +7,28 @@ import pylearn2.datasets.icml07 as icml07
 
 # Basic tests to see if data is loadable
 def test_MNIST_rot_back():
+    """test loading of icml07.MNIST_rotated_background train/valid/test sets"""
     skip_if_no_data()
     data = icml07.MNIST_rotated_background(which_set='train')
     data = icml07.MNIST_rotated_background(which_set='valid')
     data = icml07.MNIST_rotated_background(which_set='test')
 
 def test_Convex():
+    """test loading of icml07.Convex train/valid/test sets"""
     skip_if_no_data()
     data = icml07.Convex(which_set='train')
     data = icml07.Convex(which_set='valid')
     data = icml07.Convex(which_set='test')
 
 def test_Rectangles():
+    """test loading of icml07.Ractangles train/valid/test sets"""
     skip_if_no_data()
     data = icml07.Rectangles(which_set='train')
     data = icml07.Rectangles(which_set='valid')
     data = icml07.Rectangles(which_set='test')
 
 def test_RectanglesImage():
+    """test loading of icml07.RectanglesImage train/valid/test sets"""
     skip_if_no_data()
     data = icml07.RectanglesImage(which_set='train')
     data = icml07.RectanglesImage(which_set='valid')
@@ -32,6 +36,9 @@ def test_RectanglesImage():
 
 # Test features
 def test_split():
+    """
+    test loading of icml07.MNIST_rotated_background train/valid/test set splits
+    """
     skip_if_no_data()
     n_train=100
     n_valid=200
@@ -50,6 +57,7 @@ def test_split():
     assert data.y.shape[0] == n_test, "Unexpected size of test set"
 
 def test_one_hot():
+    """test icml07.MNIST_rotated_background/Rectangles datasets w/one_hot=True"""
     skip_if_no_data()
     data = icml07.MNIST_rotated_background(which_set='train', one_hot=True, split=(100,100,100))
     assert data.y.shape[1] == 10   # MNITS hast 10 classes
