a2f51499edfd1e3497364a914316c05205bec68a
David Krueger <davidscottkrueger@gmail.com>
2014-03-25 11:58:18 -0400

    docstrings

--- pylearn2/datasets/tests/test_stl10.py
+++ pylearn2/datasets/tests/test_stl10.py
@@ -6,16 +6,19 @@ from pylearn2.testing.skip import skip_if_no_data
 
 
 class TestSTL10(unittest.TestCase):
+"""This is a unittest for stl10.py."""
     def setUp(self):
+        """This loads train and test sets."""
         skip_if_no_data()
         self.train = stl10.STL10(which_set='train')
         self.test = stl10.STL10(which_set='test')
 
     def test_restrict(self):
+        """This tests the restrict function on each fold of the train set."""
         for fold in range(10):
             train = stl10.STL10(which_set='train')
             stl10.restrict(train, fold)
 
     def disabled_test_unlabeled(self):
-        # this dataset is 2.4GiB
+        """The unlabeled data is 2.4GiB.  This test is disabled by default."""
         data = stl10.STL10(which_set='unlabeled')
