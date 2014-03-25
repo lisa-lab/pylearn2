1c887861d8f4147b22f89a8e8a65ecc634cbfe6f
Frederic <nouiz@nouiz.org>
2014-03-25 11:44:26 -0400

    speed test another alternative.

--- pylearn2/tests/test_scalar_rectifier.py
+++ pylearn2/tests/test_scalar_rectifier.py
@@ -19,6 +19,7 @@ import time
 floatX = 'float32'
 relu = lambda x: T.maximum(0.0, x)
 relu_ = lambda x: x * (x > 0)
+relu__ = lambda x: T.switch(x < 0., 0., x)
 
 def test_scalar_rectifier():
     # verify the new op rectifier produces the same results as relu
@@ -26,14 +27,17 @@ def test_scalar_rectifier():
     y1 = relu(x)
     y2 = rectifier(x)
     y3 = relu_(x)
+    y4 = relu__(x)
     
     f1 = theano.function(inputs=[x], outputs=y1, name='benchmark_1_forward')
     f2 = theano.function(inputs=[x], outputs=y2, name='benchmark_2_forward')
     f3 = theano.function(inputs=[x], outputs=y3, name='benchmark_3_forward')
+    f4 = theano.function(inputs=[x], outputs=y4, name='benchmark_4_forward')
     
     g1 = theano.function(inputs=[x], outputs=T.grad(y1.sum(),x), name='benchmark_1_grad')
     g2 = theano.function(inputs=[x], outputs=T.grad(y2.sum(),x), name='benchmark_2_grad')
     g3 = theano.function(inputs=[x], outputs=T.grad(y3.sum(),x), name='benchmark_3_grad')
+    g4 = theano.function(inputs=[x], outputs=T.grad(y4.sum(),x), name='benchmark_4_grad')
     
     for i in range(10):
         value = numpy.random.uniform(-1,1,size=(100,500)).astype(floatX)
@@ -43,34 +47,42 @@ def test_scalar_rectifier():
         numpy.testing.assert_array_equal(f1(value), f3(value),
                                          err_msg='arrays not equal' )
 
+        numpy.testing.assert_array_equal(f1(value), f4(value),
+                                         err_msg='arrays not equal' )
+
         numpy.testing.assert_array_equal(g1(value), g2(value),
                                          err_msg='grad:arrays not equal' )
 
         numpy.testing.assert_array_equal(g1(value), g3(value),
                                          err_msg='grad:arrays not equal' )
         
+        numpy.testing.assert_array_equal(g1(value), g4(value),
+                                         err_msg='grad:arrays not equal' )
+
 
 def benchmark_single_op():
-    x = T.fmatrix('inputs')
+    x = T.ftensor4('inputs')
     
     ops = [
         rectifier(x).sum(), # new
         relu_(x).sum(), # old
         relu(x).sum(), # alter, short for alternative
+        relu__(x).sum(), # alter 2
         T.grad(rectifier(x).sum(),x), # grad_new
         T.grad(relu_(x).sum(),x), # grad_old
-        T.grad(relu(x).sum(),x) # grad_alter
+        T.grad(relu(x).sum(),x), # grad_alter
+        T.grad(relu__(x).sum(),x), # grad_alter2
     ]
 
-    names = ['fprop_new', 'fprop_old', 'fprop_alter',
-             'grad_new', 'grad_old', 'grad_alter']
+    names = ['fprop_new', 'fprop_old', 'fprop_alter', 'fprop_alter2',
+             'grad_new', 'grad_old', 'grad_alter', 'grad_alter2']
 
     
+    value = numpy.random.uniform(size=(512,32,32,100)).astype(floatX)
     times = []
     for op, name in zip(ops, names):
         f = theano.function(inputs=[x], outputs=op, name=name)
-        n_loops = 1000
-        value = numpy.random.uniform(size=(100,5000)).astype(floatX)
+        n_loops = 10
         
         t0 = time.time()
         for i in range(n_loops):
@@ -78,9 +90,10 @@ def benchmark_single_op():
         t1 = time.time()
         benchmark = t1-t0
         times.append(benchmark)
-
-        if name == 'fprop_old' or name == 'grad_old':
-            theano.printing.debugprint(f)
+        print name
+        theano.printing.debugprint(f, print_type=True)
+    print names
+    print times
             
 def benchmark_all():
     benchmark_single_op()
