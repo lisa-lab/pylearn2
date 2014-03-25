9cc19a09f04c63c110ab46f6f898bf205a7a23e4
Ian Goodfellow <goodfellow.ian@gmail.com>
2014-03-25 08:02:19 -0400

    more informative error message

--- pylearn2/devtools/tests/docscrape.py
+++ pylearn2/devtools/tests/docscrape.py
@@ -381,7 +381,8 @@ class NumpyDocString(object):
         self._doc.reset()
         for j, line in enumerate(self._doc):
             if len(line) > 75:
-                errors.append("Line %d too long: \"%s\"..." % (j+1, line[:30]))
+                errors.append("Line %d exceeds 75 chars"
+                        ": \"%s\"..." % (j+1, line[:30]))
 
         if check_order:
             canonical_order = ['Signature', 'Summary', 'Extended Summary',
