ea43ba4f3f93b9c3e360c0ad804bbe352e630fea
Steven Kearnes <skearnes@gmail.com>
2014-03-26 20:01:42 -0700

    Incorporate HDF5Dataset directly into test YAML

--- pylearn2/datasets/tests/test_hdf5.py
+++ pylearn2/datasets/tests/test_hdf5.py
@@ -1,6 +1,4 @@
-import pylearn2
-from pylearn2.termination_criteria import EpochCounter
-from pylearn2.utils import serial
+from pylearn2.config import yaml_parse
 from pylearn2.testing.skip import skip_if_no_data, skip_if_no_h5py
 import unittest
 import os
@@ -28,29 +26,89 @@ class TestHDF5Dataset(unittest.TestCase):
                                  data=dataset.get_topological_view())
                 f.create_dataset('y', data=dataset.get_targets())
 
-        # load Train object
-        model_path = pylearn2.__path__[0] \
-                     + "/scripts/papers/maxout/mnist_pi.yaml"
-        self.train = serial.load_train_file(model_path)
+        # instantiate Train object
+        self.train = yaml_parse.load(trainer_yaml)
 
     def test_hdf5(self):
-        """Load datasets from HDF5."""
-        from pylearn2.datasets.hdf5 import HDF5Dataset
-        train = HDF5Dataset('train.h5', X="X", y="y")
-        valid = HDF5Dataset('valid.h5', X="X", y="y")
-        test = HDF5Dataset('test.h5', X="X", y="y")
-
-        # update Train object to use HDF5 datasets
-        self.train.dataset = train
-        self.train.algorithm.monitoring_dataset = {'train': train,
-                                                   'valid': valid,
-                                                   'test': test}
-        self.train.algorithm.termination_criterion = EpochCounter(max_epochs=5)
+        """Run trainer main loop."""
         self.train.main_loop()
 
     def tearDown(self):
         os.remove("train.h5")
         os.remove("valid.h5")
         os.remove("test.h5")
-        os.remove(os.environ['PYLEARN2_TRAIN_FILE_FULL_STEM'] + '.pkl')
-        os.remove(os.environ['PYLEARN2_TRAIN_FILE_FULL_STEM'] + '_best.pkl')
+
+# trainer is a modified version of scripts/papers/maxout/mnist_pi.yaml
+trainer_yaml = """
+!obj:pylearn2.train.Train {
+    dataset: &train !obj:pylearn2.datasets.hdf5.HDF5Dataset {
+        filename: 'train.h5',
+        X: 'X',
+        y: 'y',
+    },
+    model: !obj:pylearn2.models.mlp.MLP {
+        layers: [
+                 !obj:pylearn2.models.maxout.Maxout {
+                     layer_name: 'h0',
+                     num_units: 240,
+                     num_pieces: 5,
+                     irange: .005,
+                     max_col_norm: 1.9365,
+                 },
+                 !obj:pylearn2.models.maxout.Maxout {
+                     layer_name: 'h1',
+                     num_units: 240,
+                     num_pieces: 5,
+                     irange: .005,
+                     max_col_norm: 1.9365,
+                 },
+                 !obj:pylearn2.models.mlp.Softmax {
+                     max_col_norm: 1.9365,
+                     layer_name: 'y',
+                     n_classes: 10,
+                     irange: .005
+                 }
+                ],
+        nvis: 784,
+    },
+    algorithm: !obj:pylearn2.training_algorithms.sgd.SGD {
+        batch_size: 100,
+        learning_rate: .1,
+        learning_rule: !obj:pylearn2.training_algorithms.learning_rule.Momentum {
+            init_momentum: .5,
+        },
+        monitoring_dataset:
+            {
+                'train' : *train,
+                'valid' : !obj:pylearn2.datasets.hdf5.HDF5Dataset {
+                              filename: 'valid.h5',
+                              X: 'X',
+                              y: 'y',
+                          },
+                'test'  : !obj:pylearn2.datasets.hdf5.HDF5Dataset {
+                              filename: 'test.h5',
+                              X: 'X',
+                              y: 'y',
+                          },
+            },
+        cost: !obj:pylearn2.costs.mlp.dropout.Dropout {
+            input_include_probs: { 'h0' : .8 },
+            input_scales: { 'h0': 1. }
+        },
+        termination_criterion: !obj:pylearn2.termination_criteria.EpochCounter {
+            max_epochs: 5,
+        },
+        update_callbacks: !obj:pylearn2.training_algorithms.sgd.ExponentialDecay {
+            decay_factor: 1.000004,
+            min_lr: .000001
+        }
+    },
+    extensions: [
+        !obj:pylearn2.training_algorithms.learning_rule.MomentumAdjustor {
+            start: 1,
+            saturate: 250,
+            final_momentum: .7
+        }
+    ],
+}
+"""
