db619a751346d604124ec3445de66d1532b7d37d
skearnes <kearnes@stanford.edu>
2014-03-28 11:24:02 -0700

    TrainCV with pretrained layers

--- pylearn2/cross_validation.py
+++ pylearn2/cross_validation.py
@@ -7,9 +7,13 @@ cross validation object for future use.
 """
 __author__ = "Steven Kearnes"
 
+from pylearn2.train import Train, SerializationGuard
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
-from pylearn2.utils import safe_zip
+from pylearn2.models.mlp import Layer, PretrainedLayer
+from pylearn2.utils import safe_zip, serial
 from sklearn.cross_validation import *
+from copy import deepcopy
+import os
 
 
 class DatasetIterator(object):
@@ -61,26 +65,134 @@ class DatasetKFold(DatasetIterator):
         super(DatasetKFold, self).__init__(dataset, cv)
 
 
-class DatasetStratifiedKFold(StratifiedKFold):
+class DatasetStratifiedKFold(DatasetIterator):
     def __init__(self, dataset, n_folds=3, indices=None):
         y = dataset.y
-        super(DatasetStratifiedKFold, self).__init__(y, n_folds, indices)
-        self.dataset = dataset
+        cv = StratifiedKFold(y, n_folds, indices)
+        super(DatasetStratifiedKFold, self).__init__(dataset, cv)
+
+
+class DatasetShuffleSplit(DatasetIterator):
+    def __init__(self, dataset, n_iter=10, test_size=0.1, train_size=None,
+                 indices=True, random_state=None):
+        n = dataset.X.shape[0]
+        cv = ShuffleSplit(n, n_iter, test_size, train_size, indices,
+                          random_state)
+        super(DatasetShuffleSplit, self).__init__(dataset, cv)
+
+
+class DatasetStratifiedShuffleSplit(DatasetIterator):
+    def __init__(self, dataset, n_iter=10, test_size=0.1, train_size=None,
+                 indices=True, random_state=None):
+        y = dataset.y
+        cv = StratifiedShuffleSplit(y, n_iter, test_size, train_size, indices,
+                                    random_state)
+        super(DatasetStratifiedShuffleSplit, self).__init__(dataset, cv)
 
 
 class TrainCV(object):
     """Wrapper for Train that partitions the dataset according to CV scheme."""
-    def __init__(self, dataset, model, algorithm=None, save_path=None,
-                 save_freq=0, extensions=None, allow_overwrite=True):
+    def __init__(self, dataset_iterator, model, algorithm=None,
+                 save_path=None, save_freq=0, extensions=None,
+                 allow_overwrite=True, save_subsets=False):
         """
         Create a Train object for each (train, valid, test) dataset with
         partitions given by the cv object.
 
         Parameters
         ----------
-        cv: iterable
+        dataset_iterator: iterable
             Cross validation iterator providing (test, train) or (test, valid,
             train) indices for partitioning the dataset.
+        models: Model or array_like
+            Training model. If more than one model is provided, then
+        save_subsets: bool
+            Whether to write individual files for each subset model.
         See docstring for Train for other argument descriptions.
+
+        TODO: Implement checkpointing of the entire TrainCV object.
+        It would be ideal to have each trainer's save() method actually write
+        to a master pickle to allow easy restart. But since monitors get mangled
+        when serialized, there's no way to resume training anyway.
         """
-        # need to figure out how to get a slice from a database
+        # we need a way to save all the models without writing files
+        trainers = []
+        for k, datasets in enumerate(dataset_iterator):
+            if save_subsets:
+                path, ext = os.path.splitext(save_path)
+                this_save_path = path + "-{}".format(k) + ext
+                this_save_freq = save_freq
+            else:
+                this_save_path = None
+                this_save_freq = 0
+
+            # setup pretrained layers
+            this_model = model
+            if hasattr(model, 'layers') and any(
+                    [isinstance(l, PretrainedLayers) for l in model.layers]):
+                this_model = deepcopy(model)
+                for i, layer in enumerate(this_model.layers):
+                    if isinstance(layer, PretrainedLayers):
+                        this_model.layers[i] = layer.select_layer(k)
+
+            # construct an isolated Train object
+            trainer = Train(datasets['train'], this_model, algorithm,
+                            this_save_path, this_save_freq, extensions,
+                            allow_overwrite)
+            trainer = deepcopy(trainer)  # no shared references between trainers
+            trainer.algorithm._set_monitoring_dataset(datasets)
+            trainers.append(trainer)
+        self.trainers = trainers
+        self.save_path = save_path
+
+    def main_loop(self):
+        for trainer in self.trainers:
+            trainer.main_loop()
+        if self.save_path is not None:
+            self.save()
+
+    def save(self):
+        try:
+            models = []
+            for trainer in self.trainers:
+                trainer.dataset._serialization_guard = SerializationGuard()
+                models.append(trainer.model)
+            serial.save(self.save_path, models, on_overwrite='backup')
+        finally:
+            for trainer in self.trainers:
+                trainer.dataset._serialization_guard = None
+
+
+class PretrainedLayers(Layer):
+    """Container of PretrainedLayer objects for use with TrainCV."""
+    def __init__(self, layer_name, layer_content):
+        """
+        Parameters
+        ----------
+        layer_name: str
+            Name of layer.
+        layer_content: array_like
+            Pretrained layer models for each dataset subset.
+        """
+        self.layer_name = layer_name
+        self.layer_content = [PretrainedLayer(layer_name, subset_content)
+                              for subset_content in layer_content]
+
+    def select_layer(self, k):
+        """Choose a single layer to represent. Could reassign self."""
+        return self.layer_content[k]
+
+    def set_input_space(self, space):
+        return [layer.set_input_space(space) for layer in self.layer_content]
+
+    def get_params(self):
+        return self.layer_content[0].get_params()
+
+    def get_input_space(self):
+        return self.layer_content[0].get_input_space()
+
+    def get_output_space(self):
+        return self.layer_content[0].get_output_space()
+
+    def get_monitoring_channels(self, data):
+        return self.layer_content[0].get_monitoring_channels()
--- pylearn2/tests/test_cross_validation.py
+++ pylearn2/tests/test_cross_validation.py
@@ -1,11 +1,15 @@
 """Tests for cross validation module."""
-__author__ = 'Steven Kearnes'
+__author__ = "Steven Kearnes"
 
-from pylearn2.cross_validation import DatasetKFold
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 from pylearn2.testing.skip import skip_if_no_sklearn
+from pylearn2.config import yaml_parse
 import unittest
 import numpy as np
+import cPickle
+import os
+import glob
+
 
 class TestCrossValidation(unittest.TestCase):
     def setUp(self):
@@ -14,11 +18,90 @@ class TestCrossValidation(unittest.TestCase):
         y = np.random.randint(2, size=1000)
         dataset = DenseDesignMatrix(X=X, y=y)
         dataset.convert_to_one_hot()
+        with open("test_dataset.pkl", "w") as f:
+            cPickle.dump(dataset, f, cPickle.HIGHEST_PROTOCOL)
         self.dataset = dataset
 
-    def test_dataset_k_fold(self):
-        cv = DatasetKFold(self.dataset)
-        for datasets in cv:
-            import IPython
-            IPython.embed()
-            sys.exit()
+    def test_train_cv(self):
+        # train the first hidden layer (unsupervised)
+        trainer = yaml_parse.load(test_yaml_layer0)
+        trainer.main_loop()
+
+        # train the full model (supervised)
+        trainer = yaml_parse.load(test_yaml_layer1)
+        trainer.main_loop()
+
+    def tearDown(self):
+        for filename in glob.glob("test*.pkl"):
+            os.remove(filename)
+
+test_yaml_layer0 = """
+!obj:pylearn2.cross_validation.TrainCV {
+    dataset_iterator: !obj:pylearn2.cross_validation.DatasetKFold {
+        dataset: &train !pkl: 'test_dataset.pkl'
+    },
+    model: !obj:pylearn2.models.autoencoder.Autoencoder {
+        nvis: 15,
+        nhid: 10,
+        act_enc: 'sigmoid',
+        act_dec: null
+    },
+    algorithm: !obj:pylearn2.training_algorithms.bgd.BGD {
+        batch_size: 100,
+        line_search_mode: 'exhaustive',
+        conjugate: 1,
+        monitoring_dataset: *train,
+        cost: !obj:pylearn2.costs.autoencoder.MeanSquaredReconstructionError {},
+        termination_criterion: !obj:pylearn2.termination_criteria.EpochCounter {
+            max_epochs: 5,
+        },
+    },
+    save_path: 'test_layer0.pkl',
+    save_subsets: 1,
+    save_freq: 1
+}
+"""
+
+test_yaml_layer1 = """
+!obj:pylearn2.cross_validation.TrainCV {
+    dataset_iterator: !obj:pylearn2.cross_validation.DatasetKFold {
+        dataset: &train !pkl: 'test_dataset.pkl'
+    },
+    model: !obj:pylearn2.models.mlp.MLP {
+        nvis: 15,
+        layers: [
+            !obj:pylearn2.cross_validation.PretrainedLayers {
+                layer_name: 'h0',
+                layer_content: !pkl: 'test_layer0.pkl'
+            },
+            !obj:pylearn2.models.mlp.Softmax {
+                layer_name: 'y',
+                n_classes: 2,
+                irange: 0.005,
+            },
+        ],
+    },
+    algorithm: !obj:pylearn2.training_algorithms.bgd.BGD {
+        batch_size: 100,
+        line_search_mode: 'exhaustive',
+        conjugate: 1,
+        monitoring_dataset: *train,
+        termination_criterion: !obj:pylearn2.termination_criteria.And {
+            criteria: [
+                !obj:pylearn2.termination_criteria.EpochCounter {
+                    max_epochs: 5,
+                },
+                !obj:pylearn2.termination_criteria.MonitorBased {
+                    channel_name: 'train_y_misclass',
+                    prop_decrease: 0.,
+                    N: 2,
+                },
+            ],
+        },
+
+    },
+    save_path: 'test_layer1.pkl',
+    save_subsets: 1,
+    save_freq: 1
+}
+"""
