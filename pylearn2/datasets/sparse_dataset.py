7cbdce1a6b6406d46a92f668004e1b5e10430af6
Sina Honari <honaris@iro.umontreal.ca>
2014-04-04 17:55:32 -0400

    fixes the issue #490 such that SparseDataset works with the new style of the iterator with data_specs

--- pylearn2/datasets/sparse_dataset.py
+++ pylearn2/datasets/sparse_dataset.py
@@ -1,5 +1,4 @@
 from pylearn2.datasets.dataset import Dataset
-from pylearn2.utils.iteration import SequentialSubsetIterator
 import logging
 import numpy
 import warnings
@@ -11,7 +10,10 @@ import theano
 import gzip
 floatX = theano.config.floatX
 logger = logging.getLogger(__name__)
-
+from pylearn2.utils.iteration import (
+    FiniteDatasetIterator,
+    resolve_iterator_class
+)
 
 class SparseDataset(Dataset):
     """
@@ -34,8 +36,11 @@ class SparseDataset(Dataset):
             logger.info('... building from given sparse dataset')
             self.sparse_matrix = from_scipy_sparse_dataset
 
-        self.data_n_rows = self.sparse_matrix.shape[0]
-        self.num_examples = self.data_n_rows
+        self.X = sparse_matrix
+        X_space = VectorSpace(dim=self.sparse_matrix.shape[1])
+        self.X_space = X_space
+        self._iter_data_specs = (self.X_space, 'features')
+
 
     def get_design_matrix(self):
         return self.sparse_matrix
@@ -54,21 +59,66 @@ class SparseDataset(Dataset):
         raise NotImplementedError('Not implemented for sparse dataset')
 
     def iterator(self, mode=None, batch_size=None, num_batches=None,
-                 topo=None, targets=None, rng=None):
-        """
-        method inherited from Dataset
-        """
-        self.mode = mode
-        self.batch_size = batch_size
-        self._targets = targets
+                 topo=None, targets=None, rng=None, data_specs=None,
+                 return_tuple=False):
 
-        if mode == 'sequential':
-            self.subset_iterator = SequentialSubsetIterator(self.data_n_rows,
-                                            batch_size, num_batches, rng=None)
-            return self
+        if topo is not None or targets is not None:
+            warnings.warn("Usage of `topo` and `target` arguments are "
+                          "being deprecated, and will be removed "
+                          "around November 7th, 2013. `data_specs` "
+                          "should be used instead.",
+                          stacklevel=2)
+
+        if data_specs is None:
+            data_specs = self._iter_data_specs
+
+        # If there is a view_converter, we have to use it to convert
+        # the stored data for "features" into one that the iterator
+        # can return.
+        space, source = data_specs
+        if isinstance(space, CompositeSpace):
+            sub_spaces = space.components
+            sub_sources = source
         else:
-            raise NotImplementedError('other iteration scheme not supported for now!')
+            sub_spaces = (space,)
+            sub_sources = (source,)
 
+        convert = []
+        for sp, src in safe_zip(sub_spaces, sub_sources):
+            if src == 'features' and \
+               getattr(self, 'view_converter', None) is not None:
+                conv_fn = (lambda batch, self=self, space=sp:
+                           self.view_converter.get_formatted_batch(batch,
+                                                                   space))
+            else:
+                conv_fn = None
+
+            convert.append(conv_fn)
+
+        # TODO: Refactor
+        if mode is None:
+            if hasattr(self, '_iter_subset_class'):
+                mode = self._iter_subset_class
+            else:
+                raise ValueError('iteration mode not provided and no default '
+                                 'mode set for %s' % str(self))
+        else:
+            mode = resolve_iterator_class(mode)
+
+        if batch_size is None:
+            batch_size = getattr(self, '_iter_batch_size', None)
+        if num_batches is None:
+            num_batches = getattr(self, '_iter_num_batches', None)
+        if rng is None and mode.stochastic:
+            rng = self.rng
+        return FiniteDatasetIterator(self,
+                                     mode(self.X.shape[0],
+                                          batch_size,
+                                          num_batches,
+                                          rng),
+                                     data_specs=data_specs,
+                                     return_tuple=return_tuple,
+                                     convert=convert)
 
     def __iter__(self):
         return self
