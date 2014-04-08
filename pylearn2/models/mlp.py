38c1386f1b1af272bdefadf1b56b1f77e78c03a9
John Arevalo <jearevaloo@unal.edu.co>
2014-04-08 10:10:15 -0500

    Remove call to non-existing method get_batch_axis(). Test included

--- pylearn2/models/mlp.py
+++ pylearn2/models/mlp.py
@@ -2126,8 +2126,7 @@ class Sigmoid(Linear):
             average kl divergence between Y and Y_hat.
         """
 
-        batch_axis = self.output_space.get_batch_axis()
-        div = kl(Y=Y, Y_hat=Y_hat, batch_axis=batch_axis)
+        div = kl(self, Y=Y, Y_hat=Y_hat, batch_axis=0)
         return div
 
     def get_detection_channels_from_state(self, state, target):
