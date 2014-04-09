a5e0f5e54e335d30739ef06f87c78a5d2ba46556
Axel Davy <axel.davy@ens.fr>
2014-04-09 15:00:02 -0400

    Corrects some coding style issues in test_windowlayer.
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- pylearn2/models/tests/test_windowlayer.py
+++ pylearn2/models/tests/test_windowlayer.py
@@ -14,6 +14,9 @@ from pylearn2.space import Conv2DSpace
 
 
 def build_mlp_fn(x0, y0, x1, y1, s0, s1, c, axes):
+    """
+    Creates an theano function to test the WindowLayer
+    """
     mlp = MLP(layers=[WindowLayer('h0', window=(x0, y0, x1, y1))],
               input_space=Conv2DSpace(shape=(s0, s1),
                                       num_channels=c, axes=axes))
@@ -21,19 +24,23 @@ def build_mlp_fn(x0, y0, x1, y1, s0, s1, c, axes):
     f = theano.function([X], mlp.fprop(X))
     return f
 
+
 def test_windowlayer():
+    """
+    Tests that WindowLayer reacts correctly to the error in window
+    settings, and that the window gives the correct results.
+    """
     np.testing.assert_raises(ValueError, build_mlp_fn,
                              0, 0, 20, 20, 20, 20, 3, ('c', 0, 1, 'b'))
     np.testing.assert_raises(ValueError, build_mlp_fn,
                              -1, -1, 19, 19, 20, 20, 3, ('c', 0, 1, 'b'))
-    fprop = build_mlp_fn(5, 5, 10, 15, 20, 20, 2, ('b','c', 0, 1))
+    fprop = build_mlp_fn(5, 5, 10, 15, 20, 20, 2, ('b', 'c', 0, 1))
     n = np.random.rand(3, 2, 20, 20)
     r = fprop(n)
-    assert r.shape == (3,2,6, 11)
+    assert r.shape == (3, 2, 6, 11)
     assert r[0, 0, 0, 0] == n[0, 0, 5, 5]
     assert r[2, 1, 5, 10] == n[2, 1, 10, 15]
     assert r[1, 1, 3, 3] == n[1, 1, 8, 8]
 
 if __name__ == "__main__":
     test_windowlayer()
-
