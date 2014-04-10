5e50d4d30701f270851e22940d4c368849d3c852
Axel Davy <axel.davy@ens.fr>
2014-04-09 21:22:16 -0400

    Complete documentation of test_windowlayer
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- pylearn2/models/tests/test_windowlayer.py
+++ pylearn2/models/tests/test_windowlayer.py
@@ -16,6 +16,22 @@ from pylearn2.space import Conv2DSpace
 def build_mlp_fn(x0, y0, x1, y1, s0, s1, c, axes):
     """
     Creates an theano function to test the WindowLayer
+
+    Parameters
+    ----------
+    x0: x coordinate of the left of the window
+    y0: y coordinate of the top of the window
+    x1: x coordinate of the right of the window
+    y1: y coordinate of the bottom of the window
+    s0: x shape of the images of the input space
+    s1: y shape of the images of the input space
+    c: number of channels of the input space
+    axes: description of the axes of the input space
+
+    Returns
+    -------
+    f: a theano function applicating the window layer
+    of window (x0, y0, x1, y1).
     """
     mlp = MLP(layers=[WindowLayer('h0', window=(x0, y0, x1, y1))],
               input_space=Conv2DSpace(shape=(s0, s1),
@@ -29,6 +45,10 @@ def test_windowlayer():
     """
     Tests that WindowLayer reacts correctly to the error in window
     settings, and that the window gives the correct results.
+
+    Parameters
+    ----------
+    No Parameter
     """
     np.testing.assert_raises(ValueError, build_mlp_fn,
                              0, 0, 20, 20, 20, 20, 3, ('c', 0, 1, 'b'))
