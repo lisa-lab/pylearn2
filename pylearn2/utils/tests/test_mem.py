b7a747b687e0c1c459357171bc34f031ea430f71
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2014-04-15 15:26:51 -0400

    added docstring to unit test

--- pylearn2/utils/tests/test_mem.py
+++ pylearn2/utils/tests/test_mem.py
@@ -2,6 +2,10 @@ from pylearn2.utils.mem import TypicalMemoryError
 
 
 def test_typical_memory_error():
+    """
+    A dummy test that instantiates a TypicalMemoryError
+    to see if there is no bugs.
+    """
     try:
         raise TypicalMemoryError("test")
     except TypicalMemoryError as e:
