5e46dc75b96f955cd77d46b75647fb1b35472553
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-04-25 09:50:32 -0400

    Prepare FiniteDatasetIterator for the new interface

--- pylearn2/utils/iteration.py
+++ pylearn2/utils/iteration.py
@@ -22,7 +22,7 @@ import inspect
 import numpy as np
 
 from pylearn2.space import CompositeSpace
-from pylearn2.utils import safe_izip, wraps
+from pylearn2.utils import safe_zip, safe_izip, wraps
 from pylearn2.utils.data_specs import is_flat_specs
 from pylearn2.utils.rng import make_np_rng
 
@@ -736,10 +736,6 @@ class FiniteDatasetIterator(object):
             dataset_sub_spaces = dataset_space.components
         assert len(dataset_source) == len(dataset_sub_spaces)
 
-        all_data = self._dataset.get_data()
-        if not isinstance(all_data, tuple):
-            all_data = (all_data,)
-
         space, source = data_specs
         if not isinstance(source, tuple):
             source = (source,)
@@ -749,8 +745,15 @@ class FiniteDatasetIterator(object):
             sub_spaces = space.components
         assert len(source) == len(sub_spaces)
 
-        self._raw_data = tuple(all_data[dataset_source.index(s)]
-                               for s in source)
+        # If `dataset` is incompatible with the new interface, fall back to the
+        # old interface
+        if not hasattr(self._dataset, 'get'):
+            all_data = self._dataset.get_data()
+            if not isinstance(all_data, tuple):
+                all_data = (all_data,)
+            self._raw_data = tuple(all_data[dataset_source.index(s)]
+                                   for s in source)
+
         self._source = source
 
         if convert is None:
@@ -759,9 +762,7 @@ class FiniteDatasetIterator(object):
             assert len(convert) == len(source)
             self._convert = convert
 
-        for i, (so, sp, dt) in enumerate(safe_izip(source,
-                                                   sub_spaces,
-                                                   self._raw_data)):
+        for i, (so, sp) in enumerate(safe_zip(source, sub_spaces)):
             idx = dataset_source.index(so)
             dspace = dataset_sub_spaces[idx]
 
@@ -810,15 +811,34 @@ class FiniteDatasetIterator(object):
             When there are no more batches to return.
         """
         next_index = self._subset_iterator.next()
-        # TODO: handle fancy-index copies by allocating a buffer and
-        # using np.take()
 
-        rval = tuple(
-            fn(data[next_index]) if fn else data[next_index]
-            for data, fn in safe_izip(self._raw_data, self._convert))
+        # If the dataset is incompatible with the new interface, fall back to
+        # the old one
+        if hasattr(self._dataset, 'get'):
+            rval = self._next(next_index)
+        else:
+            rval = self._fallback_next(next_index)
+
         if not self._return_tuple and len(rval) == 1:
             rval, = rval
         return rval
+
+    def _next(self, next_index):
+        # TODO: handle fancy-index copies by allocating a buffer and
+        # using numpy.take()
+        return tuple(
+            fn(batch) if fn else batch for batch, fn in
+            safe_zip(self._dataset.get(self._source, next_index),
+                     self._convert)
+        )
+
+    def _fallback_next(self, next_index):
+        # TODO: handle fancy-index copies by allocating a buffer and
+        # using np.take()
+        return tuple(
+            fn(data[next_index]) if fn else data[next_index]
+            for data, fn in safe_izip(self._raw_data, self._convert)
+        )
 
     @property
     @wraps(SubsetIterator.batch_size, assigned=(), updated=())
