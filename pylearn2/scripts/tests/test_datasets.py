e5ceccc3c2827739d3add3cfc67d6c610bb4616d
Thomas Rohée <roheethomas@gmail.com>
2014-04-27 15:04:15 -0400

    added tests for small norb browser

--- pylearn2/scripts/tests/test_datasets.py
+++ pylearn2/scripts/tests/test_datasets.py
@@ -2,10 +2,11 @@
 Tests for all datasets scripts
 """
 
+import numpy
 import os
 import shutil
 import tempfile
-
+from pylearn2.datasets import norb
 from pylearn2.scripts.datasets import (make_cifar100_gcn_whitened,
                                        make_cifar100_patches,
                                        make_cifar100_patches_8x8,
@@ -15,8 +16,8 @@ from pylearn2.scripts.datasets import (make_cifar100_gcn_whitened,
                                        make_downsampled_stl10,
                                        make_stl10_patches,
                                        make_stl10_patches_8x8,
-                                       make_stl10_whitened)
-
+                                       make_stl10_whitened,
+                                       browse_small_norb)
 from pylearn2.testing.datasets import ArangeDataset
 
 
@@ -94,3 +95,28 @@ def test_stl10_patches_8x8_output():
 def test_stl10_whitened_output():
     """Test output folder creation for stl10 whitened script"""
     dataset_output(make_stl10_whitened)
+
+
+def test_small_norb_browser_remap():
+    """Test if the remapping of label and instances is correct"""
+    train_labels = numpy.asarray([[0, 8, 6, 4, 4], [0, 0, 0, 0, 0]])
+    test_labels = numpy.asarray([[2, 5, 8, 34, 5], [0, 0, 0, 0, 0]])
+    browser = browse_small_norb.SmallNorbBrowser()
+
+    browser.instance_index = norb.SmallNORB.label_type_to_index['instance']
+    train_instance, new_train_labels = browser.remap_instances('train',
+                                                               train_labels)
+    test_instance, new_test_labels = browser.remap_instances('test',
+                                                             test_labels)
+
+    assert new_train_labels[0, :].tolist() == [0, 3, 6, 2, 4]
+    assert train_instance == [4, 6, 7, 8, 9]
+    assert new_test_labels[0, :].tolist() == [2, 4, 8, 17, 5]
+    assert test_instance == [0, 1, 2, 3, 5]
+
+
+def test_small_norb_browser_get_new_azimuth():
+    """Test the get_new_azimuth_degrees method"""
+    browser = browse_small_norb.SmallNorbBrowser()
+
+    assert browser.get_new_azimuth_degrees(4) == 80
