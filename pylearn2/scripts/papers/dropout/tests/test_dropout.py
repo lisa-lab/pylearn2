50803be33771cbc607c6e703fe1becb86cf91946
Mehdi Mirza <memirzamo@gmail.com>
2014-04-28 11:47:57 -0400

    format fix

--- pylearn2/scripts/papers/dropout/tests/test_dropout.py
+++ pylearn2/scripts/papers/dropout/tests/test_dropout.py
@@ -1,3 +1,7 @@
+"""
+Unit tests for dropout paper
+"""
+
 import os
 from pylearn2.scripts.tests.yaml_testing import limited_epoch_train
 from pylearn2.testing.skip import skip_if_no_data
@@ -6,7 +10,12 @@ yaml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                               '..'))
 save_path = os.path.dirname(os.path.realpath(__file__))
 
+
 def test_mnist_valid():
+    """
+    Tests mnist_valid.yaml by running it for only one epoch
+    """
+
     skip_if_no_data()
     limited_epoch_train(os.path.join(yaml_file_path, 'mnist_valid.yaml'))
     try:
@@ -17,6 +26,10 @@ def test_mnist_valid():
 
 
 def test_mnist():
+    """
+    Tests mnist.yaml by running it for only one epoch
+    """
+
     skip_if_no_data()
     limited_epoch_train(os.path.join(yaml_file_path, 'mnist.yaml'))
     try:
@@ -24,5 +37,3 @@ def test_mnist():
         os.remove(os.path.join(save_path, 'mnist_best.pkl'))
     except:
         pass
-
-
