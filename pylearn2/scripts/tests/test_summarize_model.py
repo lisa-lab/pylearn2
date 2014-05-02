4b7532a45f7327bb6a9d46dfb7ad41e21a9b230c
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2014-05-02 18:20:33 -0400

    Added SkipTest when matplotlib isn't available

--- pylearn2/scripts/tests/test_summarize_model.py
+++ pylearn2/scripts/tests/test_summarize_model.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 
 import pylearn2
+from pylearn2.testing.skip import skip_if_no_matplotlib
 
 
 def test_summarize_model():
@@ -12,6 +13,7 @@ def test_summarize_model():
     Asks the summarize_model.py script to inspect a pickled model and
     check that it completes succesfully
     """
+    skip_if_no_matplotlib()
     assert not subprocess.call([
         os.path.join(pylearn2.__path__[0], "scripts/summarize_model.py"),
         os.path.join(pylearn2.__path__[0], "scripts/tests/model.pkl")
