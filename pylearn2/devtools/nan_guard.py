2a2f5975e3e0f0ed153e027388a5cfa724d3414c
David Krueger <davidscottkrueger@gmail.com>
2014-05-05 16:30:28 -0400

    nanguard crap

--- pylearn2/devtools/nan_guard.py
+++ pylearn2/devtools/nan_guard.py
@@ -18,36 +18,36 @@ logger = logging.getLogger(__name__)
 
 class NanGuardMode(Mode):
     """
-A Theano compilation Mode that makes the compiled function automatically
-detect NaNs and Infs and detect an error if they occur.
+    A Theano compilation Mode that makes the compiled function automatically
+    detect NaNs and Infs and detect an error if they occur.
 
-Parameters
-----------
-nan_is_error : bool
-If True, raise an error anytime a NaN is encountered
-inf_is_error : bool
-If True, raise an error anytime an Inf is encountered
-"""
+    Parameters
+    ----------
+    nan_is_error : bool
+        If True, raise an error anytime a NaN is encountered
+    inf_is_error : bool
+        If True, raise an error anytime an Inf is encountered
+    """
 
     def __init__(self, nan_is_error, inf_is_error):
         def do_check_on(var, nd, f, is_input):
             """
-Checks `var` for NaNs / Infs. If detected, raises an exception
-and / or prints information about `nd`, `f`, and `is_input` to
-help the user determine the cause of the invalid values.
+            Checks `var` for NaNs / Infs. If detected, raises an exception
+            and / or prints information about `nd`, `f`, and `is_input` to
+            help the user determine the cause of the invalid values.
 
-Parameters
-----------
-var : numpy.ndarray
-The value to be checked.
-nd : theano.gof.Apply
-The Apply node being executed
-f : callable
-The thunk for the apply node
-is_input : bool
-If True, `var` is an input to `nd`.
-If False, it is an output.
-"""
+            Parameters
+            ----------
+            var : numpy.ndarray
+                The value to be checked.
+            nd : theano.gof.Apply
+                The Apply node being executed
+            f : callable
+                The thunk for the apply node
+            is_input : bool
+                If True, `var` is an input to `nd`.
+                If False, it is an output.
+            """
             error = False
             if nan_is_error:
                 if np.any(np.isnan(var)):
@@ -78,16 +78,16 @@ If False, it is an output.
 
         def nan_check(i, node, fn):
             """
-Runs `fn` while checking its inputs and outputs for NaNs / Infs
+            Runs `fn` while checking its inputs and outputs for NaNs / Infs
 
-Parameters
-----------
-i : currently ignored (TODO: determine why it is here or remove)
-node : theano.gof.Apply
-The Apply node currently being executed
-fn : callable
-The thunk to execute for this Apply node
-"""
+            Parameters
+            ----------
+            i : currently ignored (TODO: determine why it is here or remove)
+            node : theano.gof.Apply
+                The Apply node currently being executed
+            fn : callable
+                The thunk to execute for this Apply node
+            """
             inputs = fn.inputs
             # TODO: figure out why individual inputs are themselves lists sometimes
             for x in flatten(inputs):
