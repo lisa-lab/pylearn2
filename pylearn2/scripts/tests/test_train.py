6138e80020b39cfd1b6e3603ca1755db020c4de6
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2014-05-05 16:32:48 -0400

    Fixed docstring in test_train.py

--- pylearn2/scripts/tests/test_train.py
+++ pylearn2/scripts/tests/test_train.py
@@ -9,9 +9,8 @@ from pylearn2.scripts.train import train
 
 def test_train_cmd():
     """
-    Calls the train.py script with a help argument and
-    with a short YAML file to see if it exits without
-    an error
+    Calls the train.py script with a short YAML file
+    to see if it trains without error
     """
     train(os.path.join(pylearn2.__path__[0],
                        "scripts/autoencoder_example/dae.yaml"))
