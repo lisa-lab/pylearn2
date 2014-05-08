8722cc8a34a5c5a923e47d330b781b3b0d9d2db7
Frederic <nouiz@nouiz.org>
2014-05-08 16:16:34 -0400

    Make the dataset SmallNORB implement the stop parameter and use it in tests.
    This lower this test memory usage from 12G to 1.6G

--- pylearn2/datasets/tests/test_norb.py
+++ pylearn2/datasets/tests/test_norb.py
@@ -22,7 +22,9 @@ class TestNORB(unittest.TestCase):
                                   one_hot=1)
 
     def test_get_topological_view(self):
-        norb = SmallNORB('train')
+        #This is just to lower the memory usage. Otherwise, the
+        #buildbot use close to 10G of ram.
+        norb = SmallNORB('train', stop=1000)
 
         # Get a topological view as a single "(b, s, 0 1, c)" tensor.
         topo_tensor = norb.get_topological_view(single_tensor=True)
