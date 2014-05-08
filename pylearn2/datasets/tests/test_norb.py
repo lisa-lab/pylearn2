a710de87563610b999c6f60938d21e698f69c3c8
Frederic <nouiz@nouiz.org>
2014-05-08 14:40:11 -0400

    pep8

--- pylearn2/datasets/tests/test_norb.py
+++ pylearn2/datasets/tests/test_norb.py
@@ -9,6 +9,7 @@ from pylearn2.datasets.norb_small import FoveatedNORB
 from pylearn2.utils import safe_zip
 from pylearn2.testing.skip import skip_if_no_data
 
+
 class TestNORB(unittest.TestCase):
     def setUp(self):
         skip_if_no_data()
@@ -17,9 +18,8 @@ class TestNORB(unittest.TestCase):
 
         # Test that the FoveatedNORB class can be instantiated
         norb_train = FoveatedNORB(which_set="train",
-                scale=1, restrict_instances=[4, 6, 7, 8],
-                one_hot=1)
-
+                                  scale=1, restrict_instances=[4, 6, 7, 8],
+                                  one_hot=1)
 
     def test_get_topological_view(self):
         norb = SmallNORB('train')
