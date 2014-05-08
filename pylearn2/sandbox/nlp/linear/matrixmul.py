a954efc8046532622c070faabcaa80bba2243fc0
Mehdi Mirza <memirzamo@gmail.com>
2014-05-08 17:37:33 -0400

    made assert more general

--- pylearn2/sandbox/nlp/linear/matrixmul.py
+++ pylearn2/sandbox/nlp/linear/matrixmul.py
@@ -25,7 +25,7 @@ class MatrixMul(matrixmul.MatrixMul):
             a batch) which will be projected
         """
 
-        assert x.dtype in ['int32', 'int64', 'uint32', 'uint64']
+        assert 'int' in x.dtype
 
         if x.ndim == 2:
             shape = (x.shape[0], x.shape[1] * self._W.shape[1])
