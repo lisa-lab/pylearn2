4c62f0b7cce14c2b46d8b052f5f35ba419b4334f
Frederic <nouiz@nouiz.org>
2014-05-09 10:06:10 -0400

    pep8 remove from white list some of the file I changed.

--- pylearn2/datasets/tests/test_norb.py
+++ pylearn2/datasets/tests/test_norb.py
@@ -22,15 +22,15 @@ class TestNORB(unittest.TestCase):
                                   one_hot=1)
 
     def test_get_topological_view(self):
-        #This is just to lower the memory usage. Otherwise, the
-        #buildbot use close to 10G of ram.
+        # This is just to lower the memory usage. Otherwise, the
+        # buildbot use close to 10G of ram.
         norb = SmallNORB('train', stop=1000)
 
         # Get a topological view as a single "(b, s, 0 1, c)" tensor.
         topo_tensor = norb.get_topological_view(single_tensor=True)
         shape = (norb.X.shape[0], 2) + SmallNORB.original_image_shape + (1, )
         expected_topo_tensor = norb.X.reshape(shape)
-        #We loop to lower the peak memory usage
+        # We loop to lower the peak memory usage
         for i in range(topo_tensor.shape[0]):
             assert numpy.all(topo_tensor[i] == expected_topo_tensor[i])
 
@@ -39,6 +39,6 @@ class TestNORB(unittest.TestCase):
         expected_topo_tensors = tuple(expected_topo_tensor[:, i, ...]
                                       for i in range(2))
 
-        for topo_tensor, expected_topo_tensor in \
-            safe_zip(topo_tensors, expected_topo_tensors):
+        for topo_tensor, expected_topo_tensor in safe_zip(
+                topo_tensors, expected_topo_tensors):
             assert numpy.all(topo_tensor == expected_topo_tensor)
