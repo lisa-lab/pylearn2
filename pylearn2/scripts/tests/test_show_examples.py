863975f0925f7546c68ac8a57f101c928934512d
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2014-05-08 21:01:39 -0400

    Add test for show_examples.py

---
+++ pylearn2/scripts/tests/test_show_examples.py
@@ -0,0 +1,20 @@
+"""
+Tests for the show_examples.py script
+"""
+from pylearn2.testing.skip import skip_if_no_matplotlib, skip_if_no_data
+from pylearn2.scripts.show_examples import show_examples
+
+
+def test_show_examples():
+    """
+    Create a pickled model and show the weights
+    """
+    skip_if_no_matplotlib()
+    skip_if_no_data()
+    with open('temp.yaml', 'w') as f:
+        f.write("""
+!obj:pylearn2.datasets.mnist.MNIST {
+        which_set: 'train'
+}
+""")
+    show_examples('temp.yaml', 28, 28)
