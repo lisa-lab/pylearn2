f80507dcf596727dd53bee2e5f518c713746ef85
Thomas Rohée <roheethomas@gmail.com>
2014-05-13 15:07:27 +0200

    added tests for the show_reconstructions script and fixtures

--- pylearn2/scripts/tests/test_dbm.py
+++ pylearn2/scripts/tests/test_dbm.py
@@ -3,15 +3,19 @@ Tests scripts in the DBM folder
 """
 
 import cPickle
+import os
 import pylearn2.scripts.dbm.show_negative_chains as negative_chains
+import pylearn2.scripts.dbm.show_reconstructions as reconstructions
 from pylearn2.models.dbm.layer import BinaryVector, BinaryVectorMaxPool
 from pylearn2.datasets.mnist import MNIST
 from pylearn2.models.dbm.dbm import DBM
+from nose.tools import with_setup
+from pylearn2.datasets import control
 
 
-def test_show_negative_chains():
-    """Test the show_negative_chains script main function"""
-
+def setup():
+    """Create pickle file with a simple model."""
+    control.push_load_data(True)
     with open('dbm.pkl', 'wb') as f:
         dataset = MNIST(which_set='train', start=0, stop=300)
         vis_layer = BinaryVector(nvis=784, bias_from_marginals=dataset)
@@ -22,7 +26,7 @@ def test_show_negative_chains():
                     hidden_layers=[hid_layer])
         model.dataset_yaml_src = """
 !obj:pylearn2.datasets.binarizer.Binarizer {
-    raw: &raw_train !obj:pylearn2.datasets.mnist.MNIST {
+    raw: !obj:pylearn2.datasets.mnist.MNIST {
         which_set: "train",
         start: 0,
         stop: 300
@@ -32,4 +36,29 @@ def test_show_negative_chains():
         model.layer_to_chains = model.make_layer_to_state(1)
         cPickle.dump(model, f, protocol=cPickle.HIGHEST_PROTOCOL)
 
+
+def teardown():
+    """Delete the pickle file created for the tests"""
+    if os.path.isfile('dbm.pkl'):
+        os.remove('dbm.pkl')
+
+
+@with_setup(setup, teardown)
+def test_show_negative_chains():
+    """Test the show_negative_chains script main function"""
     negative_chains.show_negative_chains('dbm.pkl')
+
+
+@with_setup(setup, teardown)
+def test_show_reconstructions():
+    """Test the reconstruction update_viewer function"""
+    rows = 5
+    cols = 10
+    m = rows * cols
+
+    model = reconstructions.load_model('dbm.pkl', m)
+    dataset = reconstructions.load_dataset(model.dataset_yaml_src,
+                                           use_test_set='n')
+
+    recons_viewer = reconstructions.ReconsViewer(model, dataset, rows, cols)
+    recons_viewer.update_viewer()
