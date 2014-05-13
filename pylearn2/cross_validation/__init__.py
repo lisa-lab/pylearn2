cdd51cfa3c9d319af6136aae133a8bcc4b80c0ac
skearnes <kearnes@stanford.edu>
2014-05-13 12:47:38 -0700

    Add TrainCV extensions

--- pylearn2/cross_validation/__init__.py
+++ pylearn2/cross_validation/__init__.py
@@ -25,31 +25,38 @@ class TrainCV(object):
     dataset_iterator: iterable
         Cross validation iterator providing (test, train) or (test, valid,
         train) indices for partitioning the dataset.
-    models: Model or iterable
+    model: Model
         Training model.
-    save_subsets: bool
-        Whether to write individual files for each subset model.
-    See docstring for Train for other argument descriptions.
-
-    TODO: Implement checkpointing of the entire TrainCV object.
-    It would be ideal to have each trainer's save() method actually write
-    to a master pickle to allow easy restart. But since monitors get
-    mangled when serialized, there's no way to resume training anyway.
+    algorithm : TrainingAlgorithm
+        Training algorithm.
+    save_path : str or None
+        Output filename for trained models. Also used (with modification)
+        for individual models if save_folds is True.
+    save_freq : int
+        Save frequency, in epochs. Only used if save_folds is True.
+    extensions : list or None
+        TrainExtension objects for individual Train objects.
+    allow_overwrite : bool
+        Whether to overwrite pre-existing output file matching save_path.
+    save_folds: bool
+        Whether to write individual files for each cross-validation fold.
+    cv_extensions : list or None
+        TrainCVExtension objects for the parent TrainCV object.
     """
     def __init__(self, dataset_iterator, model, algorithm=None,
                  save_path=None, save_freq=0, extensions=None,
-                 allow_overwrite=True, save_subsets=False):
+                 allow_overwrite=True, save_folds=False, cv_extensions=None):
         trainers = []
         for k, datasets in enumerate(dataset_iterator):
-            if save_subsets:
+            if save_folds:
                 path, ext = os.path.splitext(save_path)
-                this_save_path = path + "-{}".format(k) + ext
+                this_save_path = path + '-{}'.format(k) + ext
                 this_save_freq = save_freq
             else:
                 this_save_path = None
                 this_save_freq = 0
 
-            # setup pretrained layers
+            # setup model, including any pretrained layers
             this_model = deepcopy(model)
             if hasattr(this_model, 'layers') and any(
                     [isinstance(l, PretrainedLayerCV)
@@ -82,6 +89,19 @@ class TrainCV(object):
         self.trainers = trainers
         self.save_path = save_path
         self.allow_overwrite = allow_overwrite
+        if cv_extensions is None:
+            self.cv_extensions = []
+        else:
+            self.cv_extensions = cv_extensions
+
+    def setup(self):
+        """Set up the main loop."""
+        self.setup_extensions()
+
+    def setup_extensions(self):
+        """Set up extensions."""
+        for extension in self.cv_extensions:
+            extension.setup(self.trainers)
 
     def main_loop(self, time_budget=None):
         """
@@ -93,25 +113,35 @@ class TrainCV(object):
             The maximum number of seconds before interrupting
             training. Default is `None`, no time limit.
         """
+        self.setup()
         for trainer in self.trainers:
             trainer.main_loop(time_budget)
-        if self.save_path is not None:
-            self.save()
+        self.save()
 
     def save(self):
-        """Serialize trained models."""
-        try:
-            models = []
-            for trainer in self.trainers:
-                for extension in trainer.extensions:
-                    extension.on_save(trainer.model, trainer.dataset,
-                                      trainer.algorithm)
-                trainer.dataset._serialization_guard = SerializationGuard()
-                models.append(trainer.model)
-            if self.save_path is not None:
+        """
+        Call on_save for Train and TrainCV extensions and serialize trained
+        models if save_path is set.
+        """
+        # Train extensions
+        for trainer in self.trainers:
+            for extension in trainer.extensions:
+                extension.on_save(trainer.model, trainer.dataset,
+                                  trainer.algorithm)
+
+        # TrainCV extensions
+        for extension in self.cv_extensions:
+            extension.on_save(self.trainers)
+
+        # serialize trained models
+        if self.save_path is not None:
+            models = [trainer.model for trainer in self.trainers]
+            try:
+                for trainer in self.trainers:
+                    trainer.dataset._serialization_guard = SerializationGuard()
                 if not self.allow_overwrite and os.path.exists(self.save_path):
                     raise IOError("Trying to overwrite file when not allowed.")
                 serial.save(self.save_path, models, on_overwrite='backup')
-        finally:
-            for trainer in self.trainers:
-                trainer.dataset._serialization_guard = None
+            finally:
+                for trainer in self.trainers:
+                    trainer.dataset._serialization_guard = None
