bd7180d5d91c6d4b368556ac5f9d42732fcd57f8
David Krueger <davidscottkrueger@gmail.com>
2014-05-15 11:09:51 -0400

    params

--- pylearn2/datasets/tests/test_stl10.py
+++ pylearn2/datasets/tests/test_stl10.py
@@ -1,18 +1,23 @@
 """module for testing datasets.stl10"""
 import unittest
-import numpy as np
 from pylearn2.datasets import stl10
-from pylearn2.space import Conv2DSpace
 from pylearn2.testing.skip import skip_if_no_data
 
 
 class TestSTL10(unittest.TestCase):
-    """This is a unittest for stl10.py."""
+    """
+    This is a unittest for stl10.py.
+
+    Parameters
+    ----------
+    none
+
+    """
     def setUp(self):
         """This loads train and test sets."""
         skip_if_no_data()
-        train = stl10.STL10(which_set='train')
-        test = stl10.STL10(which_set='test')
+        data = stl10.STL10(which_set='train')
+        data = stl10.STL10(which_set='test')
 
     def test_restrict(self):
         """This tests the restrict function on each fold of the train set."""
