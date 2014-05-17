70fac62e64c300cbbdd9a75e5b7f11c1016ccbcc
skearnes <kearnes@stanford.edu>
2014-05-16 18:29:08 -0700

    GridSearch

---
+++ pylearn2/grid_search.py
@@ -0,0 +1,44 @@
+"""
+Hyperparameter grid search.
+"""
+
+__author__ = "Steven Kearnes"
+__copyright__ = "Copyright 2014, Stanford University"
+__license__ = "3-clause BSD"
+__maintainer__ = "Steven Kearnes"
+
+import copy
+import os
+from sklearn.grid_search import ParameterGrid
+
+from pylearn2.train import Train
+
+
+class GridSearch(Train):
+    def __init__(self, dataset, model, param_grid, algorithm=None,
+                 save_path=None, save_freq=0, extensions=None,
+                 allow_overwrite=True, save_all=False):
+        super(GridSearch, self).__init__(dataset, model, algorithm, save_path,
+                                         save_freq, extensions,
+                                         allow_overwrite)
+        self.param_grid = ParameterGrid(param_grid)
+
+        trainers = []
+        for params in self.param_grid:
+            if save_all and save_path is not None:
+                prefix, ext = os.path.splitext(save_path)
+                this_save_path = prefix
+                this_save_freq = save_freq
+            else:
+                this_save_path = None
+                this_save_freq = 0
+
+            for key, value in params.items():
+                this_save_path += '-{}_{}'.format(key, value)
+            this_dataset = copy.deepcopy(dataset)
+            this_model = copy.deepcopy(model)
+            this_algorithm = copy.deepcopy(algorithm)
+            this_extensions = copy.deepcopy(extensions)
+            trainer = Train(this_dataset, this_model, this_algorithm,
+                            this_save_path, this_save_freq, this_extensions,
+                            allow_overwrite)
