989b3af508255e8b45a2643dffc72eb8843cadd4
skearnes <kearnes@stanford.edu>
2014-05-17 10:39:14 -0700

    WIP

--- pylearn2/grid_search.py
+++ pylearn2/grid_search.py
@@ -14,6 +14,14 @@ from sklearn.grid_search import ParameterGrid
 from pylearn2.train import Train
 
 
+def deep_setattr(obj, name, value):
+    children = name.split('.')
+    name = children[-1]
+    for child in children[:-1]:
+        obj = getattr(obj, child)
+    setattr(obj, name, value)
+
+
 class GridSearch(Train):
     def __init__(self, dataset, model, param_grid, algorithm=None,
                  save_path=None, save_freq=0, extensions=None,
@@ -22,19 +30,17 @@ class GridSearch(Train):
                                          save_freq, extensions,
                                          allow_overwrite)
         self.param_grid = ParameterGrid(param_grid)
-
         trainers = []
         for params in self.param_grid:
             if save_all and save_path is not None:
                 prefix, ext = os.path.splitext(save_path)
                 this_save_path = prefix
+                for key, value in params.items():
+                    this_save_path += '-{}_{}'.format(key, value)
                 this_save_freq = save_freq
             else:
                 this_save_path = None
                 this_save_freq = 0
-
-            for key, value in params.items():
-                this_save_path += '-{}_{}'.format(key, value)
             this_dataset = copy.deepcopy(dataset)
             this_model = copy.deepcopy(model)
             this_algorithm = copy.deepcopy(algorithm)
@@ -42,3 +48,11 @@ class GridSearch(Train):
             trainer = Train(this_dataset, this_model, this_algorithm,
                             this_save_path, this_save_freq, this_extensions,
                             allow_overwrite)
+            for name, value in params.items():
+                deep_setattr(trainer, name, value)
+            trainers.append(trainer)
+        self.trainers = trainers
+        self.save_path = save_path
+        self.allow_overwrite = allow_overwrite
+
+
