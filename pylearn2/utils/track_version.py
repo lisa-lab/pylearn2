a59c9e7527df3c3e63aeacab496b73d02955c90d
Bart <bart.vanmerrienboer@gmail.com>
2014-05-29 16:47:11 -0400

    Fix crash when `git` command is not available
    A missing `except:` clause caused pylearn2 to crash the moment a class is constructed when git is not installed.

--- pylearn2/utils/track_version.py
+++ pylearn2/utils/track_version.py
@@ -156,6 +156,8 @@ class LibVersion(object):
             if len(modified):
                 version += ' M'
             return version
+        except:
+            pass
         finally:
             os.chdir(cwd_backup)
 
