555c8f280dca6d1f0b03b511b7177154992f9ad7
Frederic <nouiz@nouiz.org>
2014-06-03 15:27:33 -0400

    pep8

--- pylearn2/datasets/cache.py
+++ pylearn2/datasets/cache.py
@@ -104,9 +104,9 @@ class LocalDatasetCache:
 
         # Create the $PYLEARN2_LOCAL_DATA_PATH folder if needed
         self.safe_mkdir(self.dataset_local_dir,
-                        force_perm=stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
-                                   stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
-                                   stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH)
+                        (stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
+                         stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
+                         stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH))
 
         # Determine local path to which the file is to be cached
         local_name = os.path.join(self.dataset_local_dir,
