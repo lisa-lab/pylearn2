c3e3ed4009cbf03bdd259cf690edf96a19589018
Raul Chandias <chandiar@iro.umontreal.ca>
2014-06-03 12:46:31 -0400

    test code in a function

--- pylearn2/datasets/tests/test_preprocessors.py
+++ pylearn2/datasets/tests/test_preprocessors.py
@@ -19,43 +19,48 @@ from pylearn2.datasets.preprocessing import TorontoPreprocessor, \
 from pylearn2.datasets.preprocessing import Pipeline
 
 
-num_examples = 5
-num_features = 10
+def test_preprocessors_on_toy_dataset():
+    """
+    Checks that the new preprocessors introduced in CIFAR10 are working
+    as expected on a very simple dummy dataset.
+    """
+
+    num_examples = 5
+    num_features = 10
+
+    # Apply new preprocessors
+    rng = np.random.RandomState([1, 2, 3])
+    X = as_floatX(rng.randn(num_examples, num_features))
+    dataset = DenseDesignMatrix(X=X)
+    preprocessors = []
+
+    # Apply Center preprocessing
+    preprocessors.append(CenterPreprocessor())
+    # Apply Rescale preprocessing
+    preprocessors.append(RescalePreprocessor())
+    # Apply Toronto preprocessing
+    preprocessors.append(TorontoPreprocessor())
+
+    pipeline = Pipeline(preprocessors)
+    pipeline.apply(dataset, True)
+    A = dataset.X
+
+    # Apply the same kind of preprocessors but using the old way, i.e.
+    # not using preprocessors defined in preprocessing
+    rng = np.random.RandomState([1, 2, 3])
+    B = as_floatX(rng.randn(num_examples, num_features))
+
+    # Apply Center preprocessing
+    B -= (B.max(axis=0) - B.min(axis=0))/2
+
+    # Apply Rescale preprocessing
+    B /= abs(B.max(axis=0))
+
+    # Apply Toronto preprocessing
+    B -= (B.min(axis=0) + ((B.max(axis=0) - B.min(axis=0))/2.))
+
+    # Test that both datasets are the same
+    assert np.array_equal(A, B)
 
 
-# Apply new preprocessors
-rng = np.random.RandomState([1, 2, 3])
-X = as_floatX(rng.randn(num_examples, num_features))
-dataset = DenseDesignMatrix(X=X)
-preprocessors = []
-
-# Apply Center preprocessing
-preprocessors.append(CenterPreprocessor())
-# Apply Rescale preprocessing
-preprocessors.append(RescalePreprocessor())
-# Apply Toronto preprocessing
-preprocessors.append(TorontoPreprocessor())
-
-pipeline = Pipeline(preprocessors)
-pipeline.apply(dataset, True)
-A = dataset.X
-
-
-# Apply the same kind of preprocessors but using the old way, i.e.
-# not using preprocessors defined in preprocessing
-rng = np.random.RandomState([1, 2, 3])
-B = as_floatX(rng.randn(num_examples, num_features))
-
-# Apply Center preprocessing
-B -= 127.5
-
-# Apply Rescale preprocessing
-B /= 127.5
-
-# Apply Toronto preprocessing
-B = B / 255.
-B = B - B.mean(axis=0)
-
-
-# Test that both datasets are the same
-assert np.array_equal(A, B)
+test_preprocessors_on_toy_dataset()
