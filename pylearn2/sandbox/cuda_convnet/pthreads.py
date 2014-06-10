1b786cceb81ebdf45dd85cbc57c0ce04e8613edb
Matthias Görgens <matthias.goergens@gmail.com>
2014-06-10 18:00:33 +1000

    Tabs to spaces in Python files

--- pylearn2/sandbox/cuda_convnet/pthreads.py
+++ pylearn2/sandbox/cuda_convnet/pthreads.py
@@ -3,11 +3,11 @@ from theano.configparser import AddConfigVar, StrParam
 AddConfigVar('pthreads.inc_dir',
         "location of pthread.h",
         StrParam(""))
-		
+
 AddConfigVar('pthreads.lib_dir',
         "location of library implementing pthreads",
         StrParam(""))
-		
+
 AddConfigVar('pthreads.lib',
         'name of the library that implements pthreads (e.g. "pthreadVC2" if using pthreadVC2.dll/.lib from pthreads-win32)',
         StrParam(""))
