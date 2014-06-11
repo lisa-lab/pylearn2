ad328c2eefa51cea33e0e5d32eeaba7330582816
Frederic <nouiz@nouiz.org>
2014-06-11 15:26:55 -0400

    Don't load TFD too many times before each test. Keep only 1 dataset version in ram at a time.
    setUp() is called before each tests.

--- pylearn2/datasets/tests/test_tfd.py
+++ pylearn2/datasets/tests/test_tfd.py
@@ -8,8 +8,8 @@ from pylearn2.testing.skip import skip_if_no_data
 class TestTFD(unittest.TestCase):
     def setUp(self):
         skip_if_no_data()
-        self.train = TFD(which_set='train')
-        self.test = TFD(which_set='test')
+
+    def test_init(self):
         valid = TFD(which_set='valid')
         unlabeled = TFD(which_set='unlabeled')
         full_train = TFD(which_set='full_train')
@@ -21,7 +21,8 @@ class TestTFD(unittest.TestCase):
 
     def test_topo(self):
         """Tests that a topological batch has 4 dimensions"""
-        topo = self.train.get_batch_topo(1)
+        train = TFD(which_set='train')
+        topo = train.get_batch_topo(1)
         assert topo.ndim == 4
 
     def test_topo_c01b(self):
@@ -30,16 +31,17 @@ class TestTFD(unittest.TestCase):
         can be dimshuffled back to match the standard ('b',0,1,'c')
         format.
         """
+        test = TFD(which_set='test')
         batch_size = 100
         c01b_test = TFD(which_set='test', axes=('c', 0, 1, 'b'))
         c01b_X = c01b_test.X[0:batch_size, :]
         c01b = c01b_test.get_topological_view(c01b_X)
         assert c01b.shape == (1, 48, 48, batch_size)
         b01c = c01b.transpose(3, 1, 2, 0)
-        b01c_X = self.test.X[0:batch_size, :]
+        b01c_X = test.X[0:batch_size, :]
         assert c01b_X.shape == b01c_X.shape
         assert np.all(c01b_X == b01c_X)
-        b01c_direct = self.test.get_topological_view(b01c_X)
+        b01c_direct = test.get_topological_view(b01c_X)
         assert b01c_direct.shape == b01c.shape
         assert np.all(b01c_direct == b01c)
 
@@ -47,10 +49,11 @@ class TestTFD(unittest.TestCase):
         # Tests that batches returned by an iterator with topological
         # data_specs are the same as the ones returned by calling
         # get_topological_view on the dataset with the corresponding order
+        test = TFD(which_set='test')
         batch_size = 100
-        b01c_X = self.test.X[0:batch_size, :]
-        b01c_topo = self.test.get_topological_view(b01c_X)
-        b01c_b01c_it = self.test.iterator(
+        b01c_X = test.X[0:batch_size, :]
+        b01c_topo = test.get_topological_view(b01c_X)
+        b01c_b01c_it = test.iterator(
             mode='sequential',
             batch_size=batch_size,
             data_specs=(Conv2DSpace(shape=(48, 48),
@@ -75,7 +78,7 @@ class TestTFD(unittest.TestCase):
 
         # Also check that samples from iterators with the same data_specs
         # with Conv2DSpace do not depend on the axes of the dataset
-        b01c_c01b_it = self.test.iterator(
+        b01c_c01b_it = test.iterator(
             mode='sequential',
             batch_size=batch_size,
             data_specs=(Conv2DSpace(shape=(48, 48),
