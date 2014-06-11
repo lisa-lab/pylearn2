727d9207af117c54009934c47eadbbf6527a0663
Frederic <nouiz@nouiz.org>
2014-06-11 14:46:19 -0400

    Fix file format.

--- pylearn2/tests/test_theano.py
+++ pylearn2/tests/test_theano.py
@@ -1,3 +1,12 @@
+""" Include tests related to Theano.
+
+1) One test on one thing Pylearn2 depend to be done by Theano.
+2) One test for a rare corner case crash in Theano that we where not
+able to reproduce rapidly enough without having this tests depend on
+Pylearn2.
+
+"""
+
 __authors__ = "Ian Goodfellow"
 __copyright__ = "Copyright 2010-2012, Universite de Montreal"
 __credits__ = ["Ian Goodfellow"]
@@ -5,6 +14,7 @@ __license__ = "3-clause BSD"
 __maintainer__ = "LISA Lab"
 __email__ = "pylearn-dev@googlegroups"
 from theano import tensor as T
+
 
 def test_grad():
     """Tests that the theano grad method returns a list if it is passed a list
@@ -17,8 +27,8 @@ def test_grad():
 
     G = T.grad(y, [X])
 
-    assert isinstance(G,list)
+    assert isinstance(G, list)
 
     G = T.grad(y, X)
 
-    assert not isinstance(G,list)
+    assert not isinstance(G, list)
