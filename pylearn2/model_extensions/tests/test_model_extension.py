7f385b6f4a5a49b029cb50ea15b831c4755a64b9
woodshop <sarroff@cs.dartmouth.edu>
2014-06-22 21:54:15 -0400

    Testing for ModelExtension Added a new directory 'tests' in model_extensions. Added a script t test that Model accepts a list of ModelExtension instances.

---
+++ pylearn2/model_extensions/tests/test_model_extension.py
@@ -0,0 +1,40 @@
+from pylearn2.models import Model
+from pylearn2.model_extensions.model_extension import ModelExtension
+
+def test_model_extension():
+    """
+    Test that the base class Model loads and passes tests for a list of 
+    extensions. 
+    """
+    class DummyModelExtension(ModelExtension):
+        """Simplest instance of ModelExtension"""
+    class DummyModel(Model):
+        """Simplest instance of Model"""
+
+    extensions = (DummyModelExtension())
+    try:
+        """
+        This should cause an assertion error for passing a tuple instead of 
+        a list
+        """
+        model = DummyModel(extensions=extensions)
+    except AssertionError:
+        pass
+    
+    extensions = [DummyModelExtension(), None]
+    try:
+        """
+        This should cause an assertion error for passing a list where each
+        element is not an instance of ModelExtension
+        """
+        model = DummyModel(extensions=extensions)
+    except AssertionError:
+        pass
+
+    extensions = []
+    for i in xrange(3):
+        extensions.append(DummyModelExtension())
+    """
+    This should not cause an assertion
+    """
+    model = DummyModel(extensions=extensions)
