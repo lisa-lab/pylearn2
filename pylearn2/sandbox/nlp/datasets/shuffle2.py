4cc3f0692de4735e8ad0c5012bd6796ae35e29fd
cdevin <coline.devin@gmail.com>
2014-06-23 17:56:00 -0400

    Removed unused code

--- pylearn2/sandbox/nlp/datasets/shuffle2.py
+++ pylearn2/sandbox/nlp/datasets/shuffle2.py
@@ -17,15 +17,9 @@ from pylearn2.utils.iteration import resolve_iterator_class
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 from pylearn2.datasets.dataset import Dataset
 from pylearn2.space import CompositeSpace, VectorSpace, IndexSpace, Conv2DSpace
-# from research.code.pylearn2.space import (
-#     VectorSequenceSpace,
-#     IndexSequenceSpace,
-# )
 from pylearn2.utils import serial
 from pylearn2.utils import safe_zip
-# from research.code.scripts.segmentaxis import segment_axis
 from pylearn2.utils.iteration import FiniteDatasetIterator
-# import scipy.stats
 
 def index_from_one_hot(one_hot):
     return numpy.where(one_hot == 1.0)[0][0]
@@ -36,8 +30,8 @@ class H5Shuffle(Dataset):
     """
     _default_seed = (17, 2, 946)
 
-    def __init__(self, path, node, which_set, frame_length, overlap=0,
-                 start=0, stop=None, X_labels=None,# y_labels
+    def __init__(self, path, node, which_set, frame_length,
+                 start=0, stop=None, X_labels=None,
                  rng=_default_seed):
         """
         Parameters
@@ -50,9 +44,6 @@ class H5Shuffle(Dataset):
             Either "train", "valid" or "test"
         frame_length : int
             Number of words contained in a frame
-        overlap : int, optional
-            Number of overlapping acoustic samples for two consecutive frames.
-            Defaults to 0, meaning frames don't overlap.
         start : int, optional
             Starting index of the sequences to use. Defaults to 0.
         stop : int, optional
@@ -65,8 +56,6 @@ class H5Shuffle(Dataset):
         self.base_path = path
         self.node_name = node
         self.frame_length = frame_length
-        self.overlap = overlap
-        self.offset = self.frame_length - self.overlap
         self.X_labels = X_labels
         #self.y_labels = y_labels
 
@@ -80,31 +69,9 @@ class H5Shuffle(Dataset):
         self._load_data(which_set, start, stop)
         self.samples_sequences = numpy.asarray(self.raw_data)
         self.num_examples = len(self.samples_sequences)
-        #examples_per_sequence = [0]
-
-        # for sequence_id, samples_sequence in enumerate(self.raw_wav):
-          
-            # TODO: look at this, does it force copying the data?
-            # Sequence segmentation
-            # samples_segmented_sequence = segment_axis(samples_sequence,
-            #                                           frame_length,
-            #                                           overlap)
-            # self.raw_wav[sequence_id] = samples_segmented_sequence
-
-            # # TODO: change me
-            # # Generate features/targets/phones/phonemes/words map
-            # num_frames = samples_segmented_sequence.shape[0]
-            # num_examples = num_frames - self.frames_per_example
-            # examples_per_sequence.append(num_examples)
-
+  
         self.cumulative_sequence_indexes = numpy.cumsum(len(s) for s in self.raw_data)
-        # self.samples_sequences = self.raw_wav
-        # if not self.audio_only:
-        #     self.phones_sequences = self.phones
-        #     self.phonemes_sequences = self.phonemes
-        #     self.words_sequences = self.words
-        # self.num_examples = self.cumulative_example_indexes[-1]
-
+  
         # DataSpecs
         features_space = IndexSpace(
             dim=self.frame_length,
@@ -112,33 +79,11 @@ class H5Shuffle(Dataset):
         )
         features_source = 'features'
 
-        # # Maps from iterator index in a word index.
-        # def features_map_fn(indexes):
-        #     rval = []
-        #     for sequence_index, example_index in self._fetch_index(indexes):
-        #         rval.append(self.samples_sequences[sequence_index][example_index:example_index
-        #             + self.frames_per_example].ravel())
-        #     return rval
-
         targets_space = VectorSpace(dim=self.frame_length-1)
         targets_source = 'targets'
         # def targets_map_fn(indexes):
-        #     rval = []
-        #     for sequence_index, example_index in self._fetch_index(indexes):
-        #         rval.append(self.samples_sequences[sequence_index][example_index
-        #             + self.frames_per_example].ravel())
-        #     return rval
 
-        space_components = [features_space, targets_space]
-        source_components = [features_source, targets_source]
-        # map_fn_components = [features_map_fn, targets_map_fn] 
-        batch_components = [None, None]
-
-        space = CompositeSpace(space_components)
-        source = tuple(source_components)
         self.data_specs = (space, source)
-        # self.map_functions = tuple(map_fn_components)
-        self.batch_buffers = batch_components
 
         # Defaults for iterators
         self._iter_mode = resolve_iterator_class('shuffled_sequential')
@@ -146,18 +91,22 @@ class H5Shuffle(Dataset):
                                                  targets_space)),
                                  (features_source, targets_source))
 
-        def getExample(indexes):
+        def getFeatures(indexes):
             """
             .. todo::
                 Write me
             """
             sequences = self.samples_sequences[indexes]
+
+            # Remove sequences that are shorter than frame length to avoid padding
             shorts = []
             for i in range(len(sequences)):
                 if len(sequences[i]) < self.frame_length:
                     shorts.append(i)
 
             sequences = numpy.delete(sequences, shorts)
+
+            # Get random start point for ngram
             wis = [numpy.random.randint(0, len(s)-self.frame_length+1, 1)[0] for s in sequences]
             # end = min(len(s), self.frame_length+wi)
             # diff = max(self.frame_length +wi - len(s), 0)
@@ -166,7 +115,10 @@ class H5Shuffle(Dataset):
             # X = numpy.asarray([numpy.concatenate((s[wi:(min(len(s), self.frame_length+wi))],
             #      [0]*(max(self.frame_length +wi - len(s), 0)))) for s, wi in 
             #      zip(sequences, wis)])
+
             X = numpy.asarray([s[wi:self.frame_length+wi] for s, wi in zip(sequences, wis)])
+
+            # Words mapped to integers greater than input max are set to 1 (unknown)
             X[X>=self.X_labels] = 1
 
             swaps = numpy.random.randint(0, self.frame_length - 1, len(X))
@@ -176,6 +128,8 @@ class H5Shuffle(Dataset):
             for sample, swap in enumerate(swaps):
                 X[sample, swap], X[sample, swap + 1] = \
                                                   X[sample, swap + 1], X[sample, swap]
+
+            # Store the targets generated by these indices.
             self.lastY = (y, indexes)
             return X
 
@@ -186,12 +140,7 @@ class H5Shuffle(Dataset):
                 print "You can only ask for targets immediately after asking for those features"
                 return None
 
-        self.sourceFNs = {'features': getExample, 'targets': getTarget}
-
-    def _fetch_index(self, indexes):
-        digit = numpy.digitize(indexes, self.cumulative_sequence_indexes) - 1
-        return zip(digit,
-                   numpy.array(indexes) - self.cumulative_sequence_indexes[digit])
+        self.sourceFNs = {'features': getFeatures, 'targets': getTarget}
 
     def _load_data(self, which_set, start, stop):
         """
