bda8bf6d583ad761f1d5713c93823069498dbf91
cdevin <coline.devin@gmail.com>
2014-06-24 15:37:08 -0400

    Probably fixed something

--- pylearn2/sandbox/nlp/datasets/shuffle2.py
+++ pylearn2/sandbox/nlp/datasets/shuffle2.py
@@ -70,12 +70,14 @@ class H5Shuffle(Dataset):
         # Load data from disk
         self._load_data(which_set, start, stop)
         self.samples_sequences = numpy.asarray(self.raw_data)
+        print "removing short sentences"
         shorts = []
         for i in range(len(self.samples_sequences)):
             if len(self.samples_sequences[i]) < self.frame_length:
                 shorts.append(i)
 
         self.samples_sequences = numpy.delete(self.samples_sequences, shorts)
+        print "finished removing short sentences"
         self.num_examples = len(self.samples_sequences)
   
         self.cumulative_sequence_indexes = numpy.cumsum(len(s) for s in self.raw_data)
