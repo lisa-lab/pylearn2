507d7860c7fc73e0e890f539721a562b1a15c3a3
Trung Huynh <thuynh@palantir.com>
2014-06-30 20:31:43 +0100

    Added unit test for Stanford Sentiment Treebank dataset

--- pylearn2/datasets/tests/test_sentiment_treebank.py
+++ pylearn2/datasets/tests/test_sentiment_treebank.py
@@ -1,4 +1,23 @@
 from unittest import TestCase
+from pylearn2.datasets.text.sentiment_treebank import StanfordSentimentTreebank
 
 
-class TestSentimentTreebank(
+class TestSentimentTreebank(TestCase):
+
+    def setUp(self):
+        self.train = StanfordSentimentTreebank('train')
+        self.valid = StanfordSentimentTreebank('valid')
+        self.test = StanfordSentimentTreebank('test')
+
+    def test_topo(self):
+        assert self.train.X.shape == (8544, 21701)
+        assert self.train.y.shape == (8544,)
+
+        assert self.valid.X.shape == (1101, 21701)
+        assert self.valid.y.shape == (1101,)
+
+        assert self.test.X.shape == (2210, 21701)
+        assert self.test.y.shape == (2210,)
+
+
+
