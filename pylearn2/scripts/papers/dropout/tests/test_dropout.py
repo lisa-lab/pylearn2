86979fda9c2f91bbd38fcdeef05c7d87cffe0c5b
Alassane Ndiaye <alassane.gueye.ndiaye@umontreal.ca>
2014-07-04 11:28:41 -0400

    Reduced the runtime of the script test_dropout.

--- pylearn2/scripts/papers/dropout/tests/test_dropout.py
+++ pylearn2/scripts/papers/dropout/tests/test_dropout.py
@@ -11,29 +11,35 @@ yaml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
 save_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_mnist_valid():
+def test_mnist_valid(fast=True):
     """
     Tests mnist_valid.yaml by running it for only one epoch
     """
-
+    if fast:
+        yaml_file = 'mnist_valid_fast'
+    else:
+        yaml_file = 'mnist_valid'
     skip_if_no_data()
-    limited_epoch_train(os.path.join(yaml_file_path, 'mnist_valid.yaml'))
+    limited_epoch_train(os.path.join(yaml_file_path, '%s.yaml' % yaml_file))
     try:
-        os.remove(os.path.join(save_path, 'mnist_valid.pkl'))
-        os.remove(os.path.join(save_path, 'mnist_valid_best.pkl'))
+        os.remove(os.path.join(save_path, '%s.pkl' % yaml_file))
+        os.remove(os.path.join(save_path, '%s_best.pkl' % yaml_file))
     except:
         pass
 
 
-def test_mnist():
+def test_mnist(fast=True):
     """
     Tests mnist.yaml by running it for only one epoch
     """
-
+    if fast:
+        yaml_file = 'mnist_fast'
+    else:
+        yaml_file = 'mnist'
     skip_if_no_data()
-    limited_epoch_train(os.path.join(yaml_file_path, 'mnist.yaml'))
+    limited_epoch_train(os.path.join(yaml_file_path, '%s.yaml' % yaml_file))
     try:
-        os.remove(os.path.join(save_path, 'mnist.pkl'))
-        os.remove(os.path.join(save_path, 'mnist_best.pkl'))
+        os.remove(os.path.join(save_path, '%s.pkl' % yaml_file))
+        os.remove(os.path.join(save_path, '%s_best.pkl' % yaml_file))
     except:
         pass
