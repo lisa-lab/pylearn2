2f0e5ec2abec1061a49e87f794baff3028526111
Alassane Ndiaye <alassane.gueye.ndiaye@umontreal.ca>
2014-07-08 07:08:45 -0400

    From now on, the script runs using the faster settings only if the mode is set to DEBUG_MODE.

--- pylearn2/scripts/papers/dropout/tests/test_dropout.py
+++ pylearn2/scripts/papers/dropout/tests/test_dropout.py
@@ -5,17 +5,18 @@ Unit tests for dropout paper
 import os
 from pylearn2.scripts.tests.yaml_testing import limited_epoch_train
 from pylearn2.testing.skip import skip_if_no_data
+from theano import config
 
 yaml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                               '..'))
 save_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_mnist_valid(fast=True):
+def test_mnist_valid():
     """
     Tests mnist_valid.yaml by running it for only one epoch
     """
-    if fast:
+    if config.mode == "DEBUG_MODE":
         yaml_file = 'mnist_valid_fast'
     else:
         yaml_file = 'mnist_valid'
@@ -28,11 +29,11 @@ def test_mnist_valid(fast=True):
         pass
 
 
-def test_mnist(fast=True):
+def test_mnist():
     """
     Tests mnist.yaml by running it for only one epoch
     """
-    if fast:
+    if config.mode == "DEBUG_MODE":
         yaml_file = 'mnist_fast'
     else:
         yaml_file = 'mnist'
