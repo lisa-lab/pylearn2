ed8c9d766f7fc82ca2e4033622daf8503456f7e0
cdevin <coline.devin@gmail.com>
2014-07-09 16:28:33 -0400

    Added an rnn language model for characters, but slow

--- pylearn2/sandbox/nlp/datasets/penntree.py
+++ pylearn2/sandbox/nlp/datasets/penntree.py
@@ -9,46 +9,40 @@ __copyright__ = "Copyright 2010-2014, Universite de Montreal"
 __license__ = "3-clause BSD"
 
 import warnings
-
+import functools
+import numpy
 from numpy.lib.stride_tricks import as_strided
 
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 from pylearn2.sandbox.nlp.datasets.text import TextDatasetMixin
 from pylearn2.utils import serial
 from pylearn2.utils.iteration import resolve_iterator_class
+from pylearn2.sandbox.rnn.space import SequenceSpace
+from pylearn2.sandbox.rnn.utils.iteration import (
+    SequentialSubsetIterator, ShuffledSequentialSubsetIterator
+)
+from pylearn2.space import IndexSpace, CompositeSpace, VectorSpace
+from pylearn2.datasets.vector_spaces_dataset import VectorSpacesDataset
+from pylearn2.utils.iteration import FiniteDatasetIterator
 
-
-class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
+class PennTreebank(TextDatasetMixin):
     """
-    Loads n-grams from the word-level Penn Treebank corpus.
-
-    Parameters
-    ----------
-    which_set : {'train', 'valid', 'test'}
-        Choose the set to use
-    context_len : int
-        The size of the context i.e. the number of words used
-        to predict the subsequent word.
-    shuffle : bool
-        Whether to shuffle the samples or go through the dataset
-        linearly
+    Loads data from the word-level Penn Treebank corpus. Meant to be
+    inherited by a class that would structure data and an iterator.
     """
-    def __init__(self, which_set, context_len, shuffle=True):
-        """
-        Loads the data and turns it into n-grams
-        """
-
-        self.__dict__.update(locals())
-        del self.self
-
+    def _load_data(self, which_set, context_len, data_mode):
+        if data_mode not in ['words', 'chars']:
+            raise ValueError("Only 'words' and 'chars' are possible values"
+                             "for data_mode, not %s" %(data_mode,))
+        
         path = "${PYLEARN2_DATA_PATH}/PennTreebankCorpus/"
         npz_data = serial.load(path + "penntree_char_and_word.npz")
         if which_set == 'train':
-            self._raw_data = npz_data['train_words']
+            self._raw_data = npz_data['train_' + data_mode]
         elif which_set == 'valid':
-            self._raw_data = npz_data['valid_words']
+            self._raw_data = npz_data['valid_' + data_mode]
         elif which_set == 'test':
-            self._raw_data = npz_data['test_words']
+            self._raw_data = npz_data['test_' + data_mode]
         else:
             raise ValueError("Dataset must be one of 'train', 'valid' "
                              "or 'test'")
@@ -57,8 +51,13 @@ class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
         # that is capitalized for some reason: N
         npz_data = serial.load(path + "dictionaries.npz")
         self._vocabulary = dict((word.lower(), word_index) for word_index, word
-                                in enumerate(npz_data['unique_words']))
-        self._unknown_index = 591
+                                in enumerate(npz_data['unique_' + data_mode]))
+
+        if data_mode == 'words':
+            self._unknown_index = 591
+        else:
+            self._unknown_index = 50
+
         self._is_case_sensitive = False
 
         self._data = as_strided(self._raw_data,
@@ -67,7 +66,37 @@ class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
                                 strides=(self._raw_data.itemsize,
                                          self._raw_data.itemsize))
 
-        super(PennTreebank, self).__init__(
+        
+
+class PennTreebank_NGrams(DenseDesignMatrix, PennTreebank):
+    """
+    Loads n-grams from the PennTreebank corpus.
+
+    Parameters
+    ----------
+    which_set : {'train', 'valid', 'test'}
+        Choose the set to use
+    context_len : int
+        The size of the context i.e. the number of words or chars used
+        to predict the subsequent word.
+    data_mode : {'words', 'chars'}s
+        Specifies which PennTreebank corpus to load.
+    shuffle : bool
+        Whether to shuffle the samples or go through the dataset
+        linearly
+    """
+    def __init__(self, which_set, context_len, data_mode, shuffle=True):
+        self.__dict__.update(locals())
+        del self.self
+
+        # Load data into self._data (defined in PennTreebank)
+        self._load_data(which_set, context_len, data_mode)
+
+        print self._raw_data[0:30]
+        print self._data[:, :-1][:10]
+        print "_____________"
+        print self._data[:, -1:][:10]
+        super(PennTreebank_NGrams, self).__init__(
             X=self._data[:, :-1],
             y=self._data[:, -1:],
             X_labels=10000, y_labels=10000
@@ -80,3 +109,86 @@ class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
             self._iter_subset_class = resolve_iterator_class(
                 'shuffled_sequential'
             )
+
+class PennTreebank_Sequences(VectorSpacesDataset, PennTreebank):
+    """
+    Loads sequences from the PennTreebank corpus.
+    Parameters
+    ----------
+    which_set : {'train', 'valid', 'test'}
+        Choose the set to use
+    context_len : int
+        The size of the context i.e. the number of words or chars used
+        to predict the subsequent word.
+    data_mode : {'words', 'chars'}s
+        Specifies which PennTreebank corpus to load.
+    shuffle : bool
+        Whether to shuffle the samples or go through the dataset
+        linearly
+    """
+    def __init__(self, which_set, context_len, data_mode):
+
+
+        self._load_data(which_set, context_len, data_mode)
+        source = ('features', 'targets')
+        space = CompositeSpace([
+            SequenceSpace(IndexSpace(dim=1,max_labels=50)),
+            IndexSpace(dim=1, max_labels=50)
+        ])
+
+        self._sequence_lengths = [context_len]*len(self._data)
+        X = numpy.asarray(
+            [sequence[:, numpy.newaxis] for sequence in self._data[:, :-1]]
+        )
+    
+        super(PennTreebank_Sequences, self).__init__(
+            data=(X, self._data[:, -1:]), 
+            data_specs=(space, source)
+        )
+
+    @functools.wraps(VectorSpacesDataset.iterator)
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
+                 topo=None, targets=None, rng=None, data_specs=None,
+                 return_tuple=False):
+        if rng is None:
+            rng = self.rng
+        if mode is None or mode == 'shuffled_sequential':
+            subset_iterator = ShuffledSequentialSubsetIterator(
+                dataset_size=self.get_num_examples(),
+                batch_size=batch_size,
+                num_batches=num_batches,
+                rng=rng,
+                sequence_lengths=self._sequence_lengths
+            )
+        elif mode == 'sequential':
+            subset_iterator = SequentialSubsetIterator(
+                dataset_size=self.get_num_examples(),
+                batch_size=batch_size,
+                num_batches=num_batches,
+                rng=None,
+                sequence_lengths=self._sequence_lengths
+            )
+        else:
+            raise ValueError('For sequential datasets only the '
+                             'SequentialSubsetIterator and '
+                             'ShuffledSequentialSubsetIterator have been '
+                             'ported, so the mode `%s` is not supported.' %
+                             (mode,))
+
+        if data_specs is None:
+            data_specs = self.data_specs
+        return FiniteDatasetIterator(
+            dataset=self,
+            subset_iterator=subset_iterator,
+            data_specs=data_specs,
+            return_tuple=return_tuple
+        )
+        
+
+# class Test(PennTreebank):
+#     def __init__(self, which_set, context_len, data_mode):
+#         self._load_data(which_set, context_len, data_mode)
+
+#         print "Max is ", max(self._raw_data)
+
+# test = Test('test', 1, 'chars')
