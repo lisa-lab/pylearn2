06cff235492d2386d9c3f75e811d34c29ec018e7
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2014-07-17 16:31:57 -0400

    Added 2D test

--- pylearn2/sandbox/rnn/models/tests/test_rnn.py
+++ pylearn2/sandbox/rnn/models/tests/test_rnn.py
@@ -88,20 +88,15 @@ class TestRNNs(unittest.TestCase):
         y_data_vals_plus = y_data_vals + (1 - y_mask_vals[:, :, None])
         assert f(X_data_vals, X_mask_vals, y_data_vals_plus, y_mask_vals) == 0
 
-    def test_gradient(self):
+    def test_1d_gradient(self):
         """
-        Testing to see whether the gradient can be calculated.
+        Testing to see whether the gradient can be calculated when using
+        a 1-dimensional hidden state.
         """
         rnn = MLP(input_space=SequenceSpace(VectorSpace(dim=1)),
                   layers=[Recurrent(dim=1, layer_name='recurrent',
                                     irange=0, nonlinearity=lambda x: x),
                           Linear(dim=1, layer_name='linear', irange=0)])
-        W, U, b = rnn.layers[0].get_params()
-        W.set_value([[1]])
-        U.set_value([[2]])
-
-        W, b = rnn.layers[1].get_params()
-        W.set_value([[1]])
 
         X_data, X_mask = rnn.get_input_space().make_theano_batch()
         y_data, y_mask = rnn.get_output_space().make_theano_batch()
@@ -109,6 +104,23 @@ class TestRNNs(unittest.TestCase):
         default_cost = Default()
         cost = default_cost.expr(rnn, ((X_data, X_mask), (y_data, y_mask)))
         tensor.grad(cost, rnn.get_params(), disconnected_inputs='ignore')
+
+    def test_gradient(self):
+        """
+        Testing to see whether the gradient can be calculated.
+        """
+        rnn = MLP(input_space=SequenceSpace(VectorSpace(dim=1)),
+                  layers=[Recurrent(dim=2, layer_name='recurrent',
+                                    irange=0, nonlinearity=lambda x: x),
+                          Linear(dim=1, layer_name='linear', irange=0)])
+
+        X_data, X_mask = rnn.get_input_space().make_theano_batch()
+        y_data, y_mask = rnn.get_output_space().make_theano_batch()
+
+        default_cost = Default()
+        cost = default_cost.expr(rnn, ((X_data, X_mask), (y_data, y_mask)))
+        tensor.grad(cost, rnn.get_params(), disconnected_inputs='ignore')
+
 
 if __name__ == '__main__':
     unittest.main()
