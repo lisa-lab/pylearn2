8094b97e4e3f71bc3457d6f1fefaff9b8e5b04f3
cdevin <coline.devin@gmail.com>
2014-07-21 14:51:26 -0400

    Added a recurrent class that uses a separate recurrent weight matrix for each input index

--- pylearn2/sandbox/rnn/models/mlp.py
+++ pylearn2/sandbox/rnn/models/mlp.py
@@ -6,10 +6,10 @@ from theano import scan
 from theano import tensor
 
 from pylearn2.models import mlp
-from pylearn2.space import VectorSpace
+from pylearn2.space import VectorSpace, IndexSpace
 from pylearn2.sandbox.rnn.space import SequenceSpace
 from pylearn2.utils import sharedX
-
+from pylearn2.sandbox.nlp.linear.matrixmul import MatrixMul
 
 class Recurrent(mlp.Layer):
     """
@@ -17,6 +17,7 @@ class Recurrent(mlp.Layer):
     tangent activation function which only returns its last state
     """
     def __init__(self, dim, layer_name, irange):
+
         self._rnn_friendly = True
         self.__dict__.update(locals())
         del self.self
@@ -30,14 +31,17 @@ class Recurrent(mlp.Layer):
         # Construct the weight matrices and biases
         self.rng = self.mlp.rng
         W_recurrent = self.rng.uniform(-self.irange, self.irange,
-                                  (self.dim, self.dim))
+                                       (self.dim, self.dim))
         W_recurrent,_,_ = scipy.linalg.svd(W_recurrent)
+        
         W_in = self.rng.uniform(-self.irange, self.irange,
-                           (space.dim, self.dim))
+                                (space.dim, self.dim))
+        
+
         W_recurrent, W_in = sharedX(0.9 * W_recurrent), sharedX(W_in)
-        W_recurrent.name, W_in.name = [self.layer_name + '_' + param
-                                       for param in ['W_recurrent', 'W_in']]
-        b = sharedX(np.zeros((self.dim,)), name=self.layer_name + '_b')
+        W_recurrent.name, W_in.name = [self.layer_name + '_' + param for param in ['W_recurrent', 'W_in']]
+
+        b = sharedX(np.zeros((self.dim,)), name=self.layer_name + '_b') 
 
         # Save the parameters and set the output space
         self.params = [W_recurrent, W_in, b]
@@ -49,15 +53,16 @@ class Recurrent(mlp.Layer):
         h0 = tensor.alloc(np.cast[config.floatX](0),
                           state_below.shape[1], self.dim)
         W_in = self.params[1]
-        state_below = tensor.dot(state_below, W_in)
 
-        def _fprop_step(state_below, state_before, W_recurrent, W_in, b):
-            pre_h = (state_below +
+        state_below_in = tensor.dot(state_below, W_in)
+        
+        def _fprop_step(state_below_in, state_before, W_recurrent, W_in, b):
+            pre_h = (state_below_in +
                      tensor.dot(state_before, W_recurrent) + b)
             h = tensor.tanh(pre_h)
             return h
 
-        h, updates = scan(fn=_fprop_step, sequences=[state_below],
+        h, updates = scan(fn=_fprop_step, sequences=[state_below_in],
                           outputs_info=[h0], non_sequences=self.params)
         self._scan_updates.update(updates)
 
@@ -68,7 +73,7 @@ class Recurrent(mlp.Layer):
         return rval
 
     def get_params(self):
-        return self.params
+        return self.params 
 
 
 class Gated_Recurrent(Recurrent):
@@ -105,7 +110,8 @@ class Gated_Recurrent(Recurrent):
         self.params.extend([W_z, U_z, b_z, W_r, U_r, b_r])
 
     def fprop(self, state_below):
-        # The initial hidden state is just zeros
+        # The initial hidden state is just batch_size zeros 
+        
         h0 = tensor.alloc(np.cast[config.floatX](0),
                           state_below.shape[1], self.dim)
         W_in = self.params[1]
@@ -114,14 +120,18 @@ class Gated_Recurrent(Recurrent):
         state_below_in = tensor.dot(state_below, W_in)
         state_below_z = tensor.dot(state_below, W_z)
         state_below_r = tensor.dot(state_below, W_r)
-        
 
         # state_below is the new input, state_before is hidden state
-        def _fprop_step(state_below_in, state_below_z, state_below_r, state_before, W_recurrent, W_in, b,
+        def _fprop_step(state_below_in, state_below_z, state_below_r, 
+                        state_before, W_recurrent, W_in, b,
                         W_z, U_z, b_z, W_r, U_r, b_r):
+    
             z = tensor.nnet.sigmoid(state_below_z + tensor.dot(state_before, U_z) + b_z)
             r = tensor.nnet.sigmoid(state_below_r + tensor.dot(state_before, U_r) + b_r)
-            pre_h = state_below_in + r * tensor.dot(state_before, W_recurrent) + b
+            pre_h = (
+                state_below_in + r * tensor.dot(state_before, W_recurrent)
+                + b
+            )
             new_h = tensor.tanh(pre_h)
             
             h = z * state_before + (1. - z) * new_h
@@ -133,7 +143,130 @@ class Gated_Recurrent(Recurrent):
         self._scan_updates.update(updates)
 
         assert h.ndim == 3
-        rval = h[-1]
+        rval = h[-1] #.reshape(state_below.shape[1], self.dim)
         assert rval.ndim == 2
 
         return rval
+
+class Multiplicative_Gated_Recurrent(mlp.Layer):
+    def __init__(self, proj_dim, dim, layer_name, max_labels, irange):
+        self.max_labels = max_labels
+        self.proj_dim = proj_dim
+        self._rnn_friendly = True
+        self.__dict__.update(locals())
+        del self.self
+        super(Multiplicative_Gated_Recurrent, self).__init__()
+
+    def set_input_space(self, space):
+        # This space expects an IndexSpace sequence
+        assert isinstance(space, SequenceSpace)
+        assert isinstance(space.space, IndexSpace)
+
+        # Construct the weight matrices and biases
+        self.rng = self.mlp.rng
+        W_proj = sharedX(self.rng.uniform(-self.irange, self.irange,
+                                       (self.max_labels, self.proj_dim)))
+        W_proj.name = self.layer_name + '_proj'
+        self.projection = W_proj
+      
+        W_recurrent = self.rng.uniform(-self.irange, self.irange,
+                                       (self.max_labels, self.dim, self.dim)) 
+        W_recurrent = np.asarray([scipy.linalg.svd(W)[0] for W in W_recurrent])
+        
+        W_in = self.rng.uniform(-self.irange, self.irange,
+                                (self.proj_dim, self.dim))
+        
+
+        W_recurrent, W_in = sharedX(0.9 * W_recurrent), sharedX(W_in)
+        W_recurrent.name, W_in.name = [self.layer_name + '_' + param for param in ['W_recurrent', 'W_in']]
+        b = sharedX(np.zeros((self.max_labels, self.dim)), name=self.layer_name + '_b') 
+
+        # Save the parameters and set the output space
+        self.params = [W_recurrent, W_in, b]
+        self.output_space = VectorSpace(dim=self.dim)
+        self.input_space = space
+
+        # Following the notation in
+        # "Learning Phrase Representations using RNN Encoder-Decoder
+        # for Statistical Machine Translation", W weighs the input
+        # and U weighs the recurrent value.
+        W_z = self.rng.uniform(-self.irange, self.irange,
+                                  (self.proj_dim, self.dim))
+        W_r = self.rng.uniform(-self.irange, self.irange,
+                                  (self.proj_dim, self.dim))
+        U_z = self.rng.uniform(-self.irange, self.irange,
+                                  (self.dim, self.dim))
+        U_r = self.rng.uniform(-self.irange, self.irange,
+                                  (self.dim, self.dim))
+        W_z, W_r = sharedX(W_z), sharedX(W_r)
+        U_z, U_r = sharedX(U_z), sharedX(U_r)
+        W_z.name, W_r.name, U_z.name, U_r.name = [
+            self.layer_name + '_' + param for param in 
+            ['W_z', 'W_r', 'U_z', 'U_r']
+        ]
+        b_z = sharedX(np.zeros((self.dim,)), name=self.layer_name + '_b_z')
+        b_r = sharedX(np.zeros((self.dim,)), name=self.layer_name + '_b_r')
+
+        self.params.extend([W_z, U_z, b_z, W_r, U_r, b_r])
+
+    def fprop(self, state_below):
+        # The initial hidden state is just batch_size zeros 
+        # !!! Move to efficient indexing
+        # proj = self.projection.project(state_below)
+        #proj = self.project1(self.projection, state_below)
+        state_below = state_below.reshape((state_below.shape[0]*state_below.shape[2], state_below.shape[1]))
+        proj = self.projection[state_below]
+        print "proj dim", proj.ndim
+        
+        h0 = tensor.alloc(np.cast[config.floatX](0),
+                          state_below.shape[1],# 1, 
+                          self.dim)
+        W_in = self.params[1]
+        W_z = self.params[3]
+        W_r = self.params[6]
+        state_below_in = tensor.dot(proj, W_in)
+        state_below_z = tensor.dot(proj, W_z)
+        state_below_r = tensor.dot(proj, W_r)
+
+        # state_below is the new input, state_before is hidden state
+        def _fprop_step(state_below, state_below_in, state_below_z, state_below_r, 
+                        state_before, W_recurrent, W_in, b,
+                        W_z, U_z, b_z, W_r, U_r, b_r):
+    
+            z = tensor.nnet.sigmoid(state_below_z + tensor.dot(state_before, U_z) + b_z)
+            r = tensor.nnet.sigmoid(state_below_r + tensor.dot(state_before, U_r) + b_r)
+            print "r dim", r.type.ndim
+            #W_rec = self.project1(W_recurrent, state_below)
+            print "State below step", state_below.ndim
+            print "state before", state_before.ndim
+            W_rec = W_recurrent[state_below]
+            #b = self.project0(b,state_below)
+            bias = b[state_below]
+            # !!! Move to efficient indexing
+            #shape = (state_below.shape[0], state_below.shape[1], self.dim)
+            pre_h = (
+                state_below_in + r * tensor.batched_dot(state_before, W_rec)#.reshape(shape)
+                + bias
+            )
+            print "pre_h dim", pre_h.type.ndim
+            print "W_recurrent[state_below] dim", W_rec.ndim
+            print "W_rec * state before", (state_before* W_rec).ndim
+
+            new_h = tensor.tanh(pre_h)
+            
+            h = z * state_before + (1. - z) * new_h
+
+            return h
+
+        h, updates = scan(fn=_fprop_step, sequences=[state_below, state_below_in, state_below_z, state_below_r],
+                          outputs_info=[h0], non_sequences=self.params)
+        self._scan_updates.update(updates)
+
+        assert h.ndim == 3
+        rval = h[-1] 
+        assert rval.ndim == 2
+
+        return rval
+
+    def get_params(self):
+        return self.params + [self.projection]
