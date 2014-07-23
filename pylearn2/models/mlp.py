Intermediate commit with wrapped costs, fixed iterator/masking, no more word2vec dataset, etc.

pylearn2/models/mlp.py
@@ -1190,11 +1190,11 @@ class Softmax(Layer):
         if binary_target_dim is not None:
             assert isinstance(binary_target_dim, py_integer_types)
             self._has_binary_target = True
-            self._target_space = IndexSpace(dim=binary_target_dim, 
+            self._target_space = IndexSpace(dim=binary_target_dim,
                                             max_labels=n_classes)
         else:
             self._has_binary_target = False
-    
+
         self.output_space = VectorSpace(n_classes)
         if not no_affine:
             self.b = sharedX(np.zeros((n_classes,)), name='softmax_b')
@@ -1454,6 +1454,7 @@ class Softmax(Layer):
 
             Z = T.dot(state_below, self.W) + b
 
+        Z.tag.softmax_input = self.layer_name
         rval = T.nnet.softmax(Z)
 
         for value in get_debug_values(rval):
@@ -1466,26 +1467,25 @@ class Softmax(Layer):
 
         assert hasattr(Y_hat, 'owner')
         owner = Y_hat.owner
-        assert owner is not None
-        op = owner.op
-        if isinstance(op, Print):
-           assert len(owner.inputs) == 1
-           Y_hat, = owner.inputs
-           owner = Y_hat.owner
-           op = owner.op
-        assert isinstance(op, T.nnet.Softmax)
-        z, = owner.inputs
+        z = None
+        while owner is not None:
+            if isinstance(owner.op, T.nnet.Softmax):
+                z, = owner.inputs
+                break
+            else:
+                owner = owner.inputs[0].owner
+        assert getattr(z.tag, 'softmax_input', None) == self.layer_name
         assert z.ndim == 2
 
         z = z - z.max(axis=1).dimshuffle(0, 'x')
         log_prob = z - T.log(T.exp(z).sum(axis=1).dimshuffle(0, 'x'))
         # we use sum and not mean because this is really one variable per row
-        
+
         if self._has_binary_target:
             # The following code is the equivalent of accessing log_prob by the
-            # indices in Y, but it is written such that the computation can 
+            # indices in Y, but it is written such that the computation can
             # happen on the GPU rather than CPU.
-            
+
             flat_Y = Y.flatten()
             flat_log_prob = log_prob.flatten()
             flat_indices = flat_Y + T.arange(Y.shape[0])*self.n_classes
@@ -1495,7 +1495,6 @@ class Softmax(Layer):
             log_prob_of = (Y * log_prob)
 
         return log_prob_of
-        
 
     @wraps(Layer.cost)
     def cost(self, Y, Y_hat):

pylearn2/sandbox/nlp/datasets/penntree.py
@@ -3,52 +3,45 @@ Dataset wrapper for the Penn Treebank dataset
 
 See: http://www.cis.upenn.edu/~treebank/
 """
-
 __authors__ = "Bart van Merrienboer"
 __copyright__ = "Copyright 2010-2014, Universite de Montreal"
 __license__ = "3-clause BSD"
 
+from functools import wraps
 import warnings
 
+import numpy as np
 from numpy.lib.stride_tricks import as_strided
 
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 from pylearn2.sandbox.nlp.datasets.text import TextDatasetMixin
 from pylearn2.utils import serial
 from pylearn2.utils.iteration import resolve_iterator_class
+from pylearn2.utils.rng import make_np_rng
+from pylearn2.sandbox.rnn.space import SequenceDataSpace
+from pylearn2.space import IndexSpace, CompositeSpace
+from pylearn2.datasets.vector_spaces_dataset import VectorSpacesDataset
+from pylearn2.sandbox.rnn.utils.iteration import SequenceDatasetIterator
 
 
-class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
+class PennTreebank(TextDatasetMixin):
     """
-    Loads n-grams from the word-level Penn Treebank corpus.
-
-    Parameters
-    ----------
-    which_set : {'train', 'valid', 'test'}
-        Choose the set to use
-    context_len : int
-        The size of the context i.e. the number of words used
-        to predict the subsequent word.
-    shuffle : bool
-        Whether to shuffle the samples or go through the dataset
-        linearly
+    Loads data from the word-level Penn Treebank corpus. Meant to be
+    inherited by a class that would structure data and an iterator.
     """
-    def __init__(self, which_set, context_len, shuffle=True):
-        """
-        Loads the data and turns it into n-grams
-        """
-
-        self.__dict__.update(locals())
-        del self.self
+    def _load_data(self, which_set, context_len, data_mode):
+        if data_mode not in ['words', 'chars']:
+            raise ValueError("Only 'words' and 'chars' are possible values"
+                             "for data_mode, not %s" % (data_mode,))
 
         path = "${PYLEARN2_DATA_PATH}/PennTreebankCorpus/"
         npz_data = serial.load(path + "penntree_char_and_word.npz")
         if which_set == 'train':
-            self._raw_data = npz_data['train_words']
+            self._raw_data = npz_data['train_' + data_mode]
         elif which_set == 'valid':
-            self._raw_data = npz_data['valid_words']
+            self._raw_data = npz_data['valid_' + data_mode]
         elif which_set == 'test':
-            self._raw_data = npz_data['test_words']
+            self._raw_data = npz_data['test_' + data_mode]
         else:
             raise ValueError("Dataset must be one of 'train', 'valid' "
                              "or 'test'")
@@ -57,9 +50,46 @@ class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
         # that is capitalized for some reason: N
         npz_data = serial.load(path + "dictionaries.npz")
         self._vocabulary = dict((word.lower(), word_index) for word_index, word
-                                in enumerate(npz_data['unique_words']))
-        self._unknown_index = 591
+                                in enumerate(npz_data['unique_' + data_mode]))
+
+        if data_mode == 'words':
+            self._unknown_index = 591
+            self._max_labels = 10000
+        else:
+            self._unknown_index = 50
+            self._max_labels = 51
+
         self._is_case_sensitive = False
+
+
+class PennTreebankNGrams(DenseDesignMatrix, PennTreebank):
+    """
+    Loads n-grams from the PennTreebank corpus.
+
+    Parameters
+    ----------
+    which_set : {'train', 'valid', 'test'}
+        Choose the set to use
+    context_len : int
+        The size of the context i.e. the number of words or chars used
+        to predict the subsequent word.
+    data_mode : {'words', 'chars'}
+        Specifies which PennTreebank corpus to load.
+    shuffle : bool
+        Whether to shuffle the samples or go through the dataset
+        linearly
+    permutation : int, optional
+        If given our target is a binary vector of length
+        (context_len - 1), with value i being 1 if the words
+        i and i + 1 are in the wrong order, and 0 otherwise.
+    """
+
+    def __init__(self, which_set, context_len, data_mode, shuffle=True):
+        self.__dict__.update(locals())
+        del self.self
+
+        # Load data into self._data (defined in PennTreebank)
+        self._load_data(which_set, context_len, data_mode)
 
         self._data = as_strided(self._raw_data,
                                 shape=(len(self._raw_data) - context_len,
@@ -67,10 +97,10 @@ class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
                                 strides=(self._raw_data.itemsize,
                                          self._raw_data.itemsize))
 
-        super(PennTreebank, self).__init__(
+        super(PennTreebankNGrams, self).__init__(
             X=self._data[:, :-1],
             y=self._data[:, -1:],
-            X_labels=10000, y_labels=10000
+            X_labels=self._max_labels, y_labels=self._max_labels
         )
 
         if shuffle:
@@ -80,3 +110,64 @@ class PennTreebank(DenseDesignMatrix, TextDatasetMixin):
             self._iter_subset_class = resolve_iterator_class(
                 'shuffled_sequential'
             )
+
+
+class PennTreebankSequences(VectorSpacesDataset, PennTreebank):
+    """
+    Loads sequences from the PennTreebank corpus.
+    Parameters
+    ----------
+    which_set : {'train', 'valid', 'test'}
+        Choose the set to use
+    context_len : int
+        The size of the context i.e. the number of words or chars used
+        to predict the subsequent word.
+    data_mode : {'words', 'chars'}s
+        Specifies which PennTreebank corpus to load.
+    shuffle : bool
+        Whether to shuffle the samples or go through the dataset
+        linearly
+    """
+    def __init__(self, which_set, context_len, data_mode, shuffle=True):
+        self._load_data(which_set, context_len, data_mode)
+        source = ('features', 'targets')
+        space = CompositeSpace([
+            SequenceDataSpace(IndexSpace(dim=1, max_labels=self._max_labels)),
+            SequenceDataSpace(IndexSpace(dim=1, max_labels=self._max_labels))
+        ])
+
+        X = np.asarray(
+            [self._raw_data[:-1][i * context_len:(i + 1) * context_len,
+                                 np.newaxis]
+             for i in range(int(np.ceil((len(self._raw_data) - 1) /
+                                        float(context_len))))]
+        )
+        y = np.asarray(
+            [self._raw_data[1:][i * context_len:(i + 1) * context_len,
+                                np.newaxis]
+             for i in range(int(np.ceil((len(self._raw_data) - 1) /
+                                        float(context_len))))]
+        )
+        super(PennTreebankSequences, self).__init__(
+            data=(X, y),
+            data_specs=(space, source)
+        )
+
+    def _create_subset_iterator(self, mode, batch_size=None, num_batches=None,
+                                rng=None):
+        subset_iterator = resolve_iterator_class(mode)
+        if rng is None and subset_iterator.stochastic:
+            rng = make_np_rng()
+        return subset_iterator(self.get_num_examples(), batch_size,
+                               num_batches, rng)
+
+    @wraps(VectorSpacesDataset.iterator)
+    def iterator(self, batch_size=None, num_batches=None, rng=None,
+                 data_specs=None, return_tuple=False, mode=None):
+        subset_iterator = self._create_subset_iterator(
+            mode=mode, batch_size=batch_size, num_batches=num_batches, rng=rng
+        )
+        # This should be fixed to allow iteration with default data_specs
+        # i.e. add a mask automatically maybe?
+        return SequenceDatasetIterator(self, data_specs, subset_iterator,
+                                       return_tuple=return_tuple)

pylearn2/sandbox/rnn/models/rnn.py
@@ -1,16 +1,62 @@
 """
 Recurrent Neural Network Layer
 """
-
 from functools import wraps
+
 import numpy as np
 from theano import tensor
-from pylearn2.models.mlp import Layer
-from pylearn2.space import CompositeSpace, VectorSpace
-from pylearn2.sandbox.rnn.space import SequenceSpace
-from pylearn2.utils import sharedX
 from theano import config, scan
 from theano.compat.python2x import OrderedDict
+
+from pylearn2.models.mlp import Layer, MLP
+from pylearn2.sandbox.rnn.space import SequenceSpace
+from pylearn2.space import CompositeSpace, VectorSpace
+from pylearn2.utils import sharedX
+
+
+class RNN(MLP):
+    """
+    This method overrides MLP's __init__ method. It recursively
+    goes through the spaces and sources, and for each SequenceSpace
+    it adds an extra source for the mask. This is just syntactic sugar,
+    preventing people from adding a source for each mask.
+
+    Eventually this behaviour could maybe be moved to the
+    DataSpecsMapping class.
+
+    Parameters
+    ----------
+    See https://docs.python.org/2/reference/datamodel.html#object.__new__
+    """
+    def __init__(self, layers, batch_size=None, input_space=None,
+                 input_source='features', nvis=None, seed=None,
+                 layer_name=None, **kwargs):
+        input_source = self.add_mask_source(input_space, input_source)
+        super(RNN, self).__init__(layers, batch_size, input_space,
+                                  input_source, nvis, seed, layer_name,
+                                  **kwargs)
+
+    def get_target_source(self):
+        target_source = self.add_mask_source(self.get_target_space(),
+                                             'targets')
+        return target_source
+
+    @classmethod
+    def add_mask_source(cls, space, source):
+        """
+        This is a recursive helper function to go
+        through the nested spaces and tuples
+        """
+        if isinstance(space, CompositeSpace):
+            if not isinstance(space, SequenceSpace):
+                source = tuple(
+                    cls.add_mask_source(component, source)
+                    for component, source in zip(space.components, source)
+                )
+            else:
+                assert isinstance(source, basestring)
+                source = (source, source + '_mask')
+        return source
 
 
 class Recurrent(Layer):
@@ -130,6 +176,8 @@ class Recurrent(Layer):
         if (state is not None) or (state_below is not None):
             if state is None:
                 state = self.fprop(state_below)
+            state, _ = state
+            state_below, _ = state_below
 
             mx = state.max(axis=0)
             mean = state.mean(axis=0)

pylearn2/sandbox/rnn/space/__init__.py
@@ -4,6 +4,7 @@ Spaces specific to the RNN framework, specifically the SequenceSpace
 from functools import wraps
 
 import numpy as np
+from theano import scan
 from theano import tensor
 
 from pylearn2 import space
@@ -107,7 +108,27 @@ class SequenceDataSpace(space.SimplyTypedSpace):
         if space == self:
             return batch
         else:
-            raise NotImplementedError
+            if not isinstance(space, SequenceDataSpace):
+                raise ValueError("Can only convert SequenceDataSpace to "
+                                 "another SequenceDataSpace, not to %s"
+                                 % space)
+            if is_numeric:
+                formatted_batch = np.transpose(np.asarray([
+                    self.space._format_as_impl(is_numeric, sample, space.space)
+                    for sample in np.transpose(batch, (1, 0, 2))
+                ]), (1, 0, 2))
+            else:
+                formatted_batch, _ = scan(
+                    fn=lambda elem: self.space._format_as_impl(is_numeric,
+                                                               elem,
+                                                               space.space),
+                    sequences=[batch]
+                )
+            return formatted_batch
+
+    def _check_sizes(self, space):
+        assert isinstance(space, SequenceDataSpace)
+        return self.space._check_sizes(space.space)
 
     @wraps(space.Space.make_theano_batch)
     def make_theano_batch(self, name=None, dtype=None, batch_size=None):

pylearn2/sandbox/rnn/utils/iteration.py
@@ -102,7 +102,7 @@ class SequenceDatasetIterator(FiniteDatasetIterator):
         mask = np.zeros((max_sequence_length, len(data)), dtype=config.floatX)
         for i, sequence_length in enumerate(sequence_lengths):
             mask[:sequence_length, i] = 1
-        return mask
+        return mask.T
 
     @wraps(FiniteDatasetIterator.next)
     def next(self):
@@ -111,20 +111,25 @@ class SequenceDatasetIterator(FiniteDatasetIterator):
         for space, source, data, fn in safe_izip(self._space, self._source,
                                                  self._raw_data,
                                                  self._convert):
-            rval = fn(data[next_index]) if fn else data[next_index]
+            rval = data[next_index]
             if isinstance(space, SequenceDataSpace):
                 # Add padding
                 max_sequence_length = max(len(sample) for sample
-                                          in data)
-                batch = np.zeros((len(rval), max_sequence_length,
-                                  space.dim), dtype=space.dtype)
+                                          in rval)
+                batch = np.zeros((len(rval), max_sequence_length) +
+                                 data[0].shape[1:], dtype=data[0].dtype)
                 for i, sample in enumerate(rval):
                     batch[i, :len(sample)] = sample
-                rvals.append(np.transpose(batch, (1, 0, 2)))
+                rval = np.transpose(batch, (1, 0, 2))
+                if fn:
+                    rval = fn(rval)
+                rvals.append(rval)
 
                 # Create mask
                 rvals.append(self._create_mask(rval))
             else:
+                if fn:
+                    rval = fn(rval)
                 rvals.append(rval)
 
         # Reorder according to given data specs