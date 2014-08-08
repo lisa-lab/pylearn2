8d343318a775313363a560697951c9e048c43af0
JesseLivezey <jesse.livezey@gmail.com>
2014-08-08 11:43:55 -0700

    changed monitor.setup check on extra_costs from OrderedDict to dict

--- pylearn2/monitor.py
+++ pylearn2/monitor.py
@@ -786,7 +786,7 @@ class Monitor(object):
             will appear as the `objective` channel. Its
             `get_monitoring_channels` method will also be used to
             supply other channels.
-        extra_costs : OrderedDict, optional
+        extra_costs : dict, optional
             A dictionary mapping channel names to Cost objects.
             Their value will appear as the specified channel name.
             They will also provide more monitoring channels via their
@@ -811,7 +811,7 @@ class Monitor(object):
         if extra_costs is None:
             costs = {}
         else:
-            assert isinstance(extra_costs, OrderedDict)
+            assert isinstance(extra_costs, dict)
             costs = extra_costs
         assert '' not in costs
         costs[''] = cost
