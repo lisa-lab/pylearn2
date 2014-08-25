1203503a8fbcc58a7766b48b292658dddbbfda84
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-08-25 13:37:30 -0400

    Add integration tests, make use of 'make_np_rng'

--- pylearn2/models/tests/test_vae.py
+++ pylearn2/models/tests/test_vae.py
@@ -1,12 +1,13 @@
-import numpy
 import theano
 import theano.tensor as T
+from pylearn2.config import yaml_parse
 from pylearn2.models.mlp import MLP
 from pylearn2.models.mlp import Linear, ConvRectifiedLinear
 from pylearn2.models.vae import VAE
 from pylearn2.models.vae.visible import BinaryVisible
 from pylearn2.models.vae.latent import DiagonalGaussianPrior
 from pylearn2.space import Conv2DSpace
+from pylearn2.utils.rng import make_np_rng
 
 
 def test_one_sample_allowed():
@@ -24,7 +25,8 @@ def test_one_sample_allowed():
     X = T.matrix('X')
     lower_bound = vae.log_likelihood_lower_bound(X)
     f = theano.function(inputs=[X], outputs=lower_bound)
-    f(numpy.random.uniform(size=(10, 10)))
+    rng = make_np_rng(default_seed=11223)
+    f(rng.uniform(size=(10, 10)))
 
 
 def test_multiple_samples_allowed():
@@ -42,7 +44,8 @@ def test_multiple_samples_allowed():
     X = T.matrix('X')
     lower_bound = vae.log_likelihood_lower_bound(X)
     f = theano.function(inputs=[X], outputs=lower_bound)
-    f(numpy.random.uniform(size=(10, 10)))
+    rng = make_np_rng(default_seed=11223)
+    f(rng.uniform(size=(10, 10)))
 
 
 def test_convolutional_compatible():
@@ -71,4 +74,125 @@ def test_convolutional_compatible():
     X = T.matrix('X')
     lower_bound = vae.log_likelihood_lower_bound(X)
     f = theano.function(inputs=[X], outputs=lower_bound)
-    f(numpy.random.uniform(size=(10, 16)))
+    rng = make_np_rng(default_seed=11223)
+    f(rng.uniform(size=(10, 16)))
+
+
+def test_VAE_cost():
+    """
+    VAE trains properly with the VAE cost
+    """
+    yaml_string = """
+    !obj:pylearn2.train.Train {
+        dataset: &train !obj:pylearn2.testing.datasets.random_dense_design_matrix {
+            rng: !obj:pylearn2.utils.rng.make_np_rng {
+                default_seed: 11234,
+            },
+            num_examples: 100,
+            dim: &nvis 10,
+            num_classes: 2,
+        },
+        model: !obj:pylearn2.models.vae.VAE {
+            nvis: *nvis,
+            nhid: &nhid 5,
+            visible: !obj:pylearn2.models.vae.visible.BinaryVisible {
+                decoding_model: !obj:pylearn2.models.mlp.MLP {
+                    nvis: *nhid,
+                    layers: [
+                        !obj:pylearn2.models.mlp.Linear {
+                            layer_name: 'h_d',
+                            dim: 10,
+                            irange: .01,
+                        },
+                    ],
+                },
+            },
+            latent: !obj:pylearn2.models.vae.latent.DiagonalGaussianPrior {
+                encoding_model: !obj:pylearn2.models.mlp.MLP {
+                    nvis: *nvis,
+                    layers: [
+                        !obj:pylearn2.models.mlp.RectifiedLinear {
+                            layer_name: 'h_e',
+                            dim: 10,
+                            irange: .01,
+                        },
+                    ],
+                },
+            },
+        },
+        algorithm: !obj:pylearn2.training_algorithms.sgd.SGD {
+            batch_size: 100,
+            learning_rate: 1e-3,
+            monitoring_dataset: {
+                'train' : *train,
+            },
+            cost: !obj:pylearn2.costs.vae.VAECriterion {},
+            termination_criterion: !obj:pylearn2.termination_criteria.EpochCounter {
+                max_epochs: 2
+            },
+        },
+    }
+    """
+    train_object = yaml_parse.load(yaml_string)
+    train_object.main_loop()
+
+
+def test_IS_cost():
+    """
+    VAE trains properly with the importance sampling cost
+    """
+    yaml_string = """
+    !obj:pylearn2.train.Train {
+        dataset: &train !obj:pylearn2.testing.datasets.random_dense_design_matrix {
+            rng: !obj:pylearn2.utils.rng.make_np_rng {
+                default_seed: 11234,
+            },
+            num_examples: 100,
+            dim: &nvis 10,
+            num_classes: 2,
+        },
+        model: !obj:pylearn2.models.vae.VAE {
+            nvis: *nvis,
+            nhid: &nhid 5,
+            visible: !obj:pylearn2.models.vae.visible.BinaryVisible {
+                decoding_model: !obj:pylearn2.models.mlp.MLP {
+                    nvis: *nhid,
+                    layers: [
+                        !obj:pylearn2.models.mlp.Linear {
+                            layer_name: 'h_d',
+                            dim: 10,
+                            irange: .01,
+                        },
+                    ],
+                },
+            },
+            latent: !obj:pylearn2.models.vae.latent.DiagonalGaussianPrior {
+                encoding_model: !obj:pylearn2.models.mlp.MLP {
+                    nvis: *nvis,
+                    layers: [
+                        !obj:pylearn2.models.mlp.RectifiedLinear {
+                            layer_name: 'h_e',
+                            dim: 10,
+                            irange: .01,
+                        },
+                    ],
+                },
+            },
+        },
+        algorithm: !obj:pylearn2.training_algorithms.sgd.SGD {
+            batch_size: 100,
+            learning_rate: 1e-3,
+            monitoring_dataset: {
+                'train' : *train,
+            },
+            cost: !obj:pylearn2.costs.vae.ImportanceSamplingCriterion {
+                num_samples: 2,
+            },
+            termination_criterion: !obj:pylearn2.termination_criteria.EpochCounter {
+                max_epochs: 2
+            },
+        },
+    }
+    """
+    train_object = yaml_parse.load(yaml_string)
+    train_object.main_loop()
