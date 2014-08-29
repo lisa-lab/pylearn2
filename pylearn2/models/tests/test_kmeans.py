a643e97fcadd2e53beacedad4d3021cad007f9e6
Madison May <madison.may@students.olin.edu>
2014-08-29 19:03:29 -0400

    self.algorithm.continue_learning --> self.model.continue_learning

---
+++ pylearn2/models/tests/test_kmeans.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+from pylearn2.models.kmeans import KMeans
+from pylearn2.train import Train
+
+
+def test_kmeans():
+    X = np.random.random(size=(100, 10))
+    Y = np.random.randint(5, size=(100, 1))
+
+    dataset = DenseDesignMatrix(X, y=Y)
+
+    model = KMeans(
+        k=5,
+        nvis=10
+    )
+
+    train = Train(model=model, dataset=dataset)
+    train.main_loop()
