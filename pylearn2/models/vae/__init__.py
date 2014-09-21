636196058005a4ce74887a300a7e43c32ec31e92
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-09-21 11:37:08 -0400

    Add interface to VAE's inner components

--- pylearn2/models/vae/__init__.py
+++ pylearn2/models/vae/__init__.py
@@ -109,11 +109,11 @@ class VAE(Model):
 
         X = data
         epsilon_shape = (1, X.shape[0], self.nhid)
-        epsilon = self.latent.sample_from_epsilon(shape=epsilon_shape)
-        phi = self.latent.encode_phi(X)
-        z = self.latent.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
+        epsilon = self.sample_from_epsilon(shape=epsilon_shape)
+        phi = self.encode_phi(X)
+        z = self.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
         z = z.reshape((epsilon.shape[0] * epsilon.shape[1], epsilon.shape[2]))
-        theta = self.visible.decode_theta(z)
+        theta = self.decode_theta(z)
 
         latent_channels = self.latent.monitoring_channels_from_phi(phi)
         safe_update(rval, latent_channels)
@@ -171,15 +171,14 @@ class VAE(Model):
             Samples, and optionally conditional expectations
         """
         # Sample from p(z)
-        z = self.latent.sample_from_p_z(num_samples=num_samples, **kwargs)
+        z = self.sample_from_p_z(num_samples=num_samples, **kwargs)
         # Decode theta
-        theta = self.visible.decode_theta(z)
+        theta = self.decode_theta(z)
         # Sample from p(x | z)
-        X = self.visible.sample_from_p_x_given_z(num_samples=num_samples,
-                                                 theta=theta)
+        X = self.sample_from_p_x_given_z(num_samples=num_samples, theta=theta)
 
         if return_sample_means:
-            return (X, self.visible.means_from_theta(theta))
+            return (X, self.means_from_theta(theta))
         else:
             return X
 
@@ -206,21 +205,21 @@ class VAE(Model):
         # Sample noise
         # TODO: For now this covers our use cases, but we need something more
         # robust for the future.
-        epsilon = self.latent.sample_from_epsilon((X.shape[0], self.nhid))
+        epsilon = self.sample_from_epsilon((X.shape[0], self.nhid))
         if not noisy_encoding:
             epsilon *= 0
         # Encode q(z | x) parameters
-        phi = self.latent.encode_phi(X)
+        phi = self.encode_phi(X)
         # Compute z
-        z = self.latent.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
+        z = self.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
         # Compute expectation term
-        theta = self.visible.decode_theta(z)
-        reconstructed_X = self.visible.sample_from_p_x_given_z(
+        theta = self.decode_theta(z)
+        reconstructed_X = self.sample_from_p_x_given_z(
             num_samples=X.shape[0],
             theta=theta
         )
         if return_sample_means:
-            return (reconstructed_X, self.visible.means_from_theta(theta))
+            return (reconstructed_X, self.means_from_theta(theta))
         else:
             return reconstructed_X
 
@@ -240,26 +239,26 @@ class VAE(Model):
         """
         # Sample noise
         epsilon_shape = (num_samples, X.shape[0], self.nhid)
-        epsilon = self.latent.sample_from_epsilon(shape=epsilon_shape)
+        epsilon = self.sample_from_epsilon(shape=epsilon_shape)
         # Encode q(z | x) parameters
-        phi = self.latent.encode_phi(X)
+        phi = self.encode_phi(X)
         # Compute z
-        z = self.latent.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
+        z = self.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
         # Compute KL divergence term
-        kl_divergence_term = self.latent.kl_divergence_term(phi=phi,
-                                                            approximate=False,
-                                                            epsilon=epsilon)
+        kl_divergence_term = self.kl_divergence_term(phi=phi,
+                                                     approximate=False,
+                                                     epsilon=epsilon)
         # Compute expectation term
         # (z is flattened out in order to be MLP-compatible, and the parameters
         #  output by the decoder network are reshaped to the right shape)
         z = z.reshape((epsilon.shape[0] * epsilon.shape[1], epsilon.shape[2]))
-        theta = self.visible.decode_theta(z)
+        theta = self.decode_theta(z)
         theta = tuple(
             theta_i.reshape((epsilon.shape[0], epsilon.shape[1],
                              theta_i.shape[1]))
             for theta_i in theta
         )
-        expectation_term = self.visible.expectation_term(
+        expectation_term = self.expectation_term(
             X=X.dimshuffle('x', 0, 1),
             theta=theta
         ).mean(axis=0).sum(axis=1)
@@ -283,26 +282,26 @@ class VAE(Model):
         """
         # Sample noise
         epsilon_shape = (num_samples, X.shape[0], self.nhid)
-        epsilon = self.latent.sample_from_epsilon(shape=epsilon_shape)
+        epsilon = self.sample_from_epsilon(shape=epsilon_shape)
         # Encode q(z | x) parameters
-        phi = self.latent.encode_phi(X)
+        phi = self.encode_phi(X)
         # Compute z
-        z = self.latent.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
+        z = self.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
         # Decode p(x | z) parameters
         # (z is flattened out in order to be MLP-compatible, and the parameters
         #  output by the decoder network are reshaped to the right shape)
         flat_z = z.reshape((epsilon.shape[0] * epsilon.shape[1],
                             epsilon.shape[2]))
-        theta = self.visible.decode_theta(flat_z)
+        theta = self.decode_theta(flat_z)
         theta = tuple(
             theta_i.reshape((epsilon.shape[0], epsilon.shape[1],
                              theta_i.shape[1]))
             for theta_i in theta
         )
         # Compute log-probabilities
-        log_q_z_x = self.latent.log_q_z_given_x(z=z, phi=phi)
-        log_p_z = self.latent.log_p_z(z)
-        log_p_x_z = self.visible.log_p_x_given_z(
+        log_q_z_x = self.log_q_z_given_x(z=z, phi=phi)
+        log_p_z = self.log_p_z(z)
+        log_p_x_z = self.log_p_x_given_z(
             X=X.dimshuffle(('x', 0, 1)),
             theta=theta
         )
@@ -311,3 +310,274 @@ class VAE(Model):
             log_p_z + log_p_x_z - log_q_z_x,
             axis=0
         ) - T.log(num_samples)
+
+    def encode_phi(self, X):
+        """
+        Maps input `X` to a tuple of parameters of the
+        :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})` posterior distribution
+
+        Parameters
+        ----------
+        X : tensor_like
+            Input
+
+        Returns
+        -------
+        phi : tuple of tensor_like
+            Tuple of parameters for the posterior distribution
+        """
+        return self.latent.encode_phi(X)
+
+    def decode_theta(self, z):
+        """
+        Maps latent variable `z` to a tuple of parameters of the
+        :math:`p_\\theta(\\mathbf{x} \\mid \\mathbf{z})` distribution
+
+        Parameters
+        ----------
+        z : tensor_like
+            Latent sample
+
+        Returns
+        -------
+        theta : tuple of tensor_like
+            Tuple of parameters for the conditional distribution
+        """
+        return self.visible.decode_theta(z)
+
+    def means_from_theta(self, theta):
+        """
+        Given a tuple of parameters of the
+        :math:`p_\\theta(\\mathbf{x} \\mid \\mathbf{z})` distribution,
+        returns the expected value of `x`.
+
+        Parameters
+        ----------
+        theta : tuple of tensor_like
+            Tuple of parameters for the conditional distribution
+
+        Returns
+        -------
+        means : tensor_like
+            Expected value of `x`
+        """
+        return self.visible.means_from_theta(theta)
+
+    def expectation_term(self, X, theta):
+        """
+        Computes an approximation of :math:`\\mathrm{E}_{q_\\phi(\\mathbf{z}
+        \\mid \\mathbf{x})} [\\log p_\\theta(\\mathbf{x} \\mid \\mathbf{z})]`
+
+        Parameters
+        ----------
+        X : tensor_like
+            Input
+        theta : tuple of tensor_like
+            Tuple of parameters for the conditional distribution
+
+        Returns
+        -------
+        expectation_term : tensor_like
+            Expectation term
+        """
+        return self.visible.expectation_term(X, theta)
+
+    def kl_divergence_term(self, phi, approximate=False, epsilon=None,
+                           **kwargs):
+        """
+        Computes the KL-divergence term of the VAE criterion.
+
+        Parameters
+        ----------
+        X : tensor_like
+            Input
+        per_component : bool, optional
+            If the prior/posterior combination leads to a KL that's a sum of
+            individual KLs across latent dimensions, the user can request the
+            vector of indivitual KLs instead of the sum by setting this to
+            `True`. Defaults to `False`.
+        """
+        return self.latent.kl_divergence_term(phi, approximate, epsilon,
+                                              **kwargs)
+
+    def per_component_kl_divergence_term(self, phi, **kwargs):
+        """
+        If the prior/posterior combination allows it, analytically computes the
+        per-latent-dimension KL divergences between the prior distribution
+        :math:`p_\\theta(\\mathbf{z})` and :math:`q_\\phi(\\mathbf{z} \\mid
+        \\mathbf{x})`
+
+        Parameters
+        ----------
+        phi : tuple of tensor_like
+            Tuple of parameters for the posterior distribution
+
+        Returns
+        -------
+        kl_divergence_term : tensor_like
+            Per-component KL-divergence terms
+        """
+        return self.latent.per_component_kl_divergence_term(phi, **kwargs)
+
+    def sample_from_p_x_given_z(self, num_samples, theta):
+        """
+        Given a tuple of parameters, samples from the
+        :math:`p_\\theta(\\mathbf{x} \\mid \\mathbf{z})` conditional
+        distribution
+
+        Parameters
+        ----------
+        num_samples : int
+            Number of samples
+        theta : tuple of tensor_like
+            Tuple of parameters for the conditional distribution
+
+        Returns
+        -------
+        x : tensor_like
+            Samples
+        """
+        return self.visible.sample_from_p_x_given_z(num_samples, theta)
+
+    def sample_from_p_z(self, num_samples, **kwargs):
+        """
+        Samples from the prior distribution :math:`p_\\theta(\\mathbf{z})`
+
+        Parameters
+        ----------
+        num_samples : int
+            Number of samples
+
+        Returns
+        -------
+        z : tensor_like
+            Sample from the prior distribution
+        """
+        return self.latent.sample_from_p_z(num_samples, **kwargs)
+
+    def sample_from_q_z_given_x(self, epsilon, phi):
+        """
+        Given a tuple of parameters and an epsilon noise sample, generates
+        samples from the :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})`
+        posterior distribution using the reparametrization trick
+
+        Parameters
+        ----------
+        epsilon : tensor_like
+            Noise sample
+        phi : tuple of tensor_like
+            Tuple of parameters for the posterior distribution
+
+        Returns
+        -------
+        z : tensor_like
+            Posterior sample
+        """
+        return self.latent.sample_from_q_z_given_x(epsilon, phi)
+
+    def sample_from_epsilon(self, shape):
+        """
+        Samples from a canonical noise distribution from which posterior
+        samples will be drawn using the reparametrization trick (see
+        `_sample_from_q_z_given_x`)
+
+        Parameters
+        ----------
+        shape : tuple of int
+            Shape of the requested samples
+
+        Returns
+        -------
+        epsilon : tensor_like
+            Noise samples
+        """
+        return self.latent.sample_from_epsilon(shape)
+
+    def log_p_x_given_z(self, X, theta):
+        """
+        Computes the log-conditional probabilities of `X`
+
+        Parameters
+        ----------
+        X : tensor_like
+            Input
+        theta : tuple of tensor_like
+            Tuple of parameters for the contitional distribution
+
+        Returns
+        -------
+        log_p_x_z : tensor_like
+            Log-prior probabilities
+        """
+        return self.visible.log_p_x_given_z(X, theta)
+
+    def kl_divergence_term(self, phi, approximate=False, epsilon=None,
+                           **kwargs):
+        """
+        Computes the KL-divergence term of the VAE criterion.
+
+        Parameters
+        ----------
+        X : tensor_like
+            Input
+        per_component : bool, optional
+            If the prior/posterior combination leads to a KL that's a sum of
+            individual KLs across latent dimensions, the user can request the
+            vector of indivitual KLs instead of the sum by setting this to
+            `True`. Defaults to `False`.
+        """
+        return self.latent.kl_divergence_term(phi, approximate, epsilon,
+                                              **kwargs)
+
+    def per_component_kl_divergence_term(self, phi, **kwargs):
+        """
+        If the prior/posterior combination allows it, analytically computes the
+        per-latent-dimension KL divergences between the prior distribution
+        :math:`p_\\theta(\\mathbf{z})` and :math:`q_\\phi(\\mathbf{z} \\mid
+        \\mathbf{x})`
+
+        Parameters
+        ----------
+        phi : tuple of tensor_like
+            Tuple of parameters for the posterior distribution
+
+        Returns
+        -------
+        kl_divergence_term : tensor_like
+            Per-component KL-divergence terms
+        """
+        return self.latent.per_component_kl_divergence_term(phi, **kwargs)
+
+    def log_q_z_given_x(self, z, phi):
+        """
+        Computes the log-posterior probabilities of `z`
+
+        Parameters
+        ----------
+        z : tensor_like
+            Posterior samples
+        phi : tuple of tensor_like
+            Tuple of parameters for the posterior distribution
+
+        Returns
+        -------
+        log_q_z_x : tensor_like
+            Log-posterior probabilities
+        """
+        return self.latent.log_q_z_given_x(z, phi)
+
+    def log_p_z(self, z):
+        """
+        Computes the log-prior probabilities of `z`
+
+        Parameters
+        ----------
+        z : tensor_like
+            Posterior samples
+
+        Returns
+        -------
+        log_p_z : tensor_like
+            Log-prior probabilities
+        """
+        return self.latent.log_p_z(z)
