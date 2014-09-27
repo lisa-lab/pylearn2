be26531e8babaffb27e37ced3da059046843a32a
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-09-27 09:46:01 -0400

    Clean up BinarizedMNIST

--- pylearn2/datasets/binarized_mnist.py
+++ pylearn2/datasets/binarized_mnist.py
@@ -1,5 +1,5 @@
 """
-Binarized, unlabeled version of the MNIST dataset, as referenced in
+Binarized, unlabeled version of the MNIST dataset, used in
 
     On the Quantitative Analysis of Deep Belief Networks
     Salakhutdinov and Murray
@@ -20,32 +20,41 @@ from pylearn2.datasets.dense_design_matrix import (
     DenseDesignMatrix,
     DefaultViewConverter
 )
-from pylearn2.datasets import control
-from pylearn2.datasets import cache
+from pylearn2.datasets import control, cache
+from pylearn2.datasets.exp import NotInstalledError
 from pylearn2.utils import serial
 from pylearn2.utils.rng import make_np_rng
 
 
 class BinarizedMNIST(DenseDesignMatrix):
     """
+    Binarized, unlabeled version of the MNIST dataset
+
     Parameters
     ----------
     which_set : str
-        Must be in ['train', 'valid', 'test']
-    center : WRITEME
-    shuffle : WRITEME
-    one_hot : WRITEME
-    binarize : WRITEME
-    start : WRITEME
-    stop : WRITEME
-    axes : WRITEME
-    preprocessor : WRITEME
-    fit_preprocessor : WRITEME
-    fit_test_preprocessor : WRITEME
+        Which subset of the dataset. Must be in ['train', 'valid', 'test']
+    shuffle : bool, optional
+        Whether to shuffle the dataset. Defaults to `False`.
+    start : int, optional
+        Defaults to `None`. If set, excludes examples whose index is inferior
+        to `start`.
+    stop : int, optional
+        Defaults to `None`. If set, excludes examples whose index is superior
+        to `stop`.
+    axes : permutation of ['b', 0, 1, 'c'], optional
+        Desired axes ordering of the topological view of the data. Defaults to
+        ['b', 0, 1, 'c'].
+    preprocessor : pylearn2.datasets.preprocessing.Preprocessor, optional
+        Preprocessing to apply to the data. Defaults to `None`.
+    fit_preprocessor : bool, optional
+        Whether to fit the preprocessor to the data
+    fit_test_preprocessor : bool, optional
+        Whether to fit the preprocessor to the test data
     """
 
-    def __init__(self, which_set, center=False, shuffle=False, one_hot=None,
-                 binarize=False, start=None, stop=None, axes=['b', 0, 1, 'c'],
+    def __init__(self, which_set, shuffle=False,
+                 start=None, stop=None, axes=['b', 0, 1, 'c'],
                  preprocessor=None, fit_preprocessor=False,
                  fit_test_preprocessor=False):
         self.args = locals()
@@ -68,13 +77,20 @@ class BinarizedMNIST(DenseDesignMatrix):
             datasetCache = cache.datasetCache
             im_path = datasetCache.cache_file(im_path)
 
-            X = serial.load(im_path)
+            try:
+                X = serial.load(im_path)
+            except IOError:
+                raise NotInstalledError("BinarizedMNIST data files cannot be "
+                                        "found in ${PYLEARN2_DATA_PATH}. Run "
+                                        "pylearn2/scripts/datasets/"
+                                        "download_binarized_mnist.py to get "
+                                        "the data")
         else:
             if which_set == 'train':
                 size = 50000
             else:
                 size = 10000
-            X = numpy.random.rand(size, 28 ** 2)
+            X = numpy.random.binomial(n=1, p=0.5, size=(size, 28 ** 2))
 
         m, d = X.shape
         assert d == 28 ** 2
@@ -82,9 +98,6 @@ class BinarizedMNIST(DenseDesignMatrix):
             assert m == 50000
         else:
             assert m == 10000
-
-        if center:
-            X -= X.mean(axis=0)
 
         if shuffle:
             self.shuffle_rng = make_np_rng(None, [1, 2, 3],
@@ -123,25 +136,21 @@ class BinarizedMNIST(DenseDesignMatrix):
 
     def adjust_for_viewer(self, X):
         """
-        .. todo::
-
-            WRITEME
+        Adjusts the data to be compatible with a viewer that expects values to
+        be in [-1, 1].
         """
         return numpy.clip(X * 2. - 1., -1., 1.)
 
     def adjust_to_be_viewed_with(self, X, other, per_example=False):
         """
-        .. todo::
-
-            WRITEME
+        Adjusts the data to be compatible with a viewer that expects values to
+        be in [-1, 1].
         """
         return self.adjust_for_viewer(X)
 
     def get_test_set(self):
         """
-        .. todo::
-
-            WRITEME
+        Returns the test set corresponding to this BinarizedMNIST instance
         """
         args = {}
         args.update(self.args)
