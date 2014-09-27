15b5b771e34913e626818cdc48036cf56842b686
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-09-27 14:39:37 -0400

    Adapt VAE to the new prior/conditional/posterior interface and factor out KL computing

--- pylearn2/models/vae/__init__.py
+++ pylearn2/models/vae/__init__.py
@@ -3,14 +3,15 @@ Variational autoencoder (VAE) implementation, as described in
 
     Kingma, D. and Welling, M. Auto-Encoding Variational Bayes
 
-`VAE` expects to receive two objects to do its job properly:
+`VAE` expects to receive three objects to do its job properly:
 
-1. An instance of `Visible` (`pylearn2.models.vae.visible` module), which
-   handles methods related to visible space, like its conditional distribution
+1. An instance of `Prior` (`pylearn2.models.vae.prior` module), which handles
+   methods related to the prior distribution :math:`p_\\theta(\\mathbf{z})`.
+2. An instance of `Conditional` (`pylearn2.models.vae.conditional` module),
+   which handles methods related to the conditional distribution
    :math:`p_\\theta(\\mathbf{x} \\mid \\mathbf{z})`.
-2. An instance of `Latent` (`pylearn2.models.vae.latent` module), which handles
-   methods related to latent space, such as its prior distribution
-   :math:`p_\\theta(\\mathbf{z})` and its posterior distribution
+1. An instance of `Posterior` (`pylearn2.models.vae.posterior` module), which
+   handles methods related to the posterior distribution
    :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})`.
 
 For an example on how to use the VAE framework, see
@@ -50,18 +51,26 @@ class VAE(Model):
     ----------
     nvis : int
         Number of dimensions in the input data
-    visible : pylearn2.models.vae.visible.Visible
-        Handles the visible space-related methods necessary for `VAE` to work
-    latent : pylearn2.models.vae.latent.Latent
-        Handles the latent space-related methods necessary for `VAE` to work
+    prior : pylearn2.models.vae.prior.Prior
+        Handles the prior-related methods necessary for `VAE` to work
+    conditional : pylearn2.models.vae.conditional.Conditional
+        Handles the conditional-related methods necessary for `VAE` to work
+    posterior : pylearn2.models.vae.posterior.Posterior
+        Handles the posterior-related methods necessary for `VAE` to work
     nhid : int
         Number of dimensions in latent space, i.e. the space in which :math:`z`
         lives
+    kl_integrator : pylearn2.models.vae.kl.KLIntegrator, optional
+        Object providing methods for computing KL-related quantities. Defaults
+        to `None`, in which case the approximate KL is computed instead.
+    batch_size : int, optional
+        Sometimes required for some MLPs representing encoding/decoding models.
+        Defaults to `None`.
     seed : int or list of int
         Seed for the VAE's numpy RNG used by its subcomponents
     """
-    def __init__(self, nvis, visible, latent, nhid, batch_size=None,
-                 seed=None):
+    def __init__(self, nvis, prior, conditional, posterior, nhid,
+                 kl_integrator=None, batch_size=None, seed=None):
         super(VAE, self).__init__()
 
         self.__dict__.update(locals())
@@ -70,8 +79,9 @@ class VAE(Model):
         self.rng = make_np_rng(self.seed, default_seed,
                                ['uniform', 'randint', 'randn'])
 
-        self.visible.set_vae(self)
-        self.latent.set_vae(self)
+        self.prior.set_vae(self)
+        self.conditional.set_vae(self)
+        self.posterior.set_vae(self)
 
         # Space initialization
         self.input_space = VectorSpace(dim=self.nvis)
@@ -79,18 +89,17 @@ class VAE(Model):
         self.latent_space = VectorSpace(dim=self.nhid)
 
         # Parameter initialization
-        self.visible.initialize_parameters(
+        self.prior.initialize_parameters(nhid=self.nhid)
+        self.conditional.initialize_parameters(
             decoder_input_space=self.latent_space,
-            nvis=self.nvis
+            nvis=self.vis
         )
-        self.latent.initialize_parameters(
+        self.posterior.initialize_parameters(
             encoder_input_space=self.input_space,
             nhid=self.nhid
         )
-        self._params = (
-            self.get_prior_params() + self.get_posterior_params() +
-            self.get_conditional_params()
-        )
+        self._params = (self.get_prior_params() + self.get_posterior_params() +
+                        self.get_conditional_params())
         names = []
         for param in self._params:
             if param.name not in names:
@@ -109,7 +118,7 @@ class VAE(Model):
         Notes
         -----
         Monitors quantities related to the approximate posterior parameters phi
-        and the distributions parameters theta.
+        and the conditional parameters theta.
         """
         space, source = self.get_monitoring_data_specs()
         space.validate(data)
@@ -124,50 +133,51 @@ class VAE(Model):
         z = z.reshape((epsilon.shape[0] * epsilon.shape[1], epsilon.shape[2]))
         theta = self.decode_theta(z)
 
-        latent_channels = self.latent.monitoring_channels_from_phi(phi)
-        safe_update(rval, latent_channels)
+        posterior_channels = self.posterior.monitoring_channels_from_phi(phi)
+        safe_update(rval, posterior_channels)
 
-        visible_channels = self.visible.monitoring_channels_from_theta(theta)
-        safe_update(rval, visible_channels)
+        conditional_channels = \
+            self.conditional.monitoring_channels_from_theta(theta)
+        safe_update(rval, conditional_channels)
 
         return rval
 
     @wraps(Model.get_lr_scalers)
     def get_lr_scalers(self):
-        rval = self.visible.get_lr_scalers()
-        safe_update(rval, self.latent.get_lr_scalers())
+        rval = self.prior.get_lr_scalers()
+        safe_update(rval, self.conditional.get_lr_scalers())
+        safe_update(rval, self.posterior.get_lr_scalers())
         return rval
 
     @wraps(Model._modify_updates)
     def _modify_updates(self, updates):
-        self.visible.modify_updates(updates)
-        self.latent.modify_updates(updates)
+        self.prior.modify_updates(updates)
+        self.conditional.modify_updates(updates)
+        self.posterior.modify_updates(updates)
 
     @wraps(Model.get_weights)
     def get_weights(self):
         # TODO: This choice is arbitrary. It's something that's useful to
         # visualize, but is it the most intuitive choice?
-        return self.visible.get_weights()
+        return self.conditional.get_weights()
 
     def get_prior_params(self):
         """
         Returns the model's prior distribution parameters
         """
-        return self.latent.get_prior_params()
-
-    def get_posterior_params(self):
-        """
-        Returns the model's posterior distribution parameters (i.e. parameters
-        of the encoding model)
-        """
-        return self.latent.get_posterior_params()
+        return self.prior.get_params()
 
     def get_conditional_params(self):
         """
-        Returns the model's conditional distribution parameters (i.e.
-        parameters of the decoding model)
+        Returns the model's conditional distribution parameters
         """
-        return self.visible.get_conditional_params()
+        return self.conditional.get_params()
+
+    def get_posterior_params(self):
+        """
+        Returns the model's posterior distribution parameters
+        """
+        return self.posterior.get_params()
 
     def sample(self, num_samples, return_sample_means=True, **kwargs):
         """
@@ -242,7 +252,7 @@ class VAE(Model):
         else:
             return reconstructed_X
 
-    def log_likelihood_lower_bound(self, X, num_samples):
+    def log_likelihood_lower_bound(self, X, num_samples, approximate_KL=False):
         """
         Computes the VAE lower-bound on the marginal log-likelihood of X.
 
@@ -253,6 +263,9 @@ class VAE(Model):
         num_samples : int
             Number of posterior samples per data point, e.g. number of times z
             is sampled for each x.
+        approximate_kl : bool, optional
+            Whether to compute a stochastic approximation of the KL divergence
+            term. Defaults to `False`.
 
         Returns
         -------
@@ -264,12 +277,17 @@ class VAE(Model):
         epsilon = self.sample_from_epsilon(shape=epsilon_shape)
         # Encode q(z | x) parameters
         phi = self.encode_phi(X)
+        # Get prior parameters
+        prior_theta = self.get_prior_theta()
         # Compute z
         z = self.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
         # Compute KL divergence term
-        kl_divergence_term = self.kl_divergence_term(phi=phi,
-                                                     approximate=False,
-                                                     epsilon=epsilon)
+        kl_divergence_term = self.kl_divergence_term(
+            phi=phi,
+            theta=prior_theta,
+            approximate=approximate_kl,
+            epsilon=epsilon
+        )
         # Compute expectation term
         # (z is flattened out in order to be MLP-compatible, and the parameters
         #  output by the decoder network are reshaped to the right shape)
@@ -283,7 +301,7 @@ class VAE(Model):
         expectation_term = self.expectation_term(
             X=X.dimshuffle('x', 0, 1),
             theta=theta
-        ).mean(axis=0).sum(axis=1)
+        ).mean(axis=0)
 
         return -kl_divergence_term + expectation_term
 
@@ -351,7 +369,7 @@ class VAE(Model):
         phi : tuple of tensor_like
             Tuple of parameters for the posterior distribution
         """
-        return self.latent.encode_phi(X)
+        return self.posterior.encode_phi(X)
 
     def decode_theta(self, z):
         """
@@ -368,7 +386,14 @@ class VAE(Model):
         theta : tuple of tensor_like
             Tuple of parameters for the conditional distribution
         """
-        return self.visible.decode_theta(z)
+        return self.conditional.decode_theta(z)
+
+    def get_prior_theta(self):
+        """
+        Returns parameters of the prior distribution
+        :math:`p_\\theta(\\mathbf{z})`
+        """
+        return self.prior.get_theta()
 
     def means_from_theta(self, theta):
         """
@@ -386,7 +411,7 @@ class VAE(Model):
         means : tensor_like
             Expected value of `x`
         """
-        return self.visible.means_from_theta(theta)
+        return self.conditional.means_from_theta(theta)
 
     def expectation_term(self, X, theta):
         """
@@ -405,27 +430,61 @@ class VAE(Model):
         expectation_term : tensor_like
             Expectation term
         """
-        return self.visible.expectation_term(X, theta)
+        return self.log_p_x_given_z(X, theta)
 
-    def kl_divergence_term(self, phi, approximate=False, epsilon=None,
-                           **kwargs):
+    def kl_divergence_term(self, phi, theta, approximate=False, epsilon=None):
         """
         Computes the KL-divergence term of the VAE criterion.
 
         Parameters
         ----------
-        X : tensor_like
-            Input
-        per_component : bool, optional
-            If the prior/posterior combination leads to a KL that's a sum of
-            individual KLs across latent dimensions, the user can request the
-            vector of indivitual KLs instead of the sum by setting this to
-            `True`. Defaults to `False`.
+        phi : tuple of tensor_like
+            Parameters of the distribution
+            :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})`
+        theta : tuple of tensor_like
+            Parameters of the distribution :math:`p_\\theta(\\mathbf{z})`
+        approximate_kl : bool, optional
+            Whether to compute a stochastic approximation of the KL divergence
+            term. Defaults to `False`.
+        epsilon : tensor_like, optional
+            Noise samples used to compute the approximate KL term. Defaults to
+            `None`.
         """
-        return self.latent.kl_divergence_term(phi, approximate, epsilon,
-                                              **kwargs)
+        if self.kl_integrator is None:
+            warnings.warn("computing the analytical KL divergence term is not "
+                          "supported for this prior/posterior combination, "
+                          "computing a stochastic approximate KL instead")
+            return self._approximate_kl_divergence_term(phi, epsilon)
+        if approximate:
+            return self._approximate_kl_divergence_term(phi, epsilon)
+        else:
+            return self.kl_integrator.kl_divergence(
+                phi=phi,
+                theta=theta,
+                prior=self.prior,
+                posterior=self.posterior
+            )
 
-    def per_component_kl_divergence_term(self, phi, **kwargs):
+    def _approximate_kl_divergence_term(self, phi, epsilon):
+        """
+        Returns a Monte Carlo approximation of the KL divergence term.
+
+        Parameters
+        ----------
+        phi : tuple of tensor_like
+            Tuple of parameters for the posterior distribution
+        epsilon : tensor_like
+            Noise term from which z is computed
+        """
+        if epsilon is None:
+            raise ValueError("stochastic KL is requested but no epsilon is "
+                             "given")
+        z = self.sample_from_q_z_given_x(epsilon=epsilon, phi=phi)
+        log_q_z_x = self.log_q_z_given_x(z=z, phi=phi)
+        log_p_z = self.log_p_z(z)
+        return (log_q_z_x - log_p_z).mean(axis=0)
+
+    def per_component_kl_divergence_term(self, phi, theta):
         """
         If the prior/posterior combination allows it, analytically computes the
         per-latent-dimension KL divergences between the prior distribution
@@ -435,14 +494,21 @@ class VAE(Model):
         Parameters
         ----------
         phi : tuple of tensor_like
-            Tuple of parameters for the posterior distribution
-
-        Returns
-        -------
-        kl_divergence_term : tensor_like
-            Per-component KL-divergence terms
+            Parameters of the distribution
+            :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})`
+        theta : tuple of tensor_like
+            Parameters of the distribution :math:`p_\\theta(\\mathbf{z})`
         """
-        return self.latent.per_component_kl_divergence_term(phi, **kwargs)
+        if self.kl_integrator is None:
+            raise NotImplementedError("impossible to compute the analytical "
+                                      "KL divergence")
+        else:
+            return self.kl_integrator.per_component_kl_divergence_term(
+                phi=phi,
+                theta=theta,
+                prior=self.prior,
+                posterior=self.posterior
+            )
 
     def sample_from_p_x_given_z(self, num_samples, theta):
         """
@@ -462,7 +528,7 @@ class VAE(Model):
         x : tensor_like
             Samples
         """
-        return self.visible.sample_from_p_x_given_z(num_samples, theta)
+        return self.conditional.sample_from_p_x_given_z(num_samples, theta)
 
     def sample_from_p_z(self, num_samples, **kwargs):
         """
@@ -478,7 +544,7 @@ class VAE(Model):
         z : tensor_like
             Sample from the prior distribution
         """
-        return self.latent.sample_from_p_z(num_samples, **kwargs)
+        return self.prior.sample_from_p_z(num_samples, **kwargs)
 
     def sample_from_q_z_given_x(self, epsilon, phi):
         """
@@ -498,7 +564,7 @@ class VAE(Model):
         z : tensor_like
             Posterior sample
         """
-        return self.latent.sample_from_q_z_given_x(epsilon, phi)
+        return self.posterior.sample_from_q_z_given_x(epsilon, phi)
 
     def sample_from_epsilon(self, shape):
         """
@@ -516,7 +582,23 @@ class VAE(Model):
         epsilon : tensor_like
             Noise samples
         """
-        return self.latent.sample_from_epsilon(shape)
+        return self.posterior.sample_from_epsilon(shape)
+
+    def log_p_z(self, z):
+        """
+        Computes the log-prior probabilities of `z`
+
+        Parameters
+        ----------
+        z : tensor_like
+            Posterior samples
+
+        Returns
+        -------
+        log_p_z : tensor_like
+            Log-prior probabilities
+        """
+        return self.prior.log_p_z(z)
 
     def log_p_x_given_z(self, X, theta):
         """
@@ -534,44 +616,7 @@ class VAE(Model):
         log_p_x_z : tensor_like
             Log-prior probabilities
         """
-        return self.visible.log_p_x_given_z(X, theta)
-
-    def kl_divergence_term(self, phi, approximate=False, epsilon=None,
-                           **kwargs):
-        """
-        Computes the KL-divergence term of the VAE criterion.
-
-        Parameters
-        ----------
-        X : tensor_like
-            Input
-        per_component : bool, optional
-            If the prior/posterior combination leads to a KL that's a sum of
-            individual KLs across latent dimensions, the user can request the
-            vector of indivitual KLs instead of the sum by setting this to
-            `True`. Defaults to `False`.
-        """
-        return self.latent.kl_divergence_term(phi, approximate, epsilon,
-                                              **kwargs)
-
-    def per_component_kl_divergence_term(self, phi, **kwargs):
-        """
-        If the prior/posterior combination allows it, analytically computes the
-        per-latent-dimension KL divergences between the prior distribution
-        :math:`p_\\theta(\\mathbf{z})` and :math:`q_\\phi(\\mathbf{z} \\mid
-        \\mathbf{x})`
-
-        Parameters
-        ----------
-        phi : tuple of tensor_like
-            Tuple of parameters for the posterior distribution
-
-        Returns
-        -------
-        kl_divergence_term : tensor_like
-            Per-component KL-divergence terms
-        """
-        return self.latent.per_component_kl_divergence_term(phi, **kwargs)
+        return self.conditional.log_p_x_given_z(X, theta)
 
     def log_q_z_given_x(self, z, phi):
         """
@@ -589,20 +634,4 @@ class VAE(Model):
         log_q_z_x : tensor_like
             Log-posterior probabilities
         """
-        return self.latent.log_q_z_given_x(z, phi)
-
-    def log_p_z(self, z):
-        """
-        Computes the log-prior probabilities of `z`
-
-        Parameters
-        ----------
-        z : tensor_like
-            Posterior samples
-
-        Returns
-        -------
-        log_p_z : tensor_like
-            Log-prior probabilities
-        """
-        return self.latent.log_p_z(z)
+        return self.posterior.log_q_z_given_x(z, phi)
---
+++ pylearn2/models/vae/kl.py
@@ -0,0 +1,109 @@
+"""
+Classes implementing logic related to the analytical computation of the KL
+divergence between :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})` and
+:math:`p_\\theta(\\mathbf{z})` in the VAE framework
+"""
+__authors__ = "Vincent Dumoulin"
+__copyright__ = "Copyright 2014, Universite de Montreal"
+__credits__ = ["Vincent Dumoulin"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Vincent Dumoulin"
+__email__ = "pylearn-dev@googlegroups"
+
+from pylearn2.utils import wraps
+from pylearn2.models.vae import prior, posterior
+
+
+class KLIntegrator(object):
+    """
+    Class responsible for computing the analytical KL divergence term in the
+    VAE criterion
+    """
+    prior_class = None
+    posterior_class = None
+
+    def kl_divergence(self, phi, theta, prior, posterior):
+        """
+        Computes the KL-divergence term of the VAE criterion.
+
+        Parameters
+        ----------
+        phi : tuple of tensor_like
+            Parameters of the distribution
+            :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})`
+        theta : tuple of tensor_like
+            Parameters of the distribution :math:`p_\\theta(\\mathbf{z})`
+        """
+        raise NotImplementedError(str(self.__class__) + " does not " +
+                                  "implement kl_divergence")
+
+    def per_component_kl_divergence(self, phi, theta, prior, posterior):
+        """
+        If the prior/posterior combination allows it, computes the
+        per-component KL divergence term
+
+        Parameters
+        ----------
+        phi : tuple of tensor_like
+            Parameters of the distribution
+            :math:`q_\\phi(\\mathbf{z} \\mid \\mathbf{x})`
+        theta : tuple of tensor_like
+            Parameters of the distribution :math:`p_\\theta(\\mathbf{z})`
+        """
+        raise NotImplementedError(str(self.__class__) + " does not " +
+                                  "implement per_component_kl_divergence")
+
+    def _validate_prior_posterior(self, prior, posterior):
+        """
+        Checks that the prior/posterior combination is what the integrator
+        expects and raises an exception otherwise
+
+        Parameters
+        ----------
+        prior : pylearn2.models.vae.prior.Prior
+            Prior distribution on z
+        posterior : pylearn2.models.vae.posterior.Posterior
+            Posterior distribution on z given x
+        """
+        if self.prior is None or self.posterior is None:
+            raise NotImplementedError(str(self.__class__) + " has not set " +
+                                      "the required 'prior_class' and " +
+                                      "'posterior_class' class attributes")
+        if not isinstance(prior, self.prior_class):
+            raise ValueError("prior class " + str(prior.__class__) + " is " +
+                             "incompatible with expected prior class " +
+                             str(self.prior_class.__class__))
+        if not isinstance(posterior, self.posterior_class):
+            raise ValueError("posterior class " + str(posterior.__class__) +
+                             " is incompatible with expected posterior " +
+                             "class " + str(self.prior_class.__class__))
+
+
+class DiagonalGaussianPriorPosteriorKL(KLIntegrator):
+    """
+    Computes the analytical KL between a diagonal gaussian prior and a diagonal
+    gaussian posterior
+    """
+    prior_class = prior.DiagonalGaussianPrior
+    posterior_class = posterior.DiagonalGaussianPosterior
+
+    @wraps(KLIntegrator.kl_divergence)
+    def kl_divergence(self, phi, theta, prior, posterior):
+        return self.per_component_kl_divergence(
+            phi=phi,
+            theta=theta,
+            prior=prior,
+            posterior=posterior
+        ).sum(axis=1)
+
+    @wraps(KLIntegrator.per_component_kl_divergence)
+    def per_component_kl_divergence(self, phi, theta, prior, posterior):
+        self._validate_prior_posterior(prior, posterior)
+        (posterior_mu, posterior_log_sigma) = phi
+        (prior_mu, prior_log_sigma) = theta
+        return (
+            prior_log_sigma - posterior_log_sigma +
+            0.5 * (T.exp(2 * posterior_log_sigma) +
+                   (posterior_mu - prior_mu) ** 2) /
+                  T.exp(2 * prior_log_sigma) - 0.5
+        )
