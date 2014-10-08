37131de71827a58b2415d79fc7ed525e80f484e3
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-10-08 10:27:33 -0400

    Monitor KL and expectation terms in VAE criterion cost

--- pylearn2/costs/vae.py
+++ pylearn2/costs/vae.py
@@ -8,6 +8,7 @@ __license__ = "3-clause BSD"
 __maintainer__ = "Vincent Dumoulin"
 __email__ = "pylearn-dev@googlegroups"
 
+from theano.compat.python2x import OrderedDict
 from pylearn2.costs.cost import Cost, DefaultDataSpecsMixin
 from pylearn2.utils import wraps
 
@@ -32,6 +33,19 @@ class VAECriterion(DefaultDataSpecsMixin, Cost):
         space, sources = self.get_data_specs(model)
         space.validate(data)
         return -model.log_likelihood_lower_bound(data, self.num_samples).mean()
+
+    @wraps(Cost.get_monitoring_channels)
+    def get_monitoring_channels(self, model, data, **kwargs):
+        space, sources = self.get_data_specs(model)
+        space.validate(data)
+
+        rval = model.log_likelihood_lower_bound(data, self.num_samples,
+                                                return_individual_terms=True)
+        kl_divergence_term = rval[0].mean()
+        expectation_term = -rval[1].mean()
+
+        return OrderedDict([('kl_divergence_term', kl_divergence_term),
+                            ('expectation_term', expectation_term)])
 
 
 class ImportanceSamplingCriterion(DefaultDataSpecsMixin, Cost):
