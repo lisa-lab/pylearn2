ae20246069d546ccb1ee529b6d69ecaa0d181f80
carlo <carlo.deramo@gmail.com>
2014-10-25 14:16:58 +0200

    correction to fast version of test script

--- pylearn2/scripts/papers/dbm/tests/test_dbm_mnist_fast.py
+++ pylearn2/scripts/papers/dbm/tests/test_dbm_mnist_fast.py
@@ -15,12 +15,6 @@ from pylearn2.testing import no_debug_mode
 from pylearn2.datasets import mnist_augmented
 from theano import function
 
-PRETRAINING = 1
-TRAINING = 1
-FINETUNING = 1
-SOFTMAX = 0
-DROPOUT = 0
-
 @no_debug_mode
 def test_train_example():
 
@@ -34,31 +28,29 @@ def test_train_example():
         # load and train first layer   
         train_yaml_path = os.path.join(train_path, 'dbm_mnist_l1.yaml')
         layer1_yaml = open(train_yaml_path, 'r').read()
-        hyper_params_l1 = {'train_stop': 5000,
-                           'batch_size': 100,
+        hyper_params_l1 = {'train_stop': 20,
+                           'batch_size': 5,
                            'monitoring_batches': 5,
                            'nhid': 100,
                            'max_epochs': 10,
                            'save_path': train_path
                            }
 
-        if PRETRAINING:
+        layer1_yaml = layer1_yaml % (hyper_params_l1)
+        train = yaml_parse.load(layer1_yaml)
 
-            layer1_yaml = layer1_yaml % (hyper_params_l1)
-            train = yaml_parse.load(layer1_yaml)
+        print '\n-----------------------------------'
+        print '     Unsupervised pre-training'
+        print '-----------------------------------\n'
 
-            print '\n-----------------------------------'
-            print '     Unsupervised pre-training'
-            print '-----------------------------------\n'
-
-            print '\nPre-Training first layer...\n'
-            train.main_loop()
+        print '\nPre-Training first layer...\n'
+        train.main_loop()
 
         # load and train second layer
         train_yaml_path = os.path.join(train_path, 'dbm_mnist_l2.yaml')
         layer2_yaml = open(train_yaml_path, 'r').read()
-        hyper_params_l2 = {'train_stop': 1000,
-                           'batch_size': 100,
+        hyper_params_l2 = {'train_stop': 20,
+                           'batch_size': 5,
                            'monitoring_batches': 5,
                            'nvis': hyper_params_l1['nhid'],
                            'nhid': 200,
@@ -66,127 +58,113 @@ def test_train_example():
                            'save_path': train_path
                            }
 
-        if PRETRAINING:
+        layer2_yaml = layer2_yaml % (hyper_params_l2)
+        train = yaml_parse.load(layer2_yaml)
 
-            layer2_yaml = layer2_yaml % (hyper_params_l2)
-            train = yaml_parse.load(layer2_yaml)
+        print '\n...Pre-training second layer...\n'
+        train.main_loop()
 
-            print '\n...Pre-training second layer...\n'
-            train.main_loop()
+        # START TRAINING
+        train_yaml_path = os.path.join(train_path, 'dbm_mnist.yaml')
+        yaml = open(train_yaml_path, 'r').read()
+        hyper_params_dbm = {'train_stop': 20,
+                            'valid_stop': 20,
+                            'batch_size': 5,
+                            'detector_layer_1_dim': hyper_params_l1['nhid'],
+                            'detector_layer_2_dim': hyper_params_l2['nhid'],
+                            'monitoring_batches': 5,
+                            'max_epochs': 5,
+                            'save_path': train_path
+                            }
 
-        if TRAINING:
+        yaml = yaml % (hyper_params_dbm)
+        train = yaml_parse.load(yaml)
 
-            # START TRAINING
-            if SOFTMAX:
-                train_yaml_path = os.path.join(train_path, 'dbm_mnist_softmax.yaml')
-            else:
-                train_yaml_path = os.path.join(train_path, 'dbm_mnist.yaml')
-            yaml = open(train_yaml_path, 'r').read()
-            hyper_params_dbm = {'train_stop': 1000,
-                                'valid_stop': 1000,
-                                'batch_size': 100,
-                                'detector_layer_1_dim': hyper_params_l1['nhid'],
-                                'detector_layer_2_dim': hyper_params_l2['nhid'],
-                                'monitoring_batches': 5,
-                                'max_epochs': 20,
-                                'save_path': train_path
-                                }
+        rbm1 = serial.load(os.path.join(train_path, 'dbm_mnist_l1.pkl'))
+        rbm2 = serial.load(os.path.join(train_path, 'dbm_mnist_l2.pkl'))
+        pretrained_rbms = [rbm1, rbm2]
 
-            yaml = yaml % (hyper_params_dbm)
-            train = yaml_parse.load(yaml)
+        # clamp pretrained weights into respective dbm layers
+        for h, l in zip(train.model.hidden_layers, pretrained_rbms):
+            h.set_weights(l.get_weights())
 
-            rbm1 = serial.load(os.path.join(train_path, 'dbm_mnist_l1.pkl'))
-            rbm2 = serial.load(os.path.join(train_path, 'dbm_mnist_l2.pkl'))
-            pretrained_rbms = [rbm1, rbm2]
+        # clamp pretrained biases into respective dbm layers
+        bias_param = pretrained_rbms[0].get_params()[1]
+        fun = function([], bias_param)
+        cuda_bias = fun()
+        bias = numpy.asarray(cuda_bias)
+        train.model.visible_layer.set_biases(bias)
+        bias_param = pretrained_rbms[-1].get_params()[1]
+        fun = function([], bias_param)
+        cuda_bias = fun()
+        bias = numpy.asarray(cuda_bias)
+        train.model.hidden_layers[0].set_biases(bias)
+        bias_param = pretrained_rbms[-1].get_params()[2]
+        fun = function([], bias_param)
+        cuda_bias = fun()
+        bias = numpy.asarray(cuda_bias)
+        train.model.hidden_layers[1].set_biases(bias)
 
-            # clamp pretrained weights into respective dbm layers
-            for h, l in zip(train.model.hidden_layers, pretrained_rbms):
-                h.set_weights(l.get_weights())
+        print '\nAll layers weights and biases have been clamped to the respective layers of the DBM'
 
-            # clamp pretrained biases into respective dbm layers
-            bias_param = pretrained_rbms[0].get_params()[1]
-            fun = function([], bias_param)
-            cuda_bias = fun()
-            bias = numpy.asarray(cuda_bias)
-            train.model.visible_layer.set_biases(bias)
-            bias_param = pretrained_rbms[-1].get_params()[1]
-            fun = function([], bias_param)
-            cuda_bias = fun()
-            bias = numpy.asarray(cuda_bias)
-            train.model.hidden_layers[0].set_biases(bias)
-            bias_param = pretrained_rbms[-1].get_params()[2]
-            fun = function([], bias_param)
-            cuda_bias = fun()
-            bias = numpy.asarray(cuda_bias)
-            train.model.hidden_layers[1].set_biases(bias)
+        print '\n-----------------------------------'
+        print '     Unsupervised training'
+        print '-----------------------------------\n'
 
-            print '\nAll layers weights and biases have been clamped to the respective layers of the DBM'
+        print '\nTraining phase...'
+        train.main_loop()
 
-            print '\n-----------------------------------'
-            print '     Unsupervised training'
-            print '-----------------------------------\n'
+        # START SUPERVISED TRAINING WITH BACKPROPAGATION
+        print '\n-----------------------------------'
+        print '       Supervised training'
+        print '-----------------------------------'
 
-            print '\nTraining phase...'
-            train.main_loop()
+        # load dbm as a mlp
+        train_yaml_path = os.path.join(train_path, 'dbm_mnist_mlp.yaml')
+        
+        mlp_yaml = open(train_yaml_path, 'r').read()
+        hyper_params_mlp = {'train_stop': 20,
+                            'valid_stop': 20,
+                            'batch_size': 5,
+                            'nvis': 784 + hyper_params_l2['nhid'],
+                            'n_h0': hyper_params_l1['nhid'],
+                            'n_h1': hyper_params_l2['nhid'],
+                            'max_epochs': 20,
+                            'save_path': train_path
+                            }
 
-        if FINETUNING:
+        mlp_yaml = mlp_yaml % (hyper_params_mlp)
+        train = yaml_parse.load(mlp_yaml)
 
-            # START SUPERVISED TRAINING WITH BACKPROPAGATION
-            print '\n-----------------------------------'
-            print '       Supervised training'
-            print '-----------------------------------'
+        dbm = serial.load(os.path.join(train_path, 'dbm_mnist.pkl'))
 
-            # load dbm as a mlp
-            if DROPOUT:
-                train_yaml_path = os.path.join(train_path, 'dbm_mnist_mlp_dropout.yaml')
-            else:
-                train_yaml_path = os.path.join(train_path, 'dbm_mnist_mlp.yaml')
-            mlp_yaml = open(train_yaml_path, 'r').read()
-            hyper_params_mlp = {'train_stop': 1000,
-                                'valid_stop': 1000,
-                                'batch_size': 200,
-                                'nvis': 784 + hyper_params_l2['nhid'],
-                                'n_h0': hyper_params_l1['nhid'],
-                                'n_h1': hyper_params_l2['nhid'],
-                                'max_epochs': 100,
-                                'save_path': train_path
-                                }
+        # dataset & monitoring dataset insertion without .yaml file to avoid problems 
+        # (don't know how to pass 'model' hyperparameter)
+        train.dataset = mnist_augmented.MNIST_AUGMENTED(dataset = train.dataset,
+                                                      which_set = 'train', one_hot = 1, model = dbm, start = 0,
+                                                      stop = hyper_params_mlp['train_stop'], mf_steps = 1)
+        train.algorithm.monitoring_dataset = {  #'valid' : mnist_augmented.MNIST_AUGMENTED(dataset = train.algorithm.monitoring_dataset['valid'], which_set = 'train', one_hot = 1, model = dbm, start = hyper_params_mlp['train_stop'], stop = hyper_params_mlp['valid_stop'], mf_steps = 1), 
+                                              'test' : mnist_augmented.MNIST_AUGMENTED(dataset = train.algorithm.monitoring_dataset['test'], 
+                                                                                    which_set = 'test', one_hot = 1, model = dbm, start = 0, stop = 20, mf_steps = 1)}
 
-            mlp_yaml = mlp_yaml % (hyper_params_mlp)
-            train = yaml_parse.load(mlp_yaml)
+        # DBM TRAINED WEIGHTS CLAMPED FOR FINETUNING AS EXPLAINED BY HINTON
 
-            if SOFTMAX:
-                dbm = serial.load(os.path.join(train_path, 'dbm_mnist_softmax.pkl'))
-            else:
-                dbm = serial.load(os.path.join(train_path, 'dbm_mnist.pkl'))
+        # concatenate weights between first and second hidden layer & weights between visible and first hidden layer
+        train.model.layers[0].set_weights(numpy.concatenate((dbm.hidden_layers[1].get_weights().transpose(), 
+                                                             dbm.hidden_layers[0].get_weights())))
 
-            # dataset & monitoring dataset insertion without .yaml file to avoid problems 
-            # (don't know how to pass 'model' hyperparameter)
-            train.dataset = mnist_augmented.MNIST_AUGMENTED(dataset = train.dataset,
-                                                          which_set = 'train', one_hot = 1, model = dbm, start = 0,
-                                                          stop = hyper_params_mlp['train_stop'], mf_steps = 1)
-            train.algorithm.monitoring_dataset = {  #'valid' : mnist_augmented.MNIST_AUGMENTED(dataset = train.algorithm.monitoring_dataset['valid'], which_set = 'train', one_hot = 1, model = dbm, start = hyper_params_mlp['train_stop'], stop = hyper_params_mlp['valid_stop'], mf_steps = 1), 
-                                                  'test' : mnist_augmented.MNIST_AUGMENTED(dataset = train.algorithm.monitoring_dataset['test'], 
-                                                                                        which_set = 'test', one_hot = 1, model = dbm, mf_steps = 1)}
+        # then clamp all the others normally
+        for l, h in zip(train.model.layers[1:], dbm.hidden_layers[1:]):
+            l.set_weights(h.get_weights())
 
-            # DBM TRAINED WEIGHTS CLAMPED FOR FINETUNING AS EXPLAINED BY HINTON
+        # clamp biases       
+        for l, h in zip(train.model.layers, dbm.hidden_layers):
+            l.set_biases(h.get_biases())
 
-            # concatenate weights between first and second hidden layer & weights between visible and first hidden layer
-            train.model.layers[0].set_weights(numpy.concatenate((dbm.hidden_layers[1].get_weights().transpose(), 
-                                                                 dbm.hidden_layers[0].get_weights())))
+        print '\nDBM trained weights and biases have been clamped in the MLP.'
 
-            # then clamp all the others normally
-            for l, h in zip(train.model.layers[1:], dbm.hidden_layers[1:]):
-                l.set_weights(h.get_weights())
-
-            # clamp biases       
-            for l, h in zip(train.model.layers, dbm.hidden_layers):
-                l.set_biases(h.get_biases())
-
-            print '\nDBM trained weights and biases have been clamped in the MLP.'
-
-            print '\n...Finetuning...\n'
-            train.main_loop()
+        print '\n...Finetuning...\n'
+        train.main_loop()
 
     finally:
         os.chdir(cwd)
