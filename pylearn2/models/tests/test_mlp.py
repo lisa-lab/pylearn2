fix definition of the 'misclass' channel in Softmax where _has_binary_target enhance test_mlp.py to check both misclass and nll with binary_target_dim

pylearn2/models/tests/test_mlp.py
@@ -400,14 +400,14 @@ def test_softmax_binary_targets():
     np.testing.assert_allclose(cost_bin(X_data, y_bin_data),
                                cost_vec(X_data, y_vec_data))
 
-def test_softmax_bin_targets_misclass(seed=0):
+def test_softmax_bin_targets_channels(seed=0):
     """
     Constructs softmax layers with binary target and with vector targets
     to check that they give the same 'misclass' channel value.
     """
     np.random.seed(seed)
     num_classes = 2
-    batch_size = 4
+    batch_size = 5
     mlp_bin = MLP(
         layers=[Softmax(num_classes, 's1', irange=0.1,
                         binary_target_dim=1)],
@@ -428,14 +428,16 @@ def test_softmax_bin_targets_misclass(seed=0):
     y_vec_data = np.zeros((batch_size, num_classes))
     y_vec_data[np.arange(batch_size),y_bin_data.flatten()] = 1
 
-    def get_misclass(model, y, y_data):
+    def get_misclass(channel_name, model, y, y_data):
         chans = model.get_monitoring_channels((X,y))
-        f_misclass = theano.function([X,y], chans['s1_misclass'])
-        return f_misclass(X_data, y_data)
+        f_channel = theano.function([X,y], chans['s1_'+channel_name])
+        return f_channel(X_data, y_data)
 
-    vec_misclass = get_misclass(mlp_vec, y_vec, y_vec_data)
-    bin_misclass = get_misclass(mlp_bin, y_bin, y_bin_data)
-    np.testing.assert_allclose(vec_misclass, bin_misclass)
+    for channel_name in ['misclass', 'nll']:
+      vec_val = get_misclass(channel_name, mlp_vec, y_vec, y_vec_data)
+      bin_val = get_misclass(channel_name, mlp_bin, y_bin, y_bin_data)
+      print channel_name, vec_val, bin_val
+      np.testing.assert_allclose(vec_val, bin_val)
     
 def test_set_get_weights_Softmax():
     """