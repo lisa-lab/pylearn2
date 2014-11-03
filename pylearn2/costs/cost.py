0dcb77d03abf09f56c4a77acaa3cb247b1eab66b
Kelvin Xu <xukelvin@eos20.iro.umontreal.ca>
2014-11-03 11:49:35 -0500

    removed cost.py from whitelist

--- pylearn2/costs/cost.py
+++ pylearn2/costs/cost.py
@@ -22,6 +22,7 @@ from pylearn2.utils.exc import reraise_as
 
 logger = logging.getLogger(__name__)
 
+
 class Cost(object):
     """
     Represents an objective function to be minimized by some
@@ -220,6 +221,7 @@ class Cost(object):
         """
         raise NotImplementedError(str(type(self)) + " does not implement " +
                                   "get_data_specs.")
+
 
 class SumOfCosts(Cost):
     """
@@ -451,6 +453,7 @@ class SumOfCosts(Cost):
 
         return reduce(merge, descrs)
 
+
 def scaled_cost(cost, scaling):
     """
     Deprecated. Switch to SumOfCosts([[scaling, cost]]), or just quit using it.
@@ -469,6 +472,7 @@ SumOfCosts allows you to scale individual terms, and if this is the only cost,
 you may as well just change the learning rate.""")
 
     return SumOfCosts([[scaling, cost]])
+
 
 class NullDataSpecsMixin(object):
     """
@@ -490,7 +494,9 @@ class NullDataSpecsMixin(object):
         ----------
         model : pylearn2.models.Model
         """
+
         return (NullSpace(), '')
+
 
 class DefaultDataSpecsMixin(object):
     """
@@ -521,6 +527,7 @@ class DefaultDataSpecsMixin(object):
             return (space, sources)
         else:
             return (model.get_input_space(), model.get_input_source())
+
 
 class LpPenalty(NullDataSpecsMixin, Cost):
     """
@@ -576,8 +583,9 @@ class CrossEntropy(DefaultDataSpecsMixin, Cost):
 
     def __init__(self):
         warnings.warn("CrossEntropy is deprecated. You should use a "
-                "model-specific cross entropy cost function. CrossEntropy"
-                "will be removed on or after August 3, 2014", stacklevel=2)
+                      "model-specific cross entropy cost function. "
+                      "CrossEntropy will be removed on or after August "
+                      "3, 2014", stacklevel=2)
         self.supervised = True
 
     def expr(self, model, data, ** kwargs):
@@ -663,11 +671,13 @@ def _no_op(data):
     An on_load_batch callback that does nothing.
     """
 
+
 class FixedVarDescrDataSpecsError(TypeError):
     """
     An error raised when code attempts to use the unused
     data_specs field of FixedVarDescr
     """
+
 
 class FixedVarDescr(object):
     """
@@ -706,13 +716,15 @@ class FixedVarDescr(object):
         self.fixed_vars = {}
         self.on_load_batch = []
 
-    def _data_specs_err(self, x = None):
+    def _data_specs_err(self, x=None):
         raise FixedVarDescrDataSpecsError("The data_specs field of "
-                "FixedVarDescr has been "
-                "removed. While this field existed and was documented at "
-                "one time, no TrainingAlgorithm respected it. The "
-                "data_specs of all members of on_load_batch must match "
-                "those of the cost.")
+                                          "FixedVarDescr has been removed. "
+                                          "While this field existed and was "
+                                          "documented at one time, no "
+                                          "TrainingAlgorithm respected it. "
+                                          "The data_specs of all members of "
+                                          "on_load_batch must match those of "
+                                          "the cost.")
 
     data_specs = property(_data_specs_err, _data_specs_err)
 
@@ -748,6 +760,6 @@ def merge(left, right):
     merged.fixed_vars.update(right.fixed_vars)
 
     merged.on_load_batch = safe_union(left.on_load_batch,
-                                        right.on_load_batch)
+                                      right.on_load_batch)
 
     return merged
