061a263085beeb9c4f80091edbde5fd041896a8a
Kelvin Xu <xukelvin@bart3.iro.umontreal.ca>
2014-11-28 17:30:57 -0500

    added docstring and class

--- pylearn2/datasets/tests/test_adult.py
+++ pylearn2/datasets/tests/test_adult.py
@@ -1,13 +1,21 @@
+import unittest
 import numpy as np
 from pylearn2.datasets.adult import adult
 from pylearn2.testing.skip import skip_if_no_data
 
+class TestAdult(unittest.Test(case):
 
-def test_adult():
-    skip_if_no_data()
-    data = adult(which_set='train')
-    assert data.X is not None
-    assert data.X is not np.inf
-    data = adult(which_set='test')
-    assert data.X is not None
-    assert data.X is not np.inf
+    def setUp(self):
+        skip_if_no_data()
+
+    def test_adult():
+        """
+        Tests that the dataset loads correctly
+        """
+        skip_if_no_data()
+        data = adult(which_set='train')
+        assert data.X is not None
+        assert data.X is not np.inf
+        data = adult(which_set='test')
+        assert data.X is not None
+        assert data.X is not np.inf
