670d3527f5f1e26d4c02af8608a122835f3a3e1b
Kelvin Xu <xukelvin@bart3.iro.umontreal.ca>
2014-11-28 17:50:56 -0500

    seperated out mnist and mnist_rotated, added __init__ to make test_mnist importable

---
+++ pylearn2/datasets/tests/__init__.py
@@ -0,0 +1,3 @@
+"""
+Dataset testing classes
+"""
--- pylearn2/datasets/tests/test_adult.py
+++ pylearn2/datasets/tests/test_adult.py
@@ -1,16 +1,33 @@
+"""
+Testing class that simply checks to see if the adult dataset
+"""
 import unittest
 import numpy as np
 from pylearn2.datasets.adult import adult
 from pylearn2.testing.skip import skip_if_no_data
 
-class TestAdult(unittest.Test(case):
 
+class TestAdult(unittest.TestCase):
+    """
+    Parameters
+    ----------
+    None
+
+    Notes
+    -----
+    Testing class that simply checks to see if the adult dataset
+    is loadable
+    """
     def setUp(self):
+        """
+        Skips test if data does not exist
+        """
         skip_if_no_data()
 
     def test_adult():
         """
         Tests that the dataset loads correctly
+        and that there is no inf in the data
         """
         skip_if_no_data()
         data = adult(which_set='train')
