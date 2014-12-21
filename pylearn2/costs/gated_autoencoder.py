b25baa3a9faba04d50e9c80a6b847f3c6ee88675
Raul Peralta Lozada <raulpl25@gmail.com>
2014-12-20 19:49:55 -0600

    Added gated-autoencoder and an example

---
+++ pylearn2/costs/gated_autoencoder.py
@@ -0,0 +1,77 @@
+from pylearn2.costs.cost import Cost, DefaultDataSpecsMixin
+from pylearn2.utils import wraps
+from pylearn2.space import VectorSpace
+import theano
+
+
+class SymmetricCost(DefaultDataSpecsMixin, Cost):
+    @staticmethod
+    def cost(x, y, rx, ry):
+        """
+        Symmetric reconstruction cost.
+
+        Parameters
+        ----------
+        x : tensor_like
+            Theano symbolic representing the first input minibatch.
+            Assumed to be 2-tensors, with the first dimension
+            indexing training examples and the second indexing
+            data dimensions.
+        y : tensor_like
+            Theano symbolic representing the seconde input minibatch.
+            Assumed to be 2-tensors, with the first dimension
+            indexing training examples and the second indexing
+            data dimensions.
+        rx : tensor_like
+            Reconstruction of the first minibatch by the model.
+        ry: tensor_like
+            Reconstruction of the second minibatch by the model.
+        """
+        raise NotImplementedError
+
+    def expr(self, model, data, *args, **kwargs):
+
+        self.get_data_specs(model)[0].validate(data)
+        x, y = data
+        input_space = model.get_input_space()
+        if not isinstance(input_space.components[0], VectorSpace):
+            conv = input_space.components[0]
+            vec = VectorSpace(conv.get_total_dimension())
+            x = conv.format_as(x, vec)
+        if not isinstance(input_space.components[1], VectorSpace):
+            conv = input_space.components[1]
+            vec = VectorSpace(conv.get_total_dimension())
+            y = conv.format_as(y, vec)
+        rx, ry = model.reconstructXY((x, y))
+        return self.cost(x, y, rx, ry)
+
+
+class SymmetricMSRE(SymmetricCost):
+    @staticmethod
+    @wraps(SymmetricCost.cost)
+    def cost(x, y, rx, ry):
+        """
+        Notes
+        -----
+        Symmetric reconstruction cost as defined by Memisevic in:
+        "Gradient-based learning of higher-order image features".
+        This function only works with real valued data.
+        """
+        return (
+            ((0.5*((x - rx)**2)) + (0.5*((y - ry)**2)))).sum(axis=1).mean()
+
+
+class NormalizedSymmetricMSRE(SymmetricCost):
+    @staticmethod
+    @wraps(SymmetricCost.cost)
+    def cost(x, y, rx, ry):
+        """
+        Notes
+        -----
+        Do not use this function to train, only to monitor the
+        average percentage of reconstruction achieved when training on
+        real valued data.
+        """
+        num = (((0.5*((x - rx)**2)) + (0.5*((y - ry)**2)))).sum(axis=1).mean()
+        den = ((0.5*(x.norm(2, 1)**2)) + (0.5*(y.norm(2, 1)**2))).mean()
+        return num/den
