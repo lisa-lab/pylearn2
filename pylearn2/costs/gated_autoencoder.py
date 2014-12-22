89ca2a80d454f13a1eb0c6df7cc660442f537ed4
Raul Peralta Lozada <raulpl25@gmail.com>
2014-12-21 22:33:46 -0600

    PEP 8 corrections and adding several docstrings

--- pylearn2/costs/gated_autoencoder.py
+++ pylearn2/costs/gated_autoencoder.py
@@ -1,3 +1,7 @@
+"""
+Definitions of the cost for the gated-autoencoder.
+"""
+
 from pylearn2.costs.cost import Cost, DefaultDataSpecsMixin
 from pylearn2.utils import wraps
 from pylearn2.space import VectorSpace
@@ -5,10 +9,11 @@ from pylearn2.space import VectorSpace
 
 class SymmetricCost(DefaultDataSpecsMixin, Cost):
     """
-    Class representing the symmetric cost, subclasses can
-    define the type of data they will use. Mean reconstruction
-    error is used for real valued data and cross-Entropy loss
-    is used for binary.
+    Summary (Class representing the symmetric cost).
+
+    Subclasses can define the type of data they will use.
+    Mean reconstruction error is used for real valued data
+    and cross-Entropy loss is used for binary.
 
     See Also
     --------
@@ -40,12 +45,25 @@ class SymmetricCost(DefaultDataSpecsMixin, Cost):
         -------
         Cost: theano_like expression
             Representation of the cost
-
         """
         raise NotImplementedError
 
-    @wraps(Cost.expr)
     def expr(self, model, data, *args, **kwargs):
+        """
+        Returns a theano expression for the cost function.
+
+        Returns a symbolic expression for a cost function applied to the
+        minibatch of data.
+        Optionally, may return None. This represents that the cost function
+        is intractable but may be optimized via the get_gradients method.
+
+        Parameters
+        ----------
+        model : a pylearn2 Model instance
+        data : a batch in cost.get_data_specs() form
+        kwargs : dict
+            Optional extra arguments. Not used by the base class.
+        """
         self.get_data_specs(model)[0].validate(data)
         x, y = data
         input_space = model.get_input_space()
@@ -63,7 +81,7 @@ class SymmetricCost(DefaultDataSpecsMixin, Cost):
 
 class SymmetricMSRE(SymmetricCost):
     """
-    Symmetric cost for real valued data.
+    Summary (Symmetric cost for real valued data).
 
     See Also
     --------
@@ -73,6 +91,28 @@ class SymmetricMSRE(SymmetricCost):
     @wraps(SymmetricCost.cost)
     def cost(x, y, rx, ry):
         """
+        Parameters
+        ----------
+        x : tensor_like
+            Theano symbolic representing the first input minibatch.
+            Assumed to be 2-tensors, with the first dimension
+            indexing training examples and the second indexing
+            data dimensions.
+        y : tensor_like
+            Theano symbolic representing the seconde input minibatch.
+            Assumed to be 2-tensors, with the first dimension
+            indexing training examples and the second indexing
+            data dimensions.
+        rx : tensor_like
+            Reconstruction of the first minibatch by the model.
+        ry: tensor_like
+            Reconstruction of the second minibatch by the model.
+
+        Returns
+        -------
+        Cost: theano_like expression
+            Representation of the cost
+
         Notes
         -----
         Symmetric reconstruction cost as defined by Memisevic in:
@@ -85,8 +125,7 @@ class SymmetricMSRE(SymmetricCost):
 
 class NormalizedSymmetricMSRE(SymmetricCost):
     """
-    Normalized Symmetric cost for real valued data.
-    Values between one and zero.
+    Summary (Normalized Symmetric cost for real valued data).
 
     Notes
     -----
@@ -96,6 +135,28 @@ class NormalizedSymmetricMSRE(SymmetricCost):
     @wraps(SymmetricCost.cost)
     def cost(x, y, rx, ry):
         """
+        Parameters
+        ----------
+        x : tensor_like
+            Theano symbolic representing the first input minibatch.
+            Assumed to be 2-tensors, with the first dimension
+            indexing training examples and the second indexing
+            data dimensions.
+        y : tensor_like
+            Theano symbolic representing the seconde input minibatch.
+            Assumed to be 2-tensors, with the first dimension
+            indexing training examples and the second indexing
+            data dimensions.
+        rx : tensor_like
+            Reconstruction of the first minibatch by the model.
+        ry: tensor_like
+            Reconstruction of the second minibatch by the model.
+
+        Returns
+        -------
+        Cost: theano_like expression
+            Representation of the cost
+
         Notes
         -----
         Do not use this function to train, only to monitor the
