c7bdccd22004dd6777cf2e650ac5f429f376a1b5
Minh Le <minhle.r7@gmail.com>
2015-01-22 23:27:17 +0100

    add info, use better method name

--- pylearn2/sandbox/nlp/models/tests/test_mlp.py
+++ pylearn2/sandbox/nlp/models/tests/test_mlp.py
@@ -1,13 +1,18 @@
-'''
-Created on Jan 17, 2015
+"""
+Tests for MLP
+"""
+__authors__ = "Minh Ngoc Le"
+__copyright__ = "Copyright 2010-2015, Universite de Montreal"
+__credits__ = ["Minh Ngoc Le"]
+__license__ = "3-clause BSD"
+__maintainer__ = "LISA Lab"
+__email__ = "pylearn-dev@googlegroups"
 
-@author: Minh Ngoc Le
-'''
 import os
 from pylearn2.config import yaml_parse
 
 
-def test_yaml():
+def test_projection_layer_yaml():
     """Test loading and running a complex model with ProjectionLayer."""
     test_dir = os.path.dirname(__file__)
     with open(os.path.join(test_dir, 'composite.yaml')) as f:
