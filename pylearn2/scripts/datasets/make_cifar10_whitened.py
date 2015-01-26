e669d6a6a2db27aa25f03d367c1435b48c8d6555
Saizheng Zhang <saizhenglisa@gmail.com>
2015-01-26 11:28:48 -0500

    fixed pep8 error, fixed errors in docstring in test_format

--- pylearn2/scripts/datasets/make_cifar10_whitened.py
+++ pylearn2/scripts/datasets/make_cifar10_whitened.py
@@ -12,59 +12,61 @@ import numpy as np
 from pylearn2.datasets.cifar10 import CIFAR10
 import textwrap
 
+
 def main():
     data_dir = string_utils.preprocess('${PYLEARN2_DATA_PATH}/cifar10')
-    
+
     print('Loading CIFAR-10 train dataset...')
-    train = CIFAR10(which_set = 'train')
-    
+    train = CIFAR10(which_set='train')
+
     print("Preparing output directory...")
     output_dir = data_dir + '/pylearn2_whitened'
-    serial.mkdir( output_dir )
-    README = open(output_dir + '/README','w')
-    
+    serial.mkdir(output_dir)
+    README = open(output_dir + '/README', 'w')
+
     README.write(textwrap.dedent("""
     The .pkl files in this directory may be opened in python using
     cPickle, pickle, or pylearn2.serial.load.
-    
+
     train.pkl, and test.pkl each contain
     a pylearn2 Dataset object defining a labeled
     dataset of a 32x32 approximately whitened version of the STL-10
     dataset. train.pkl contains labeled train examples. test.pkl
     contains labeled test examples.
-    
+
     preprocessor.pkl contains a pylearn2 ZCA object that was used
     to approximately whiten the images. You may want to use this
     object later to preprocess other images.
-    
+
     They were created with the pylearn2 script make_cifar10_whitened.py.
-    
+
     All other files in this directory, including this README, were
     created by the same script and are necessary for the other files
     to function correctly.
     """))
-    
+
     README.close()
-    
-    print("Learning the preprocessor and preprocessing the unsupervised train data...")
+
+    print("Learning the preprocessor and preprocessing \
+          the unsupervised train data...")
     preprocessor = preprocessing.ZCA()
-    train.apply_preprocessor(preprocessor = preprocessor, can_fit = True)
-    
+    train.apply_preprocessor(preprocessor=preprocessor, can_fit=True)
+
     print('Saving the unsupervised data')
     train.use_design_loc(output_dir+'/train.npy')
     serial.save(output_dir + '/train.pkl', train)
-    
+
     print("Loading the test data")
-    test = CIFAR10(which_set = 'test')
-    
+    test = CIFAR10(which_set='test')
+
     print("Preprocessing the test data")
-    test.apply_preprocessor(preprocessor = preprocessor, can_fit = False)
-    
+    test.apply_preprocessor(preprocessor=preprocessor, can_fit=False)
+
     print("Saving the test data")
     test.use_design_loc(output_dir+'/test.npy')
     serial.save(output_dir+'/test.pkl', test)
-    
-    serial.save(output_dir + '/preprocessor.pkl',preprocessor)
+
+    serial.save(output_dir + '/preprocessor.pkl', preprocessor)
 
 if __name__ == "__main__":
     main()
