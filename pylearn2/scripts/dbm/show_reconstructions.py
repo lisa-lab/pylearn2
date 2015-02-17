77c6140ba1d2d1f5fe2ca10d96f15a17c9a01833
Thomas Rohée <roheethomas@gmail.com>
2015-02-17 15:59:39 -0500

    Removed unecessary classes

--- pylearn2/scripts/dbm/show_reconstructions.py
+++ pylearn2/scripts/dbm/show_reconstructions.py
@@ -22,110 +22,81 @@ from theano.compat.six.moves import input, xrange
 from theano import function
 
 
-class ReconsViewer(object):
+def init_viewer(dataset, rows, cols, vis_batch):
+    m = rows * cols
+    _, patch_rows, patch_cols, channels = vis_batch.shape
+
+    assert _ == m
+
+    mapback = hasattr(dataset, 'mapback_for_viewer')
+    actual_cols = 2 * cols * (1 + mapback) * (1 + (channels == 2))
+    pv = PatchViewer((rows, actual_cols),
+                     (patch_rows, patch_cols),
+                     is_color=(channels == 3))
+    return pv
+
+
+def get_mapped_batch(dataset, design_batch):
     """
     Parameters
     ----------
-    model: Model
-        The model from which to display the reconstructions.
-    dataset: Dataset
-        The dataset used to train the model.
-    rows: int
-        Number of rows to display.
-    cols: int
-        Number of columns to display.
+    batch: numpy array
+        WRITEME
     """
-    def __init__(self, model, dataset, rows, cols):
-        self.dataset = dataset
-        self.rows = rows
-        self.cols = cols
+    if design_batch.ndim != 2:
+        design_batch = dataset.get_design_matrix(design_batch.copy())
+    mapped_design = dataset.mapback(design_batch.copy())
+    mapped_batch = dataset.get_topological_view(mapped_design.copy())
 
-        m = self.rows * self.cols
-        self.vis_batch = self.dataset.get_batch_topo(m)
-        _, patch_rows, patch_cols, channels = self.vis_batch.shape
+    return mapped_batch
 
-        assert _ == m
+def update_viewer(dataset, batch, rows, cols, pv, recons_func, vis_batch):
+    """
+    Method to update the viewer with a new visible batch.
+    """
+    mapback = hasattr(dataset, 'mapback_for_viewer')
+    topo = batch.ndim > 2
 
-        self.mapback = hasattr(self.dataset, 'mapback_for_viewer')
+    ipt = vis_batch.copy()
+    if not topo:
+        ipt = dataset.get_design_matrix(ipt)
+    recons_batch = recons_func(ipt.astype(batch.dtype))
+    if not topo:
+        recons_batch = dataset.get_topological_view(recons_batch)
+    if mapback:
+        mapped_batch = get_mapped_batch(dataset, vis_batch)
+        mapped_r_batch = get_mapped_batch(dataset, recons_batch.copy())
+    for row in xrange(rows):
+        row_start = cols * row
+        for j in xrange(cols):
+            vis_patch = vis_batch[row_start+j, :, :, :].copy()
+            adjusted_vis_patch = dataset.adjust_for_viewer(vis_patch)
+            if vis_patch.shape[-1] == 2:
+                pv.add_patch(adjusted_vis_patch[:, :, 1],
+                             rescale=False)
+                pv.add_patch(adjusted_vis_patch[:, :, 0],
+                             rescale=False)
+            else:
+                pv.add_patch(adjusted_vis_patch, rescale=False)
 
-        actual_cols = 2 * self.cols * (1 + self.mapback) * (1 + (channels == 2))
-        self.pv = PatchViewer((self.rows, actual_cols),
-                              (patch_rows, patch_cols),
-                              is_color=(channels == 3))
-
-        self.batch = model.visible_layer.space.make_theano_batch()
-        self.topo = self.batch.ndim > 2
-        reconstruction = model.reconstruct(self.batch)
-        self.recons_func = function([self.batch], reconstruction)
-
-        if hasattr(model.visible_layer, 'beta'):
-            beta = model.visible_layer.beta.get_value()
-            print('beta: ', (beta.min(), beta.mean(), beta.max()))
-
-    def get_mapped_batch(self, batch):
-        """
-        Parameters
-        ----------
-        batch: numpy array
-            WRITEME
-        """
-        design_batch = batch
-        if design_batch.ndim != 2:
-            design_batch = self.dataset.get_design_matrix(design_batch.copy())
-        mapped_design = self.dataset.mapback(design_batch.copy())
-        mapped_batch = self.dataset.get_topological_view(mapped_design.copy())
-
-        return mapped_batch
-
-    def update_viewer(self):
-        """
-        Method to update the viewer with a new visible batch.
-        """
-        ipt = self.vis_batch.copy()
-        if not self.topo:
-            ipt = self.dataset.get_design_matrix(ipt)
-        recons_batch = self.recons_func(ipt.astype(self.batch.dtype))
-        if not self.topo:
-            recons_batch = self.dataset.get_topological_view(recons_batch)
-        if self.mapback:
-            mapped_batch = self.get_mapped_batch(self.vis_batch)
-            mapped_r_batch = self.get_mapped_batch(recons_batch.copy())
-        for row in xrange(self.rows):
-            row_start = self.cols * row
-            for j in xrange(self.cols):
-                vis_patch = self.vis_batch[row_start+j, :, :, :].copy()
-                adjusted_vis_patch = self.dataset.adjust_for_viewer(vis_patch)
-                if vis_patch.shape[-1] == 2:
-                    self.pv.add_patch(adjusted_vis_patch[:, :, 1],
-                                      rescale=False)
-                    self.pv.add_patch(adjusted_vis_patch[:, :, 0],
-                                      rescale=False)
-                else:
-                    self.pv.add_patch(adjusted_vis_patch, rescale=False)
-                r = vis_patch
-
-                if self.mapback:
-                    self.pv.add_patch(dataset.adjust_for_viewer(
-                        mapped_batch[row_start+j, :, :, :].copy()),
-                        rescale=False)
-                if recons_batch.shape[-1] == 2:
-                    self.pv.add_patch(self.dataset.adjust_to_be_viewed_with(
-                        recons_batch[row_start+j, :, :, 1].copy(), vis_patch),
-                        rescale=False)
-                    self.pv.add_patch(self.dataset.adjust_to_be_viewed_with(
-                        recons_batch[row_start+j, :, :, 0].copy(), vis_patch),
-                        rescale=False)
-                else:
-                    self.pv.add_patch(self.dataset.adjust_to_be_viewed_with(
-                        recons_batch[row_start+j, :, :, :].copy(), vis_patch),
-                        rescale=False)
-                r = recons_batch[row_start+j, :, :, :]
-
-                if self.mapback:
-                    self.pv.add_patch(self.dataset.adjust_to_be_viewed_with(
-                        mapped_r_batch[row_start+j, :, :, :].copy(),
-                        mapped_batch[row_start+j, :, :, :].copy()),
-                        rescale=False)
+            if mapback:
+                pv.add_patch(dataset.adjust_for_viewer(mapped_batch[row_start+j, :, :, :].copy()),
+                             rescale=False)
+                pv.add_patch(dataset.adjust_to_be_viewed_with(
+                    mapped_r_batch[row_start+j, :, :, :].copy(),
+                    mapped_batch[row_start+j, :, :, :].copy()),
+                             rescale=False)
+            if recons_batch.shape[-1] == 2:
+                pv.add_patch(dataset.adjust_to_be_viewed_with(
+                    recons_batch[row_start+j, :, :, 1].copy(), vis_patch),
+                             rescale=False)
+                pv.add_patch(dataset.adjust_to_be_viewed_with(
+                    recons_batch[row_start+j, :, :, 0].copy(), vis_patch),
+                             rescale=False)
+            else:
+                pv.add_patch(dataset.adjust_to_be_viewed_with(
+                    recons_batch[row_start+j, :, :, :].copy(), vis_patch),
+                             rescale=False)
 
 
 def load_model(model_path, m):
@@ -166,22 +137,26 @@ def load_dataset(dataset_yml, use_test_set):
 
     return dataset
 
-if __name__ == '__main__':
-    rows = 5
-    cols = 10
-    m = rows * cols
-    _, model_path = sys.argv
 
+def show_reconstructions(m, model_path):
     model = load_model(model_path, m)
 
-    x = raw_input('use test set? (y/n) ')
+    x = input('use test set? (y/n) ')
     dataset = load_dataset(model.dataset_yaml_src, x)
+    vis_batch = dataset.get_batch_topo(m)
+    pv = init_viewer(dataset, rows, cols, vis_batch)
 
-    recons_viewer = ReconsViewer(model, dataset, rows, cols)
+    batch = model.visible_layer.space.make_theano_batch()
+    reconstruction = model.reconstruct(batch)
+    recons_func = function([batch], reconstruction)
+
+    if hasattr(model.visible_layer, 'beta'):
+        beta = model.visible_layer.beta.get_value()
+        print('beta: ', (beta.min(), beta.mean(), beta.max()))
 
     while True:
-        recons_viewer.update_viewer()
-        recons_viewer.pv.show()
+        update_viewer(dataset, batch, rows, cols, pv, recons_func, vis_batch)
+        pv.show()
         print('Displaying reconstructions. (q to quit, ENTER = show more)')
         while True:
             x = input()
@@ -193,4 +168,13 @@ if __name__ == '__main__':
             else:
                 print('Invalid input, try again')
 
-        recons_viewer.vis_batch = dataset.get_batch_topo(m)
+        vis_batch = dataset.get_batch_topo(m)
+
+
+if __name__ == '__main__':
+    rows = 5
+    cols = 10
+    m = rows * cols
+    _, model_path = sys.argv
+
+    show_reconstructions(m, model_path)
