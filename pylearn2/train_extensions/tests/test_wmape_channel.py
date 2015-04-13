ef50c96c2ebbc2641595e0c51915791d0b2b2e75
junbochen <junbochen2000@gmail.com>
2015-04-13 16:19:40 +0800

    add wmape channel

---
+++ pylearn2/train_extensions/tests/test_wmape_channel.py
@@ -0,0 +1,67 @@
+"""
+Tests for ROC AUC.
+"""
+from pylearn2.config import yaml_parse
+from pylearn2.testing.skip import skip_if_no_sklearn
+
+
+def test_wmape():
+    """Test RocAucChannel."""
+#     return 
+    skip_if_no_sklearn()
+    print 'wahaha'
+    trainer = yaml_parse.load(test_yaml)
+    trainer.main_loop()
+
+
+test_yaml = """
+!obj:pylearn2.train.Train {
+    dataset:
+      &train !obj:pylearn2.testing.datasets.random_dense_design_matrix_for_regression
+      {
+          rng: !obj:numpy.random.RandomState { seed: 1 },
+          num_examples: 10,
+          dim: 10,
+          reg_min: 1,
+          reg_max: 1000
+      },
+    model: !obj:pylearn2.models.mlp.MLP {
+        nvis: 10,
+        layers: [
+            !obj:pylearn2.models.mlp.Sigmoid {
+                layer_name: h0,
+                dim: 10,
+                irange: 0.05,
+            },
+            !obj:pylearn2.models.mlp.Linear {
+                layer_name: y,
+                dim: 1,
+                irange: 0.,
+            }
+        ],
+    },
+    algorithm: !obj:pylearn2.training_algorithms.bgd.BGD {
+        monitoring_dataset: {
+            'train': *train,
+        },
+        batches_per_iter: 1,
+        monitoring_batches: 1,
+        termination_criterion: !obj:pylearn2.termination_criteria.And {
+            criteria: [
+                !obj:pylearn2.termination_criteria.EpochCounter {
+                    max_epochs: 1,
+                },
+                !obj:pylearn2.termination_criteria.MonitorBased {
+                    channel_name: train_y_wmape,
+                    prop_decrease: 0.,
+                    N: 1,
+                },
+            ],
+        },
+    },
+    extensions: [
+        !obj:pylearn2.train_extensions.wmape_channel.WMapeChannel {},
+    ],
+}
+"""
+
---
+++ pylearn2/train_extensions/wmape_channel.py
@@ -0,0 +1,126 @@
+"""
+TrainExtension subclass for calculating Weighted Mean Average Percentage Error scores for regression models on monitoring
+dataset(s), reported via monitor channels.
+"""
+
+__author__ = "Junbo chen"
+__copyright__ = "Copyright 2015, Alibaba Group"
+__license__ = "3-clause BSD"
+__maintainer__ = "Junbo Chen"
+
+import numpy as np
+
+import theano
+from theano import gof, config
+from theano import tensor as T
+
+from pylearn2.train_extensions import TrainExtension
+
+
+class WMapeOp(gof.Op):
+    """
+    Theano Op calculate wmape score.
+
+    Parameters
+    ----------
+    name : str, optional (default 'wmape')
+        Name of this Op.
+    use_c_code : WRITEME
+    """
+    def __init__(self, name='wmape', use_c_code=theano.config.cxx):
+        super(WMapeOp, self).__init__(use_c_code)
+        self.name = name
+
+    def make_node(self, y_true, y_score):
+        """
+        Calculate ROC AUC score.
+
+        Parameters
+        ----------
+        y_true : tensor_like
+            Target regression values.
+        y_score : tensor_like
+            Predicted regression values.
+        """
+        y_true = T.as_tensor_variable(y_true)
+        y_score = T.as_tensor_variable(y_score)
+        output = [T.scalar(name=self.name, dtype=config.floatX)]
+        return gof.Apply(self, [y_true, y_score], output)
+
+    def perform(self, node, inputs, output_storage):
+        """
+        Calculate WMAPE score.
+
+        Parameters
+        ----------
+        node : Apply instance
+            Symbolic inputs and outputs.
+        inputs : list
+            Sequence of inputs.
+        output_storage : list
+            List of mutable 1-element lists.
+        """
+        y_true, y_score = inputs
+        print y_true
+        print y_true.shape
+        print y_score
+        print y_score.shape
+        print output_storage[0][0]
+        print type(output_storage[0][0])
+        wmape = np.sum(abs(y_true - y_score)) / np.sum(abs(y_true))
+        
+        print wmape
+        print type(wmape)
+        print theano._asarray(wmape, dtype=config.floatX)
+        
+        output_storage[0][0] = theano._asarray(wmape, dtype=config.floatX)
+
+
+class WMapeChannel(TrainExtension):
+    """
+    Adds a WMape channel to the monitor for each monitoring dataset.
+
+    This monitor will return nan unless sum of y_true is not 0.
+    For this reason, it is recommended to set monitoring_batches
+    to 1, especially when using unbalanced datasets.
+
+    Parameters
+    ----------
+    channel_name_suffix : str, optional (default 'wmape')
+        Channel name suffix.
+    """
+    def __init__(self, channel_name_suffix='wmape'):
+        self.channel_name_suffix = channel_name_suffix
+
+    def setup(self, model, dataset, algorithm):
+        """
+        Add ROC AUC channels for monitoring dataset(s) to model.monitor.
+
+        Parameters
+        ----------
+        model : object
+            The model being trained.
+        dataset : object
+            Training dataset.
+        algorithm : object
+            Training algorithm.
+        """
+        m_space, m_source = model.get_monitoring_data_specs()
+        state, target = m_space.make_theano_batch()
+
+        y = target[:,0]
+        y_hat = model.fprop(state)[:,0]
+
+        wmape = WMapeOp(self.channel_name_suffix)(y, y_hat)
+        wmape = T.cast(wmape, config.floatX)
+        for dataset_name, dataset in algorithm.monitoring_dataset.items():
+            if dataset_name:
+                channel_name = '{0}_{1}'.format(dataset_name,
+                                                self.channel_name_suffix)
+            else:
+                channel_name = self.channel_name_suffix
+            model.monitor.add_channel(name=channel_name,
+                                      ipt=(state, target),
+                                      val=wmape,
+                                      data_specs=(m_space, m_source),
+                                      dataset=dataset)
