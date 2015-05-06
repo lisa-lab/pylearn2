e68cd9f3bcc9b3687f5e155dcb7e0c4a66ce5476
Mathew Donald Rogers <rogersmd@bart4.iro.umontreal.ca>
2015-05-05 22:30:55 -0400

    Cleaned up file

--- pylearn2/models/tests/test_costs.py
+++ pylearn2/models/tests/test_costs.py
@@ -12,11 +12,15 @@ from pylearn2.models.mlp import MLP
 from pylearn2.models.mlp import Sigmoid, Tanh, Linear, RectifiedLinear
 from pylearn2.models.mlp import ConvElemwise
 from pylearn2.space import Conv2DSpace
-from pylearn2.models.mlp import SigmoidConvNonlinearity, TanhConvNonlinearity, IdentityConvNonlinearity, RectifierConvNonlinearity
+from pylearn2.models.mlp import SigmoidConvNonlinearity
+from pylearn2.models.mlp import TanhConvNonlinearity
+from pylearn2.models.mlp import IdentityConvNonlinearity
+from pylearn2.models.mlp import  RectifierConvNonlinearity
 
 def test_costs():
 
-    ImplementedNonLinearities = [[SigmoidConvNonlinearity(), Sigmoid], [IdentityConvNonlinearity(), Linear]]
+    ImplementedNonLinearities = [[SigmoidConvNonlinearity(), Sigmoid], \
+                                 [IdentityConvNonlinearity(), Linear]]
 
     for ConvNonlinearity, MLPNonlinearity in ImplementedNonLinearities:
 
@@ -30,8 +34,11 @@ def test_costs():
         output_channels = 13
         batch_size = 103
 
-        x = np.random.rand(batch_size, r, s, 1).astype('float32')
-        y = np.random.randint(2, size = [batch_size, output_channels, 1 ,1]).astype('float32')
+        x = np.random.rand(batch_size, r, s, 1)
+        y = np.random.randint(2, size = [batch_size, output_channels, 1 ,1])
+
+        x = x.astype('float32')
+        y = y.astype('float32')
 
         x_mlp = x.flatten().reshape(batch_size, nvis)
         y_mlp = y.flatten().reshape(batch_size, output_channels)
@@ -40,7 +47,9 @@ def test_costs():
 
         conv_model = MLP(
             input_space = Conv2DSpace(shape = shape, axes = ['b', 0, 1, 'c'], num_channels = 1),
-            layers = [ConvElemwise(layer_name='conv', nonlinearity = ConvNonlinearity, output_channels = output_channels, kernel_shape = shape, pool_shape = [1,1], pool_stride = shape, irange= 1.0)],
+            layers = [ConvElemwise(layer_name='conv', nonlinearity = ConvNonlinearity, \
+                      output_channels = output_channels, kernel_shape = shape, \
+                      pool_shape = [1,1], pool_stride = shape, irange= 1.0)],
             batch_size = batch_size
         )
 
@@ -49,7 +58,8 @@ def test_costs():
         Y_hat = conv_model.fprop(X).flatten()
         g = theano.function([X], Y_hat)
 
-        # Construct an equivalent MLP which gives the same output after flattening.
+        # Construct an equivalent MLP which gives the same output after flattening.  Notice
+        # that we use W and b from the previously initialized convnet.
 
         mlp_model = MLP(
             layers = [MLPNonlinearity(dim = output_channels, layer_name = 'mlp', irange = 1.0)],
@@ -58,15 +68,19 @@ def test_costs():
         )
 
         W, b = conv_model.get_param_values()
+
         W = W.astype('float32')
         b = b.astype('float32')
+
         W_mlp = np.zeros(shape = (output_channels, nvis))
         for k in range(output_channels):
             W_mlp[k] = W[k, 0].flatten()[::-1]
         W_mlp = W_mlp.T
         b_mlp = b.flatten()
+
         W_mlp = W_mlp.astype('float32')
         b_mlp = b_mlp.astype('float32')
+
         mlp_model.set_param_values([W_mlp, b_mlp])
 
         X1 = mlp_model.get_input_space().make_theano_batch()
