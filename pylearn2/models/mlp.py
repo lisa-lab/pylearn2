8c1d1e5fe0796e6d8a699f3388296cc131d58c75
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2015-09-16 20:44:19 -0400

    Fix following code review

--- pylearn2/models/mlp.py
+++ pylearn2/models/mlp.py
@@ -2764,11 +2764,6 @@ class RectifierConvNonlinearity(ConvNonlinearity):
             linear_response * (linear_response < 0.)
         return p
 
-    @wraps(ConvNonlinearity.cost)
-    def cost(self, Y, Y_hat, batch_axis):
-        raise NotImplementedError(
-            str(type(self)) + " does not implement cost function.")
-
 
 class SigmoidConvNonlinearity(ConvNonlinearity):
 
@@ -2879,11 +2874,6 @@ class TanhConvNonlinearity(ConvNonlinearity):
         """
         p = T.tanh(linear_response)
         return p
-
-    @wraps(ConvNonlinearity.cost)
-    def cost(self, Y, Y_hat, batch_axis):
-        raise NotImplementedError(
-            str(type(self)) + " does not implement cost function.")
 
 
 class ConvElemwise(Layer):
